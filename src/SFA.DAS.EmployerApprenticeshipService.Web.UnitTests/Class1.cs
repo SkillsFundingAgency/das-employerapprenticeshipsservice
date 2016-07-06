2863525aa23c2805f6a733ec3e3f4a7238276066
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-06 16:31:35 +0100

    Initial commit for web project with nunit test project. Included FAKEBuildScript for integrating into VSTS

---
+++ src/SFA.DAS.EmployerApprenticeshipService.Web.UnitTests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using Moq;
+using NUnit.Framework;
+
+namespace SFA.DAS.EmployerApprenticeshipService.Web.UnitTests
+{
+    public class Class1
+    {
+        private Mock<HttpContextBase> _httpContextBase;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _httpContextBase = new Mock<HttpContextBase>();
+
+        }
+
+        [Test]
+        public void TestAssert()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipService.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFA.DAS.EmployerApprenticeshipService.Web.MvcApplication" Language="C#" %>
