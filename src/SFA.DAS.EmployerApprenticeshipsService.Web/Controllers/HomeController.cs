9f4d4931983752992e5016933b92a2c7b5f7cbad
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-07 21:57:35 +0100

    Added in new controller action to do the sign in, linked in tests for this

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/HomeController.cs
@@ -1,9 +1,18 @@
 ﻿using System.Web.Mvc;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Authentication;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Models;
 
 namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IOwinWrapper _owinWrapper;
+
+        public HomeController(IOwinWrapper owinWrapper)
+        {
+            _owinWrapper = owinWrapper;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -21,6 +30,21 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignInUser(SignInUserModel model)
+        {
+            LoginUser(model.UserId,model.FirstName,model.LastName);
+
+            return View("Index");
+        }
+
+        private void LoginUser(string id, string firstName, string lastName)
+        {
+            _owinWrapper.IssueLoginCookie(id, $"{firstName} {lastName}");
+
+            _owinWrapper.RemovePartialLoginCookie();
         }
     }
 }
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Models/SignInUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.EmployerApprenticeshipsService.Web.Models
+{
+    public class SignInUserModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
