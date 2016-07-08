8b9cd72350b3a287a2d71c374575ef941bb00672
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-08 11:55:20 +0100

    Added in FileSystemRepository reader and FileSystemUserRepository for retrieving the JSON list of users.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.EmployerApprenticeshipsService.Domain
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/FileSystemUserRepository.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerApprenticeshipsService.Domain;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Data
+{
+    public class FileSystemUserRepository : FileSystemRepository, IUserRepository
+    {
+        private const string UserDataFileName = "user_data";
+
+        public FileSystemUserRepository()
+            : base("Users")
+        {
+        }
+
+        public async Task<User> GetById(string id)
+        {
+            return await ReadFileById<User>(id);
+        }
+        public async Task<User> GetByEmailAddress(string emailAddress)
+        {
+            var userFiles = GetDataFiles();
+
+            foreach (var path in userFiles)
+            {
+                var user = await ReadFile<User>(path);
+                if (user.Email.Equals(emailAddress, StringComparison.OrdinalIgnoreCase))
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+        public async Task Create(User registerUser)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task Update(User user)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await ReadFileById<List<User>>(UserDataFileName);
+        }
+    }
+}
