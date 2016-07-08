85adbf38b5aa6fea2fbd76d6a66a659335aea9b5
ijrussell <i.j.russell@tesco.net>
2016-07-08 13:44:55 +0100

    Revert "initial push"
    This reverts commit 9640417b92580b90dc82c36526bea90b1fa6d3ff.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEmployerInformation/GetEmployerInformationRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetEmployerInformation
-{
-    public class GetEmployerInformationRequest : IAsyncRequest<GetEmployerInformationResponse>
-    {
-        public string Id { get; set; }
-    }
-}
