6d7663ae5d35063bae71f1a5ea255b17abdb862e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-08 11:04:06 +0100

    Update to the user model and also to the data file that we will be using to store the users available for authentication

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Models/SignInUserModel.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Models/SignInUserModel.cs
@@ -5,5 +5,6 @@
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Models/SignInUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Models
+{
+    public class SignInUserViewModel
+    {
+        public List<SignInUserModel> AvailableUsers { get; set; }
+    }
+}
