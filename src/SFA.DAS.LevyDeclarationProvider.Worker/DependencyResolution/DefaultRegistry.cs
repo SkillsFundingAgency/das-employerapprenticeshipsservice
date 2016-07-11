b3aee823afb4fb9583bb8cf805cdb69053c274c3
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-11 13:54:19 +0100

    Add in structure map and also some stub data to use for the HMRC response.

---
+++ src/SFA.DAS.LevyDeclarationProvider.Worker/DependencyResolution/DefaultRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap.Configuration.DSL;
+
+namespace SFA.DAS.LevyDeclarationProvider.Worker.DependencyResolution
+{
+    public class DefaultRegistry : Registry
+    {
+
+    }
+}
