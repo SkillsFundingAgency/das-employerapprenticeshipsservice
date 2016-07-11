d74c2f5f7ad824cbd26ad767a33d94055ee6eb0d
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-11 09:51:10 +0100

    Added in first page of the Employer Account creation process. Added in small test to make sure correct view is returned

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountControllerTests/WhenIStartTheProcess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Controllers;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests.Controllers.EmployerAccountControllerTests
+{
+    public class WhenIStartTheProcess
+    {
+        private EmployerAccountController _employerAccountController;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _employerAccountController = new EmployerAccountController();
+        }
+
+        [Test]
+        public void ThenIAmPresentedWithTheEligibilityPage()
+        {
+            //Act
+            var actual = _employerAccountController.Index();
+
+            //Assert
+            Assert.IsNotNull(actual);
+            var actualViewResult = actual as ViewResult;
+            Assert.IsNotNull(actualViewResult);
+            Assert.AreEqual(string.Empty,actualViewResult.ViewName);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/EmployerAccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
+{
+    [Authorize]
+    public class EmployerAccountController : Controller
+    {
+        // GET: EmployerAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
