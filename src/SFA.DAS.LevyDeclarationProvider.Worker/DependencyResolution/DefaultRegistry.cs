a0adf38463c9668828a661e54cd96da9bc1dc126
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-11 14:08:35 +0100

    Added structure map and mediator and included DefaultRegistry. Also added in LevyDeclaration which will be responsible for processing data for the worker role

--- src/SFA.DAS.LevyDeclarationProvider.Worker/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.LevyDeclarationProvider.Worker/DependencyResolution/DefaultRegistry.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.LevyDeclarationProvider.Worker.Providers;
 using StructureMap.Configuration.DSL;
 
 namespace SFA.DAS.LevyDeclarationProvider.Worker.DependencyResolution
@@ -10,5 +12,19 @@ namespace SFA.DAS.LevyDeclarationProvider.Worker.DependencyResolution
     public class DefaultRegistry : Registry
     {
 
+        public DefaultRegistry()
+        {
+            
+            For<ILevyDeclaration>().Use<LevyDeclaration>();
+
+            AddMediatrRegistrations();
+        }
+
+        private void AddMediatrRegistrations()
+        {
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+            For<IMediator>().Use<Mediator>();
+        }
     }
 }
---
+++ src/SFA.DAS.LevyDeclarationProvider.Worker/Providers/LevyDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyDeclarationProvider.Worker.Providers
+{
+    public class LevyDeclaration : ILevyDeclaration
+    {
+        public Task Handle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
