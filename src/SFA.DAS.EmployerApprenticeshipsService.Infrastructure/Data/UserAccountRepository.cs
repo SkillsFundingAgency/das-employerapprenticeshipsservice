8b2da600bfb7a5e1300fe416a39965ed17532f41
Floyd Price <f.price@floydprice.com>
2016-07-12 09:28:32 +0100

    Use configuration for DB connection string

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/UserAccountRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/UserAccountRepository.cs
@@ -12,14 +12,17 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Data
 {
     public class UserAccountRepository : IUserAccountRepository
     {
+        string _connectionString = String.Empty;
+        public UserAccountRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
         public async Task<List<Account>> GetAccountsByUserId(string userId)
         {
-            var connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=SFA.DAS.EmployerApprenticeshipsService.Database;Integrated Security=True;Pooling=False;Connect Timeout=30");
+            var connection = new SqlConnection(_connectionString);
             var sql = @"select a.* from [dbo].[User] u 
-                        left join[dbo].[Membership]
-                        m on m.UserId = u.Id
-                        left join[dbo].[Account]
-                        a on m.AccountId = a.Id
+                        left join[dbo].[Membership] m on m.UserId = u.Id
+                        left join[dbo].[Account]  a on m.AccountId = a.Id
                         where u.PireanKey = @Id";
            var accounts =  connection.Query<Account>(sql, new {Id = userId});
             return (List<Account>) accounts;
--- src/SFA.DAS.EmployerApprenticeshipsService.Web/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/DependencyResolution/DefaultRegistry.cs
@@ -69,7 +69,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web.DependencyResolution {
             //For<IExample>().Use<Example>();
 
             IConfigurationRepository configurationRepository;
-            For<IUserAccountRepository>().Use<UserAccountRepository>();
+            
 
             if (bool.Parse(ConfigurationManager.AppSettings["LocalConfig"]))
             {
@@ -88,7 +88,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web.DependencyResolution {
             var config = configurationService.Get<EmployerApprenticeshipsServiceConfiguration>();
 
             For<IEmployerVerificationService>().Use<CompaniesHouseEmployerVerificationService>().Ctor<string>().Is(config.CompaniesHouse.ApiKey);
-
+            For<IUserAccountRepository>().Use<UserAccountRepository>().Ctor<string>().Is(config.Employer.DatabaseConnectionString);
             For<IUserRepository>().Use<FileSystemUserRepository>();
             For<IMediator>().Use<Mediator>();
         }
