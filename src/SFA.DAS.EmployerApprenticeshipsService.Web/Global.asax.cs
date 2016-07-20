7f1d45b7114f2432b215bf2d28ed47a8dc844e8e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-20 16:20:30 +0100

    Added in the telemetry config key into the Global.asax so that it is read from the cloud service configuration. The value is substituted at release.

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Configuration;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure;
 
 namespace SFA.DAS.EmployerApprenticeshipsService.Web
 {
@@ -8,6 +11,9 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web
     {
         protected void Application_Start()
         {
+
+            TelemetryConfiguration.Active.InstrumentationKey = CloudConfigurationManager.GetSetting("InstrumentationKey");
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
