1ac7eac1bde85a21886d451c2270cf872e0d7c48
floydprice <f.price@floydprice.com>
2016-07-22 14:07:13 +0100

    Team members view is not based an a view that does a union on actual members and invitations so we can see both in the same list

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/Views/GetTeamMembers.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [dbo].[GetTeamMembers]
+	AS 
+	
+select a.Id as 'AccountId', u.Id ,'' as Name,  u.Email,CONVERT(varchar(64), u.PireanKey) as 'UserRef', r.Name as 'Role', 2 as 'Status' from [User] u
+                            left join [Membership] m on m.UserId = u.Id
+                            left join [Role] r on r.Id = m.RoleId
+                            left join [Account] a on a.Id = m.AccountId
+
+Union	
+SELECT 
+	i.AccountId,
+	i.Id,
+	i.Name,
+	i.Email,
+	'' as 'UserRef',
+	r.Name AS 'Role',
+	i.Status
+FROM [dbo].[Invitation] i
+	JOIN [dbo].[Account] a
+		ON a.Id = i.AccountId
+	JOIN [dbo].[Role] r
+		ON r.Id = i.RoleId
