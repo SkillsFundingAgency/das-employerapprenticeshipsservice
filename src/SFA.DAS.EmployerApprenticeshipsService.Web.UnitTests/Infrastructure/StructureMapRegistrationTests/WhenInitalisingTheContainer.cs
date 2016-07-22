98e918ecc8b82de8ebfb83a2661f72dd372725f5
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-22 14:32:07 +0100

    Added in the ConfigurationPolicy which will be responsible for registering the configuration of the application based on a policy being defined in StructureMap. Test added around this to make sure that the Configuration is resolved as part of the policy. Upgraded structuremap to be consistent across all projects in the solution.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Infrastructure/StructureMapRegistrationTests/WhenInitalisingTheContainer.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Configuration;
+using SFA.DAS.EmployerApprenticeshipsService.Web.DependencyResolution;
+using StructureMap;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests.Infrastructure.StructureMapRegistrationTests
+{
+    public class WhenInitalisingTheContainer
+    {
+        private Container _container;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _container = new Container(
+                c =>
+                    {
+                        c.AddRegistry<TestRegistry>();
+                        c.Policies.Add<ConfigurationPolicy<EmployerApprenticeshipsServiceConfiguration>>();
+                    }
+                );
+        }
+
+        [Test]
+        public void ThenTheConfigurationPolicyIsCorrectlyInitalised()
+        {
+            //Act
+            var item = _container.GetInstance<TestClass>();
+
+            //Assert
+            Assert.IsNotNull(item.Configuration);
+        }
+
+        public class TestRegistry : Registry
+        {
+            public TestRegistry()
+            {
+                For<ITestClass>().Use<TestClass>();
+            }
+        }
+
+        public interface ITestClass
+        {
+            
+        }
+
+        public class TestClass : ITestClass
+        {
+            public readonly EmployerApprenticeshipsServiceConfiguration Configuration;
+
+            public TestClass(EmployerApprenticeshipsServiceConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/DependencyResolution/ConfigurationPolicy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Azure;
+using SFA.DAS.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.Configuration.FileStorage;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Configuration;
+using StructureMap;
+using StructureMap.Pipeline;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.DependencyResolution
+{
+    public class ConfigurationPolicy<T> : ConfiguredInstancePolicy
+    {
+        private const string ServiceName = "SFA.DAS.EmployerApprenticeshipsService";
+        protected override void apply(Type pluginType, IConfiguredInstance instance)
+        {
+            var serviceConfigurationParamater =
+                instance?.Constructor.GetParameters()
+                    .FirstOrDefault(x => x.ParameterType == typeof(T));
+
+            if (serviceConfigurationParamater != null)
+            {
+                var environment = Environment.GetEnvironmentVariable("DASENV");
+                if (string.IsNullOrEmpty(environment))
+                {
+                    environment = CloudConfigurationManager.GetSetting("EnvironmentName");
+                }
+
+                var configurationRepository = GetConfigurationRepository();
+                var configurationService = new ConfigurationService(configurationRepository, new ConfigurationOptions(ServiceName, environment, "1.0"));
+
+                var result = configurationService.Get<EmployerApprenticeshipsServiceConfiguration>();
+                if (result != null)
+                {
+                    instance.Dependencies.AddForConstructorParameter(serviceConfigurationParamater, result);
+                }
+            }
+        }
+        
+        private static IConfigurationRepository GetConfigurationRepository()
+        {
+            IConfigurationRepository configurationRepository;
+            if (bool.Parse(ConfigurationManager.AppSettings["LocalConfig"]))
+            {
+                configurationRepository = new FileStorageConfigurationRepository();
+            }
+            else
+            {
+                configurationRepository = new AzureTableStorageConfigurationRepository(CloudConfigurationManager.GetSetting("ConfigurationStorageConnectionString"));
+            }
+            return configurationRepository;
+        }
+        
+    }
+}
