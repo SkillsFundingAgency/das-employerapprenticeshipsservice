0061fb081eddefa7f47d61454b6cd9b534b479e9
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-25 14:28:39 +0100

    Added in nlog and new LoggingPolicy to initalize the ILogger when used in the constructor parameters.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/DepedencyResolution/LoggingPolicy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using NLog;
+using StructureMap;
+using StructureMap.Pipeline;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.DepedencyResolution
+{
+    public class LoggingPolicy : ConfiguredInstancePolicy
+    {
+        protected override void apply(Type pluginType, IConfiguredInstance instance)
+        {
+            var logger = instance?.Constructor?.GetParameters().FirstOrDefault(x => x.ParameterType == typeof(ILogger));
+
+            if (logger != null)
+            {
+                instance.Dependencies.AddForConstructorParameter(logger, LogManager.GetLogger(pluginType.Name));
+            }
+
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Infrastructure/StructureMapRegistrationTests/WhenAddingLogging.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.DepedencyResolution;
+using StructureMap;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests.Infrastructure.StructureMapRegistrationTests
+{
+    public class WhenAddingLogging
+    {
+        private Container _container;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _container = new Container(
+                c =>
+                    {
+                        c.AddRegistry<TestRegistry>();
+                        c.Policies.Add<LoggingPolicy>();
+                    }
+                );
+        }
+
+        [Test]
+        public void ThenTheLoggerCanBeResolvedForThatClass()
+        {
+            //Act
+            var actual = _container.GetInstance<TestClass>();
+
+            //Assert
+            Assert.IsNotNull(actual.Logger);
+            
+        }
+
+        public interface ITestClass
+        {
+
+        }
+
+        public class TestClass : ITestClass
+        {
+            public readonly NLog.ILogger Logger;
+
+            public TestClass(NLog.ILogger logger)
+            {
+                Logger = logger;
+            }
+        }
+        public class TestRegistry : Registry
+        {
+            public TestRegistry()
+            {
+                For<ITestClass>().Use<TestClass>();
+            }
+        }
+    }
+}
