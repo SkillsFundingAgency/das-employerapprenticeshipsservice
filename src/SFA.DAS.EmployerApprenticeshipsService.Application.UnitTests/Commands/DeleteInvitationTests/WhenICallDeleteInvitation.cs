13bee5bcfc1a3a35280c26c71e7c22a67106fe9d
ijrussell <i.j.russell@tesco.net>
2016-07-25 08:02:42 +0100

    Added delete command tests

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/DeleteInvitationTests/WhenICallDeleteInvitation.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/DeleteInvitationTests/WhenICallDeleteInvitation.cs
@@ -1,11 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.EmployerApprenticeshipsService.Application.Commands.CreateInvitation;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Commands.DeleteInvitation;
 using SFA.DAS.EmployerApprenticeshipsService.Domain;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
-using SFA.DAS.TimeProvider;
 
 namespace SFA.DAS.EmployerApprenticeshipsService.Application.Tests.Commands.DeleteInvitationTests
 {
@@ -16,6 +14,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Tests.Commands.Dele
         private Mock<IAccountTeamRepository> _accountTeamRepository;
         private DeleteInvitationCommandHandler _handler;
         private Invitation _invitation;
+        private DeleteInvitationCommand _command;
 
         [SetUp]
         public void Setup()
@@ -29,6 +28,12 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Tests.Commands.Dele
                 AccountId = 101,
                 Email = "test.user@test.local"
             };
+            _command = new DeleteInvitationCommand
+            {
+                Id = _invitation.Id,
+                AccountId = _invitation.AccountId,
+                ExternalUserId = "EXT_USER"
+            };
         }
 
         [Test]
@@ -40,14 +45,33 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Tests.Commands.Dele
                 RoleId = (int)Role.Owner
             });
 
-            await _handler.Handle(new DeleteInvitationCommand
-            {
-                Id = _invitation.Id,
-                AccountId = _invitation.AccountId,
-                ExternalUserId = "EXT_USER"
-            });
+            await _handler.Handle(_command);
 
             _invitationRepository.Verify(x => x.ChangeStatus(It.Is<Invitation>(z => z.Id == _invitation.Id && z.Status == InvitationStatus.Deleted)), Times.Once);
+        }
+
+        [Test]
+        public void ThrowExceptionWhenInvitationNotFound()
+        {
+            _invitationRepository.Setup(x => x.Get(_command.Id)).ReturnsAsync(_invitation);
+
+            var exception = Assert.ThrowsAsync<InvalidRequestException>(() => _handler.Handle(_command));
+
+            Assert.That(exception.ErrorMessages.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenCallerIsNotAccountOwner()
+        {
+            _invitationRepository.Setup(x => x.Get(_command.Id)).ReturnsAsync(_invitation);
+            _accountTeamRepository.Setup(x => x.GetMembership(_command.AccountId, _command.ExternalUserId)).ReturnsAsync(new Membership
+            {
+                RoleId = 0
+            });
+
+            var exception = Assert.ThrowsAsync<InvalidRequestException>(() => _handler.Handle(_command));
+
+            Assert.That(exception.ErrorMessages.Count, Is.EqualTo(1));
         }
     }
 }
