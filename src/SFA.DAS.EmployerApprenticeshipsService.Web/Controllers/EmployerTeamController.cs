dada1e60a487b3c25a79ff8d847eeff87b1891c7
floydprice <f.price@floydprice.com>
2016-07-26 15:06:32 +0100

    Removed the manual chack for the users id (claim) and delagated it to the new method on the iOwnWrapper (which should be injected in to each controller)

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/EmployerTeamController.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/EmployerTeamController.cs
@@ -21,7 +21,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
         }
         public async Task<ActionResult> Index(int accountId)
         {
-            var userIdClaim = ((ClaimsIdentity)System.Web.HttpContext.Current.User.Identity).Claims.FirstOrDefault(claim => claim.Type == @"sub");
+            var userIdClaim = _owinWrapper.GetPersistantUserIdClaimFromProvider();
             if (userIdClaim?.Value == null) return RedirectToAction("Index", "Home");
 
             var teamVieWModel = await _employerTeamOrchestrator.GetTeamMembers(accountId, userIdClaim.Value);
