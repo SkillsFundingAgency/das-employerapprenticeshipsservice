7a802d6b01e5c80eb81efb107590ec8d7e47644e
the-daniel-rothig <daniel.rothig@gmail.com>
2016-07-26 16:23:02 +0100

    Add visual tests
    It's tricky to visually test MVC views because they can't be rendered
    reliably from a separate test project. The workaround is to have a
    static page serving navigation states for easy visual verification and
    then exclude this page and associated controller from the release build.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/VisualTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
+{
+    public class VisualTestController : Controller
+    {
+        // GET: VisualTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
