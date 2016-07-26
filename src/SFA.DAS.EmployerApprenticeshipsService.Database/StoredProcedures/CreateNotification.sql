324e900274158cad72918d845f13115d03c09688
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-26 15:04:00 +0100

    Add stored procedure for inserting the notification into the database

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/CreateNotification.sql
@@ -0,0 +1,18 @@
+﻿
+
+CREATE PROCEDURE [dbo].[CreateNotification]
+	@UserId INT, 
+	@DateTime DATETIME ,
+	@ForecFormat BIT = 0,
+	@TemplateId VARCHAR(20),
+	@Data NVARCHAR(MAX),
+	@MessageFormat TINYINT
+as
+BEGIN
+	INSERT INTO [dbo].[Notification]
+		(UserId, DateTime, ForecFormat, TemplateId,Data,MessageFormat)
+	VALUES
+		(@UserId,@DateTime, @ForecFormat, @TemplateId, @Data, @MessageFormat)
+
+	SELECT Id = SCOPE_IDENTITY()
+END
