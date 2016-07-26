b3e1f106f8a1be2b00d41d2f4cb0175ebab19adb
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-26 15:21:15 +0100

    Implement NotificationRepository Create method, wiring up stored procedure call

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/NotificationRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Configuration;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Models.Notification;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Data
+{
+    public class NotificationRepository : BaseRepository, INotificationRepository
+    {
+        public NotificationRepository(EmployerApprenticeshipsServiceConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public async Task<int> Create(NotificationMessage message)
+        {
+            return await WithConnection(async c =>
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@UserdId", message.UserId, DbType.Int32);
+                parameters.Add("@DateTime", message.DateTime, DbType.DateTime);
+                parameters.Add("@ForecFormat", message.ForceFormat, DbType.Int16);
+                parameters.Add("@TemplateId", message.TemplatedId, DbType.String);
+                parameters.Add("@Data", message.Data, DbType.String);
+                parameters.Add("@id", null, DbType.Int32, ParameterDirection.Output, 4);
+
+                await c.ExecuteAsync("[dbo].[CreateNotification]", parameters, commandType: CommandType.StoredProcedure);
+
+                return parameters.Get<int>("@Id");
+            });
+        }
+    }
+}
