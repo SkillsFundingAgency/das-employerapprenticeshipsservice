b532be10d3aff7bc950bdae07e7d52e955906484
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-26 14:19:14 +0100

    Created new classes in the domain project for handling notification messages to be written to the repository

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Notification/NotificationMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Models.Notification
+{
+    public class NotificationMessage
+    {
+        public string UserId { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool ForceFormat { get; set; }
+        public string TemplatedId { get; set; }
+        public string Data { get; set; }
+        public MessageFormat MessageFormat { get; set; }
+    }
+}
