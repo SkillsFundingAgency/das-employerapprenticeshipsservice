5688371b537e4baf10759f7250ba6d72207b7b87
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-26 16:36:08 +0100

    Implment validator for the SendNotificationCommand. Created tests around this as well

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SendNotification/SendNotificationCommandValidator.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SendNotification/SendNotificationCommandValidator.cs
@@ -7,7 +7,26 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.SendNotifi
     {
         public ValidationResult Validate(SendNotificationCommand item)
         {
-            throw new NotImplementedException();
+            var validationResult = new ValidationResult();
+
+            if (item.UserId == 0)
+            {
+                validationResult.AddError(nameof(item.UserId),"User Id has not been supplied");
+            }
+
+            if (item.Data == null)
+            {
+                validationResult.AddError(nameof(item.Data), "EmailContent has not been supplied");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(item.Data.RecipientsAddress))
+                {
+                    validationResult.AddError(nameof(item.Data.RecipientsAddress), "Recipients Address has not been supplied");
+                }
+            }
+
+            return validationResult;
         }
     }
 }
