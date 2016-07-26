9dda2680e65131ea56d4ef281cbdb0a5c7bc665e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-26 14:19:58 +0100

    Added INotificationRepository interface, this will take a NotificationMessage and write it to the repository. The interface is not yet implemented

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Data/INotificationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Models.Notification;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Data
+{
+    public interface INotificationRepository
+    {
+        Task<int> Create(NotificationMessage message);
+    }
+}
