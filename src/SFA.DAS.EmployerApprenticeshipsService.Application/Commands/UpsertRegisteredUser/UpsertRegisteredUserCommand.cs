97b85ecc1647bb436ab388a1b4afa6a7a7ab8510
floydprice <f.price@floydprice.com>
2016-07-27 23:26:13 +0100

    Command to support the upsert user request

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/UpsertRegisteredUser/UpsertRegisteredUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.UpsertRegisteredUser
+{
+    public class UpsertRegisteredUserCommand : IAsyncRequest
+    {
+        public string UserRef { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; } 
+    }
+}
