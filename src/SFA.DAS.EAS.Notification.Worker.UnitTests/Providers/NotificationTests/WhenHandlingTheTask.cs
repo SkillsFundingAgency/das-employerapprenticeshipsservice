99326a8f97a775eacf06d97375ec8a94c47258eb
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-07-27 10:24:15 +0100

    Add test project for the notification worker

---
+++ src/SFA.DAS.EAS.Notification.Worker.UnitTests/Providers/NotificationTests/WhenHandlingTheTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NLog;
+using NUnit.Framework;
+
+namespace SFA.DAS.EAS.Notification.Worker.UnitTests.Providers.NotificationTests
+{
+    
+    public class WhenHandlingTheTask
+    {
+        private Worker.Providers.Notification _notification;
+        private Mock<ILogger> _logger;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _notification = new Worker.Providers.Notification(_logger.Object);
+        }
+
+        [Test]
+        public void ThenTheQueueIsRead()
+        {
+
+        }
+    }
+}
