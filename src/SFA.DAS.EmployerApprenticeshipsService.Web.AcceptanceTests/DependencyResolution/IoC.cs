fcd63b2c0a2513bb674d930e733e9962f070d0b2
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-02 09:09:43 +0100

    Tidy up for the dependency resolution and also implemented the MockMessagePolicy, so that we dont process acceptance test messages. Also made the DB cleanup happen before and after scenario running so we dont have stray database values.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.AcceptanceTests/DependencyResolution/IoC.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Configuration;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.DepedencyResolution;
+using SFA.DAS.EmployerApprenticeshipsService.Web.AcceptanceTests.MockPolicy;
+using SFA.DAS.Messaging;
+using StructureMap;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.AcceptanceTests.DependencyResolution
+{
+    public static class IoC
+    {
+        public static Container CreateContainer(Mock<IMessagePublisher> messagePublisher)
+        {
+            return new Container(c =>
+            {
+                c.Policies.Add<ConfigurationPolicy<EmployerApprenticeshipsServiceConfiguration>>();
+                c.Policies.Add<LoggingPolicy>();
+                c.Policies.Add(new MockMessagePolicy(messagePublisher));
+                c.AddRegistry<DefaultRegistry>();
+            });
+        }
+    }
+}
