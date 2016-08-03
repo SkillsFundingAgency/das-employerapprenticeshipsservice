6ccd97d8621827b9da7140499d6229e77e753bd0
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-03 13:35:26 +0100

    Created query validator and implemented. Updated tests for this

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetHmrcEmployerInformation/WhenValidatingTheRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetHmrcEmployerInformation;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests.Queries.GetHmrcEmployerInformation
+{
+    public class WhenValidatingTheRequest
+    {
+        private GetHmrcEmployerInformationValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new GetHmrcEmployerInformationValidator();            
+        }
+
+        [Test]
+        public void ThenMessageIsValidWhenAllFieldsArePopulated()
+        {
+            //Act
+            var actual = _validator.Validate(new GetHmrcEmployerInformatioQuery {AuthToken = "someValue", Empref = "someValue"});
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+
+        [Test]
+        public void ThenTheDictionaryIsPopulatedWithEmptyFields()
+        {
+            //Act
+            var actual = _validator.Validate(new GetHmrcEmployerInformatioQuery());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string,string>("AuthToken","AuthToken has not been supplied"),actual.ValidationDictionary );
+            Assert.Contains(new KeyValuePair<string,string>("Empref","Empref has not been supplied"),actual.ValidationDictionary);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetHmrcEmployerInformation/GetHmrcEmployerInformationValidator.cs
@@ -0,0 +1,24 @@
+﻿using SFA.DAS.EmployerApprenticeshipsService.Application.Validation;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetHmrcEmployerInformation
+{
+    public class GetHmrcEmployerInformationValidator : IValidator<GetHmrcEmployerInformatioQuery>
+    {
+        public ValidationResult Validate(GetHmrcEmployerInformatioQuery item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrWhiteSpace(item.AuthToken))
+            {
+                validationResult.AddError(nameof(item.AuthToken),"AuthToken has not been supplied");
+            }
+
+            if (string.IsNullOrWhiteSpace(item.Empref))
+            {
+                validationResult.AddError(nameof(item.Empref), "Empref has not been supplied");
+            }
+
+            return validationResult;
+        }
+    }
+}
