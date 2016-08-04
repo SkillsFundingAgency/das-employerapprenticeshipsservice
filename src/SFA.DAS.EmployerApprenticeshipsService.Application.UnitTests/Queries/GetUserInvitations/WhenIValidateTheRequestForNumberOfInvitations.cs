23d0dc40ee685d41d4df83bf5cc410d9c4a6a28f
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-04 17:55:05 +0100

    Implement the validator for the GetNumberOfUserInvitationsHandler

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetUserInvitations/WhenIValidateTheRequestForNumberOfInvitations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetUserInvitations;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests.Queries.GetUserInvitations
+{
+    public class WhenIValidateTheRequestForNumberOfInvitations
+    {
+        private GetNumberOfUserInvitationsValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new GetNumberOfUserInvitationsValidator();   
+        }
+
+        [Test]
+        public void ThenIfAllFieldsArePopulatedItIsValid()
+        {
+            //Act
+            var actual = _validator.Validate(new GetNumberOfUserInvitationsQuery {UserId = Guid.NewGuid().ToString()});
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+            
+        }
+
+        [Test]
+        public void ThenIfTheUserIdIsNotAGuidItIsNotValid()
+        {
+            //Act
+            var actual = _validator.Validate(new GetNumberOfUserInvitationsQuery { UserId = "someId" });
+            
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string, string>("UserId", "UserId is not in the correct format"), actual.ValidationDictionary);
+        }
+
+        [Test]
+        public void ThenIfFieldsArentPopulatedTheErrorDictionaryIsPopulated()
+        {
+            //Act
+            var actual = _validator.Validate(new GetNumberOfUserInvitationsQuery { UserId = "" });
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string, string>("UserId", "UserId has not been supplied"), actual.ValidationDictionary);
+        }
+
+            
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetUserInvitations/GetNumberOfUserInvitationsValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Validation;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetUserInvitations
+{
+    public class GetNumberOfUserInvitationsValidator : IValidator<GetNumberOfUserInvitationsQuery>
+    {
+        public ValidationResult Validate(GetNumberOfUserInvitationsQuery item)
+        {
+            var result = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.UserId))
+            {
+                result.AddError(nameof(item.UserId), "UserId has not been supplied");
+            }
+            else
+            {
+                Guid value;
+                var guidResult = Guid.TryParse(item.UserId, out value);
+                if (!guidResult)
+                {
+                    result.AddError(nameof(item.UserId), "UserId is not in the correct format");
+                }
+            }
+
+            return result;
+        }
+    }
+}
