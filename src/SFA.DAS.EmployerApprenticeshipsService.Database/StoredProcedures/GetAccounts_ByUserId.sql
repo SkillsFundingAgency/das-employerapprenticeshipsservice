5ddee2cd6a9cfb15b4e984aca5481bf3c94eb15e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-04 15:10:10 +0100

    Created stored procedure for getting accounts that a user is linked to and the role id associated to that

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/GetAccounts_ByUserId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetAccounts_ByUserId]
+	@id UNIQUEIDENTIFIER
+	
+AS
+select 
+	a.Name, m.RoleId 
+from 
+	[dbo].[User] u 
+inner join 
+	[dbo].[Membership] m on m.UserId = u.Id
+inner join
+	[dbo].[Account]  a on m.AccountId = a.Id
+where 
+u.PireanKey = @id
