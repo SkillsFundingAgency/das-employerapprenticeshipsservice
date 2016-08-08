d84cd6c073666e018ec410c65634b3a2cf2a5711
ijrussell <i.j.russell@tesco.net>
2016-08-08 10:00:38 +0100

    Added index view, controller and orchestrator

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/EmployerAccountPayeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Authentication;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Orchestrators;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
+{
+    public class EmployerAccountPayeController : Controller
+    {
+        private readonly IOwinWrapper _owinWrapper;
+        private readonly EmployerAccountPayeOrchestrator _employerAccountPayeOrchestrator;
+
+        public EmployerAccountPayeController(IOwinWrapper owinWrapper, EmployerAccountPayeOrchestrator employerAccountPayeOrchestrator)
+        {
+            if (owinWrapper == null)
+                throw new ArgumentNullException(nameof(owinWrapper));
+            if (employerAccountPayeOrchestrator == null)
+                throw new ArgumentNullException(nameof(employerAccountPayeOrchestrator));
+            _owinWrapper = owinWrapper;
+            _employerAccountPayeOrchestrator = employerAccountPayeOrchestrator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(long accountid)
+        {
+            var userIdClaim = _owinWrapper.GetClaimValue(@"sub");
+            if (string.IsNullOrWhiteSpace(userIdClaim)) return RedirectToAction("Index", "Home");
+
+            var viewModel = await _employerAccountPayeOrchestrator.Get(accountid, userIdClaim);
+
+            return View(viewModel);
+        }
+    }
+}
