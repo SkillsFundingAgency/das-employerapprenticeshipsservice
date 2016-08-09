30216d9e325635f7596932ce5a3718696ab344dc
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-09 13:58:04 +0100

    Add validator for the GetAccountLegalEntitiesQueryRequest. Also added tests around this

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountLegalEntities/GetAccountLegalEntitiesResponse.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountLegalEntities/GetAccountLegalEntitiesResponse.cs
@@ -1,7 +1,9 @@
-﻿namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetAccountLegalEntities
+﻿using SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetAccountLegalEntities
 {
     public class GetAccountLegalEntitiesResponse
     {
-        
+        public LegalEntities Entites { get; set; }
     }
 }
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Entities/Account/LegalEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account
+{
+    public class LegalEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
