01ab58fe7c58215f60b5a78e92b33418505b5137
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-10 09:43:52 +0100

    Change legal entity to include the CompanyNumber, RegisteredAddress and DateOfIncorporation

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Entities/Account/LegalEntity.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Entities/Account/LegalEntity.cs
@@ -1,9 +1,17 @@
-﻿namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account
+﻿using System;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account
 {
     public class LegalEntity
     {
         public long Id { get; set; }
 
+        public string CompanyNumber { get; set; }
+
         public string Name { get; set; }
+
+        public string RegisteredAddress { get; set; }
+
+        public DateTime DateOfIncorporation { get; set; }
     }
 }
