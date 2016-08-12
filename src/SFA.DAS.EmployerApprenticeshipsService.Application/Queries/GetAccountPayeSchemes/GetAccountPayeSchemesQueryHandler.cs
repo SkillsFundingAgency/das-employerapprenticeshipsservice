b1a3d46233f9a9102ef37d0408ed12caf1ecdeb4
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-12 09:42:09 +0100

    Removed the check on getting PAYE schemes, not to check that you are an owner. Anyone can view them

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountPayeSchemes/GetAccountPayeSchemesQueryHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountPayeSchemes/GetAccountPayeSchemesQueryHandler.cs
@@ -10,27 +10,16 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetAccountP
     public class GetAccountPayeSchemesQueryHandler : IAsyncRequestHandler<GetAccountPayeSchemesRequest, GetAccountPayeSchemesResponse>
     {
         private readonly IAccountRepository _accountRepository;
-        private readonly IMembershipRepository _membershipRepository;
 
-        public GetAccountPayeSchemesQueryHandler(IAccountRepository accountRepository, IMembershipRepository membershipRepository)
+        public GetAccountPayeSchemesQueryHandler(IAccountRepository accountRepository)
         {
             if (accountRepository == null)
                 throw new ArgumentNullException(nameof(accountRepository));
-            if (membershipRepository == null)
-                throw new ArgumentNullException(nameof(membershipRepository));
             _accountRepository = accountRepository;
-            _membershipRepository = membershipRepository;
         }
 
         public async Task<GetAccountPayeSchemesResponse> Handle(GetAccountPayeSchemesRequest message)
         {
-            var membership = await _membershipRepository.GetCaller(message.AccountId, message.ExternalUserId);
-
-            if (membership == null)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Membership", "Caller is not a member of this account" } });
-            if (membership.RoleId != (short)Role.Owner)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Membership", "Caller is not an owner of this account" } });
-
             var payeSchemes = await _accountRepository.GetPayeSchemes(message.AccountId);
 
             return new GetAccountPayeSchemesResponse
