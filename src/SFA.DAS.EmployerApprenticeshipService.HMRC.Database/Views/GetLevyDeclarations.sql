946bb9bbdfcc39b3f6e711052e9d4d244a17ac84
Robin O'Connor <GitHub@bytepointsoftware.com>
2016-08-18 14:01:30 +0100

    HMRC tables have been moved out of the Levy database project and into the new HMRC database project. A view that uses the levy declaration table has also be altered to show only details available to it in the HMRC database as well so this may need to be removed or altered further later on

---
+++ src/SFA.DAS.EmployerApprenticeshipService.HMRC.Database/Views/GetLevyDeclarations.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[GetLevyDeclarations]
+AS
+SELECT ld.Id AS Id,	
+	ld.empRef AS EmpRef,
+	ld.SubmissionDate AS SubmissionDate,
+	ld.SubmissionId AS SubmissionId,
+	ld.SubmissionType AS SubmissionType,
+	ld.Amount AS Amount,
+	t.Amount AS EnglishFraction
+FROM [dbo].[LevyDeclaration] ld
+OUTER APPLY
+(
+	SELECT TOP 1 *
+	FROM [dbo].[EnglishFraction] ef
+	WHERE ef.EmpRef = ld.empRef 
+		AND ef.[DateCalculated] <= ld.[SubmissionDate]
+	ORDER BY [DateCalculated] DESC
+) t
