f2cbc2234920d2bceededae9b559e8e2d2284398
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-22 17:01:35 +0100

    New table structure for the LevyDeclaration

--- src/SFA.DAS.EmployerApprenticeshipService.HMRC.Database/Tables/LevyDeclaration.sql
+++ src/SFA.DAS.EmployerApprenticeshipService.HMRC.Database/Tables/LevyDeclaration.sql
@@ -3,8 +3,11 @@
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
 	[AccountId] BIGINT NOT NULL DEFAULT 0,
     [empRef] NVARCHAR(50) NOT NULL, 
-    [Amount] DECIMAL(18, 4) NULL, 
+    [LevyDueYTD] DECIMAL(18, 4) NULL, 
+    [LevyAllowanceForYear] DECIMAL(18, 4) NULL, 
     [SubmissionDate] DATETIME NULL, 
     [SubmissionType] NCHAR(10) NULL, 
-    [SubmissionId] NCHAR(255) NULL
+    [SubmissionId] BIGINT NOT NULL DEFAULT 0,
+	[PayrollYear] NVARCHAR(10) NULL,
+	[PayrollMonth] TINYINT NULL
 )
