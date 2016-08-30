a29310cea35a69d8fe12a08457b26a9cd195ff80
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-08-30 10:06:42 +0100

    Update made so that multiple PAYEs can be added for the test data.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateAccount/CreateAccountCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateAccount/CreateAccountCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Commands.CreateEmployerAccountCreatedNotification;
@@ -20,7 +21,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.CreateAcco
         private readonly IUserRepository _userRepository;
         private readonly IMessagePublisher _messagePublisher;
         private readonly IMediator _mediator;
-        private readonly CreateAccountCommandValidator _validator;
+        private readonly IValidator<CreateAccountCommand> _validator;
 
         public CreateAccountCommandHandler(IAccountRepository accountRepository, IUserRepository userRepository, IMessagePublisher messagePublisher, IMediator mediator, IValidator<CreateAccountCommand> validator)
         {
@@ -34,7 +35,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.CreateAcco
             _userRepository = userRepository;
             _messagePublisher = messagePublisher;
             _mediator = mediator;
-            _validator = new CreateAccountCommandValidator();
+            _validator = validator;
         }
 
         protected override async Task HandleCore(CreateAccountCommand message)
@@ -49,7 +50,18 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.CreateAcco
             if (user == null)
                 throw new InvalidRequestException(new Dictionary<string, string> { { "User", "User does not exist" } });
 
-            var accountId = await _accountRepository.CreateAccount(user.Id, message.CompanyNumber, message.CompanyName, message.CompanyRegisteredAddress, message.CompanyDateOfIncorporation, message.EmployerRef, message.AccessToken, message.RefreshToken);
+            var emprefs = message.EmployerRef.Split(',');
+
+            var accountId = await _accountRepository.CreateAccount(user.Id, message.CompanyNumber, message.CompanyName, message.CompanyRegisteredAddress, message.CompanyDateOfIncorporation, emprefs[0], message.AccessToken, message.RefreshToken);
+
+            if (emprefs.Length > 1)
+            {
+                var schemes = await _accountRepository.GetPayeSchemes(accountId);
+                for (var i = 1; i < emprefs.Length; i++)
+                {
+                    await _accountRepository.AddPayeToAccountForExistingLegalEntity(accountId, schemes.First().LegalEntityId, emprefs[i], message.AccessToken, message.RefreshToken);
+                }
+            }
 
             await _messagePublisher.PublishAsync(new EmployerRefreshLevyQueueMessage
             {
