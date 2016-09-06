c87495876351c9a639418c936cdee40803b9f60a
Lee Wadhams <lee.wadhams@valtech.com>
2016-09-06 17:23:32 +0100

    Added basic structure to retrieve all commitments for an account.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/EmployerCommitmentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Authentication;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Orchestrators;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
+{
+    [Authorize]
+    public class EmployerCommitmentsController : BaseController
+    {
+        private readonly IOwinWrapper _owinWrapper;
+        private readonly EmployerCommitmentsOrchestrator _employerCommitmentsOrchestrator;
+
+        public EmployerCommitmentsController(IOwinWrapper owinWrapper,
+            EmployerCommitmentsOrchestrator employerCommitmentsOrchestrator)
+        {
+            if (owinWrapper == null)
+                throw new ArgumentNullException(nameof(owinWrapper));
+            if (employerCommitmentsOrchestrator == null)
+                throw new ArgumentNullException(nameof(employerCommitmentsOrchestrator));
+            _owinWrapper = owinWrapper;
+            _employerCommitmentsOrchestrator = employerCommitmentsOrchestrator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index(long accountid)
+        {
+            var model = await _employerCommitmentsOrchestrator.GetAll(accountid);
+
+            return View(model);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Orchestrators/EmployerCommitmentsOrchestrator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetCommitments;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Models;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Orchestrators
+{
+    public sealed class EmployerCommitmentsOrchestrator
+    {
+        private readonly IMediator _mediator;
+
+        public EmployerCommitmentsOrchestrator(IMediator mediator)
+        {
+            if (mediator == null)
+                throw new ArgumentNullException(nameof(mediator));
+
+            _mediator = mediator;
+        }
+
+        internal async Task<OrchestratorResponse<CommitmentListViewModel>> GetAll(long accountid)
+        {
+            var data = await _mediator.SendAsync(new GetCommitmentsQuery
+            {
+                Accountid = accountid
+            });
+
+            return new OrchestratorResponse<CommitmentListViewModel>
+            {
+                Data = new CommitmentListViewModel
+                {
+                    Commitments = data.Commitments
+                }
+            };
+        }
+    }
+}
