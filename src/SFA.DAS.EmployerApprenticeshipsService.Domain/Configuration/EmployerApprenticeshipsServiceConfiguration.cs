8655cf2f7b9d2517c5313f8ed608437c9da8a1e7
Lee Wadhams <lee.wadhams@valtech.com>
2016-09-06 14:43:22 +0100

    Added Commitments Api configuration class

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/EmployerApprenticeshipsServiceConfiguration.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/EmployerApprenticeshipsServiceConfiguration.cs
@@ -5,11 +5,12 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Configuration
     public class EmployerApprenticeshipsServiceConfiguration : IConfiguration
     {
         public CompaniesHouseConfiguration CompaniesHouse { get; set; }
-        public string  ServiceBusConnectionString { get; set; }
+        public string ServiceBusConnectionString { get; set; }
         public IdentityServerConfiguration Identity { get; set; }
         public SmtpConfiguration SmtpServer { get; set; }
         public string DashboardUrl { get; set; }
         public HmrcConfiguration Hmrc { get; set; }
         public string DatabaseConnectionString { get; set; }
+        public CommitmentsApiConfiguration CommitmentsApi { get; set; }
     }
 }
