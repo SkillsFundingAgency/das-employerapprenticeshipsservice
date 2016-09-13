2d5fbd56e824addc83bc0273267be0c08bde57fc
Robin O'Connor <GitHub@bytepointsoftware.com>
2016-09-13 10:31:34 +0100

    UI for landing page has been updated to reflect the latest prototype design. We have also removed the redirect url param for registering as this is no longer needed.

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Controllers/HomeController.cs
@@ -47,8 +47,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Web.Controllers
         {
             var schema = System.Web.HttpContext.Current.Request.Url.Scheme;
             var authority = System.Web.HttpContext.Current.Request.Url.Authority;
-            //return new RedirectResult("https://ppidentity.apprenticeships.sfa.bis.gov.uk/Login/dialog/appl/selfcare/wflow/register?redirect_uri=https://localhost");
-            return new RedirectResult($"{_configuration.Identity.BaseAddress}/Login/dialog/appl/selfcare/wflow/register?redirect_uri={schema}://{authority}");
+            return new RedirectResult("https://ppidentity.apprenticeships.sfa.bis.gov.uk/Login/dialog/appl/selfcare/wflow/register");
         }
 
         [Authorize]
