748ffb3083d25b976a329dd9f478b2bab80ac84d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-09-14 16:08:16 +0100

    Implement the validator for RemovePayeScheme

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/RemovePayeFromAccountTests/WhenIValidateTheRequest.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Commands.RemovePayeFromAccount;
+using SFA.DAS.EmployerApprenticeshipsService.Domain;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests.Commands.RemovePayeFromAccountTests
+{
+    public class WhenIValidateTheRequest
+    {
+        private RemovePayeFromAccountCommandValidator _validator;
+        private Mock<IMembershipRepository> _membershipRepository;
+
+
+        [SetUp]
+        public void Arrange()
+        {
+            _membershipRepository = new Mock<IMembershipRepository>();
+            _membershipRepository.Setup(x => x.GetCaller(It.IsAny<long>(), It.IsAny<string>())).ReturnsAsync(new MembershipView { AccountId = 12345, RoleId = (short)Role.Owner });
+
+            _validator = new RemovePayeFromAccountCommandValidator(_membershipRepository.Object);
+        }
+
+        [Test]
+        public async Task ThenItIsValidIfAllFieldsArePopulated()
+        {
+            //Act
+            var result = await _validator.ValidateAsync(new RemovePayeFromAccountCommand
+                {
+                    AccountId = 12345,
+                    PayeRef = "123RFD",
+                    UserId = "123edds"
+                });
+
+            //Assert
+            Assert.IsTrue(result.IsValid());
+        }
+
+        [Test]
+        public async Task ThenTheValidationDictionaryIsPopulatedWhenThereAreErrors()
+        {
+            //Act
+            var result = await _validator.ValidateAsync(new RemovePayeFromAccountCommand());
+
+            //Assert
+            Assert.IsFalse(result.IsValid());
+            Assert.IsNotEmpty(result.ValidationDictionary);
+            Assert.Contains(new KeyValuePair<string,string>("AccountId","AccountId has not been supplied"), result.ValidationDictionary);
+            Assert.Contains(new KeyValuePair<string,string>("PayeRef","PayeRef has not been supplied"), result.ValidationDictionary);
+            Assert.Contains(new KeyValuePair<string,string>("UserId","UserId has not been supplied"), result.ValidationDictionary);
+        }
+
+        [Test]
+        public async Task ThenTheUserIsCheckedToSeeIfTheyAreAnOwnerAndUnauthroizedIsSetOnTheResult()
+        {
+            //Arrange
+            _membershipRepository.Setup(x => x.GetCaller(It.IsAny<long>(), It.IsAny<string>())).ReturnsAsync(new MembershipView {AccountId = 12345, RoleId = (short)Role.Transactor});
+
+            //Act
+            var result = await _validator.ValidateAsync(new RemovePayeFromAccountCommand
+            {
+                AccountId = 12345,
+                PayeRef = "123RFD",
+                UserId = "123edds"
+            });
+
+            //Assert
+            Assert.IsTrue(result.IsUnauthorized);
+        }
+
+        [Test]
+        public async Task ThenTheUserIsCheckedToSeeIfTheyArePartOfTheAccount()
+        {
+            //Arrange
+            _membershipRepository.Setup(x => x.GetCaller(It.IsAny<long>(), It.IsAny<string>())).ReturnsAsync(null);
+
+            //Act
+            var result = await _validator.ValidateAsync(new RemovePayeFromAccountCommand
+            {
+                AccountId = 12345,
+                PayeRef = "123RFD",
+                UserId = "123edds"
+            });
+
+            //Assert
+            Assert.IsTrue(result.IsUnauthorized);
+        }
+    }
+}
--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/RemovePayeFromAccount/RemovePayeFromAccountCommandValidator.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/RemovePayeFromAccount/RemovePayeFromAccountCommandValidator.cs
@@ -1,18 +1,51 @@
 ﻿using System.Threading.Tasks;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Validation;
+using SFA.DAS.EmployerApprenticeshipsService.Domain;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
 
 namespace SFA.DAS.EmployerApprenticeshipsService.Application.Commands.RemovePayeFromAccount
 {
     public class RemovePayeFromAccountCommandValidator : IValidator<RemovePayeFromAccountCommand>
     {
+        private readonly IMembershipRepository _membershipRepository;
+
+        public RemovePayeFromAccountCommandValidator(IMembershipRepository membershipRepository)
+        {
+            _membershipRepository = membershipRepository;
+        }
+
         public ValidationResult Validate(RemovePayeFromAccountCommand item)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<ValidationResult> ValidateAsync(RemovePayeFromAccountCommand item)
+        public async Task<ValidationResult> ValidateAsync(RemovePayeFromAccountCommand item)
         {
-            throw new System.NotImplementedException();
+            var validationResult = new ValidationResult();
+
+            if (item.AccountId == 0)
+            {
+                validationResult.AddError(nameof(item.AccountId), "AccountId has not been supplied");
+            }
+            if (string.IsNullOrWhiteSpace(item.PayeRef))
+            {
+                validationResult.AddError(nameof(item.PayeRef), "PayeRef has not been supplied");
+            }
+            if (string.IsNullOrWhiteSpace(item.UserId))
+            {
+                validationResult.AddError(nameof(item.UserId), "UserId has not been supplied");
+            }
+
+            if (validationResult.IsValid())
+            {
+                var member = await _membershipRepository.GetCaller(item.AccountId, item.UserId);
+                if (member== null || member.RoleId != (short) Role.Owner)
+                {
+                    validationResult.IsUnauthorized = true;
+                }
+            }
+
+            return validationResult;
         }
     }
 }
