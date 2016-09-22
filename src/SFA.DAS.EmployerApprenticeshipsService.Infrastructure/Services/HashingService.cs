3cc669e49020b9a6a68e2a7f92afd59a6cafe082
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-09-22 11:29:06 +0100

    Implement the hashing service using Hashids.net nuget package. Since we are not concerned with decryption the salt key can be stored in code at present

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/HashingService.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.EmployerApprenticeshipsService.Domain.Interfaces;
+using HashidsNet;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Services
+{
+    public class HashingService : IHashingService
+    {
+        public string HashValue(long id)
+        {
+            var hashIds = new Hashids("SFA: digital apprenticeship service",10, "46789BCDFGHJKLMNPRSTVWXY");
+
+            return hashIds.EncodeLong(id);
+        }
+    }
+}
