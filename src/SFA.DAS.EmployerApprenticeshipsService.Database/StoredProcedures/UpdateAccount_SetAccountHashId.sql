b8ae508305adee064d104c8cf0e43688ab2e2bb9
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-09-22 11:22:22 +0100

    Added new procedure for updating the hashed id on the account table. Updated the account table to have a new column for storing the hashed id. Also added a unique non-clustered index for that column.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/UpdateAccount_SetAccountHashId.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [account].[UpdateAccount_SetAccountHashId]
+	@AccountId BIGINT,
+	@HashedAccountId varchar(100)
+AS
+	UPDATE [account].[Account]
+	SET HashedAccountId = @HashedAccountId
+	WHERE Id = @AccountId 
+
--- src/SFA.DAS.EmployerApprenticeshipsService.Database/Tables/Account.sql
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/Tables/Account.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [account].[Account]
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+	[HashedAccountId] NVARCHAR(100) NOT NULL,
     [Name] NVARCHAR(100) NOT NULL 
 )
+GO
+
+CREATE UNIQUE INDEX [IX_Account_HashedAccountId] ON [account].[Account] ([HashedAccountId])
