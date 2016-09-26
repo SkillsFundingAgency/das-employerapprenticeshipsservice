2ce8dc78360b7e55018793ad3dac4659e03fad73
Lee Wadhams <lee.wadhams@valtech.com>
2016-09-26 16:32:14 +0100

    Formated dates as per GDS guidelines

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToGdsFormat(this DateTime date)
+        {
+            return date.ToString("d MMM yyyy");
+        }
+    }
+}
