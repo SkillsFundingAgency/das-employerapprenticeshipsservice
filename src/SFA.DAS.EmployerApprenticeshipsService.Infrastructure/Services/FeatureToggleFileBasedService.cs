f79a7260031b0899e78abf40663efc03edf3523f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-09-29 14:54:40 +0100

    Implement the azure storage for the feature toggle service

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/FeatureToggleFileBasedService.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/FeatureToggleFileBasedService.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
+using Microsoft.Azure;
+using SFA.DAS.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.Configuration.FileStorage;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Models.FeatureToggle;
-using SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Data;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Interfaces;
 using SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Caching;
 
 namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Services
 {
-    public class FeatureToggleFileBasedService : FileSystemRepository, IFeatureToggle
+    public class FeatureToggleFileBasedService : IFeatureToggle
     {
         private readonly ICacheProvider _cacheProvider;
+        private const string ConfigurationName = "SFA.DAS.EmployerApprenticeshipsService.Features";
 
-        public FeatureToggleFileBasedService(ICacheProvider cacheProvider) : base("Features")
+        public FeatureToggleFileBasedService(ICacheProvider cacheProvider)
         {
             _cacheProvider = cacheProvider;
         }
@@ -22,7 +27,7 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Services
             var features = _cacheProvider.Get<FeatureToggleLookup>(nameof(FeatureToggleLookup));
             if(features == null)
             {
-                features = ReadFileByIdSync<FeatureToggleLookup>("features_data");
+                features = GetFeatureToggleListLookup();
                 if (features.Data != null && features.Data.Any())
                 {
                     _cacheProvider.Set(nameof(FeatureToggleLookup),features,new TimeSpan(0,30,0));
@@ -30,5 +35,41 @@ namespace SFA.DAS.EmployerApprenticeshipsService.Infrastructure.Services
             }
             return features;
         }
+
+        private FeatureToggleLookup GetFeatureToggleListLookup()
+        {
+            var environment = Environment.GetEnvironmentVariable("DASENV");
+            if (string.IsNullOrEmpty(environment))
+            {
+                environment = CloudConfigurationManager.GetSetting("EnvironmentName");
+            }
+
+            var configurationRepository = GetDataFromAzure();
+            var configurationService = new ConfigurationService(
+               configurationRepository,
+               new ConfigurationOptions(ConfigurationName, environment, "1.0"));
+
+            var config = configurationService.Get<FeatureToggleLookup>();
+
+            return config;
+        }
+
+        private static IConfigurationRepository GetDataFromAzure()
+        {
+            IConfigurationRepository configurationRepository;
+            if (bool.Parse(ConfigurationManager.AppSettings["LocalConfig"]))
+            {
+                configurationRepository = new FileStorageConfigurationRepository();
+            }
+            else
+            {
+                configurationRepository =
+                    new AzureTableStorageConfigurationRepository(
+                        CloudConfigurationManager.GetSetting("ConfigurationStorageConnectionString"));
+            }
+            return configurationRepository;
+        }
     }
+
+    
 }
