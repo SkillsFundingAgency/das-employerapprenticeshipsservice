c3511d76511c2c9423b0983c0af732108158d8f3
Robin O'Connor <github@bytepointsoftware.com>
2016-09-30 12:12:54 +0100

    Now signed date gets converted to GMT time from UTC one the view agreement page

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/Extensions/DateTimeExtensions.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Extensions/DateTimeExtensions.cs
@@ -6,5 +6,10 @@
         {
             return date.ToString("d MMM yyyy");
         }
+
+        public static DateTime ToGMTStandardTime(this DateTime date)
+        {
+            return TimeZoneInfo.ConvertTime(date, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+        }
     }
 }
