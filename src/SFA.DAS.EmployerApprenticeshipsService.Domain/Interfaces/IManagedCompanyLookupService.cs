ffab9652f2b8eb7534d69414a4a4f5166b6cf631
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-10-05 11:12:03 +0100

    Added in new service for getting a list of managed companies from a cloud storage table.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Interfaces/IManagedCompanyLookupService.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.EmployerApprenticeshipsService.Domain.Models.FeatureToggle;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Models.ManagedCompany;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Interfaces
+{
+    public interface IManagedCompanyLookupService
+    {
+        ManagedCompanyLookup GetCompanies();
+    }
+}
