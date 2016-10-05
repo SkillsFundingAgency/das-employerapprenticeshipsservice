2f49b7c2de172c8589675e98cd12ebdd9f8f5c98
floydprice <f.price@floydprice.com>
2016-10-05 16:11:19 +0100

    New blank API project for the account api

---
+++ src/SFA.DAS.EAS.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace SFA.DAS.EAS.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
