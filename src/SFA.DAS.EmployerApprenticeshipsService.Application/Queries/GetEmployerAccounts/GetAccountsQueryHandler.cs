8d759cc4355f2235ace8201c01d902dac9fd3476
floydprice <f.price@floydprice.com>
2016-10-12 12:35:07 +0100

    New queries to support fetching of all accounts and a batch of account transactions.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEmployerAccounts/GetAccountsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetEmployerAccounts
+{
+    public class GetAccountsQueryHandler : IAsyncRequestHandler<GetEmployerAccountsQuery, GetEmployerAccountsResponse>
+    {
+        private readonly IEmployerAccountRepository _employerAccountRepository;
+
+        public GetAccountsQueryHandler(IEmployerAccountRepository employerAccountRepository)
+        {
+            _employerAccountRepository = employerAccountRepository;
+        }
+
+        public async Task<GetEmployerAccountsResponse> Handle(GetEmployerAccountsQuery message)
+        {
+            var accounts = await _employerAccountRepository.GetAccounts();
+            return new GetEmployerAccountsResponse() {Accounts = accounts.AccountList};
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEmployerAccounts/GetEmployerAccountsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetEmployerAccounts
+{
+    public class GetEmployerAccountsResponse
+    {
+        public List<Account> Accounts { get; set; }
+    }
+}
