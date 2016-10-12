c9ff75124e98117fc11bf68a6956b355d2a403a2
floydprice <f.price@floydprice.com>
2016-10-12 12:36:25 +0100

    Setup up Accounts.API project and cloud service configs.

--- src/SFA.DAS.EAS.Api/App_Start/WebApiConfig.cs
+++ src/SFA.DAS.EAS.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 
@@ -11,7 +12,8 @@ namespace SFA.DAS.EAS.Api
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API routes
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
             config.MapHttpAttributeRoutes();
         }
     }
