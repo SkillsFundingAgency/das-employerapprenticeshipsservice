5646df8bd1275e0d74d9dc7cf4ead6d412eaf171
floydprice <f.price@floydprice.com>
2016-10-12 12:37:07 +0100

    Expose a /api/accounts endpoint that yields a merge of all accounts (name,id, hash) and the current balance.

---
+++ src/SFA.DAS.EAS.Api/Orchestrators/AccountsOrchestrator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using MediatR;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetBatchEmployerAccountTransactions;
+using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetEmployerAccounts;
+
+namespace SFA.DAS.EAS.Api.Orchestrators
+{
+    public class AccountsOrchestrator
+    {
+        private readonly IMediator _mediator;
+
+        public AccountsOrchestrator(IMediator mediator)
+        {
+            if (mediator == null)
+                throw new ArgumentNullException(nameof(mediator));
+            _mediator = mediator;
+        }
+
+
+        public async Task<OrchestratorResponse<List<AccountWithBalanceViewModel>>> GetAllAccountsWithBalances()
+        {
+            var accountsResult = await _mediator.SendAsync(new GetEmployerAccountsQuery());
+            var transactionResult =
+                await
+                    _mediator.SendAsync(new GetBatchEmployerAccountTransactionsQuery()
+                    {
+                        AccountIds = accountsResult.Accounts.Select(account => account.Id).ToList()
+                    });
+
+            var data = new List<AccountWithBalanceViewModel>();
+
+            accountsResult.Accounts.ForEach(account =>
+            {
+                var transactions =
+                    transactionResult.Data.Find(aggregationData => aggregationData.AccountId == account.Id);
+                var latestLineItem = transactions?.Data.FirstOrDefault();
+
+
+                var currentBalance = latestLineItem?.Balance ?? 0;
+                data.Add(new AccountWithBalanceViewModel() {AccountId = account.Id, AccountName = account.Name, AccountHashId = account.HashedId, Balance = currentBalance });
+                
+
+            });
+
+
+
+            return new OrchestratorResponse<List<AccountWithBalanceViewModel>>() {Data = data};
+        } 
+    }
+
+    public class AccountWithBalanceViewModel
+    {
+        public string AccountName { get; set; }
+
+        public string AccountHashId { get; set; }
+
+        public long AccountId { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
