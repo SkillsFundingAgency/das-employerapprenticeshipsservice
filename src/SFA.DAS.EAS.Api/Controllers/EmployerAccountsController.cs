f73906c6868cff592fb2c042ecedf742baff8498
floydprice <f.price@floydprice.com>
2016-10-18 11:07:31 +0100

    Add paging params to interface
    Add Generic response envelope to expose total pages

--- src/SFA.DAS.EAS.Api/Controllers/EmployerAccountsController.cs
+++ src/SFA.DAS.EAS.Api/Controllers/EmployerAccountsController.cs
@@ -21,9 +21,9 @@ namespace SFA.DAS.EAS.Api.Controllers
 
         [Route("", Name = "AccountsIndex")]
         [HttpGet]
-        public async Task<IHttpActionResult> Index()
+        public async Task<IHttpActionResult> Index(string fromDate = null, int pageSize = 1000, int pageNumber = 1)
         {
-            var result = await _orchestrator.GetAllAccountsWithBalances();
+            var result = await _orchestrator.GetAllAccountsWithBalances(fromDate, pageSize, pageNumber);
             
             if (result.Status == HttpStatusCode.OK)
             {
--- src/SFA.DAS.EAS.Api/Orchestrators/AccountsOrchestrator.cs
+++ src/SFA.DAS.EAS.Api/Orchestrators/AccountsOrchestrator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using MediatR;
+using SFA.DAS.EAS.Api.Models;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetBatchEmployerAccountTransactions;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Queries.GetEmployerAccounts;
 
@@ -21,45 +22,26 @@ namespace SFA.DAS.EAS.Api.Orchestrators
         }
 
 
-        public async Task<OrchestratorResponse<List<AccountWithBalanceViewModel>>> GetAllAccountsWithBalances()
+        public async Task<OrchestratorResponse<PagedApiResponseViewModel<AccountWithBalanceViewModel>>> GetAllAccountsWithBalances(string fromDate, int pageSize, int pageNumber)
         {
             var accountsResult = await _mediator.SendAsync(new GetEmployerAccountsQuery());
-            var transactionResult =
-                await
-                    _mediator.SendAsync(new GetBatchEmployerAccountTransactionsQuery()
-                    {
-                        AccountIds = accountsResult.Accounts.Select(account => account.Id).ToList()
-                    });
+            var transactionResult = await _mediator.SendAsync(new GetBatchEmployerAccountTransactionsQuery()
+            {
+                AccountIds = accountsResult.Accounts.Select(account => account.Id).ToList()
+            });
 
             var data = new List<AccountWithBalanceViewModel>();
 
             accountsResult.Accounts.ForEach(account =>
             {
-                var transactions =
-                    transactionResult.Data.Find(aggregationData => aggregationData.AccountId == account.Id);
+                var transactions = transactionResult.Data.Find(aggregationData => aggregationData.AccountId == account.Id);
                 var latestLineItem = transactions?.Data.FirstOrDefault();
-
-
                 var currentBalance = latestLineItem?.Balance ?? 0;
-                data.Add(new AccountWithBalanceViewModel() {AccountId = account.Id, AccountName = account.Name, AccountHashId = account.HashedId, Balance = currentBalance });
-                
-
+                data.Add(new AccountWithBalanceViewModel() { AccountId = account.Id, AccountName = account.Name, AccountHashId = account.HashedId, Balance = currentBalance });
             });
 
+            return new OrchestratorResponse<PagedApiResponseViewModel<AccountWithBalanceViewModel>>() {Data = new PagedApiResponseViewModel<AccountWithBalanceViewModel>() {Data = data, Page = 0, TotalPages = 0} };
+        }
 
-
-            return new OrchestratorResponse<List<AccountWithBalanceViewModel>>() {Data = data};
-        } 
-    }
-
-    public class AccountWithBalanceViewModel
-    {
-        public string AccountName { get; set; }
-
-        public string AccountHashId { get; set; }
-
-        public long AccountId { get; set; }
-
-        public decimal Balance { get; set; }
     }
 }
