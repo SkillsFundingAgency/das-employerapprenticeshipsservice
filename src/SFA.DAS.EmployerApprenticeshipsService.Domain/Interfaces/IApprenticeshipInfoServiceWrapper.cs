d78cc6e5b4bc7a4f249bbb4510d2ce150608f4c6
ijrussell <i.j.russell@tesco.net>
2016-10-20 13:05:55 +0100

    start plugging in apprenticeship api

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Interfaces/IApprenticeshipInfoServiceWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Interfaces
+{
+    public interface IApprenticeshipInfoServiceWrapper
+    {
+        Task<StandardsView> GetStandardsAsync(bool refreshCache = false);
+        Task<FrameworksView> GetFrameworksAsync(bool refreshCache = false);
+        ProvidersView GetProvider(int ukPrn);
+    }
+}
