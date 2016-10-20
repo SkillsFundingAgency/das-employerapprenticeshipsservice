a50e19c202e15a835c09f2ef2338b6faf0663db2
Ewan <Ewan@DESKTOP-T61UENO>
2016-10-20 16:25:26 +0100

    Add code for IIS Application Initialization

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/WebRole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Web.Administration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web
+{
+    public class WebRole : RoleEntryPoint
+    {
+        public override bool OnStart()
+        {
+            using (var serverManager = new ServerManager())
+            {
+                var mainSite = serverManager.Sites[RoleEnvironment.CurrentRoleInstance.Id + "_Web"];
+                var mainApplication = mainSite.Applications["/"];
+                mainApplication["preloadEnabled"] = true;
+
+                var mainApplicationPool = serverManager.ApplicationPools[mainApplication.ApplicationPoolName];
+                mainApplicationPool["startMode"] = "AlwaysRunning";
+
+                serverManager.CommitChanges();
+            }
+            return base.OnStart();
+        }
+    }
+}
