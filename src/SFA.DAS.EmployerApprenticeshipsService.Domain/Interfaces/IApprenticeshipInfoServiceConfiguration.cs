d24cae1dcf12c5015bf921ccbe99a2bc2f4f92c9
ijrussell <i.j.russell@tesco.net>
2016-10-21 11:59:10 +0100

    Added apprenticeship api client

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Interfaces/IApprenticeshipInfoServiceConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.EmployerApprenticeshipsService.Domain.Interfaces
+{
+    public interface IApprenticeshipInfoServiceConfiguration
+    {
+        string BaseUrl { get; set; }
+    }
+}
