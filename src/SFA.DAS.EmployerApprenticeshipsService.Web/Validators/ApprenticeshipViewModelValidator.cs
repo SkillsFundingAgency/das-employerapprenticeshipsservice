4db7edbcdeeae9551363e02f28d3ceefa32e91d2
Lee Wadhams <lee.wadhams@valtech.co.uk>
2016-10-26 09:51:21 +0100

    Commitments validation (#241)
    * Remove buttons for Pause and Resume Apprenticship
    
    * Add FluentValidation and setup to validate ViewModel
    
    * Added server-side validation on add apprenticeship.
    
    * Extracted form validation text boxes into Editor Templates
    
    * validate uln and cost on the client side.
    
    * Test apprenticeshipview model validation
    
    * Refactor of controller steps to create commitment.
    
    * Validate the steps to create a commitment
    
    * Validate commitment steps using client side validation.
    
    * Remove EditorTemplates and inlined the markup
    
    * Fix missing property name which caused validation to fail
    
    Also updated unobtrusive javasctipt to add custom style to container.
    
    * Show field level validation error correctly
    
    Update to unobtrusive javasctipt file to correctly style the controls when
    a custom field validation message is used.
    
    * Remove input validation ranges
    
    * Update mark-up for add apprenticeship to follow GDS validation guidelines
    
    * Format the list of legal entities as radio button list
    
    * Update validation messages
    
    * Index Page modified to GDS standards.
    
    * View commitment page markup modified to fix layout to full width and table added and DIV's used for table removed.
    
    * page button formats added. Error message summary box style fixed.
    
    * Validation error messages layout fixes completed
    
    * merge conflict resolved
    
    * Merge conflict resolved
    
    * Correct label for on legal entity selection
    
    * Fix merge issue
    
    * Removed spurious merge comments.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/Validators/ApprenticeshipViewModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SFA.DAS.EmployerApprenticeshipsService.Web.Models;
+
+namespace SFA.DAS.EmployerApprenticeshipsService.Web.Validators
+{
+    public class ApprenticeshipViewModelValidator : AbstractValidator<ApprenticeshipViewModel>
+    {
+        public ApprenticeshipViewModelValidator()
+        {
+            RuleFor(x => x.ULN).Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter the unique learner number - this should be 10 digits long.");
+            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the cost in whole pounds. For example, for £1500 you should enter 1500.");
+        }
+    }
+}
