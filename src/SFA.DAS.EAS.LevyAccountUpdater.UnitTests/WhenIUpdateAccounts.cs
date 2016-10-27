246ae14af5d59eef086490495a4d1dc9cc539f09
Robin O'Connor <GitHub@bytepointsoftware.com>
2016-10-27 14:11:15 +0100

    Added logging to updater

--- src/SFA.DAS.EAS.LevyAccountUpdater.UnitTests/WhenIUpdateAccounts.cs
+++ src/SFA.DAS.EAS.LevyAccountUpdater.UnitTests/WhenIUpdateAccounts.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
+using NLog;
 using NUnit.Framework;
 using SFA.DAS.EAS.LevyAccountUpdater.Updater;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Messages;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Entities.Account;
 using SFA.DAS.Messaging;
+
 
 namespace SFA.DAS.EAS.LevyAccountUpdater.UnitTests
 {
@@ -16,19 +19,22 @@ namespace SFA.DAS.EAS.LevyAccountUpdater.UnitTests
         private Mock<IMessagePublisher> _messagePublisher;
         private AccountUpdater _updater;
         private List<Account> _accounts;
+        private Mock<ILogger> _logger;
 
         [SetUp]
         public void Init()
         {
             _employerAccountRepository = new Mock<IEmployerAccountRepository>();
             _messagePublisher = new Mock<IMessagePublisher>();
+            _logger = new Mock<ILogger>();
+
             _accounts = new List<Account>
             {
                 new Account { Id = 1 },
                 new Account { Id = 2 }
             };
 
-            _updater = new AccountUpdater(_employerAccountRepository.Object, _messagePublisher.Object);
+            _updater = new AccountUpdater(_employerAccountRepository.Object, _messagePublisher.Object, _logger.Object);
 
             _employerAccountRepository.Setup(x => x.GetAllAccounts()).ReturnsAsync(_accounts);
             _messagePublisher.Setup(x => x.PublishAsync(It.IsAny<EmployerRefreshLevyQueueMessage>())).Returns(Task.Delay(0));
@@ -59,6 +65,21 @@ namespace SFA.DAS.EAS.LevyAccountUpdater.UnitTests
             //Assert
             _employerAccountRepository.Verify(x => x.GetAllAccounts(), Times.Once);
             _messagePublisher.Verify(x => x.PublishAsync(It.IsAny<EmployerRefreshLevyQueueMessage>()), Times.Never);
+        }
+
+        [Test]
+        public void ThenIfAnErrorOccursTheErrorShouldBeLogged()
+        {
+            //Assign
+            var exception = new Exception("Test exception");
+            _employerAccountRepository.Setup(x => x.GetAllAccounts()).Throws(exception);
+            _logger.Setup(x => x.Error(It.IsAny<Exception>()));
+
+            //Act
+            Assert.ThrowsAsync<Exception>(async () => await _updater.RunUpdate());
+
+            //Assert
+            _logger.Verify(x => x.Error(exception), Times.Once);
         }
     }
 }
--- src/SFA.DAS.EAS.LevyMonthlyUpdater/Updater/AccountUpdater.cs
+++ src/SFA.DAS.EAS.LevyMonthlyUpdater/Updater/AccountUpdater.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using NLog;
 using SFA.DAS.EmployerApprenticeshipsService.Application.Messages;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Attributes;
 using SFA.DAS.EmployerApprenticeshipsService.Domain.Data;
@@ -9,26 +12,50 @@ namespace SFA.DAS.EAS.LevyAccountUpdater.Updater
 {
     public class AccountUpdater : IAccountUpdater
     {
+        private const string ServiceName = "SFA.DAS.EAS.LevyAccountUpdater";
         private readonly IEmployerAccountRepository _accountRepository;
         private readonly IMessagePublisher _messagePublisher;
+        private readonly ILogger _logger;
 
         [QueueName]
         public string get_employer_levy { get; set; }
 
-        public AccountUpdater(IEmployerAccountRepository accountRepository, IMessagePublisher messagePublisher)
+        public AccountUpdater(IEmployerAccountRepository accountRepository, IMessagePublisher messagePublisher,
+            ILogger logger)
         {
             _accountRepository = accountRepository;
             _messagePublisher = messagePublisher;
+            _logger = logger;
         }
 
         public async Task RunUpdate()
         {
-            var employerAccounts = await _accountRepository.GetAllAccounts();
+            _logger.Info($"{ServiceName}: Running update schedule");
 
-            var tasks = employerAccounts.Select(
-                x => _messagePublisher.PublishAsync(new EmployerRefreshLevyQueueMessage {AccountId = x.Id})).ToArray();
+            try
+            {
+                var timer = Stopwatch.StartNew();
+               
+                var employerAccounts = await _accountRepository.GetAllAccounts();
 
-            await Task.WhenAll(tasks);
+                _logger.Info($"{ServiceName}: Updating {employerAccounts.Count} levy accounts");
+
+                var tasks = employerAccounts.Select(
+                    x =>
+                    {
+                        _logger.Trace($"{ServiceName}: Creating update levy account message for account {x.Name} (ID: {x.Id})");
+                        return _messagePublisher.PublishAsync(new EmployerRefreshLevyQueueMessage {AccountId = x.Id});
+                    }).ToArray();
+
+                await Task.WhenAll(tasks);
+
+                _logger.Info($"{ServiceName}: update schedule completed in {timer.Elapsed:g} (hh:mm:ss:ms)");
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
         }
     }
 }
