7b7c5b71bf74899c2d14dac6e9405a22e176847a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-10-31 11:47:51 +0000

    Update RefreshEmployerLevyDataCommandHandler
    The publishing of the refresh_employer_levy queue message has been
    removed and replaced with the ProcessDeclarationsEvent which will be
    responsible for putting data on the TransactionLine table

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Events/ProcessDeclarationsEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.EAS.Application.Events
+{
+    public class ProcessDeclarationsEvent : IAsyncNotification
+    {
+    }
+}
