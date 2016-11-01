db140265c0a29275b7dd60c041aaa1d75069a36f
RajeshV <rajesh@digi2al.co.uk>
2016-11-01 10:41:15 +0000

    checkbox outline added, your organisation page table made responsive.

--- src/SFA.DAS.EmployerApprenticeshipsService.Web/assets/javascripts/commitments-custom.js
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web/assets/javascripts/commitments-custom.js
@@ -2,13 +2,17 @@
 $(document).ready(function () {
 
 
-    // Radio buttons with outline on click
+    // Radio with outline on click
     $('input:radio').click(function () {
         $('input:radio').parent().removeClass('selected');
         $(this).parent(this).addClass('selected');
     });
 
-
+    // checkbox with outline on click
+    $('input:checkbox').click(function () {
+        $('input:checkbox').parent().removeClass('selected');
+        $(this).parent(this).addClass('selected');
+    });
 
 
 
