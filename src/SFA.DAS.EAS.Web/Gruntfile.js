4fad52b46338e005f23278dbd74666499e6cb1eb
James King <hello@jamesmichaelking.com>
2016-11-01 11:16:54 +0000

    SASS structure, Grunt file and packages.json

---
+++ src/SFA.DAS.EAS.Web/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      dev: {
+        files: {
+          'dist/css/screen.css': 'src/sass/screen.scss'
+        },
+        options: {
+	      includePaths: [
+            'src/govuk_template/assets/stylesheets',
+            'src/govuk_frontend_toolkit/stylesheets'
+          ],
+          outputStyle: 'compressed'
+        }
+      }
+    },
+    // Watches styles and specs for changes
+    watch: {
+      css: {
+        files: ['src/sass/**/*.scss', 'src/govuk_frontend_toolkit/stylesheets/*.scss'],
+        tasks: ['sass'],
+        options: { nospawn: true }
+      }
+    },
+
+  });
+
+  ;[
+	'grunt-contrib-watch',
+    'grunt-sass'
+  ].forEach(function (task) {
+    grunt.loadNpmTasks(task)
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass']);
+
+};
