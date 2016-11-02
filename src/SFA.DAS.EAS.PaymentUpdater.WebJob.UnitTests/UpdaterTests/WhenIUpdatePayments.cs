9c07981e6b8a093111fe046d143d4aec15f50691
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-02 09:16:03 +0000

    Add PaymentProcessor WebJob unit test
    Added project for payment processor webjob unit tests. Also added stubs
    for unit tests that need to be implemented

---
+++ src/SFA.DAS.EAS.PaymentUpdater.WebJob.UnitTests/UpdaterTests/WhenIUpdatePayments.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.EAS.PaymentUpdater.WebJob.UnitTests.UpdaterTests
+{
+    public class WhenIUpdatePayments
+    {
+        private Updater.PaymentProcessor _paymentProcessor;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _paymentProcessor = new Updater.PaymentProcessor();
+        }
+
+        [Test]
+        public async Task ThenThePaymentsApiShouldBeCheckedForTheCurrentPeriodEnd()
+        {
+            //Act
+            await _paymentProcessor.RunUpdate();
+
+            //Assert
+
+        }
+
+        [Test]
+        public async Task ThenTheCurrentPeriodEndShouldBeStoredIfDifferent()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenTheAccountMessagesShouldBeAddedToTheQueueIfThePeriodEndHasChangedI()
+        {
+            
+        }
+    }
+}
