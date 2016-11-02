e26554eb8b926dc568498613cbf2054f538e182b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-02 15:44:43 +0000

    Implement GetAccountTransactionsQueryHandler
    Implemented query for getting Account Transactions based on the account
    id. The DasLevyService will use this

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/AccountTransactions/GetAccountTransactions/GetAccountTransactionsRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.EAS.Application.Queries.AccountTransactions.GetAccountTransactions
+{
+    public class GetAccountTransactionsRequest : IAsyncRequest<GetAccountTransactionsResponse>, IAsyncRequest
+    {
+        public long AccountId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/AccountTransactions/GetAccountTransactions/GetAccountTransactionsValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EAS.Application.Validation;
+
+namespace SFA.DAS.EAS.Application.Queries.AccountTransactions.GetAccountTransactions
+{
+    public class GetAccountTransactionsValidator : IValidator<GetAccountTransactionsRequest>
+    {
+        public ValidationResult Validate(GetAccountTransactionsRequest item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ValidationResult> ValidateAsync(GetAccountTransactionsRequest item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
