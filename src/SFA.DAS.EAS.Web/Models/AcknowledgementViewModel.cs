e2103cf8a65782c4e3842067d38a8a17bbfae805
ijrussell <i.j.russell@tesco.net>
2016-11-03 12:50:02 +0000

    Pass data to acknowledgement view

--- src/SFA.DAS.EAS.Web/Models/AcknowledgementViewModel.cs
+++ src/SFA.DAS.EAS.Web/Models/AcknowledgementViewModel.cs
@@ -3,5 +3,8 @@
     public class AcknowledgementViewModel
     {
         public string HashedCommitmentId { get; set; }
+        public string ProviderName { get; set; }
+        public string LegalEntityName { get; set; }
+        public string Message { get; set; }
     }
 }
