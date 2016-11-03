58e2922d66f44de44dc26ee5c65693be27bde927
Robin O'Connor <GitHub@bytepointsoftware.com>
2016-11-03 11:08:54 +0000

    Account history repository calls added
    A new method for the IEmployerAccountRepository has been created to pull all history items from the database based based on account ID.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/GetAccountHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [account].[GetAccountHistory]	
+	@accountId BIGINT
+	
+AS
+select 
+	history.AccountId, history.PayeRef, history.AddedDate as DateAdded ,history.RemovedDate as DateRemoved
+from 
+	[account].[AccountHistory] history
+where 
+	history.AccountId = @accountId
