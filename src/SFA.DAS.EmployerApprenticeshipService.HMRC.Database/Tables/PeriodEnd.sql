e81f70b5d467064bab9ce5fb09d871e060e00945
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-04 05:42:38 +0000

    Implement CreatePeriodEnd in repository
    Implemented the create period end in the DasLevyRepository and created
    the new table and stored procedure for dealing with this

---
+++ src/SFA.DAS.EmployerApprenticeshipService.HMRC.Database/Tables/PeriodEnd.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [levy].[PeriodEnd]
+(
+	[Id] INT IDENTITY(0,1) NOT NULL PRIMARY KEY,
+	[PeriodEndId] nvarchar(20) NOT NULL,
+	[CalendarPeriodMonth] int not null,
+	[CalendarPeriodYear] int not null,
+	[AccountDataValidAt] Datetime not null,
+	[CommitmentDataValidAt] Datetime not null,
+	[CompletionDateTime] Datetime not null,
+	[PaymentsForPeriod] nvarchar(250) not null
+)
--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/DasLevyRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/DasLevyRepository.cs
@@ -139,9 +139,24 @@ namespace SFA.DAS.EAS.Infrastructure.Data
             
         }
 
-        public Task CreateNewPeriodEnd(PeriodEnd periodEnd)
+        public async Task CreateNewPeriodEnd(PeriodEnd periodEnd)
         {
-            throw new NotImplementedException();
+            await WithConnection(async c =>
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@PeriodEndId", periodEnd.Id, DbType.Decimal);
+                parameters.Add("@CalendarPeriodMonth", periodEnd.CalendarPeriod.Month, DbType.Int32);
+                parameters.Add("@CalendarPeriodYear", periodEnd.CalendarPeriod.Year, DbType.Int32);
+                parameters.Add("@AccountDataValidAt", periodEnd.ReferenceData.AccountDataValidAt, DbType.DateTime);
+                parameters.Add("@CommitmentDataValidAt", periodEnd.ReferenceData.CommitmentDataValidAt, DbType.DateTime);
+                parameters.Add("@CompletionDateTime", periodEnd.CompletionDateTime, DbType.DateTime);
+                parameters.Add("@PaymentsForPeriod", periodEnd.Links.PaymentsForPeriod, DbType.String);
+                
+                return await c.ExecuteAsync(
+                    sql: "[levy].[CreatePeriodEnd]",
+                    param: parameters,
+                    commandType: CommandType.StoredProcedure);
+            });
         }
     }
 }
