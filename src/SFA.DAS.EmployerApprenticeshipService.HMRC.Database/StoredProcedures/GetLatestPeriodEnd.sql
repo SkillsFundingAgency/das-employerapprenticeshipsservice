844a6248bf61574cc9933e5be5864967816ea3c2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-04 06:28:35 +0000

    Create GetCurrentPeriodEndQueryHandler
    Implemented GetCurrentPeriodEndQueryHandler which gets the latest
    Period End that we have stored for comparison against the PaymentsAPI

---
+++ src/SFA.DAS.EmployerApprenticeshipService.HMRC.Database/StoredProcedures/GetLatestPeriodEnd.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [levy].[GetLatestPeriodEnd]
+	
+AS
+
+select top 1 
+	* 
+from 
+	[levy].[PeriodEnd] 
+order by 
+	completiondatetime desc
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/Payments/GetCurrentPeriodEnd/GetCurrentPeriodEndRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediatR;
+
+namespace SFA.DAS.EAS.Application.Queries.Payments.GetCurrentPeriodEnd
+{
+    public class GetCurrentPeriodEndRequest : IAsyncRequest<GetPeriodEndResponse>
+    {
+    }
+}
