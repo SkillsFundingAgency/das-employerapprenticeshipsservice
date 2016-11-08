e816c04471de3a29753a8a5bdbf49b0b2787bc44
Lee Wadhams <lee.wadhams@valtech.com>
2016-11-08 15:46:40 +0000

    Turned off client-side validation

--- src/SFA.DAS.EAS.Web/Validators/ApprenticeshipViewModelValidator.cs
+++ src/SFA.DAS.EAS.Web/Validators/ApprenticeshipViewModelValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
 using SFA.DAS.EAS.Web.Models;
 
 namespace SFA.DAS.EAS.Web.Validators
@@ -7,8 +8,14 @@ namespace SFA.DAS.EAS.Web.Validators
     {
         public ApprenticeshipViewModelValidator()
         {
+            var currentYear = DateTime.Now.Year;
+
             RuleFor(x => x.ULN).Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter the unique learner number - this should be 10 digits long.");
             RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the cost in whole pounds. For example, for £1500 you should enter 1500.");
+            RuleFor(x => x.StartMonth).InclusiveBetween(1, 12).WithMessage("Enter a valid month");
+            RuleFor(x => x.StartYear).InclusiveBetween(currentYear, 9999).WithMessage("Enter a valid year");
+            RuleFor(x => x.EndMonth).InclusiveBetween(1, 12).WithMessage("Enter a valid month");
+            RuleFor(x => x.EndYear).InclusiveBetween(currentYear, 9999).WithMessage("Enter a valid year");
         }
     }
 }
