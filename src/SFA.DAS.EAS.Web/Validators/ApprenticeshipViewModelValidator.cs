8d287900acc77dcc0c07df0d5691b132cf5d9e71
Lee Wadhams <lee.wadhams@valtech.com>
2016-11-08 17:21:07 +0000

    Update error text for start/end month and year, fixed provider search bug

--- src/SFA.DAS.EAS.Web/Validators/ApprenticeshipViewModelValidator.cs
+++ src/SFA.DAS.EAS.Web/Validators/ApprenticeshipViewModelValidator.cs
@@ -10,12 +10,12 @@ namespace SFA.DAS.EAS.Web.Validators
         {
             var currentYear = DateTime.Now.Year;
 
-            RuleFor(x => x.ULN).Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter the unique learner number - this should be 10 digits long.");
-            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the cost in whole pounds. For example, for £1500 you should enter 1500.");
-            RuleFor(x => x.StartMonth).InclusiveBetween(1, 12).WithMessage("Enter a valid month");
-            RuleFor(x => x.StartYear).InclusiveBetween(currentYear, 9999).WithMessage("Enter a valid year");
-            RuleFor(x => x.EndMonth).InclusiveBetween(1, 12).WithMessage("Enter a valid month");
-            RuleFor(x => x.EndYear).InclusiveBetween(currentYear, 9999).WithMessage("Enter a valid year");
+            RuleFor(x => x.ULN).Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter the unique learner number - this should be 10 digits long");
+            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the cost in whole pounds. For example, for £1500 you should enter 1500");
+            RuleFor(x => x.StartMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid start month");
+            RuleFor(x => x.StartYear).InclusiveBetween(currentYear, 9999).WithMessage("Please enter a valid start year");
+            RuleFor(x => x.EndMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid end month.");
+            RuleFor(x => x.EndYear).InclusiveBetween(currentYear, 9999).WithMessage("Please enter a valid end year");
         }
     }
 }
