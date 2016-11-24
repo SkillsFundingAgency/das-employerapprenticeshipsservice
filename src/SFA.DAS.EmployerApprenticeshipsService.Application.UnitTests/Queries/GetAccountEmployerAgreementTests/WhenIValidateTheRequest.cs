061660a284cd67852a01c516f9fe8d3aed912b47
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-24 11:37:42 +0000

    Add validator for GetAccountEmployerAgreements
    Added a validator for the GetAccountEmployerAgreements to validate the
    request.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetAccountEmployerAgreementTests/WhenIValidateTheRequest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Application.Queries.GetAccountEmployerAgreements;
+using SFA.DAS.EAS.Domain;
+using SFA.DAS.EAS.Domain.Data;
+
+namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetAccountEmployerAgreementTests
+{
+    public class WhenIValidateTheRequest
+    {
+        private const string ExpectedExternalUserId = "456";
+        private const string ExpectedHashedId = "456487";
+        private GetAccountEmployerAgreementsValidator _validator;
+        private Mock<IMembershipRepository> _membershipRepository;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _membershipRepository = new Mock<IMembershipRepository>();
+            _membershipRepository.Setup(x => x.GetCaller(ExpectedHashedId, ExpectedExternalUserId)).ReturnsAsync(new MembershipView());
+
+            _validator = new GetAccountEmployerAgreementsValidator(_membershipRepository.Object);
+        }
+
+        [Test]
+        public async Task ThenTrueIsReturnedWhenAllTheFieldsArePopulated()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetAccountEmployerAgreementsRequest { ExternalUserId = ExpectedExternalUserId, HashedId = ExpectedHashedId });
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+
+        [Test]
+        public async Task TheFalseIsReturnedWhenTheFieldsArentPopulated()
+        {
+            //Act
+            var actual = await _validator.ValidateAsync(new GetAccountEmployerAgreementsRequest());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string, string>("ExternalUserId", "ExternalUserId has not been supplied"), actual.ValidationDictionary);
+            Assert.Contains(new KeyValuePair<string, string>("HashedId", "AccountId has not been supplied"), actual.ValidationDictionary);
+        }
+
+        [Test]
+        public async Task ThenIfTheRequestIsValidTheUserIsCheckedToSeeIfTheyAreConnectedToTheAccount()
+        {
+            //Act
+            await _validator.ValidateAsync(new GetAccountEmployerAgreementsRequest { ExternalUserId = ExpectedExternalUserId, HashedId = ExpectedHashedId });
+
+            //Arrange
+            _membershipRepository.Verify(x=>x.GetCaller(ExpectedHashedId,ExpectedExternalUserId));
+        }
+
+        [Test]
+        public async Task ThenUnauthorizedIsSetToTrueIfTheUserIsNotPartOfTheAccount()
+        {
+            //Arrange
+            _membershipRepository.Setup(x => x.GetCaller(ExpectedHashedId, ExpectedExternalUserId)).ReturnsAsync(null);
+
+            //Act
+            var actual = await _validator.ValidateAsync(new GetAccountEmployerAgreementsRequest { ExternalUserId = ExpectedExternalUserId, HashedId = ExpectedHashedId });
+
+            //Assert
+            Assert.IsTrue(actual.IsUnauthorized);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountEmployerAgreements/GetAccountEmployerAgreementsValidator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.EAS.Application.Validation;
+using SFA.DAS.EAS.Domain.Data;
+
+namespace SFA.DAS.EAS.Application.Queries.GetAccountEmployerAgreements
+{
+    public class GetAccountEmployerAgreementsValidator : IValidator<GetAccountEmployerAgreementsRequest> 
+    {
+        private readonly IMembershipRepository _membershipRepository;
+
+        public GetAccountEmployerAgreementsValidator(IMembershipRepository membershipRepository)
+        {
+            _membershipRepository = membershipRepository;
+        }
+
+        public ValidationResult Validate(GetAccountEmployerAgreementsRequest item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ValidationResult> ValidateAsync(GetAccountEmployerAgreementsRequest item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.ExternalUserId))
+            {
+                validationResult.AddError(nameof(item.ExternalUserId),"ExternalUserId has not been supplied");
+            }
+            if (string.IsNullOrEmpty(item.HashedId))
+            {
+                validationResult.AddError(nameof(item.HashedId),"AccountId has not been supplied");
+            }
+
+            if (!validationResult.IsValid())
+            {
+                return validationResult;
+            }
+
+            var membership = await _membershipRepository.GetCaller(item.HashedId, item.ExternalUserId);
+
+            if (membership == null)
+            {
+                validationResult.IsUnauthorized = true;
+                validationResult.AddError("Membership", "Caller is not a member of this account");
+            }
+                
+
+            return validationResult;
+        }
+    }
+}
