f24ef263ad6bea7efc04b6654e076bf63652ba3e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-25 17:33:42 +0000

    Correct arrange test for Remove PAYE
    Corrected the bool that is set when the remove scheme has been
    selected. The default is now zero state as opposed to remove or keep

--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountPayeControllerTests/WhenIRemoveAPayeScheme.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountPayeControllerTests/WhenIRemoveAPayeScheme.cs
@@ -29,39 +29,39 @@ namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountPayeControllerTes
             _userWhiteList = new Mock<IUserWhiteList>();
 
             _controller = new EmployerAccountPayeController(
-                _owinWrapper.Object,_employerAccountPayeOrchestrator.Object, _featureToggle.Object, _userWhiteList.Object);
+                _owinWrapper.Object, _employerAccountPayeOrchestrator.Object, _featureToggle.Object, _userWhiteList.Object);
         }
 
         [Test]
         public async Task ThenTheOrchestratorIsCalledIfYouConfirmToRemoveTheScheme()
         {
             //Act
-            var actual = await _controller.RemovePaye("",new RemovePayeScheme { RemoveScheme = 1 });
+            var actual = await _controller.RemovePaye("", new RemovePayeScheme { RemoveScheme = 2 });
 
             //Assert
-            _employerAccountPayeOrchestrator.Verify(x=>x.RemoveSchemeFromAccount(It.IsAny<RemovePayeScheme>()), Times.Once);
+            _employerAccountPayeOrchestrator.Verify(x => x.RemoveSchemeFromAccount(It.IsAny<RemovePayeScheme>()), Times.Once);
             Assert.IsNotNull(actual);
             var actualRedirect = actual as RedirectToRouteResult;
             Assert.IsNotNull(actualRedirect);
-            Assert.AreEqual("Index",actualRedirect.RouteValues["Action"]);
+            Assert.AreEqual("Index", actualRedirect.RouteValues["Action"]);
             Assert.AreEqual("EmployerAccountPaye", actualRedirect.RouteValues["Controller"]);
             Assert.IsTrue(_controller.TempData.ContainsKey("successMessage"));
         }
-        
+
         [Test]
         public async Task ThenTheConfirmRemoveSchemeViewIsReturnedIfThereIsAValidationError()
         {
             //Arrange
-            _employerAccountPayeOrchestrator.Setup(x => x.RemoveSchemeFromAccount(It.IsAny<RemovePayeScheme>())).ReturnsAsync(new OrchestratorResponse<RemovePayeScheme> {Status = HttpStatusCode.BadRequest});
+            _employerAccountPayeOrchestrator.Setup(x => x.RemoveSchemeFromAccount(It.IsAny<RemovePayeScheme>())).ReturnsAsync(new OrchestratorResponse<RemovePayeScheme> { Status = HttpStatusCode.BadRequest });
 
             //Act
-            var actual = await _controller.RemovePaye("",new RemovePayeScheme());
+            var actual = await _controller.RemovePaye("", new RemovePayeScheme());
 
             //Assert
             Assert.IsNotNull(actual);
             var actualView = actual as ViewResult;
             Assert.IsNotNull(actualView);
-            Assert.AreEqual("Remove",actualView.ViewName);
+            Assert.AreEqual("Remove", actualView.ViewName);
         }
     }
 }
