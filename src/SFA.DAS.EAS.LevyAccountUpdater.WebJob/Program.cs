bab98d8f95f42871fc85c7a43639e692070404a8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-11-25 11:49:24 +0000

    Add redis logging info to LevyAccountUpdated
    Added the redis logging information to the LevyAccountUpdater.

--- src/SFA.DAS.EAS.LevyAccountUpdater.WebJob/Program.cs
+++ src/SFA.DAS.EAS.LevyAccountUpdater.WebJob/Program.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.EAS.LevyAccountUpdater.WebJob.DependencyResolution;
+﻿using SFA.DAS.EAS.Infrastructure.Logging;
+using SFA.DAS.EAS.LevyAccountUpdater.WebJob.DependencyResolution;
 using SFA.DAS.EAS.LevyAccountUpdater.WebJob.Updater;
 
 namespace SFA.DAS.EAS.LevyAccountUpdater.WebJob
@@ -7,6 +8,8 @@ namespace SFA.DAS.EAS.LevyAccountUpdater.WebJob
     {
         static void Main(string[] args)
         {
+            LoggingConfig.ConfigureLogging();
+
             var container = IoC.Initialize();
 
             var updater = container.GetInstance<IAccountUpdater>();
