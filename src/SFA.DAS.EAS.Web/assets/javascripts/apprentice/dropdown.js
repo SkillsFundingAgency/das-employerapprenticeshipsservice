a84abd2c65f9385dc905701f2579f71b82e9bd4d
RajeshV <rajesh@digi2al.co.uk>
2016-11-28 13:58:11 +0000

    On space bar select dropdown

--- src/SFA.DAS.EAS.Web/assets/javascripts/apprentice/dropdown.js
+++ src/SFA.DAS.EAS.Web/assets/javascripts/apprentice/dropdown.js
@@ -8,5 +8,11 @@
     };
     init();
     // add focus to span element for accessibility while using tabs 
-    $('span.select2').attr('tabindex', 0);
+    $('span.select2').attr('tabindex', 0), 
+    keydown: function(e) { // spacebar press to behave like click
+        var code = e.which;
+        if ((code === 13) || (code === 32)) {
+            $(this).click();
+        }
+
 }());
