369cee0f6b39bee696f0af9b35971714508e8d02
RajeshV <rajesh@digi2al.co.uk>
2016-11-29 09:56:21 +0000

    content change for empty state and dropdown open on tabbed cycle

--- src/SFA.DAS.EAS.Web/assets/javascripts/apprentice/dropdown.js
+++ src/SFA.DAS.EAS.Web/assets/javascripts/apprentice/dropdown.js
@@ -7,12 +7,18 @@
         }
     };
     init();
-    // add focus to span element for accessibility while using tabs 
-    $('span.select2').attr('tabindex', 0), 
-    keydown: function(e) { // spacebar press to behave like click
-        var code = e.which;
-        if ((code === 13) || (code === 32)) {
-            $(this).click();
-        }
+
+    // open dropdownon on focus
+    $(document).on('focus', '.select2', function () {
+        $(this).siblings('select').select2('open');
+    });
+
+        // add focus to span element for accessibility while using tabs 
+       // $('span.select2').attr('tabindex', 0), 
+       // keydown: function(e) { // spacebar press to behave like click
+       //     var code = e.which;
+       //     if ((code === 13) || (code === 32)) {
+       //         $(this).click();
+        //    }
 
 }());
