796bac0e4b72ad7321e3027dcd993f9b46ce4e49
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-06 10:51:38 +0000

    Implement validator for CreateEFCalc Date
    Added the validator for the creating of the last EF calculated date

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateEnglishFractionCalculationDate/CreateEnglishFractionCalculationDateCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.EAS.Application.Commands.CreateEnglishFractionCalculationDate
+{
+    public class CreateEnglishFractionCalculationDateCommandHandler : AsyncRequestHandler<CreateEnglishFractionCalculationDateCommand>
+    {
+        protected override Task HandleCore(CreateEnglishFractionCalculationDateCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
