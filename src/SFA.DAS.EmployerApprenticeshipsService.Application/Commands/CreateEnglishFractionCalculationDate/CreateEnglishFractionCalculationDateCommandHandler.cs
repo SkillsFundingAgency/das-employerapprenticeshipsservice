47f19549aa948ee68d46dbdee88a24a6ba081e8a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-06 11:06:38 +0000

    Implement Fraction last calculated date command
    Implemented the command for updating the fraction last calculated date
    received from HMRC

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateEnglishFractionCalculationDate/CreateEnglishFractionCalculationDateCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateEnglishFractionCalculationDate/CreateEnglishFractionCalculationDateCommandHandler.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
+using NLog;
+using SFA.DAS.EAS.Application.Validation;
+using SFA.DAS.EAS.Domain.Data;
 
 namespace SFA.DAS.EAS.Application.Commands.CreateEnglishFractionCalculationDate
 {
     public class CreateEnglishFractionCalculationDateCommandHandler : AsyncRequestHandler<CreateEnglishFractionCalculationDateCommand>
     {
-        protected override Task HandleCore(CreateEnglishFractionCalculationDateCommand message)
+        private readonly IValidator<CreateEnglishFractionCalculationDateCommand> _validator;
+        private readonly IEnglishFractionRepository _englishFractionRepository;
+        private readonly ILogger _logger;
+
+        public CreateEnglishFractionCalculationDateCommandHandler(IValidator<CreateEnglishFractionCalculationDateCommand> validator, IEnglishFractionRepository englishFractionRepository, ILogger logger)
         {
-            throw new NotImplementedException();
+            _validator = validator;
+            _englishFractionRepository = englishFractionRepository;
+            _logger = logger;
+        }
+
+        protected override async Task HandleCore(CreateEnglishFractionCalculationDateCommand message)
+        {
+            var validationResult = _validator.Validate(message);
+
+            if (!validationResult.IsValid())
+            {
+                throw new InvalidRequestException(validationResult.ValidationDictionary);
+            }
+
+            await _englishFractionRepository.SetLastUpdateDate(message.DateCalculated);
+
+            _logger.Info($"English Fraction CalculationDate updated to {message.DateCalculated.ToString("dd MMM yyyy")}");
         }
     }
 }
