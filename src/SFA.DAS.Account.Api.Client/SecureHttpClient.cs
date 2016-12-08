1e5aaa3143f82cc1f784d121473cd66dd1010f2b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-08 15:46:52 +0000

    Correct nuget references
    Nuget package reference to
    Microsoft.IdentityModel.Cleints.ActiveDirectory had not been correctly
    added

--- src/SFA.DAS.Account.Api.Client/SecureHttpClient.cs
+++ src/SFA.DAS.Account.Api.Client/SecureHttpClient.cs
@@ -21,7 +21,7 @@ namespace SFA.DAS.EAS.Account.Api.Client
 
         private async Task<AuthenticationResult> GetAuthenticationResult(string clientId, string appKey, string resourceId, string tenant)
         {
-            var authority = string.Format("https://login.microsoftonline.com/{0}",tenant);
+            var authority = $"https://login.microsoftonline.com/{tenant}";
             var clientCredential = new ClientCredential(clientId, appKey);
             var context = new AuthenticationContext(authority, true);
             var result = await context.AcquireTokenAsync(resourceId, clientCredential);
