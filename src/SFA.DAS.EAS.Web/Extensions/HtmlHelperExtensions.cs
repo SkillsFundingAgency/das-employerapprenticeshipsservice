272b07e7a9333e164c67e45d6cc62914b1553bbd
froberg <christofer.froberg@gmail.com>
2016-12-09 13:12:37 +0000

    Added validation for approval

--- src/SFA.DAS.EAS.Web/Extensions/HtmlHelperExtensions.cs
+++ src/SFA.DAS.EAS.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
+
+using FluentValidation.Results;
 
 namespace SFA.DAS.EAS.Web.Extensions
 {
@@ -22,5 +25,25 @@ namespace SFA.DAS.EAS.Web.Extensions
             
             return new MvcHtmlString(errorClass);
         }
+
+        public static MvcHtmlString AddClassIfPropertyIsMissingForApproval<TModel, TProperty>(
+             this HtmlHelper<TModel> htmlHelper,
+             Expression<Func<TModel, TProperty>> expression,
+             string cssClass)
+         {
+             var expressionText = ExpressionHelper.GetExpressionText(expression);
+             var fullHtmlFieldName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(expressionText);
+             var state = htmlHelper.ViewData.ModelState;
+ 
+             var approvalWarningState = htmlHelper.ViewBag.ApprovalWarningState as ValidationResult;
+             var error = approvalWarningState?.Errors.FirstOrDefault(m => m.PropertyName == fullHtmlFieldName);
+ 
+             if (error == null || !state.IsValid)
+             {
+                 return MvcHtmlString.Empty;
+             }
+ 
+             return new MvcHtmlString(cssClass);
+         }
     }
 }
