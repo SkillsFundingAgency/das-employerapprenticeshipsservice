e8c570e5b684884460ff938c7a669daa71e0e700
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2016-12-09 10:48:16 +0000

    Change the invite team member to redirect
    Change the logic so it redirects back to the team member page with a
    tempdata value to indicate what email should be displayed as being
    added. This corrects the problem with refreshing the page

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerTeamOrchestratorTests/WhenIGetMyTeamMembers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Application.Queries.GetAccountTeamMembers;
+using SFA.DAS.EAS.Domain;
+using SFA.DAS.EAS.Web.Orchestrators;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Orchestrators.EmployerTeamOrchestratorTests
+{
+    public class WhenIGetMyTeamMembers
+    {
+        private Mock<IMediator> _mediator;
+        private EmployerTeamOrchestrator _orchestrator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mediator = new Mock<IMediator>();
+            _mediator.Setup(x => x.SendAsync(It.IsAny<GetAccountTeamMembersQuery>())).ReturnsAsync(new GetAccountTeamMembersResponse {TeamMembers = new List<TeamMember> {new TeamMember()} });
+
+            _orchestrator = new EmployerTeamOrchestrator(_mediator.Object);
+        }
+
+        [Test]
+        public async Task ThenTheGetTeamMembersCallWillShowASuccessMessageIfAnEmailHasBeenPassed()
+        {
+            //Arrange
+            var expectedEmail = "test@test.com";
+
+            //Act
+            var actual = await _orchestrator.GetTeamMembers("ABF45", "123", expectedEmail);
+
+            //Assert
+            Assert.IsNotNull(actual.FlashMessage);
+            Assert.AreEqual("Invitation sent", actual.FlashMessage.Headline);
+            Assert.AreEqual($"You've sent an invitation to <strong>{expectedEmail}</strong>", actual.FlashMessage.Message);
+        }
+
+        [Test]
+        public async Task ThenTheTeamMembersArePopulatedToTheResponse()
+        {
+            //Act
+            var actual = await _orchestrator.GetTeamMembers("ABF45", "123");
+
+            //Assert
+            Assert.IsNotEmpty(actual.Data.TeamMembers);
+        }
+    }
+}
