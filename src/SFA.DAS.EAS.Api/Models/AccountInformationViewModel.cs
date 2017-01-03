2ef579672773f1ce84595c4b6a2555693910649e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-01-03 14:25:12 +0000

    Add new api controller for AccountInfo
    Added a new api endpoint for getting the account information. Added new
    unit test project for the API project

---
+++ src/SFA.DAS.EAS.Api/Models/AccountInformationViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SFA.DAS.EAS.Api.Models
+{
+    public class AccountInformationViewModel
+    {
+        public string DasAccountName { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public string OrganisationRegisteredAddress { get; set; }
+        public string OrganisationSource { get; set; }
+        public string OrganisationStatus { get; set; }
+        public string OrganisationName { get; set; }
+        public string OwnerEmail { get; set; }
+    }
+}
