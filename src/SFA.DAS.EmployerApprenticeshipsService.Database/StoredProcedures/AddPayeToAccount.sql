751eb70ceeac0c924ae899c446e0a92775605d29
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-01-05 11:16:46 +0000

    Update to Paye table to have name
    Updated the Paye table to store the Scheme name, updated the
    AccountRepository and stored procedures to pass this value through.

--- src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/AddPayeToAccount.sql
+++ src/SFA.DAS.EmployerApprenticeshipsService.Database/StoredProcedures/AddPayeToAccount.sql
@@ -3,7 +3,8 @@
 	@employerRef  NVARCHAR(16),
 	@accessToken VARCHAR(30),
 	@refreshToken VARCHAR(30),
-	@addedDate DATETIME
+	@addedDate DATETIME,
+	@employerRefName VARCHAR(500) NULL
 AS
 BEGIN
 	DECLARE @employerAgreementId BIGINT
@@ -11,11 +12,11 @@ BEGIN
 	
 	IF EXISTS(select 1 from [account].[Paye] where ref = @employerRef)
 	BEGIN
-		EXEC [account].[UpdatePaye] @employerRef,@accessToken, @refreshToken
+		EXEC [account].[UpdatePaye] @employerRef,@accessToken, @refreshToken,@employerRefName
 	END
 	ELSE
 	BEGIN
-		EXEC [account].[CreatePaye] @employerRef,@accessToken, @refreshToken
+		EXEC [account].[CreatePaye] @employerRef,@accessToken, @refreshToken,@employerRefName
 	END
 
 	EXEC [account].[CreateAccountHistory] @accountId,@employerRef,@addedDate
