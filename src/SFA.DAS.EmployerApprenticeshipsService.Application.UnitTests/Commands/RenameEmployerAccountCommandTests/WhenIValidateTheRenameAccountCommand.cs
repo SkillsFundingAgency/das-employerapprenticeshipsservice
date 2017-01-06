c3f60d43949560dcd332b46f815e20589e9802bc
Christopher Foster <Christopher Foster>
2017-01-06 09:42:23 +0000

    Adds application unit test

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/RenameEmployerAccountCommandTests/WhenIValidateTheRenameAccountCommand.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Commands/RenameEmployerAccountCommandTests/WhenIValidateTheRenameAccountCommand.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using NUnit.Framework;
+using SFA.DAS.EAS.Application.Commands.RenameEmployerAccount;
 
 namespace SFA.DAS.EAS.Application.UnitTests.Commands.RenameEmployerAccountCommandTests
 {
     public class WhenIValidateTheRenameAccountCommand
     {
+        private RenameEmployerAccountCommandValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new RenameEmployerAccountCommandValidator();
+        }
+
         [Test]
         public void ThenNewAccountNameCannotBeEmpty()
         {
-            throw new NotImplementedException();
+            //Arrange
+            var command = new RenameEmployerAccountCommand
+            {
+                NewName = String.Empty
+            };
+
+            //Act
+            var result = _validator.Validate(command);
+
+            //Assert
+            Assert.IsFalse(result.IsValid());
+        }
+
+        [Test]
+        public void ThenNewAccountNameIsValidIfNotEmpty()
+        {
+            //Arrange
+            var command = new RenameEmployerAccountCommand
+            {
+                NewName = "Test Renamed Account"
+            };
+
+            //Act
+            var result = _validator.Validate(command);
+
+            //Assert
+            Assert.IsTrue(result.IsValid());
         }
     }
 }
