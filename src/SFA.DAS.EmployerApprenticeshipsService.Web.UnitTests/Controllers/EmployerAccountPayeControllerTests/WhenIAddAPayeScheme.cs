d9aa92ee8aee83f94b71891d3aaf4e1a8dc61496
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-01-13 10:26:53 +0000

    Update content for add/remove scheme
    Updated the content for adding and removing PAYE schemes.
    
    Also added in content to inform the user that no levy funds will appear
    until May 20th

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountPayeControllerTests/WhenIAddAPayeScheme.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Web.Authentication;
+using SFA.DAS.EAS.Web.Controllers;
+using SFA.DAS.EAS.Web.Models;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountPayeControllerTests
+{
+    public class WhenIAddAPayeScheme
+    {
+        private Mock<Web.Orchestrators.EmployerAccountPayeOrchestrator> _employerAccountPayeOrchestrator;
+        private Mock<IOwinWrapper> _owinWrapper;
+        private EmployerAccountPayeController _controller;
+        private Mock<IFeatureToggle> _featureToggle;
+        private Mock<IUserWhiteList> _userWhiteList;
+        private const string ExpectedAccountId = "AFD123";
+        private const string ExpectedUserId = "456TGF3";
+
+        [SetUp]
+        public void Arrange()
+        {
+            _employerAccountPayeOrchestrator = new Mock<Web.Orchestrators.EmployerAccountPayeOrchestrator>();
+            
+            _owinWrapper = new Mock<IOwinWrapper>();
+            _owinWrapper.Setup(x => x.GetClaimValue("sub")).Returns(ExpectedUserId);
+            _featureToggle = new Mock<IFeatureToggle>();
+            _userWhiteList = new Mock<IUserWhiteList>();
+
+            _controller = new EmployerAccountPayeController(
+                _owinWrapper.Object, _employerAccountPayeOrchestrator.Object, _featureToggle.Object, _userWhiteList.Object);
+        }
+
+        [Test]
+        public async Task ThenTheAddPayeSchemeToAccountIsCalledWithTheCorrectParameters()
+        {
+            //Arrange
+            var expectedAddNewPayeScheme = new AddNewPayeScheme {AccessToken = "123DFG",HashedAccountId = ExpectedAccountId,PayeName = "123/ABC",RefreshToken = "987TGH"};
+
+            //Act
+            await _controller.ConfirmPayeScheme(ExpectedAccountId, expectedAddNewPayeScheme);
+
+            //Assert
+            _employerAccountPayeOrchestrator.Verify(x=>x.AddPayeSchemeToAccount(It.Is<AddNewPayeScheme>(
+                c=>c.AccessToken.Equals(expectedAddNewPayeScheme.AccessToken) &&
+                c.HashedAccountId.Equals(expectedAddNewPayeScheme.HashedAccountId) &&
+                c.PayeName.Equals(expectedAddNewPayeScheme.PayeName) &&
+                c.RefreshToken.Equals(expectedAddNewPayeScheme.RefreshToken)
+                ),ExpectedUserId));
+        }
+
+        [Test]
+        public async Task ThenTheSuccessMessageIsCorrectlyPopulated()
+        {
+            //Act
+            await _controller.ConfirmPayeScheme(ExpectedAccountId, new AddNewPayeScheme());
+
+            //Assert
+            Assert.IsTrue(_controller.TempData.ContainsKey("successHeader"));
+        }
+    }
+}
--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountPayeControllerTests/WhenIRemoveAPayeScheme.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountPayeControllerTests/WhenIRemoveAPayeScheme.cs
@@ -45,7 +45,7 @@ namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountPayeControllerTes
             Assert.IsNotNull(actualRedirect);
             Assert.AreEqual("Index", actualRedirect.RouteValues["Action"]);
             Assert.AreEqual("EmployerAccountPaye", actualRedirect.RouteValues["Controller"]);
-            Assert.IsTrue(_controller.TempData.ContainsKey("successMessage"));
+            Assert.IsTrue(_controller.TempData.ContainsKey("successHeader"));
         }
 
         [Test]
