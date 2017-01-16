5ff98276850f31407417676313cf53a64bf8e8c2
Christopher Foster <Christopher Foster>
2017-01-16 11:28:41 +0000

    Changes to postcode validation regex, max length, whitespace trimming

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateOrganisationAddress/CreateOrganisationAddressValidator.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/CreateOrganisationAddress/CreateOrganisationAddressValidator.cs
@@ -12,9 +12,11 @@ namespace SFA.DAS.EAS.Application.Commands.CreateOrganisationAddress
         //than enough to cover this. You can see the stackoverflow post here:
         //http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
         private const string PostcodeRegExPattern =
-            "(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)" + 
-            "|(([A-Z-[QVX]][0-9][A-HJKPSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY]" + 
-            ")))) [0-9][A-Z-[CIKMOV]]{2})";
+            "(GIR 0AA)|(GIR0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)" + 
+            "|(([A-Z-[QVX]][0-9][A-HJKPSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY]" +
+            "))))[ ]?[0-9][A-Z-[CIKMOV]]{2})";
+
+        private const short MaxPostCodeLength = 8;
 
         public ValidationResult Validate(CreateOrganisationAddressRequest item)
         {
@@ -35,10 +37,14 @@ namespace SFA.DAS.EAS.Application.Commands.CreateOrganisationAddress
                 results.ValidationDictionary.Add(nameof(item.Postcode), "Enter a valid postcode");
             }
 
-            if (!string.IsNullOrEmpty(item.Postcode) && !Regex.IsMatch(item.Postcode.ToUpper(), PostcodeRegExPattern))
-            {
+            if (!results.ValidationDictionary.ContainsKey(nameof(item.Postcode)) &&
+                (!string.IsNullOrEmpty(item.Postcode) && item.Postcode.Trim().Length > MaxPostCodeLength))
                 results.ValidationDictionary.Add(nameof(item.Postcode), "Enter a valid postcode");
-            }
+
+            if (!results.ValidationDictionary.ContainsKey(nameof(item.Postcode)) &&
+                (!string.IsNullOrEmpty(item.Postcode) &&
+                 !Regex.IsMatch(item.Postcode.ToUpper(), PostcodeRegExPattern)))
+                results.ValidationDictionary.Add(nameof(item.Postcode), "Enter a valid postcode");
 
             return results;
         }
