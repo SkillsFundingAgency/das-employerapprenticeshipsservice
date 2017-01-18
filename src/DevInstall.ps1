4b2d8f3401f4679dd2592acb5731c436ff7545d6
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-01-18 08:59:45 +0000

    Add cert and update install powershell script

--- src/DevInstall.ps1
+++ src/DevInstall.ps1
@@ -6,3 +6,6 @@ Import-PfxCertificate -FilePath localhost.pfx -CertStoreLocation cert://LocalMac
 Import-PfxCertificate -FilePath localhost.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
 Import-PfxCertificate -FilePath DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
 Import-PfxCertificate -FilePath DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
+
+$idppwd = ConvertTo-SecureString -String idsrv3test -Force -AsPlainText
+Import-PfxCertificate -FilePath DasIDPCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $idppwd -Exportable
