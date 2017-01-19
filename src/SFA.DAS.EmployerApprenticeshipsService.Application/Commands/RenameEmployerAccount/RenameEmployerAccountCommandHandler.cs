67315e32af83a40e0a97553abe0fd77fd71fd2d8
Robin O'Connor <github@bytepointsoftware.com>
2017-01-19 14:25:43 +0000

    Adding audit entry for renaming account

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/RenameEmployerAccount/RenameEmployerAccountCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/RenameEmployerAccount/RenameEmployerAccountCommandHandler.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
+using SFA.DAS.Audit.Types;
+using SFA.DAS.EAS.Application.Commands.AuditCommand;
 using SFA.DAS.EAS.Application.Commands.CreateAccountEvent;
 using SFA.DAS.EAS.Application.Validation;
+using SFA.DAS.EAS.Domain;
 using SFA.DAS.EAS.Domain.Data;
 using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Domain.Models.Audit;
 using SFA.DAS.Events.Api.Client;
 using SFA.DAS.Events.Api.Types;
 
@@ -13,13 +18,15 @@ namespace SFA.DAS.EAS.Application.Commands.RenameEmployerAccount
     public class RenameEmployerAccountCommandHandler : AsyncRequestHandler<RenameEmployerAccountCommand>
     {
         private readonly IEmployerAccountRepository _accountRepository;
+        private readonly IMembershipRepository _membershipRepository;
         private readonly IValidator<RenameEmployerAccountCommand> _validator;
         private readonly IHashingService _hashingService;
         private readonly IMediator _mediator;
 
-        public RenameEmployerAccountCommandHandler(IEmployerAccountRepository accountRepository, IValidator<RenameEmployerAccountCommand> validator, IHashingService hashingService, IMediator mediator)
+        public RenameEmployerAccountCommandHandler(IEmployerAccountRepository accountRepository, IMembershipRepository membershipRepository, IValidator<RenameEmployerAccountCommand> validator, IHashingService hashingService, IMediator mediator)
         {
             _accountRepository = accountRepository;
+            _membershipRepository = membershipRepository;
             _validator = validator;
             _hashingService = hashingService;
             _mediator = mediator;
@@ -42,10 +49,33 @@ namespace SFA.DAS.EAS.Application.Commands.RenameEmployerAccount
             var accountId = _hashingService.DecodeValue(message.HashedAccountId);
 
             await _accountRepository.RenameAccount(accountId, message.NewName);
+
+            var owner = await _membershipRepository.GetCaller(message.HashedAccountId, message.ExternalUserId);
+
+            await AddAuditEntry(owner.Email, accountId, message.NewName);
+
             await _mediator.PublishAsync(new CreateAccountEventCommand
             {
                 HashedAccountId = message.HashedAccountId,
                 Event = "AccountRenamed"
+            });
+        }
+
+        private async Task AddAuditEntry(string ownerEmail, long accountId, string name)
+        {
+            await _mediator.SendAsync(new CreateAuditCommand
+            {
+                EasAuditMessage = new EasAuditMessage
+                {
+                    Description = $"User {ownerEmail} has renamed account {accountId} to {name}",
+                    ChangedProperties = new List<PropertyUpdate>
+                    {
+                        new PropertyUpdate {PropertyName = "AccountId", NewValue = accountId.ToString()},
+                        new PropertyUpdate {PropertyName = "Name", NewValue = name},
+                    },
+                    RelatedEntities = new List<Entity> { new Entity { Id = accountId.ToString(), Type = "Account" } },
+                    AffectedEntity = new Entity { Type = "Account", Id = accountId.ToString() }
+                }
             });
         }
     }
