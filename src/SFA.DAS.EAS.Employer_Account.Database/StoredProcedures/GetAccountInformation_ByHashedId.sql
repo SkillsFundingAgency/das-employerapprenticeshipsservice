21105541fc8a44973aabc674cb6409c9f09b491b
Michael Young <mick.young1981@googlemail.com>
2017-01-20 11:05:33 +0000

    Added stored proc to get account info by hashed id

---
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetAccountInformation_ByHashedId.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE employer_account.GetAccountInformation_ByHashedId
+	@HashedId VARCHAR(100)
+AS
+Select 
+	DasAccountId, 
+	DasAccountName, 
+	DateRegistered, 
+	OrganisationId, 
+	OrganisationName, 
+	OrganisationRegisteredAddress, 
+	OrganisationStatus, 
+	OrgansiationCreatedDate, 
+	OrganisationNumber,
+	OwnerEmail,
+	OrganisationSource,
+	PayeSchemeName
+FROM [employer_account].[AccountInformation]
+WHERE DasAccountId = @HashedId
