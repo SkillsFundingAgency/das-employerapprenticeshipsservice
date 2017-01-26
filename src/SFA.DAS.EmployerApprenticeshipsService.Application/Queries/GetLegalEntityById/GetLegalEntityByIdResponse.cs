164e0f140790a0a36290674bf42a0fe6a74214cd
Michael Young <mick.young1981@googlemail.com>
2017-01-26 10:00:25 +0000

    Added query to get a legal entity by its id.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetLegalEntityById/GetLegalEntityByIdResponse.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.EAS.Domain.Entities.Account;
+
+namespace SFA.DAS.EAS.Application.Queries.GetLegalEntityById
+{
+    public class GetLegalEntityByIdResponse
+    {
+        public LegalEntityView LegalEntity { get; set; }
+    }
+}
