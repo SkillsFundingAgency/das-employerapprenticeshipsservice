d0ddc98dbb290bb47f67fde41589c6220f1bfe1d
Michael Young <mick.young1981@googlemail.com>
2017-01-26 09:23:54 +0000

    Added repository to get a legal entity by its id.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Entities/Account/LegalEntityView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.EAS.Domain.Entities.Account
+{
+    public class LegalEntityView
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string RegisteredAddress { get; set; }
+        public DateTime? DateOfIncorporation { get; set; }
+        public string CompanyStatus { get; set; }
+        public string Source { get; set; }
+    }
+}
