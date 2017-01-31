a4e953b014d402024caefb9326dde3538274a51e
froberg <christofer.froberg@gmail.com>
2017-01-31 10:11:32 +0000

    Removed provider name from view

--- src/SFA.DAS.EAS.Web/Orchestrators/EmployerManageApprenticeshipsOrchestrator.cs
+++ src/SFA.DAS.EAS.Web/Orchestrators/EmployerManageApprenticeshipsOrchestrator.cs
@@ -4,12 +4,9 @@ using System.Threading.Tasks;
 
 using MediatR;
 
-using NLog;
-
 using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.EAS.Application.Queries.GetAllApprenticeships;
 using SFA.DAS.EAS.Application.Queries.GetApprenticeship;
-using SFA.DAS.EAS.Application.Queries.GetProvider;
 using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Web.Models.ManageApprenticeships;
 
@@ -19,24 +16,16 @@ namespace SFA.DAS.EAS.Web.Orchestrators
     {
         private readonly IMediator _mediator;
         private readonly IHashingService _hashingService;
-        private readonly ILogger _logger;
-        private readonly ICommitmentStatusCalculator _statusCalculator;
 
-        public EmployerManageApprenticeshipsOrchestrator(IMediator mediator, IHashingService hashingService, ICommitmentStatusCalculator statusCalculator, ILogger logger)
+        public EmployerManageApprenticeshipsOrchestrator(IMediator mediator, IHashingService hashingService)
         {
             if (mediator == null)
                 throw new ArgumentNullException(nameof(mediator));
             if (hashingService == null)
                 throw new ArgumentNullException(nameof(hashingService));
-            if (statusCalculator == null)
-                throw new ArgumentNullException(nameof(statusCalculator));
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
 
             _mediator = mediator;
             _hashingService = hashingService;
-            _statusCalculator = statusCalculator;
-            _logger = logger;
         }
 
         public async Task<ManageApprenticeshipsViewModel> GetApprenticeships(string hashedaccountId, string getClaimValue)
@@ -49,7 +38,7 @@ namespace SFA.DAS.EAS.Web.Orchestrators
             var model = new ManageApprenticeshipsViewModel
                             {
                                 HashedaccountId = hashedaccountId,
-                                Apprenticeships = await Task.WhenAll(apprenticeships)
+                                Apprenticeships = apprenticeships
                             };
 
             return await Task.FromResult(model);
@@ -60,17 +49,11 @@ namespace SFA.DAS.EAS.Web.Orchestrators
             var accountId = _hashingService.DecodeValue(hashedaccountId);
             var data = await _mediator.SendAsync(new GetApprenticeshipQueryRequest { AccountId = accountId, ApprenticeshipId = apprenticeshipId });
 
-            return await MapFrom(data.Apprenticeship);
+            return MapFrom(data.Apprenticeship);
         }
 
-        private async Task<ApprenticeshipDetailsViewModel> MapFrom(Apprenticeship apprenticeship)
+        private ApprenticeshipDetailsViewModel MapFrom(Apprenticeship apprenticeship)
         {
-            var data = await _mediator.SendAsync(new GetProviderQueryRequest
-                                       {
-                                           ProviderId = (int)apprenticeship.ProviderId
-                                       } ) ;
-
-            var result = data.ProvidersView.Providers.FirstOrDefault();
             return new ApprenticeshipDetailsViewModel
             {
                 Id = apprenticeship.Id,
@@ -82,7 +65,7 @@ namespace SFA.DAS.EAS.Web.Orchestrators
                 TrainingName = apprenticeship.TrainingName,
                 Cost = apprenticeship.Cost,
                 Status = MapPaymentStatus(apprenticeship.PaymentStatus),
-                ProviderName = result?.ProviderName ?? string.Empty
+                ProviderName = string.Empty
             };
         }
 
