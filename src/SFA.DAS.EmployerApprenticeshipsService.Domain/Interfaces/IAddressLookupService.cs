8a8ba1e225671f11682f32eae37c493eb2f12c91
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-02-01 09:42:23 +0000

    Added skeleton service for the address lookup

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Interfaces/IAddressLookupService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.Domain.Interfaces
+{
+    public interface IAddressLookupService
+    {
+        Task<ICollection<Address>> GetAddressesByPostcode(string postcode);
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/AddressLookupService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.EAS.Domain;
+using SFA.DAS.EAS.Domain.Interfaces;
+
+namespace SFA.DAS.EAS.Infrastructure.Services
+{
+    public class AddressLookupService : IAddressLookupService
+    {
+        public async Task<ICollection<Address>> GetAddressesByPostcode(string postcode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
