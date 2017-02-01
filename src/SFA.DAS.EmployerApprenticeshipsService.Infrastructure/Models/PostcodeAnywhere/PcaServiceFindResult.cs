7f7f9ac1289611773b385d464ca922ce03e15690
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-02-01 15:12:20 +0000

    Added DTO models for the Postcode Anywhere API service

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Models/PostcodeAnywhere/PcaServiceFindResult.cs
@@ -0,0 +1,17 @@
+﻿namespace SFA.DAS.EAS.Infrastructure.Models.PostcodeAnywhere
+{
+    internal class PcaServiceFindResult
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 1st line of the address
+        /// </summary>
+        public string StreetAddress { get; set; }
+
+        /// <summary>
+        /// This is the rest of the address, after line1
+        /// </summary>
+        public string Place { get; set; }
+    }
+}
