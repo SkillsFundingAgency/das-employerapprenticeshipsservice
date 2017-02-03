d35ff01b2996dd783ec75359a630b51c32fee1c7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-02-03 10:17:40 +0000

    Add sector to API data for legal entity
    Added the sector property to the legal entity response in the API

--- src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLegalEntity_ById.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLegalEntity_ById.sql
@@ -9,6 +9,7 @@ Select
 	le.DateOfIncorporation AS DateOfInception, 
 	le.Code,
     CASE le.Source WHEN 1 THEN 'Companies House' WHEN 2 THEN 'Charities' WHEN 3 THEN 'Public Bodies' ELSE 'Other' END AS Source,
-	CASE le.PublicSectorDataSource WHEN 1 THEN 'ONS' WHEN 2 THEN 'NHS' WHEN 3 THEN 'Police' ELSE '' END as PublicSectorDataSource
+	CASE le.PublicSectorDataSource WHEN 1 THEN 'ONS' WHEN 2 THEN 'NHS' WHEN 3 THEN 'Police' ELSE '' END as PublicSectorDataSource,
+	le.Sector
 from employer_account.LegalEntity le
 WHERE le.Id = @Id
