c5fedccc6df406c2c7f9c9bbba13cb942c3b0f08
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-02-13 15:52:42 +0000

    Add validator for GetLastLevyDeclarationRequest
    Added the validator for the GetLastLevyDeclarationRequest

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/AccountTransactions/GetLastLevyDeclaration/GetLastLevyDeclarationValidator.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using AutoMapper.Configuration.Conventions;
+using SFA.DAS.EAS.Application.Validation;
+
+namespace SFA.DAS.EAS.Application.Queries.AccountTransactions.GetLastLevyDeclaration
+{
+    public class GetLastLevyDeclarationValidator : IValidator<GetLastLevyDeclarationRequest>
+    {
+        public ValidationResult Validate(GetLastLevyDeclarationRequest item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.Empref))
+            {
+                validationResult.AddError(nameof(item.Empref));
+            }
+
+            return validationResult;
+        }
+
+        public Task<ValidationResult> ValidateAsync(GetLastLevyDeclarationRequest item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
