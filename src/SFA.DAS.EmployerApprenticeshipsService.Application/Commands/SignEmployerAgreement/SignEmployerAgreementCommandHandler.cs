389cac38ea443613bca78c99e884993c9e05eb5b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-02-17 14:23:59 +0000

    Update SignAgreement Handler
    Changed the signAgreement handler to call the new validator message.
    Updated the call to the repository to use a model instead of individual
    properties

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SignEmployerAgreement/SignEmployerAgreementCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SignEmployerAgreement/SignEmployerAgreementCommandHandler.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
-using SFA.DAS.EAS.Domain;
-using SFA.DAS.EAS.Domain.Data;
+using SFA.DAS.EAS.Application.Validation;
 using SFA.DAS.EAS.Domain.Data.Repositories;
 using SFA.DAS.EAS.Domain.Interfaces;
-using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
 using SFA.DAS.EAS.Domain.Models.UserProfile;
 
 namespace SFA.DAS.EAS.Application.Commands.SignEmployerAgreement
@@ -16,45 +14,39 @@ namespace SFA.DAS.EAS.Application.Commands.SignEmployerAgreement
         private readonly IMembershipRepository _membershipRepository;
         private readonly IEmployerAgreementRepository _employerAgreementRepository;
         private readonly IHashingService _hashingService;
-        private readonly SignEmployerAgreementCommandValidator _validator;
+        private readonly IValidator<SignEmployerAgreementCommand> _validator;
 
-        public SignEmployerAgreementCommandHandler(IMembershipRepository membershipRepository, IEmployerAgreementRepository employerAgreementRepository, IHashingService hashingService)
+        public SignEmployerAgreementCommandHandler(IMembershipRepository membershipRepository, IEmployerAgreementRepository employerAgreementRepository, IHashingService hashingService, IValidator<SignEmployerAgreementCommand> validator)
         {
-            if (membershipRepository == null)
-                throw new ArgumentNullException(nameof(membershipRepository));
-            if (employerAgreementRepository == null)
-                throw new ArgumentNullException(nameof(employerAgreementRepository));
             _membershipRepository = membershipRepository;
             _employerAgreementRepository = employerAgreementRepository;
             _hashingService = hashingService;
-            _validator = new SignEmployerAgreementCommandValidator();
+            _validator = validator;
         }
 
         protected override async Task HandleCore(SignEmployerAgreementCommand message)
         {
-            var validationResult = _validator.Validate(message);
+            var validationResult = await _validator.ValidateAsync(message);
 
             if (!validationResult.IsValid())
                 throw new InvalidRequestException(validationResult.ValidationDictionary);
 
             var owner = await _membershipRepository.GetCaller(message.HashedAccountId, message.ExternalUserId);
-
-            if (owner == null)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Membership", "User is not a member of this Account" } });
-            if ((Role)owner.RoleId != Role.Owner)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Membership", "User is not an Owner" } });
+            
+            if (owner == null || (Role)owner.RoleId != Role.Owner)
+                throw new UnauthorizedAccessException();
 
             var agreementId = _hashingService.DecodeValue(message.HashedAgreementId);
-            var agreement = await _employerAgreementRepository.GetEmployerAgreement(agreementId);
+            
+            var agreement = new Domain.Models.EmployerAgreement.SignEmployerAgreement
+            {
+                SignedDate = message.SignedDate,
+                AgreementId = agreementId,
+                SignedById = owner.UserId,
+                SignedByName = $"{owner.FirstName} {owner.LastName}"
+            };
 
-            if (agreement == null)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Agreement", $"Agreement {message.HashedAgreementId} does not exist" } });
-            if (agreement.Status == EmployerAgreementStatus.Signed)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Agreement", $"Agreement {message.HashedAgreementId} has already been signed" } });
-            if (agreement.ExpiredDate.HasValue)
-                throw new InvalidRequestException(new Dictionary<string, string> { { "Agreement", $"Agreement {message.HashedAgreementId} has expired" } });
-
-            await _employerAgreementRepository.SignAgreement(agreementId, owner.UserId, $"{owner.FirstName} {owner.LastName}", message.SignedDate);
+            await _employerAgreementRepository.SignAgreement(agreement);
         }
     }
 }
