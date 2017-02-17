3a42ae2f910832a14f03f13d588c66b23b62609f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-02-17 11:02:03 +0000

    Update validator for SignAgreement
    Updated the validator to use the async method and to check the
    agreement status and existence.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SignEmployerAgreement/SignEmployerAgreementCommandValidator.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/SignEmployerAgreement/SignEmployerAgreementCommandValidator.cs
@@ -1,33 +1,67 @@
 ﻿using System;
 using System.Threading.Tasks;
 using SFA.DAS.EAS.Application.Validation;
+using SFA.DAS.EAS.Domain.Data.Repositories;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
 
 namespace SFA.DAS.EAS.Application.Commands.SignEmployerAgreement
 {
     public class SignEmployerAgreementCommandValidator : IValidator<SignEmployerAgreementCommand>
     {
+        private readonly IEmployerAgreementRepository _employerAgreementRepository;
+        private readonly IHashingService _hashingService;
+
+        public SignEmployerAgreementCommandValidator(IEmployerAgreementRepository employerAgreementRepository, IHashingService hashingService)
+        {
+            _employerAgreementRepository = employerAgreementRepository;
+            _hashingService = hashingService;
+        }
+
         public ValidationResult Validate(SignEmployerAgreementCommand item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ValidationResult> ValidateAsync(SignEmployerAgreementCommand item)
         {
             var validationResult = new ValidationResult();
 
             if (string.IsNullOrEmpty(item.HashedAgreementId))
-                validationResult.AddError("AgreementId", "No AgreementId supplied");
+                validationResult.AddError(nameof(item.HashedAgreementId));
 
             if (string.IsNullOrEmpty(item.HashedAccountId))
-                validationResult.AddError("HashedId", "No HashedId supplied");
+                validationResult.AddError(nameof(item.HashedAccountId));
 
             if (string.IsNullOrWhiteSpace(item.ExternalUserId))
-                validationResult.AddError("ExternalUserId", "No ExternalUserId supplied");
+                validationResult.AddError(nameof(item.ExternalUserId));
 
             if (item.SignedDate == default(DateTime))
-                validationResult.AddError("SignedDate", "No SignedDate supplied");
+                validationResult.AddError(nameof(item.SignedDate));
 
+            if (!validationResult.IsValid())
+            {
+                return validationResult;
+            }
+
+            var agreementId = _hashingService.DecodeValue(item.HashedAgreementId);
+            var agreement = await _employerAgreementRepository.GetEmployerAgreement(agreementId);
+
+            if (agreement == null)
+            {
+                validationResult.AddError(nameof(agreement), "Agreement does not exist");
+                return validationResult;
+            }
+
+            if (agreement.Status == EmployerAgreementStatus.Signed ||
+                agreement.Status == EmployerAgreementStatus.Expired ||
+                agreement.Status == EmployerAgreementStatus.Superceded)
+            {
+                validationResult.AddError(nameof(agreement.Status),$"Agreement status is {agreement.Status}");
+                return validationResult;
+            }
+            
             return validationResult;
-        }
-
-        public Task<ValidationResult> ValidateAsync(SignEmployerAgreementCommand item)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
