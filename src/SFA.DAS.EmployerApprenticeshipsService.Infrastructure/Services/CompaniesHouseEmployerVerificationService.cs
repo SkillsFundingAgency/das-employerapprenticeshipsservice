6f9ce750a46f5b48aa5d1c1f7e17083653dc921a
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-24 12:58:30 +0000

    Implement execution policies and utilise in companies house service

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/CompaniesHouseEmployerVerificationService.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/CompaniesHouseEmployerVerificationService.cs
@@ -7,6 +7,7 @@ using SFA.DAS.EAS.Domain.Configuration;
 using SFA.DAS.EAS.Domain.Http;
 using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Domain.Models.Employer;
+using SFA.DAS.EAS.Infrastructure.ExecutionPolicies;
 
 namespace SFA.DAS.EAS.Infrastructure.Services
 {
@@ -15,19 +16,22 @@ namespace SFA.DAS.EAS.Infrastructure.Services
         private readonly EmployerApprenticeshipsServiceConfiguration _configuration;
         private readonly ILogger _logger;
         private readonly IHttpClientWrapper _httpClientWrapper;
+        private readonly ExecutionPolicy _executionPolicy;
 
-        public CompaniesHouseEmployerVerificationService(EmployerApprenticeshipsServiceConfiguration configuration, ILogger logger, IHttpClientWrapper httpClientWrapper)
+        public CompaniesHouseEmployerVerificationService(EmployerApprenticeshipsServiceConfiguration configuration, ILogger logger, IHttpClientWrapper httpClientWrapper,
+            [RequiredPolicy(CompaniesHouseExecutionPolicy.Name)]ExecutionPolicy executionPolicy)
         {
             _configuration = configuration;
             _logger = logger;
             _httpClientWrapper = httpClientWrapper;
+            _executionPolicy = executionPolicy;
             _httpClientWrapper.AuthScheme = "Basic";
             _httpClientWrapper.BaseUrl = _configuration.CompaniesHouse.BaseUrl;
         }
 
         public async Task<EmployerInformation> GetInformation(string id)
         {
-            try
+            return await _executionPolicy.ExecuteAsync(async () =>
             {
                 _logger.Info($"GetInformation({id})");
 
@@ -37,11 +41,7 @@ namespace SFA.DAS.EAS.Infrastructure.Services
                     $"{Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(_configuration.CompaniesHouse.ApiKey))}",
                     $"{_configuration.CompaniesHouse.BaseUrl}/company/{id}");
                 return result;
-            }
-            catch
-            {
-                return null;
-            }
+            });
         }
     }
 }
