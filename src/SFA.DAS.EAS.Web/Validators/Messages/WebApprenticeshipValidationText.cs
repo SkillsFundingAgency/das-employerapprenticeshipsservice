fa47bbac7a063c4ef52477c9e162543fe8bb2b80
Lee Wadhams <lee.wadhams@valtech.com>
2017-02-28 14:04:19 +0000

    Updated apprenticeship validation messages

--- src/SFA.DAS.EAS.Web/Validators/Messages/WebApprenticeshipValidationText.cs
+++ src/SFA.DAS.EAS.Web/Validators/Messages/WebApprenticeshipValidationText.cs
@@ -8,9 +8,9 @@ namespace SFA.DAS.EAS.Web.Validators.Messages
         public ValidationMessage CohortRef02 { get { throw new NotImplementedException(); } }
 
         public ValidationMessage Uln01 =>
-            new ValidationMessage("You must enter a number that's 10 digits", "ULN_01");
+            new ValidationMessage("You must enter a 10-digit unique learner number", "ULN_01");
         public ValidationMessage Uln02 =>
-            new ValidationMessage("The Unique Learner number of 9999999999 is not valid", "ULN_02");
+            new ValidationMessage("The unique learner number of 9999999999 isn't valid", "ULN_02");
 
         public ValidationMessage FamilyName01 =>
             new ValidationMessage("Last name must be entered", "FamilyName_01");
@@ -24,7 +24,7 @@ namespace SFA.DAS.EAS.Web.Validators.Messages
         public ValidationMessage DateOfBirth01 =>
             new ValidationMessage("The Date of birth is not valid", "DateOfBirth_01");
         public ValidationMessage DateOfBirth02 =>
-            new ValidationMessage("The apprentice must be at least 15 years old at the start of the programme", "DateOfBirth_02");
+            new ValidationMessage("The apprentice must be at least 15 years old at the start of their training", "DateOfBirth_02");
 
         public ValidationMessage LearnStartDate01 =>
             new ValidationMessage("The Learning start date is not valid", "LearnStartDate_01");
@@ -41,7 +41,7 @@ namespace SFA.DAS.EAS.Web.Validators.Messages
         public ValidationMessage TrainingPrice01 =>
             new ValidationMessage("Enter the total agreed training cost", "TrainingPrice_01");
         public ValidationMessage TrainingPrice02 =>
-            new ValidationMessage("The cost must not be more than £100,000", "TrainingPrice_02");
+            new ValidationMessage("The total cost must be £100,000 or less", "TrainingPrice_02");
 
         public ValidationMessage EmployerRef01 =>
             new ValidationMessage("The Reference must be 20 characters or fewer", "EmployerRef_01");
