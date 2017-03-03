1ab6cfa77a25340c69e80f6ad294754ec12d185b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-03-03 15:21:46 +0000

    Add validator for getting pdf agreement

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetEmployerAgreementPdfTests/WhenIValidateTheRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SFA.DAS.EAS.Application.Queries.GetEmployerAgreementPdf;
+
+namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetEmployerAgreementPdfTests
+{
+    public class WhenIValidateTheRequest
+    {
+        private GetEmployerAgreementPdfValidator _validator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new GetEmployerAgreementPdfValidator();
+        }
+
+        [Test]
+        public void ThenFalseIsReturnedAndTheErrorDictionaryPopulatedWhenTheFieldsArentPopulated()
+        {
+            //Act
+            var actual = _validator.Validate(new GetEmployerAgreementPdfRequest());
+
+            //Assert
+            Assert.IsFalse(actual.IsValid());
+            Assert.Contains(new KeyValuePair<string,string>("AgreementFileName", "AgreementFileName has not been supplied"), actual.ValidationDictionary);
+        }
+
+        [Test]
+        public void ThenTrueIsReturnedWhenAllFieldsArePopulated()
+        {
+            //Act
+            var actual = _validator.Validate(new GetEmployerAgreementPdfRequest {AgreementFileName = "NewFile.txt"});
+
+            //Assert
+            Assert.IsTrue(actual.IsValid());
+        }
+
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEmployerAgreementPdf/GetEmployerAgreementPdfValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EAS.Application.Validation;
+
+namespace SFA.DAS.EAS.Application.Queries.GetEmployerAgreementPdf
+{
+    public class GetEmployerAgreementPdfValidator : IValidator<GetEmployerAgreementPdfRequest>
+    {
+        public ValidationResult Validate(GetEmployerAgreementPdfRequest item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.AgreementFileName))
+            {
+                validationResult.AddError(nameof(item.AgreementFileName));
+            }
+
+            return validationResult;
+        }
+
+        public Task<ValidationResult> ValidateAsync(GetEmployerAgreementPdfRequest item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
