c1a20dca59b94bd8ac134a954eac6a3ff20dc690
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-03-03 14:09:24 +0000

    Add PdfService
    Added pdf service which uses Aspose.pdf. This gets the pdf from blob
    storage and also the license.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Interfaces/IPdfService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.Domain.Interfaces
+{
+    public interface IPdfService
+    {
+        Task<MemoryStream> SubsituteValuesForPdf(string fileName, Dictionary<string, string> valuesToSubsitute);
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/PdfService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using NLog;
+using SFA.DAS.EAS.Domain.Interfaces;
+
+namespace SFA.DAS.EAS.Infrastructure.Services
+{
+    public class PdfService : AzureServiceBase<string>, IPdfService
+    {
+        
+        public override string ConfigurationName => "legal-agreements";
+        public sealed override ILogger Logger { get; set; }
+
+        public PdfService(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public async Task<MemoryStream> SubsituteValuesForPdf(string fileName, Dictionary<string,string> valuesToSubsitute )
+        {
+            var pdfStream = await GetBlobDataFromAzure(ConfigurationName, fileName);
+
+            var licenseStream = await GetBlobDataFromAzure(ConfigurationName, "Aspose.Total.lic");
+            var license = new License();
+            license.SetLicense(licenseStream);
+
+            var pdfDocument = new Document(pdfStream);
+
+            foreach (var key in valuesToSubsitute)
+            {
+
+                var textFragmentAbsorber = new TextFragmentAbsorber(key.Key)
+                {
+                    TextSearchOptions = new TextSearchOptions(true)
+                };
+
+                pdfDocument.Pages[1].Accept(textFragmentAbsorber);
+
+                // Get the extracted text fragments
+                var textFragmentCollection = textFragmentAbsorber.TextFragments;
+
+                // Loop through the fragments
+                foreach (TextFragment textFragment in textFragmentCollection)
+                {
+                    // Update text and other properties
+                    textFragment.Text = key.Value;
+                    textFragment.TextState.FontStyle = FontStyles.Bold;
+                }
+            }
+            
+            var streamOutput = new MemoryStream();
+            pdfDocument.Save(streamOutput);
+
+            return streamOutput;
+        }
+    }
+
+ 
+}
