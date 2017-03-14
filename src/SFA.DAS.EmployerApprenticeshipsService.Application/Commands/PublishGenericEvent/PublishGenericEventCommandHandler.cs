fed34b9cbd4134fedaf8fcd3a43c6f3c34956a2a
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-03-14 11:29:35 +0000

    Added some extra logging

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/PublishGenericEvent/PublishGenericEventCommandHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Commands/PublishGenericEvent/PublishGenericEventCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using MediatR;
+using NLog;
 using SFA.DAS.Events.Api.Client;
 
 namespace SFA.DAS.EAS.Application.Commands.PublishGenericEvent
@@ -7,14 +8,18 @@ namespace SFA.DAS.EAS.Application.Commands.PublishGenericEvent
     public class PublishGenericEventCommandHandler : IAsyncRequestHandler<PublishGenericEventCommand, PublishGenericEventCommandResponse>
     {
         private readonly IEventsApi _eventsApi;
+        private readonly ILogger _logger;
 
-        public PublishGenericEventCommandHandler(IEventsApi eventsApi)
+        public PublishGenericEventCommandHandler(IEventsApi eventsApi, ILogger logger)
         {
             _eventsApi = eventsApi;
+            _logger = logger;
         }
 
         public async Task<PublishGenericEventCommandResponse> Handle(PublishGenericEventCommand command)
         {
+            _logger.Info($"Publishing Generic event of type {command.Event.Type}");
+
             await _eventsApi.CreateGenericEvent(command.Event);
 
             return new PublishGenericEventCommandResponse();
