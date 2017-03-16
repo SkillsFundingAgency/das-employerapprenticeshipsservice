c9f8dc0fe4a929f8bb17eb499f3f3ebd0503ea1d
Robin O'Connor <github@bytepointsoftware.com>
2017-03-16 10:29:06 +0000

    Marking obsolete parameter

--- src/SFA.DAS.EAS.Account.Api.Types/AccountDetailViewModel.cs
+++ src/SFA.DAS.EAS.Account.Api.Types/AccountDetailViewModel.cs
@@ -6,12 +6,14 @@ namespace SFA.DAS.EAS.Account.Api.Types
     public class AccountDetailViewModel : IAccountResource
     {
         public long AccountId { get; set; }
-        public string HashedAccountId { get; set; }
-        public string DasAccountId => HashedAccountId;
+        public string HashedAccountId { get; set; }        
         public string DasAccountName { get; set; }
         public DateTime DateRegistered { get; set; }
         public string OwnerEmail { get; set; }
         public ResourceList LegalEntities { get; set; }
         public ResourceList PayeSchemes { get; set; }
+        
+        [Obsolete]
+        public string DasAccountId => HashedAccountId;
     }
 }
