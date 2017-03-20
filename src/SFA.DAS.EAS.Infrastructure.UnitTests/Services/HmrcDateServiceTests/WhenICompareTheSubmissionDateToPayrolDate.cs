b17ad81dc7c98a6a3ec198a49718e2deba7b936b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-03-20 10:23:02 +0000

    Add HmrcDateService
    Added the HmrcDateService for determining when submission dates are
    within the payroll year. This is for making sure that end of year
    adjustments are processed correctly.

---
+++ src/SFA.DAS.EAS.Infrastructure.UnitTests/Services/HmrcDateServiceTests/WhenICompareTheSubmissionDateToPayrolDate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.EAS.Infrastructure.Services;
+
+namespace SFA.DAS.EAS.Infrastructure.UnitTests.Services.HmrcDateServiceTests
+{
+    public class WhenICompareTheSubmissionDateToPayrolDate
+    {
+        private HmrcDateService _hmrcDateService;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _hmrcDateService = new HmrcDateService();
+        }
+
+        [Test]
+        public void ThenIfTheSubmissionDateIsOutsideOfThePayrolDateThenFalseIsReturned()
+        {
+            //Arrange
+            var payroll = "16-17";
+            var submissionDate = new DateTime(2017,05,01);
+
+            //Act
+            var actual = _hmrcDateService.IsSubmissionDateInPayrollYear(payroll,submissionDate);
+                
+
+            //Assert
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void ThenIfTheSubmissionDateIsInsideOfThePayrolDateThenTrueIsReturned()
+        {
+            //Arrange
+            var payroll = "16-17";
+            var submissionDate = new DateTime(2017, 04, 30);
+
+            //Act
+            var actual = _hmrcDateService.IsSubmissionDateInPayrollYear(payroll, submissionDate);
+
+            //Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestCase("2016-04-30",false)]
+        [TestCase("2017-04-30",false)]
+        [TestCase("2017-05-01",true)]
+        [TestCase("2018-04-30",true)]
+        public void ThenIfTheSubmissionDateIsGreaterThanThePayrollYearThenTrueIsReturned(string submissionDate, bool expectedResult)
+        {
+            //Arrange
+            var payroll = "16-17";
+            var expectedDate = DateTime.Parse(submissionDate);
+
+            //Act
+            var actual = _hmrcDateService.IsSubmissionEndOfYearAdjustment(payroll, expectedDate);
+
+            //Assert
+            Assert.AreEqual(expectedResult,actual);
+        }
+        
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/HmrcDateService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.Infrastructure.Services
+{
+    public class HmrcDateService
+    {
+        public bool IsSubmissionDateInPayrollYear(string payrollYear, DateTime submissionDate)
+        {
+            DateTime endDate;
+            var startDate = GetDateRange(payrollYear, out endDate);
+
+            return submissionDate >= startDate && submissionDate <= endDate;
+        }
+        
+        public bool IsSubmissionAdjustment(string payrollYear, DateTime submissionDate)
+        {
+            DateTime endDate;
+            GetDateRange(payrollYear, out endDate);
+
+            return submissionDate >= endDate;
+        }
+
+        private static DateTime GetDateRange(string payrollYear, out DateTime endDate)
+        {
+            var payrollSplit = payrollYear.Split('-');
+
+            var startDate = new DateTime(Convert.ToInt32("20" + payrollSplit[0]), 5, 1);
+            endDate = new DateTime(Convert.ToInt32("20" + payrollSplit[1]), 4, 30, 23, 59, 59);
+            return startDate;
+        }
+    }
+}
