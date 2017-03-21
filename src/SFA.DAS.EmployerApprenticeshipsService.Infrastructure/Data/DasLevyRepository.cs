487fb58be5357b7231848f0cd332720c12142f41
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-03-21 08:46:56 +0000

    Update CreateDeclaration for extra params
    Updated the CreateDeclaration stored procedure to also set the
    DateCeased, InactiveFrom, InactiveTo, EndOfYearAdjustment and
    EndOfYearAdjustmentAmount

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/DasLevyRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/DasLevyRepository.cs
@@ -59,6 +59,21 @@ namespace SFA.DAS.EAS.Infrastructure.Data
                 parameters.Add("@SubmissionDate", dasDeclaration.SubmissionDate, DbType.DateTime);
                 parameters.Add("@SubmissionId", dasDeclaration.Id, DbType.String);
                 parameters.Add("@CreatedDate", DateTime.UtcNow, DbType.DateTime);
+                if (dasDeclaration.DateCeased.HasValue && dasDeclaration.DateCeased != DateTime.MinValue)
+                {
+                    parameters.Add("@DateCeased", dasDeclaration.DateCeased, DbType.DateTime);
+                }
+                if (dasDeclaration.InactiveFrom.HasValue && dasDeclaration.InactiveFrom != DateTime.MinValue)
+                {
+                    parameters.Add("@InactiveFrom", dasDeclaration.InactiveFrom, DbType.DateTime);
+                }
+                if (dasDeclaration.InactiveTo.HasValue && dasDeclaration.InactiveTo != DateTime.MinValue)
+                {
+                    parameters.Add("@InactiveTo", dasDeclaration.InactiveTo, DbType.DateTime);
+                }
+                
+                parameters.Add("@EndOfYearAdjustment", dasDeclaration.EndOfYearAdjustment,DbType.Boolean);
+                parameters.Add("@EndOfYearAdjustmentAmount", dasDeclaration.EndOfYearAdjustmentAmount, DbType.Decimal);
                 
                 return await c.ExecuteAsync(
                     sql: "[employer_financial].[CreateDeclaration]",
