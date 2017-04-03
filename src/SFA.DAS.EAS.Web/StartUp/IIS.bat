2c5935a6f2bf666262884ae1715521fe5bee02fd
Ewan Noble <e.noble@live.co.uk>
2017-04-03 11:19:14 +0100

    token service cert install script

--- src/SFA.DAS.EAS.Web/StartUp/IIS.bat
+++ src/SFA.DAS.EAS.Web/StartUp/IIS.bat
@@ -1,5 +1,5 @@
 
 PowerShell.exe -NonInteractive -ExecutionPolicy Unrestricted "StartUp\IIS.ps1" >> log.txt
-
+PowerShell.exe -NonInteractive -ExecutionPolicy Unrestricted "StartUp\TokenServiceCertInstall.PS1" >> log2.txt
 
 EXIT /B 0
