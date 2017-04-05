88bc0b8b08661a5229c67b7a8450719d9eb1cda3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-05 11:42:23 +0100

    Implement Cookie Service
    Added new class for CookieStorageService and added tests for calling
    the package for cookie data manipulation.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/CookieStorageService.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using SFA.DAS.CookieService;
+
+namespace SFA.DAS.EAS.Infrastructure.Services
+{
+    public class CookieStorageService<T>
+    {
+        private readonly ICookieService<T> _cookieService;
+        private readonly HttpContextBase _httpContextBase;
+
+        public CookieStorageService(ICookieService<T> cookieService, HttpContextBase httpContextBase)
+        {
+            _cookieService = cookieService;
+            _httpContextBase = httpContextBase;
+        }
+
+        public void Create(T item, string cookieName, int expiryDays = 1)
+        {
+            _cookieService.Create(_httpContextBase, cookieName, item, expiryDays);
+        }
+
+        public T Get(string cookieName)
+        {
+            return _cookieService.Get(_httpContextBase, cookieName);
+        }
+
+        public void Delete(string cookieName)
+        {
+            _cookieService.Delete(_httpContextBase, cookieName);
+        }
+
+        public void Update(string cookieName, T item)
+        {
+            _cookieService.Update(_httpContextBase, cookieName, item);
+        }
+    }
+}
