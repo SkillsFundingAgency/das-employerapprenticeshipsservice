08e411ab54a4f2f5df74d7622df0000bd376047f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-05 10:14:42 +0100

    Cache the response from HMRC
    The fraction last calculated date is now cached so we dont make
    multiple calls to the API

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetEnglishFractionsUpdateRequired/WhenGettingIfEnglishFractionsUpdateIsRequired.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetEnglishFractionsUpdateRequired/WhenGettingIfEnglishFractionsUpdateIsRequired.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EAS.Application.Queries.GetEnglishFractionUpdateRequired;
-using SFA.DAS.EAS.Domain.Data;
 using SFA.DAS.EAS.Domain.Data.Repositories;
 using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Infrastructure.Caching;
 
 namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetEnglishFractionsUpdateRequired
 {
@@ -14,13 +14,20 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetEnglishFractionsUpdateReq
         private GetEnglishFractionsUpdateRequiredQueryHandler _handler;
         private Mock<IHmrcService> _hmrcService;
         private Mock<IEnglishFractionRepository> _englishFractionRepository;
+        private Mock<ICacheProvider> _cacheProvider;
 
         [SetUp]
         public void Arrange()
         {
             _hmrcService = new Mock<IHmrcService>();
             _englishFractionRepository = new Mock<IEnglishFractionRepository>();
-            _handler = new GetEnglishFractionsUpdateRequiredQueryHandler(_hmrcService.Object, _englishFractionRepository.Object);    
+
+            _cacheProvider = new Mock<ICacheProvider>();
+            _cacheProvider.SetupSequence(c => c.Get<DateTime?>("HmrcFractionLastCalculatedDate"))
+                .Returns(null)
+                .Returns(new DateTime());
+
+            _handler = new GetEnglishFractionsUpdateRequiredQueryHandler(_hmrcService.Object, _englishFractionRepository.Object, _cacheProvider.Object);    
         }
 
         [Test]
@@ -51,6 +58,18 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetEnglishFractionsUpdateReq
 
             //Assert
             Assert.IsFalse(result.UpdateRequired);
+        }
+
+        [Test]
+        public async Task ThenTheFractionLastCaclulatedDateIsReadFromTheCacheOnSubsequentReads()
+        {
+            //Act
+            await _handler.Handle(new GetEnglishFractionUpdateRequiredRequest());
+            await _handler.Handle(new GetEnglishFractionUpdateRequiredRequest());
+
+            //Assert
+            _hmrcService.Verify(x=>x.GetLastEnglishFractionUpdate(), Times.Once);
+            _cacheProvider.Verify(x=>x.Set("HmrcFractionLastCalculatedDate",It.IsAny<DateTime>(),It.Is<TimeSpan>(c=>c.Days.Equals(1))));
         }
     }
 }
--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEnglishFractionUpdateRequired/GetEnglishFractionsUpdateRequiredQueryHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetEnglishFractionUpdateRequired/GetEnglishFractionsUpdateRequiredQueryHandler.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MediatR;
-using SFA.DAS.EAS.Domain.Data;
 using SFA.DAS.EAS.Domain.Data.Repositories;
 using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Infrastructure.Caching;
 
 namespace SFA.DAS.EAS.Application.Queries.GetEnglishFractionUpdateRequired
 {
@@ -10,23 +11,35 @@ namespace SFA.DAS.EAS.Application.Queries.GetEnglishFractionUpdateRequired
     {
         private readonly IHmrcService _hmrcService;
         private readonly IEnglishFractionRepository _englishFractionRepository;
+        private readonly ICacheProvider _cacheProvider;
 
-        public GetEnglishFractionsUpdateRequiredQueryHandler(IHmrcService hmrcService, IEnglishFractionRepository englishFractionRepository)
+        public GetEnglishFractionsUpdateRequiredQueryHandler(IHmrcService hmrcService, IEnglishFractionRepository englishFractionRepository, ICacheProvider cacheProvider)
         {
             _hmrcService = hmrcService;
             _englishFractionRepository = englishFractionRepository;
+            _cacheProvider = cacheProvider;
         }
 
         public async Task<GetEnglishFractionUpdateRequiredResponse> Handle(GetEnglishFractionUpdateRequiredRequest message)
         {
-            var hmrcLatestUpdateDate = await _hmrcService.GetLastEnglishFractionUpdate();
+            var hmrcLatestUpdateDate = _cacheProvider.Get<DateTime?>("HmrcFractionLastCalculatedDate");
+
+            if (hmrcLatestUpdateDate == null)
+            {
+                hmrcLatestUpdateDate = await _hmrcService.GetLastEnglishFractionUpdate();
+
+                if (hmrcLatestUpdateDate != null)
+                {
+                    _cacheProvider.Set("HmrcFractionLastCalculatedDate", hmrcLatestUpdateDate.Value,new TimeSpan(1,0,0,0));
+                }
+            }
 
             var levyLatestUpdateDate = await _englishFractionRepository.GetLastUpdateDate();
 
             return new GetEnglishFractionUpdateRequiredResponse
             {
                 UpdateRequired = hmrcLatestUpdateDate > levyLatestUpdateDate,
-                DateCalculated = hmrcLatestUpdateDate
+                DateCalculated = hmrcLatestUpdateDate.Value
             };
         }
     }
