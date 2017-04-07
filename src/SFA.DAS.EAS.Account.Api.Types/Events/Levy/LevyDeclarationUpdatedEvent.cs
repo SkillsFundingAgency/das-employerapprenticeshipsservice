b3585ab531948effc8c4a538a75b501ac28409fc
Michael Young <mick.young1981@googlemail.com>
2017-04-07 12:21:20 +0100

    Added levy declaration updated event

---
+++ src/SFA.DAS.EAS.Account.Api.Types/Events/Levy/LevyDeclarationUpdatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.EAS.Account.Api.Types.Events.Levy
+{
+    public class LevyDeclarationUpdatedEvent
+    {
+        public string LevyDeclarationsResourceUri { get; set; }
+        public string TransactionsResourceUri { get; set; }
+    }
+}
