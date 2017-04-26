6fdec53c2040d9e0e5c0985cf8fe68dafa6fcf3c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-04-26 16:00:30 +0100

    Correct status code for ServiceUnavailable
    Corrected status code for ServiceUnavailableException and also added
    the InternalServerErrorException.

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Http/InternalServerErrorException.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.EAS.Domain.Http
+{
+    public class InternalServerErrorException : HttpException
+    {
+        public InternalServerErrorException()
+            : base(500, "Internal server error")
+        {
+        }
+    }
+}
