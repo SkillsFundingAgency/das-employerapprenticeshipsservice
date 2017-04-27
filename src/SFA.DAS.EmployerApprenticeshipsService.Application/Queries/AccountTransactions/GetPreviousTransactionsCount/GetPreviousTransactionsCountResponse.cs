654f89698ad5ea1f1737da7de854ec58f5554e71
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-04-27 14:19:04 +0100

    Created handler for getting count of previous transactions from a given date

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/AccountTransactions/GetPreviousTransactionsCount/GetPreviousTransactionsCountResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.EAS.Application.Queries.AccountTransactions.GetPreviousTransactionsCount
+{
+    public class GetPreviousTransactionsCountResponse
+    {
+        public int Count { get; set; }
+    }
+}
