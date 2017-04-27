470fd3b34a9afac14724a672f0b71ad83bbddeb6
James King <hello@jamesmichaelking.com>
2017-04-27 13:51:45 +0100

    Added isFirstMonth setting used to hide 'previous month' links when viewing April 2017

--- src/SFA.DAS.EAS.Web/ViewModels/TransactionViewResultViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/TransactionViewResultViewModel.cs
@@ -11,5 +11,6 @@ namespace SFA.DAS.EAS.Web.ViewModels
         public int Year { get; set; }
 
         public bool IsLatestMonth => DateTime.Now.Year == Year && DateTime.Now.Month == Month;
+        public bool IsFirstMonth => DateTime.Now.Year == 2017 && DateTime.Now.Month == 4;
     }
 }
