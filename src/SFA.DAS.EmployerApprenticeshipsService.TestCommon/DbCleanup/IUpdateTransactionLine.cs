d8461fa280cf86fbbc43a489fd1b057ad77a413c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-05 13:46:15 +0100

    Add new repository method for Transactionline
    Added a new repository method for updating the transaction line
    information for the acceptance tests

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.TestCommon/DbCleanup/IUpdateTransactionLine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.TestCommon.DbCleanup
+{
+    public interface IUpdateTransactionLine
+    {
+        Task Execute(long submissionId, DateTime createdDate);
+    }
+}
