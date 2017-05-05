ac2ae1597b3f7d16f343b6edd0d51d99424ade09
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-05 13:41:13 +0100

    Add new procedure for Acceptance Tests
    Added new procedure to update the DateCreated on a submission for
    testing purposes

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/UpdateTransactionLineDate_BySubmissionId.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [employer_financial].[UpdateTransactionLineDate_BySubmissionId]
+	@submissionId bigint,
+	@createdDate datetime
+AS
+	UPDATE employer_financial.TransactionLine 
+	set DateCreated = @createdDate 
+	where SubmissionId = @submissionId
