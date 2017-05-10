b0b4af678597e599befde5bf5cd40a254a2d07fc
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-05-10 10:40:24 +0100

    Adjust transaction script to populate co-investment values

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/ProcessPaymentDataTransactions.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/ProcessPaymentDataTransactions.sql
@@ -5,32 +5,37 @@ AS
 --- Process Levy Payments ---
 INSERT INTO [employer_financial].TransactionLine
 select mainUpdate.* from
-	(
-	select 
-			x.AccountId as AccountId,
-			DATEFROMPARTS(DatePart(yyyy,GETDATE()),DatePart(MM,GETDATE()),DATEPART(dd,GETDATE())) as DateAdded,
-			null as submissionId,
-			Max(pe.CompletionDateTime) as TransactionDate,
-			3 as TransactionType,
-			null as LevyDeclared,
-			Sum(x.Amount) * -1 Amount,
-			null as empref,
-			PeriodEnd,
-			UkPrn,
-			0 as SfaCoInvestmentAmount,
-			0 as EmployerCoInvestmentAmount
-		FROM 
-			[employer_financial].[Payment] x
-		inner join
-			[employer_financial].[PeriodEnd] pe on pe.PeriodEndId = x.PeriodEnd
-		where
-			fundingsource = 1
-		Group by
-			UkPrn,PeriodEnd,accountId
-	) mainUpdate
-	inner join (
-		select accountid,ukprn,periodend from [employer_financial].Payment where FundingSource = 1
-	EXCEPT
-		select accountid,ukprn,periodend from [employer_financial].transactionline where TransactionType = 3
-	) dervx on dervx.accountId = mainUpdate.accountId and dervx.PeriodEnd = mainUpdate.PeriodEnd and dervx.Ukprn = mainUpdate.ukprn
+    (
+    select 
+            x.AccountId as AccountId,
+            DATEFROMPARTS(DatePart(yyyy,GETDATE()),DatePart(MM,GETDATE()),DATEPART(dd,GETDATE())) as DateAdded,
+            null as submissionId,
+            Max(pe.CompletionDateTime) as TransactionDate,
+            3 as TransactionType,
+            null as LevyDeclared,
+            Sum(x.Amount) * -1 Amount,
+            Sum(ISNULL(pco.Amount, 0)) * -1 as SfaCoInvestmentAmount,
+            Sum(ISNULL(pci.Amount, 0)) * -1 as EmployerCoInvestmentAmount,
+            null as empref,
+            x.PeriodEnd,
+            x.UkPrn
+        FROM 
+            employer_financial.[Payment] x
+		inner join [employer_financial].[PeriodEnd] pe 
+				on pe.PeriodEndId = x.PeriodEnd
+        left join [employer_financial].[Payment] p 
+				on p.PeriodEnd = pe.PeriodEndId and p.PaymentId = x.PaymentId and p.FundingSource = 1
+        left join [employer_financial].[payment] pco 
+				on pco.PeriodEnd = pe.PeriodEndId and pco.PaymentId = x.PaymentId and pco.FundingSource = x.FundingSource and pco.FundingSource = 2 
+        left join [employer_financial].[payment] pci 
+				on pci.PeriodEnd = pe.PeriodEndId and pci.PaymentId = x.PaymentId  and pci.FundingSource = x.FundingSource and pci.FundingSource = 3 
+        Group by
+            x.UkPrn,x.PeriodEnd,x.accountId
+    ) mainUpdate
+    inner join (
+        select accountid,ukprn,periodend from [employer_financial].Payment where FundingSource = 1        
+    EXCEPT
+        select accountid,ukprn,periodend from [employer_financial].transactionline where TransactionType = 3
+    ) dervx on dervx.accountId = mainUpdate.accountId and dervx.PeriodEnd = mainUpdate.PeriodEnd and dervx.Ukprn = mainUpdate.ukprn
+	
 
