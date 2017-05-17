29a40e3c5be540cb8f58ef864a7155b39a2b43e3
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-05-17 11:57:30 +0100

    database build fixes

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetPaymentDetail_ByAccountProviderCourseAndDateRange.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetPaymentDetail_ByAccountProviderCourseAndDateRange.sql
@@ -23,13 +23,13 @@ SELECT
 	,MAX(meta.ApprenticeNINumber) as ApprenticeNINumber	
 	,(SUM(pays3.Amount) * -1) as SfaCoInvestmentAmount
 	,(SUM(pays2.Amount) * -1) as EmployerCoInvestmentAmount	
-  FROM [SFA.DAS.EAS.Employer_Financial].[employer_financial].[Payment] p
-  inner JOIN [SFA.DAS.EAS.Employer_Financial].[employer_financial].[PaymentMetaData] meta ON p.PaymentMetaDataId = meta.Id
+  FROM [employer_financial].[Payment] p
+  inner JOIN [employer_financial].[PaymentMetaData] meta ON p.PaymentMetaDataId = meta.Id
   inner join (select PeriodEnd,AccountId,ukprn, TransactionDate, DateCreated from employer_financial.TransactionLine where DateCreated >= @fromDate AND 
         DateCreated <= @toDate) dervx on dervx.AccountId = p.AccountId and dervx.PeriodEnd = p.PeriodEnd and dervx.Ukprn = p.Ukprn
-  left join [SFA.DAS.EAS.Employer_Financial].[employer_financial].[Payment] pays1 on pays1.AccountId = p.AccountId and pays1.Ukprn = p.Ukprn and pays1.FundingSource = 1 and pays1.PaymentMetaDataId = meta.id
-  left join [SFA.DAS.EAS.Employer_Financial].[employer_financial].[Payment] pays2 on pays2.AccountId = p.AccountId and pays2.Ukprn = p.Ukprn and pays2.FundingSource = 2 and pays2.PaymentMetaDataId = meta.id
-  left join [SFA.DAS.EAS.Employer_Financial].[employer_financial].[Payment] pays3 on pays3.AccountId = p.AccountId and pays3.Ukprn = p.Ukprn and pays3.FundingSource = 3 and pays3.PaymentMetaDataId = meta.id
+  left join [employer_financial].[Payment] pays1 on pays1.AccountId = p.AccountId and pays1.Ukprn = p.Ukprn and pays1.FundingSource = 1 and pays1.PaymentMetaDataId = meta.id
+  left join [employer_financial].[Payment] pays2 on pays2.AccountId = p.AccountId and pays2.Ukprn = p.Ukprn and pays2.FundingSource = 2 and pays2.PaymentMetaDataId = meta.id
+  left join [employer_financial].[Payment] pays3 on pays3.AccountId = p.AccountId and pays3.Ukprn = p.Ukprn and pays3.FundingSource = 3 and pays3.PaymentMetaDataId = meta.id
   where 
   p.AccountId = @accountid AND
   p.Ukprn = @ukprn AND
