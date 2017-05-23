a71b20a412affba4a42751a6b8f111ade2464a0e
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-05-23 14:18:51 +0100

    Removed use of obsolete fields from AprenticeshipInfoService

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Time/Duration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.EAS.Domain.Models.Time
-{
-    public class Duration
-    {
-        public int From { get; set; }
-        public int To { get; set; }
-        public string Unit { get; set; }
-    }
-}
