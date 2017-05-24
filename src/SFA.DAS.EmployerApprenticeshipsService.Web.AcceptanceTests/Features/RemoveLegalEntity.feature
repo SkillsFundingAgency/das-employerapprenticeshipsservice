8e4a2f6aef21a9022aa999e7cfc3bdea1bc00a41
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-24 08:41:10 +0100

    Add feature file for remove org

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.AcceptanceTests/Features/RemoveLegalEntity.feature
@@ -0,0 +1,14 @@
+﻿Feature: RemoveLegalEntity
+	In order to remove connected organisations
+	As a account owner
+	I want to be able to remove legal entities from my account
+
+@mytag
+Scenario Outline: Remove legal entity
+	Given I am an account "<account_role>"
+	Then I "<result>" remove a legal entity
+	Examples:
+	| account_role | result   |
+	| Owner        | can     |
+	| Transactor   | cannot	|
+	| Viewer       | cannot	|
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.AcceptanceTests/Steps/RemoveLegalEntity/RemoveLegalEntitySteps.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.TestCommon.DependencyResolution;
+using SFA.DAS.EAS.Web.Authentication;
+using SFA.DAS.Events.Api.Client;
+using SFA.DAS.Messaging;
+using StructureMap;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EAS.Web.AcceptanceTests.Steps.RemoveLegalEntity
+{
+    [Binding]
+    public class RemoveLegalEntitySteps
+    {
+        private static IContainer _container;
+        private static Mock<IMessagePublisher> _messagePublisher;
+        private static Mock<IOwinWrapper> _owinWrapper;
+        private string _hashedAccountId;
+        private static Mock<ICookieStorageService<EmployerAccountData>> _cookieService;
+        private static Mock<IEventsApi> _eventsApi;
+
+        [BeforeScenario()]
+        public void Arrange()
+        {
+            _messagePublisher = new Mock<IMessagePublisher>();
+            _owinWrapper = new Mock<IOwinWrapper>();
+            _cookieService = new Mock<ICookieStorageService<EmployerAccountData>>();
+            _eventsApi = new Mock<IEventsApi>();
+
+            _container = IoC.CreateContainer(_messagePublisher, _owinWrapper, _cookieService, _eventsApi);
+        }
+
+        [Then(@"I ""(.*)"" remove a legal entity")]
+        public void ThenIRemoveALegalEntity(string p0)
+        {
+            var hashedId = ScenarioContext.Current["HashedAccountId"].ToString();
+            var userId = ScenarioContext.Current["ExternalUserId"].ToString();
+
+            
+        }
+
+    }
+}
