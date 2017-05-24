474a20bfbae5c7247f1a1f44ab1d3b080d90e90c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-24 16:14:11 +0100

    WIP implement orchestrator

---
+++ src/SFA.DAS.EAS.Web/ViewModels/Organisation/LegalAgreementsToRemoveViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.EAS.Web.ViewModels.Organisation
+{
+    public class LegalAgreementsToRemoveViewModel : ViewModelBase
+    {
+        
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/EmployerAgreementOrchestratorTests/WhenIRemoveAnAgreement.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NLog;
+using NUnit.Framework;
+using SFA.DAS.EAS.Domain.Configuration;
+using SFA.DAS.EAS.Web.Orchestrators;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Orchestrators.EmployerAgreementOrchestratorTests
+{
+    public class WhenIRemoveAnAgreement
+    {
+        private Mock<IMediator> _mediator;
+        private Mock<ILogger> _logger;
+        private EmployerApprenticeshipsServiceConfiguration _configuration;
+        private EmployerAgreementOrchestrator _orchestrator;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mediator = new Mock<IMediator>();
+            _logger = new Mock<ILogger>();
+
+            _configuration = new EmployerApprenticeshipsServiceConfiguration();
+
+            _orchestrator = new EmployerAgreementOrchestrator(_mediator.Object, _logger.Object, _configuration);
+        }
+
+        [Test]
+        public async Task ThenTheMediatorIsCalledWithTheListOfOrganisations()
+        {
+            //Arrange
+            var hashedAgreementId = "123RFAD";
+
+            //Act
+            await _orchestrator.GetLegalAgreementsToRemove(hashedAgreementId);
+        }
+
+        [Test]
+        public async Task ThenIfAnInvalidRequestExceptionIsThrownTheOrchestratorResponseContainsTheError()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenIfAUnauthroizedAccessExceptionIsThrownThenTheOrchestratorResponseShowsAccessDenied()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenTheValuesAreReturnedInTheResponseFromTheMediatorCall()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenWhenIRemoveAnAgreementTheMediatorCallIsMade()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenIfAnInvalidRequestExceptionIsThrownWhenRemovingTheAgreementItIsReturnedInTheOrchestratorResponse()
+        {
+            
+        }
+
+        [Test]
+        public async Task ThenIfAnUnauthorizedAccessExceptionIsThrownWhenRemovingTheAgreementItIsReturnedInTheOrchestratorResponse()
+        {
+            
+        }
+    }
+}
