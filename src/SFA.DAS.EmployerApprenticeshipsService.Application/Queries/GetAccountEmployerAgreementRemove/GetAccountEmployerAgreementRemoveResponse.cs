88a0f60fbb67987a54db569dc5d312b6549c7408
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-05-25 08:13:52 +0100

    Implement Query Handler to get single Remove Org

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountEmployerAgreementRemove/GetAccountEmployerAgreementRemoveResponse.cs
@@ -0,0 +1,9 @@
+using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
+
+namespace SFA.DAS.EAS.Application.Queries.GetAccountEmployerAgreementRemove
+{
+    public class GetAccountEmployerAgreementRemoveResponse
+    {
+        public RemoveEmployerAgreementView Agreement { get; set; }
+    }
+}
