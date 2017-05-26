0618f033ea8c1d28824536599cebf2f4de6d7891
chrisfoster76 <chrisfoster186@googlemail.com>
2017-05-26 15:45:23 +0100

    Dcm 471 filter list (#788)
    * Packages update
    Tests
    Filters implementation
    
    * Assets added
    
    * markup cleanup
    
    * Assets and markup added.
    
    * checkbox position test
    
    * Filter buttons styles added
    
    * Updates packages
    Adds No results search text

---
+++ src/SFA.DAS.EAS.Web/ViewModels/ManageApprenticeships/ApprenticeshipFiltersViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.EAS.Web.ViewModels.ManageApprenticeships
+{
+    public class ApprenticeshipFiltersViewModel
+    {
+        public ApprenticeshipFiltersViewModel()
+        {
+            Status = new List<string>();
+            RecordStatus = new List<string>();
+            Provider = new List<string>();
+            Course = new List<string>();
+
+            ApprenticeshipStatusOptions = new List<KeyValuePair<string, string>>();
+            TrainingCourseOptions = new List<KeyValuePair<string, string>>();
+            RecordStatusOptions = new List<KeyValuePair<string, string>>();
+            ProviderOrganisationOptions = new List<KeyValuePair<string, string>>();
+        }
+
+        public List<KeyValuePair<string, string>> ApprenticeshipStatusOptions { get; set; }
+        public List<KeyValuePair<string, string>> TrainingCourseOptions { get; set; }
+        public List<KeyValuePair<string, string>> RecordStatusOptions { get; set; }
+        public List<KeyValuePair<string, string>> ProviderOrganisationOptions { get; set; }
+
+        public List<string> Status { get; set; }
+        public List<string> RecordStatus { get; set; }
+        public List<string> Provider { get; set; }
+        public List<string> Course { get; set; }
+    }
+}
