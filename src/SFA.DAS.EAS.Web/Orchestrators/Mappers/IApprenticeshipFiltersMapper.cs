f42d66cd40cebdb7694e9a38504f9b9c34f7ab0c
chrisfoster76 <chrisfoster186@googlemail.com>
2017-05-26 15:45:23 +0100

    Dcm 471 filter list (#788)
    * Packages update
    Tests
    Filters implementation
    
    * Assets added
    
    * markup cleanup
    
    * Assets and markup added.
    
    * checkbox position test
    
    * Filter buttons styles added
    
    * Updates packages
    Adds No results search text

---
+++ src/SFA.DAS.EAS.Web/Orchestrators/Mappers/IApprenticeshipFiltersMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SFA.DAS.Commitments.Api.Types.Apprenticeship;
+using SFA.DAS.EAS.Web.ViewModels.ManageApprenticeships;
+
+namespace SFA.DAS.EAS.Web.Orchestrators.Mappers
+{
+    public interface IApprenticeshipFiltersMapper
+    {
+        ApprenticeshipSearchQuery MapToApprenticeshipSearchQuery(ApprenticeshipFiltersViewModel filters);
+        ApprenticeshipFiltersViewModel Map(Facets facets);
+    }
+}
