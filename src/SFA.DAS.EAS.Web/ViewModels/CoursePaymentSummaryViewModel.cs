77d0cc993ab8509cc19d8c31d4c87fc23047f2ab
Robin O'Connor <github@bytepointsoftware.com>
2017-05-31 10:50:16 +0100

    added Pathway Name to view model

--- src/SFA.DAS.EAS.Web/ViewModels/CoursePaymentSummaryViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/CoursePaymentSummaryViewModel.cs
@@ -6,6 +6,7 @@ namespace SFA.DAS.EAS.Web.ViewModels
     {
         public string CourseName { get; set; }
         public int? CourseLevel { get; set; }
+        public string PathwayName { get; set; }
         public DateTime? CourseStartDate { get; set; }
         public decimal LevyPaymentAmount { get; set; }
         public decimal SFACoInvestmentAmount { get; set; }
