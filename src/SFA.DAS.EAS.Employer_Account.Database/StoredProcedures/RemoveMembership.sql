113d8895ac72d9b9642b164755663e49d9b1193a
Christopher Foster <Christopher Foster>
2017-06-02 09:11:00 +0100

    Adds RemoveMembership proc

---
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/RemoveMembership.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [employer_account].[RemoveMembership]
+(
+	@UserId BIGINT,
+	@AccountId BIGINT
+)
+AS
+BEGIN
+
+	DELETE FROM [employer_account].[Membership]
+	WHERE AccountId = @AccountId AND UserId = @UserId;
+
+	DELETE s
+	FROM [employer_account].UserLegalEntitySettings s
+	join [employer_account].AccountEmployerAgreement a on a.EmployerAgreementId = s.EmployerAgreementId
+	where s.UserId = @UserId and a.AccountId = @AccountId
+
+END
