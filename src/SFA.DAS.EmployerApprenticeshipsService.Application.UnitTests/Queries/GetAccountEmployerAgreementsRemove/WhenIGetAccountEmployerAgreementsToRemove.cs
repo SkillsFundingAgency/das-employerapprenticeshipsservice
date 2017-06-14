b8df49b8d3d1c39ba666fa70d2185e0080c08131
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-06-14 15:09:33 +0100

    Add check for active commitments
    Added check to so that we can remove agreements that don't have
    commitments

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetAccountEmployerAgreementsRemove/WhenIGetAccountEmployerAgreementsToRemove.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetAccountEmployerAgreementsRemove/WhenIGetAccountEmployerAgreementsToRemove.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.EAS.Application.Queries.GetAccountEmployerAgreementsRemove;
 using SFA.DAS.EAS.Application.Validation;
 using SFA.DAS.EAS.Domain.Data.Repositories;
@@ -17,12 +18,14 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetAccountEmployerAgreements
     {
         private Mock<IEmployerAgreementRepository> _repository;
         private Mock<IHashingService> _hashingService;
+        private Mock<IEmployerCommitmentApi> _commitmentsApi;
         public override GetAccountEmployerAgreementsRemoveRequest Query { get; set; }
         public override GetAccountEmployerAgreementsRemoveQueryHandler RequestHandler { get; set; }
         public override Mock<IValidator<GetAccountEmployerAgreementsRemoveRequest>> RequestValidator { get; set; }
 
         private const string ExpectedUserId = "456TGFD";
         private const string ExpectedHashedAccountId = "456TGFD";
+        private const string ExpectedLegalEntityCode = "98TYG123";
         private const long ExpectedAccountId = 98172938;
         private const long ExpectedAgreementId = 8765;
 
@@ -37,14 +40,17 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetAccountEmployerAgreements
             _repository.Setup(x => x.GetEmployerAgreementsToRemove(ExpectedAccountId))
                 .ReturnsAsync(new List<RemoveEmployerAgreementView>
                 {
-                    new RemoveEmployerAgreementView {Name = "test company", CanBeRemoved = true, Id = ExpectedAgreementId }   
+                    new RemoveEmployerAgreementView {Name = "test company", CanBeRemoved = true, Id = ExpectedAgreementId,LegalEntityCode = ExpectedLegalEntityCode}   
                 });
+
+            _commitmentsApi = new Mock<IEmployerCommitmentApi>();
+            _commitmentsApi.Setup(x => x.GetEmployerAccountSummary(ExpectedAccountId)).ReturnsAsync(new List<ApprenticeshipStatusSummary> {new ApprenticeshipStatusSummary()});
 
             _hashingService = new Mock<IHashingService>();
             _hashingService.Setup(x => x.DecodeValue(ExpectedHashedAccountId)).Returns(ExpectedAccountId);
             _hashingService.Setup(x => x.HashValue(ExpectedAgreementId)).Returns("45TGBF");
 
-            RequestHandler = new GetAccountEmployerAgreementsRemoveQueryHandler(RequestValidator.Object, _repository.Object,_hashingService.Object);
+            RequestHandler = new GetAccountEmployerAgreementsRemoveQueryHandler(RequestValidator.Object, _repository.Object,_hashingService.Object, _commitmentsApi.Object);
         }
 
         [Test]
@@ -85,6 +91,54 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetAccountEmployerAgreements
 
             //Assert
             Assert.IsFalse(actual.Agreements.First().CanBeRemoved);
+        }
+
+
+        [Test]
+        public async Task ThenIfThereIsOnlyOneResultReturnedTheApiIsNotCalled()
+        {
+            //Act
+            await RequestHandler.Handle(Query);
+
+            //Assert
+            _commitmentsApi.Verify(x=>x.GetEmployerAccountSummary(It.IsAny<long>()),Times.Never);
+        }
+
+        [Test]
+        public async Task ThenIfTheAgreementIsSignedThenTheApiIsCheckedForActiveCommitments()
+        {
+            //Arrange
+            _commitmentsApi.Setup(x => x.GetEmployerAccountSummary(ExpectedAccountId))
+                .ReturnsAsync(new List<ApprenticeshipStatusSummary> { new ApprenticeshipStatusSummary {LegalEntityIdentifier = ExpectedLegalEntityCode,ActiveCount = 1} });
+            _repository.Setup(x => x.GetEmployerAgreementsToRemove(ExpectedAccountId))
+                .ReturnsAsync(new List<RemoveEmployerAgreementView>
+                {
+                    new RemoveEmployerAgreementView {Name = "test company", Status = EmployerAgreementStatus.Pending, Id = ExpectedAgreementId,LegalEntityCode = "Another Code"},
+                    new RemoveEmployerAgreementView {Name = "test company", Status = EmployerAgreementStatus.Signed, Id = ExpectedAgreementId,LegalEntityCode = ExpectedLegalEntityCode}
+                });
+
+            //Act
+            var actual = await RequestHandler.Handle(Query);
+            Assert.AreEqual(1, actual.Agreements.Count(c=>c.CanBeRemoved));
+            _commitmentsApi.Verify(x => x.GetEmployerAccountSummary(ExpectedAccountId), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenIfTheAgreementIsSignedAndNoActiceCommitmentsTheAgreementCanBeRemoved()
+        {
+            //Arrange
+            _commitmentsApi.Setup(x => x.GetEmployerAccountSummary(ExpectedAccountId))
+                .ReturnsAsync(new List<ApprenticeshipStatusSummary> { new ApprenticeshipStatusSummary { LegalEntityIdentifier = ExpectedLegalEntityCode, CompletedCount = 1 } });
+            _repository.Setup(x => x.GetEmployerAgreementsToRemove(ExpectedAccountId))
+                .ReturnsAsync(new List<RemoveEmployerAgreementView>
+                {
+                    new RemoveEmployerAgreementView {Name = "test company", Status = EmployerAgreementStatus.Pending, Id = ExpectedAgreementId,LegalEntityCode = "Another Code"},
+                    new RemoveEmployerAgreementView {Name = "test company", Status = EmployerAgreementStatus.Signed, Id = ExpectedAgreementId,LegalEntityCode = ExpectedLegalEntityCode}
+                });
+
+            //Act
+            var actual = await RequestHandler.Handle(Query);
+            Assert.IsTrue(actual.Agreements.All(c => c.CanBeRemoved));
         }
 
         [Test]
