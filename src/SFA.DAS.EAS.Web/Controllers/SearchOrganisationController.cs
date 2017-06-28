bd4be57fa809df6bde27b99f49a7c82144d65fac
Michael Young <mick.young1981@googlemail.com>
2017-06-28 09:59:48 +0100

    Added error message when search term has not been provided.

--- src/SFA.DAS.EAS.Web/Controllers/SearchOrganisationController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/SearchOrganisationController.cs
@@ -1,10 +1,12 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using AutoMapper;
 using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Domain.Models.Account;
 using SFA.DAS.EAS.Domain.Models.Organisation;
+using SFA.DAS.EAS.Domain.Models.ReferenceData;
 using SFA.DAS.EAS.Web.Authentication;
 using SFA.DAS.EAS.Web.Orchestrators;
 using SFA.DAS.EAS.Web.ViewModels;
@@ -40,15 +42,38 @@ namespace SFA.DAS.EAS.Web.Controllers
             return View("SearchForOrganisation");
         }
 
+        [HttpPost]
+        [Route("{HashedAccountId}/organisations/search", Order = 0)]
+        [Route("organisations/search", Order = 1)]
+        public ActionResult SearchForOrganisation(string hashedAccountId, string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                var model = CreateSearchTermValidationErrorModel();
+                return View("SearchForOrganisation", model);
+            }
+
+            return RedirectToAction("SearchForOrganisationResults", new { hashedAccountId, searchTerm });
+        }
+
         [Route("{HashedAccountId}/organisations/search/results", Order = 0)]
         [Route("organisations/search/results", Order = 1)]
         public async Task<ActionResult> SearchForOrganisationResults(string hashedAccountId, string searchTerm, int pageNumber = 1, OrganisationType? organisationType = null)
         {
-            var model = await _orchestrator.SearchOrganisation(searchTerm, pageNumber, organisationType, hashedAccountId, OwinWrapper.GetClaimValue(@"sub"));
+            OrchestratorResponse<SearchOrganisationViewModel> model;
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                var viewModel = new SearchOrganisationViewModel { Results = new PagedResponse<OrganisationDetailsViewModel>() };
+                model = CreateSearchTermValidationErrorModel(viewModel);
+            }
+            else
+            {
+                model = await _orchestrator.SearchOrganisation(searchTerm, pageNumber, organisationType, hashedAccountId, OwinWrapper.GetClaimValue(@"sub"));
+            }
 
             return View("SearchForOrganisationResults", model);
         }
-        
+
         [HttpPost]
         [Route("{HashedAccountId}/organisations/select", Order = 0)]
         [Route("organisations/select", Order = 1)]
@@ -75,6 +100,26 @@ namespace SFA.DAS.EAS.Web.Controllers
             CreateOrganisationCookieData(response);
 
             return RedirectToAction("GatewayInform", "EmployerAccount");
+        }
+
+        private OrchestratorResponse<T> CreateSearchTermValidationErrorModel<T>(T data)
+        {
+            var model = new OrchestratorResponse<T> { Data = data };
+            SetSearchTermValidationModelProperties(model);
+            return model;
+        }
+
+        private OrchestratorResponse CreateSearchTermValidationErrorModel()
+        {
+            var model = new OrchestratorResponse();
+            SetSearchTermValidationModelProperties(model);
+            return model;
+        }
+
+        private static void SetSearchTermValidationModelProperties(OrchestratorResponse model)
+        {
+            model.Status = HttpStatusCode.BadRequest;
+            model.FlashMessage = FlashMessageViewModel.CreateErrorFlashMessageViewModel(new Dictionary<string, string> { { "searchTerm", "Enter organisation name/number" } });
         }
 
         private void CreateOrganisationCookieData(OrchestratorResponse<OrganisationDetailsViewModel> response)
