03c189302f71f9ea6d821351856d16a9c29b37e7
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-07-03 10:53:10 +0100

    Dcm 629 pagination release branch (#895)
    * Pagination styles and markup merged from release-branch
    
    * Pagination styles conflicted with MYA fixed.
    
    * Update Commitments api packages
    
    * Update Handler to pass PageNumber
    
    * Orchestrator updated for pagination
    
    * Added partial view for pagination
    
    * Removed unused message
    
    * Corrected clause for next page link

--- src/SFA.DAS.EAS.Web/ViewModels/ManageApprenticeships/ManageApprenticeshipsViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/ManageApprenticeships/ManageApprenticeshipsViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using SFA.DAS.EAS.Web.ViewModels.Interfaces;
+using System.Collections.Generic;
 
 namespace SFA.DAS.EAS.Web.ViewModels.ManageApprenticeships
 {
-    public class ManageApprenticeshipsViewModel
+    public class ManageApprenticeshipsViewModel : IPaginationViewModel
     {
         public IEnumerable<ApprenticeshipDetailsViewModel> Apprenticeships { get; set; }
 
         public ApprenticeshipFiltersViewModel Filters { get; set; }
 
         public string HashedAccountId { get; set; }
-        public int TotalApprenticeships { get; set; }
+        public int TotalResults { get; set; }
+        public int PageNumber { get; internal set; }
+        public int TotalPages { get; internal set; }
+        public int PageSize { get; internal set; }
     }
 }
