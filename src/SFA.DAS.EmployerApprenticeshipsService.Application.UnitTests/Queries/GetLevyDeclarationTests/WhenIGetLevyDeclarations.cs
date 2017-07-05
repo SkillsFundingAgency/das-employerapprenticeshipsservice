b6c5b2ff79205ce479fc6eeb4f884a02a9130771
Michael Young <mick.young1981@googlemail.com>
2017-07-05 11:41:24 +0100

    Amended GetLevyDeclarationQuery to return just the levy declarations.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetLevyDeclarationTests/WhenIGetLevyDeclarations.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application.UnitTests/Queries/GetLevyDeclarationTests/WhenIGetLevyDeclarations.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EAS.Application.Queries.GetLevyDeclaration;
 using SFA.DAS.EAS.Application.Validation;
-using SFA.DAS.EAS.Domain;
-using SFA.DAS.EAS.Domain.Data;
 using SFA.DAS.EAS.Domain.Data.Repositories;
-using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Domain.Models.Levy;
-using SFA.DAS.EAS.Domain.Models.PAYE;
 using SFA.DAS.EAS.TestCommon.ObjectMothers;
 
 namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetLevyDeclarationTests
@@ -22,10 +17,8 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetLevyDeclarationTests
         public override GetLevyDeclarationQueryHandler RequestHandler { get; set; }
         public override Mock<IValidator<GetLevyDeclarationRequest>> RequestValidator { get; set; }
         private const long ExpectedAccountId = 757361;
-        private const string ExpectedEmpref = "123/ABC123";
         private List<LevyDeclarationView> _expectedLevyDeclarationViews;
-        private Mock<IDasAccountService> _dasAccountService;
-
+        
         [SetUp]
         public void Arrange()
         {
@@ -37,16 +30,7 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetLevyDeclarationTests
             _expectedLevyDeclarationViews = LevyDeclarationViewsObjectMother.Create(ExpectedAccountId);
             _repository.Setup(x => x.GetAccountLevyDeclarations(It.IsAny<long>())).ReturnsAsync(_expectedLevyDeclarationViews);
 
-            _dasAccountService = new Mock<IDasAccountService>();
-            _dasAccountService.Setup(x => x.GetAccountSchemes(ExpectedAccountId)).ReturnsAsync(new PayeSchemes {SchemesList = new List<PayeScheme>
-            {
-                new PayeScheme
-                {
-                    Ref = ExpectedEmpref
-                }
-            } });
-
-            RequestHandler = new GetLevyDeclarationQueryHandler(_repository.Object, RequestValidator.Object, _dasAccountService.Object);
+           RequestHandler = new GetLevyDeclarationQueryHandler(_repository.Object, RequestValidator.Object);
         }
 
         [Test]
@@ -67,44 +51,7 @@ namespace SFA.DAS.EAS.Application.UnitTests.Queries.GetLevyDeclarationTests
 
             //Assert
             Assert.IsNotNull(actual);
-            Assert.AreEqual(_expectedLevyDeclarationViews[0].AccountId,actual.Data.AccountId);
-            Assert.AreEqual(_expectedLevyDeclarationViews[0].EmpRef,actual.Data.Data[0].EmpRef);
-            Assert.AreEqual(_expectedLevyDeclarationViews[0].EnglishFraction,actual.Data.Data[0].EnglishFraction);
-            Assert.AreEqual(_expectedLevyDeclarationViews[0].SubmissionDate,actual.Data.Data[0].SubmissionDate);
-        }
-
-        [Test]
-        public async Task ThenTheSchemeInformationIsRetrieved()
-        {
-            //Act
-            await RequestHandler.Handle(Query);
-
-            //Assert
-            _dasAccountService.Verify(x=>x.GetAccountSchemes(ExpectedAccountId));
-        }
-
-        [Test]
-        public async Task ThenTheSchemeInformationIsPopulatedForTheResponse()
-        {
-            //Arrange
-            var expectedAddedDate = new DateTime(2016,01,10);
-            var expectedRemovedDate = new DateTime(2016,03,29);
-            _dasAccountService.Setup(x => x.GetAccountSchemes(ExpectedAccountId)).ReturnsAsync(new PayeSchemes { SchemesList = new List<PayeScheme> {
-                new PayeScheme
-                    {
-                        AddedDate = expectedAddedDate,
-                        RemovedDate = expectedRemovedDate,
-                        Ref = ExpectedEmpref
-                    }
-                }
-            });
-
-            //Act
-            var actual = await RequestHandler.Handle(Query);
-
-            //Assert
-            Assert.AreEqual(expectedAddedDate, actual.Data.Data[0].EmprefAddedDate);
-            Assert.AreEqual(expectedRemovedDate, actual.Data.Data[0].EmprefRemovedDate);
+            Assert.AreSame(_expectedLevyDeclarationViews, actual.Declarations);
         }
     }
 }
--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetLevyDeclaration/GetLevyDeclarationQueryHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetLevyDeclaration/GetLevyDeclarationQueryHandler.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.EAS.Application.Validation;
-using SFA.DAS.EAS.Domain.Data;
 using SFA.DAS.EAS.Domain.Data.Repositories;
-using SFA.DAS.EAS.Domain.Interfaces;
-using SFA.DAS.EAS.Domain.Models.Levy;
 
 namespace SFA.DAS.EAS.Application.Queries.GetLevyDeclaration
 {
@@ -14,13 +9,11 @@ namespace SFA.DAS.EAS.Application.Queries.GetLevyDeclaration
     {
         private readonly IDasLevyRepository _repository;
         private readonly IValidator<GetLevyDeclarationRequest> _validator;
-        private readonly IDasAccountService _dasAccountService;
-
-        public GetLevyDeclarationQueryHandler(IDasLevyRepository repository, IValidator<GetLevyDeclarationRequest> validator, IDasAccountService dasAccountService)
+        
+        public GetLevyDeclarationQueryHandler(IDasLevyRepository repository, IValidator<GetLevyDeclarationRequest> validator)
         {
             _repository = repository;
             _validator = validator;
-            _dasAccountService = dasAccountService;
         }
 
         public async Task<GetLevyDeclarationResponse> Handle(GetLevyDeclarationRequest message)
@@ -33,36 +26,8 @@ namespace SFA.DAS.EAS.Application.Queries.GetLevyDeclaration
             }
 
             var declarations = await _repository.GetAccountLevyDeclarations(message.AccountId);
-            var schemeInformation = await _dasAccountService.GetAccountSchemes(message.AccountId);
 
-            return new GetLevyDeclarationResponse
-            { 
-                Data = new LevyDeclarationSourceData
-                {
-                    AccountId = message.AccountId,
-                    Data = declarations.Select(item =>
-                    {
-                        var scheme = schemeInformation?.SchemesList?.FirstOrDefault(c => c.Ref.Equals(item.EmpRef, StringComparison.CurrentCultureIgnoreCase));
-                        return new LevyDeclarationSourceDataItem
-                        {
-                            Id = item.Id,
-                            EmpRef = item.EmpRef,
-                            SubmissionDate = item.SubmissionDate,
-                            LevyDueYtd = item.LevyDueYtd,
-                            EnglishFraction = item.EnglishFraction,
-                            PayrollDate = item.PayrollDate(),
-                            PayrollMonth = item.PayrollMonth,
-                            PayrollYear = item.PayrollYear,
-                            LastSubmission = item.LastSubmission,
-                            TopUp = item.TopUp,
-                            AccountId = item.AccountId,
-                            EmprefAddedDate = scheme?.AddedDate ?? DateTime.UtcNow,
-                            EmprefRemovedDate = scheme?.RemovedDate
-                        };
-                    }).ToList()
-                }
-            };
+            return new GetLevyDeclarationResponse { Declarations = declarations };
         }
-        
     }
 }
