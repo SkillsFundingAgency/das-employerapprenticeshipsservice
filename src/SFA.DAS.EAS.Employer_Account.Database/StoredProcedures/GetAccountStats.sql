57f4899b5adc3478e2209f3c7fbba565292677f5
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-07-11 10:37:51 +0100

    Fixed sql as team member & PAYE Scheme count was wrong

--- src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetAccountStats.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetAccountStats.sql
@@ -1,13 +1,13 @@
 ﻿CREATE PROCEDURE [employer_account].[GetAccountStats]
 	@accountId BIGINT = 0	
 AS
-	SELECT	acc.Id as AccountId,
-			Count(distinct his.Id) as PAYECount, 
-			Count(distinct ag.Id) as OrganisationCount, 
-			Count(distinct mem.AccountId) as TeamMemberCount 
+	SELECT	MAX(acc.Id) as AccountId,
+			COUNT(DISTINCT his.Id) as PayeSchemeCount, 
+			COUNT(DISTINCT ag.Id) as OrganisationCount, 
+			COUNT(DISTINCT mem.UserId) as TeamMemberCount 
 	FROM [employer_account].[Account] acc
 	INNER JOIN [employer_account].[AccountHistory] his ON acc.Id = his.AccountId
 	INNER JOIN [employer_account].[EmployerAgreement] ag ON acc.Id = ag.AccountId
 	INNER JOIN [employer_account].[Membership] mem ON acc.id = mem.AccountId
 	WHERE acc.Id = @accountId
-
+	GROUP BY acc.Id
