17f555b0078045fa29e39d0314eaa7e38e2d4003
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-17 12:23:33 +0100

    Remove code now in commitments solution

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetCommitment/GetCommitmentQueryResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.Commitments.Api.Types.Commitment;
-
-namespace SFA.DAS.EAS.Application.Queries.GetCommitment
-{
-    public class GetCommitmentQueryResponse
-    {
-        public CommitmentView Commitment { get; set; }
-    }
-}
