35fc377fa726cee0302cea635ca6daed38679ce4
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-07-17 12:05:53 +0100

    Add helper extension for external route
    Added in helper to generate urls to the commitments url

---
+++ src/SFA.DAS.EAS.Web/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.Azure;
+
+namespace SFA.DAS.EAS.Web.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ExternalUrlAction(this UrlHelper helper, string controllerName, string actionName = "")
+        {
+
+            var baseUrl = GetBaseUrl();
+
+            var accountId = helper.RequestContext.RouteData.Values["hashedAccountId"];
+
+            return $"{baseUrl}accounts/{accountId}/{controllerName}/{actionName}";
+        }
+
+        private static string GetBaseUrl()
+        {
+            return CloudConfigurationManager.GetSetting("CommitmentsBaseUrl").EndsWith("/")
+                ? CloudConfigurationManager.GetSetting("CommitmentsBaseUrl")
+                : CloudConfigurationManager.GetSetting("CommitmentsBaseUrl") + "/";
+        }
+    }
+}
