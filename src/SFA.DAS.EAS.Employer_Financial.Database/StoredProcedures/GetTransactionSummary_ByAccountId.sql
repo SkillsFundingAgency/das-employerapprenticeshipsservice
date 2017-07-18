99c649cf01b4d80cb45620b3a57fce487a78219b
Michael Young <mick.young1981@googlemail.com>
2017-07-18 13:23:21 +0100

    Added stored proc to get transaction summary for account

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetTransactionSummary_ByAccountId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [employer_financial].[GetTransactionSummary_ByAccountId]
+	@AccountId BIGINT
+AS
+	SELECT 
+		YEAR(DateCreated),
+		MONTH(DateCreated),
+		SUM(Amount) AS Total
+	FROM [employer_financial].[TransactionLine]
+	WHERE AccountId = @AccountId
+	GROUP BY YEAR(DateCreated), MONTH(DateCreated)
