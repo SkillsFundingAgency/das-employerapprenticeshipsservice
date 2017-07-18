7b6a040c3cb53d95dcaa058858d386c7223242a2
Michael Young <mick.young1981@googlemail.com>
2017-07-18 13:34:08 +0100

    Added repository method to get transaction summary.

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transaction/TransactionSummary.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transaction/TransactionSummary.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-
 namespace SFA.DAS.EAS.Domain.Models.Transaction
 {
     public class TransactionSummary
     {
-        public DateTime TransactionDate { get; set; }
-        public string Description { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
         public decimal Amount { get; set; }
-        public decimal Balance { get; set; }
-        public List<TransactionLine> TransactionLines { get; set; }
-        public string Id { get; set; }
     }
 }
