852234f2bf1bac45e4546e2852d9b03f4b26ccbd
James King <hello@jamesmichaelking.com>
2017-07-26 14:58:13 +0100

    New partial for the tasks panel

--- src/SFA.DAS.EAS.Web/dist/javascripts/app.js
+++ src/SFA.DAS.EAS.Web/dist/javascripts/app.js
@@ -110,6 +110,98 @@ sfa.navigation = {
     }
 }
 
+sfa.tabs = {
+
+    elems: {
+        tabs: $('ul.js-tabs li a'),
+        panels: $('.js-tab-pane')
+    },
+
+    init: function () {
+
+        if (this.elems.tabs) {
+            this.setUpEvents(this.elems.tabs);
+            this.hidePanels(this.elems.panels);
+        }
+
+        this.elems.tabs.eq(0).click();
+
+    },
+
+    hidePanels: function (panels) {
+        panels.hide();
+    },
+
+    showPanel: function (panel) {
+        panel.show();
+    },
+
+    setUpEvents: function (tabs) {
+
+        var that = this;
+
+        tabs.on('click touchstart', function (e) {
+
+            tabs.removeClass('selected');
+            $(this).addClass('selected');
+
+            var target = $(this).attr('href');
+
+            that.hidePanels(that.elems.panels);
+            that.showPanel($(target));
+
+            e.preventDefault();
+        });
+
+    }
+};
+
+sfa.tabs = {
+
+    elems: {
+        tabs: $('ul.js-tabs li a'),
+        panels: $('.js-tab-pane')
+    },
+
+    init: function () {
+
+        if (this.elems.tabs) {
+            this.setUpEvents(this.elems.tabs);
+            this.hidePanels(this.elems.panels);
+        }
+
+        this.elems.tabs.eq(0).click();
+
+    },
+
+    hidePanels: function (panels) {
+        panels.hide();
+    },
+
+    showPanel: function (panel) {
+        panel.show();
+    },
+
+    setUpEvents: function (tabs) {
+
+        var that = this;
+
+        tabs.on('click touchstart', function (e) {
+
+            tabs.removeClass('selected');
+            $(this).addClass('selected');
+
+            var target = $(this).attr('href');
+
+            that.hidePanels(that.elems.panels);
+            that.showPanel($(target));
+
+            e.preventDefault();
+        });
+
+    }
+};
+
 sfa.forms = {
     init: function () {
         this.preventDoubleSubmit();
@@ -272,6 +364,8 @@ window.onunload = function () {
 
 sfa.forms.init();
 sfa.navigation.init();
+sfa.tabs.init();
+
 $('ul#global-nav-links').collapsableNav();
 
 var selectionButtons = new GOVUK.SelectionButtons("label input[type='radio'], label input[type='checkbox'], section input[type='radio']");
