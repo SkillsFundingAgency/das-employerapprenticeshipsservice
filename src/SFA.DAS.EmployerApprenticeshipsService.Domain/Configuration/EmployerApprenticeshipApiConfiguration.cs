96f58706f41dd9b5ccafae83e47503b53b9491ac
chrisfoster76 <chrisfoster186@googlemail.com>
2017-07-31 10:12:15 +0100

    Updates packages Multi auth support

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/EmployerApprenticeshipApiConfiguration.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/EmployerApprenticeshipApiConfiguration.cs
@@ -1,7 +1,10 @@
-﻿namespace SFA.DAS.EAS.Domain.Configuration
+﻿using SFA.DAS.Notifications.Api.Client.Configuration;
+
+namespace SFA.DAS.EAS.Domain.Configuration
 {
     public class EmployerApprenticeshipApiConfiguration
     {
         public string BaseUrl { get; set; }
+        public NotificationsApiClientConfiguration NotificationApi { get; set; }
     }
 }
