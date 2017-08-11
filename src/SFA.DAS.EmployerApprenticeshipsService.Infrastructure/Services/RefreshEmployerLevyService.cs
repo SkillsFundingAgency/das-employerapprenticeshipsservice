f8f6a4770eb2862ca39b6cebd730f6453e43e740
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-08-11 11:15:29 +0100

    Move RefreshEmployer message to service
    Move the queuing of the EmployerRefreshLevyQueueMessage to a sepereate
    service so that it can be deleted easier later

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/RefreshEmployerLevyService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EAS.Application.Messages;
+using SFA.DAS.EAS.Domain.Attributes;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.Messaging;
+
+namespace SFA.DAS.EAS.Infrastructure.Services
+{
+    public class RefreshEmployerLevyService : IRefreshEmployerLevyService
+    {
+        [QueueName]
+        public string get_employer_levy { get; set; }
+
+        private readonly IMessagePublisher _messagePublisher;
+
+        public RefreshEmployerLevyService(IMessagePublisher messagePublisher)
+        {
+            _messagePublisher = messagePublisher;
+        }
+
+        public async Task QueueRefreshLevyMessage(long accountId, string payeRef)
+        {
+            await _messagePublisher.PublishAsync(new EmployerRefreshLevyQueueMessage
+            {
+                AccountId = accountId,
+                PayeRef = payeRef
+            });
+        }
+    }
+}
