634002f0a50ccdd2b86d8e9aadfbfe1ff339a70c
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-08-23 14:49:48 +0100

    Added skeleton Mediator code
    - (now need to upgrade all packages to .net framework 4.6.2 to make it work with the new Tasks API)

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountTasks/GetAccountTasksQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.EAS.Application.Validation;
+
+namespace SFA.DAS.EAS.Application.Queries.GetAccountTasks
+{
+    public class GetAccountTasksQueryValidator //: IValidator<GetAccountTasksQuery>
+    {
+    }
+}
