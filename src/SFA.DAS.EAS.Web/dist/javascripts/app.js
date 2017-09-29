78fa86cd63f254bb2d752ad577b5b3e42b6e88a1
James King <hello@jamesmichaelking.com>
2017-09-29 14:22:36 +0100

    Added 'Your employer account' header to the layout

--- src/SFA.DAS.EAS.Web/dist/javascripts/app.js
+++ src/SFA.DAS.EAS.Web/dist/javascripts/app.js
@@ -339,5 +339,14 @@ if (errorMessage.length > 0) {
 }
 
 
-
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 110) {
+        $('.floating-menu').addClass('fixed-header');
+        $('.js-float').addClass('width-adjust');
+    }
+    else {
+        $('.floating-menu').removeClass('fixed-header');
+        $('.js-float').removeClass('width-adjust');
+    }
+});
 
