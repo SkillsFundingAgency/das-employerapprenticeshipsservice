2872db909327b11f2c7fa7f2a53e8a5e9bc9f97e
cousinst <trevor_cousins@yahoo.com>
2017-10-09 14:43:19 +0100

    AML-1607 make EnglishFraction nullable to match stores procs that have tests for that

--- src/SFA.DAS.EAS.Employer_Financial.Database/Tables/TransactionLine.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Tables/TransactionLine.sql
@@ -12,7 +12,7 @@
 	UkPrn BIGINT null, 
     SfaCoInvestmentAmount DECIMAL(18, 4) NOT NULL DEFAULT 0, 
     EmployerCoInvestmentAmount DECIMAL(18, 4) NOT NULL DEFAULT 0,
-	[EnglishFraction] DECIMAL(18, 5) NOT NULL
+	[EnglishFraction] DECIMAL(18, 5) NULL
 )
 GO
 
