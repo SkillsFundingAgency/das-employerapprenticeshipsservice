69cd67884596a2d6aa73fb619c5965cc5f7788f9
Michael Young <mick.young1981@googlemail.com>
2017-10-13 09:15:00 +0100

    Removed commitments

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetFrameworks/GetFrameworksQueryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EAS.Application.Queries.GetFrameworks
-{
-    public class GetFrameworksQueryRequest : IAsyncRequest<GetFrameworksQueryResponse>
-    {
-        
-    }
-}
--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetProviders/GetProvidersQueryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EAS.Application.Queries.GetProviders
-{
-    public class GetProvidersQueryRequest : IAsyncRequest<GetProvidersQueryResponse>
-    {
-        
-    }
-}
