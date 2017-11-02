91c8e966c22ddbdce97213f4fa0d7aeeefca13f8
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-11-02 09:26:34 +0000

    Revert "Implementing Web Policy to strip out sensative response headers"
    This reverts commit 8541251973dc652afb00910150c1b1b0e48d8ee0.

--- src/SFA.DAS.EAS.Web/Global.asax.cs
+++ src/SFA.DAS.EAS.Web/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Helpers;
@@ -18,7 +17,6 @@ using SFA.DAS.Audit.Types;
 using SFA.DAS.EAS.Infrastructure.Logging;
 using SFA.DAS.EmployerUsers.WebClientComponents;
 using SFA.DAS.EAS.Web.Plumbing.Mvc;
-using SFA.DAS.Web.Policy;
 
 namespace SFA.DAS.EAS.Web
 {
@@ -58,7 +56,12 @@ namespace SFA.DAS.EAS.Web
             });
         }
 
-       
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var application = sender as HttpApplication;
+            application?.Context?.Response.Headers.Remove("Server");
+        }
+
         protected void Application_Error(object sender, EventArgs e)
         {
             var exception = Server.GetLastError();
@@ -67,15 +70,6 @@ namespace SFA.DAS.EAS.Web
 
             var tc = new TelemetryClient();
             tc.TrackTrace($"{exception.Message} - {exception.InnerException}");
-        }
-        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
-        {
-            new HttpContextPolicyProvider(
-                new List<IHttpContextPolicy>()
-                {
-                    new ResponseHeaderRestrictionPolicy()
-                }
-            ).Apply(new HttpContextWrapper(HttpContext.Current), PolicyConcern.HttpResponse);
         }
     }
 }
