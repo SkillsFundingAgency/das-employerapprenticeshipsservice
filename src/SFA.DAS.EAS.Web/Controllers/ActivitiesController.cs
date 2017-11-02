73b11c5e043497ba088df3069ee39f7c01b91466
Trevor Cousins <trevor_cousins@yahoo.com>
2017-11-02 14:33:09 +0000

    AML-1240 Details page added...missed files

---
+++ src/SFA.DAS.EAS.Web/Controllers/ActivitiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EAS.Web.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        // GET: Activities
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
