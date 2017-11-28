2ad374e72fc9ebc01d33bae0414fe93b9c649972
Robin O'Connor <github@bytepointsoftware.com>
2017-11-28 14:41:04 +0000

    Added orchestrator for handling dismissing of task reminders

---
+++ src/SFA.DAS.EAS.Web/Orchestrators/TaskOrchestrator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EAS.Application;
+using SFA.DAS.EAS.Application.Commands.DismissMonthlyTaskReminder;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.EAS.Web.Orchestrators
+{
+    public class TaskOrchestrator
+    {
+        private readonly IMediator _mediator;
+        private readonly ILog _logger;
+
+        public TaskOrchestrator(IMediator mediator, ILog logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task<OrchestratorResponse> DismissMonthlyReminderTask(string hashedAccountId, string hashedUserId, TaskType taskType)
+        {
+            _logger.Debug($"Dismissing task reminder {Enum.GetName(typeof(TaskType), taskType)} for account id {hashedAccountId} and user id {hashedUserId}");
+
+            try
+            {
+                await _mediator.SendAsync(new DismissMonthlyTaskReminderCommand
+                {
+                    HashedAccountId = hashedAccountId,
+                    HashedUserId = hashedUserId,
+                    TaskType = taskType
+                });
+            }
+            catch (InvalidRequestException ire)
+            {
+                _logger.Warn(ire, $"Invalid request for account (account id: {hashedAccountId}, user id: {hashedUserId}, Task type: {taskType}");
+                return new OrchestratorResponse { Status = HttpStatusCode.BadRequest};
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Error occured when dismissing a task reminder (account id: {hashedAccountId}, user id: {hashedUserId}, Task type: {Enum.GetName(typeof(TaskType), taskType)}");
+                return new OrchestratorResponse { Status = HttpStatusCode.InternalServerError, Exception = ex};
+            }
+
+            return new OrchestratorResponse{Status = HttpStatusCode.OK};
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Orchestrators/TaskOrchestratorTests/WhenIDismissAMonthlyTaskReminder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Application;
+using SFA.DAS.EAS.Application.Commands.DismissMonthlyTaskReminder;
+using SFA.DAS.EAS.Web.Orchestrators;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Tasks.API.Types.Enums;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Orchestrators.TaskOrchestratorTests
+{
+    public class WhenIDismissAMonthlyTaskReminder
+    {
+        private TaskOrchestrator _orchestrator;
+        private Mock<IMediator> _mediator;
+        private Mock<ILog> _logger;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mediator = new Mock<IMediator>();
+            _logger = new Mock<ILog>();
+            _orchestrator = new TaskOrchestrator(_mediator.Object, _logger.Object);
+        }
+
+        [Test]
+        public async Task ThenTheDismissShouldBeSaved()
+        {
+            //Arrange
+            const string hashedAccountId = "ABC123";
+            const string hashedUserId = "DEF456";
+            const TaskType taskType = TaskType.LevyDeclarationDue;
+
+            //Act
+            var result = await _orchestrator.DismissMonthlyReminderTask(hashedAccountId, hashedUserId, taskType);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, result.Status);
+            _mediator.Verify(x => x.SendAsync(It.Is<DismissMonthlyTaskReminderCommand>( command =>
+            command.HashedAccountId.Equals(hashedAccountId) &&
+            command.HashedUserId.Equals(hashedUserId) && 
+            command.TaskType == taskType)));
+        }
+
+        [Test]
+        public async Task ThenIfTheRequestIsInvalidIShouldBeTold()
+        {
+            //Arrange
+            _mediator.Setup(x => x.SendAsync(It.IsAny<DismissMonthlyTaskReminderCommand>()))
+                .ThrowsAsync(new InvalidRequestException(new Dictionary<string, string>
+                {
+                    {nameof(DismissMonthlyTaskReminderCommand.HashedAccountId),"test error" }
+                }));
+
+            //Act
+            var result = await _orchestrator.DismissMonthlyReminderTask("ABC123", "DEF123", TaskType.LevyDeclarationDue);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.Status);
+        }
+
+        [Test]
+        public async Task ThenIfAnInternalServerErrorOccursIShouldBeTold()
+        {
+            //Arrange
+            _mediator.Setup(x => x.SendAsync(It.IsAny<DismissMonthlyTaskReminderCommand>()))
+                .ThrowsAsync(new Exception());
+
+            //Act
+            var result = await _orchestrator.DismissMonthlyReminderTask("ABC123", "DEF123", TaskType.LevyDeclarationDue);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.InternalServerError, result.Status);
+        }
+    }
+}
