6f053d76c055e451837080c52408ab772b3ac506
Robin O'Connor <github@bytepointsoftware.com>
2017-12-05 15:55:56 +0000

    Fixed issue around cohort that were deleted being counted.

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/CommitmentService.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/CommitmentService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.Commitments.Api.Types.Commitment.Types;
 using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Domain.Models.Commitment;
 
@@ -20,7 +21,8 @@ namespace SFA.DAS.EAS.Infrastructure.Services
         {
             var commitmentItems = await _commitmentApi.GetEmployerCommitments(employerAccountId);
 
-            return commitmentItems.Select(x => new Cohort {Id = x.Id}).ToList();
+            return commitmentItems.Where(x => x.CommitmentStatus != CommitmentStatus.Deleted)
+                                  .Select(x => new Cohort {Id = x.Id}).ToList();
         }
     }
 }
