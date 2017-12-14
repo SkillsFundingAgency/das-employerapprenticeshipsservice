763684b2e87c2c6a310564c5426a7163ca481a0f
Robin O'Connor <github@bytepointsoftware.com>
2017-12-14 16:18:31 +0000

    fixed logic that was messy and not consistent with other parts of the code

---
+++ src/SFA.DAS.EAS.Web/Extensions/DynamicExtentions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SFA.DAS.EAS.Web.Extensions
+{
+    public static class DynamicExtentions
+    {
+        public static bool ParseHideNavFromViewBag(dynamic viewBag)
+        {
+            if (viewBag.HideNav == null)
+            {
+                return false;
+            }
+
+            var hideNavString = viewBag.HideNav as string;
+
+            if (hideNavString != null)
+            {
+                bool hideNavFlag;
+
+                if (bool.TryParse(hideNavString, out hideNavFlag))
+                {
+                    return hideNavFlag;
+                }
+
+                return false;
+            }
+
+            if (viewBag.HideNav is bool)
+            {
+                return viewBag.HideNav;
+            }
+
+            return false;
+        }
+    }
+}
