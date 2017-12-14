939ad482dd755a6134b97c156dea77bf1f72f7a9
Robin O'Connor <github@bytepointsoftware.com>
2017-12-14 10:04:12 +0000

    Fixed bug with layout and hide nav not working
    - The fix was to use the preexisting code to turn the hidnav property into a bool whatever that property was set to as it can be set to a strong or bool depending on what its being used for

--- src/SFA.DAS.EAS.Web/Extensions/StringExtensions.cs
+++ src/SFA.DAS.EAS.Web/Extensions/StringExtensions.cs
@@ -23,34 +23,5 @@
 
             return result;
         }
-
-        public static bool ParseHideNavFromViewBag(dynamic viewBag)
-        {
-            if (viewBag.HideNav == null)
-            {
-                return false;
-            }
-
-            var hideNavString = viewBag.HideNav as string;
-
-            if (hideNavString != null)
-            {
-                bool hideNavFlag;
-
-                if (bool.TryParse(hideNavString, out hideNavFlag))
-                {
-                    return hideNavFlag;
-                }
-
-                return !string.IsNullOrWhiteSpace(hideNavString);
-            }
-
-            if (viewBag.HideNav is bool)
-            {
-                return viewBag.HideNav;
-            }
-
-            return false;
-        }
     }
 }
