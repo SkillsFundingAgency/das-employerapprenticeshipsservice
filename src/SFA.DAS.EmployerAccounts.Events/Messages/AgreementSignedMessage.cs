54a79a8f0fb23cd519ed70bca16ea24e1759f4bc
Robin O'Connor <github@bytepointsoftware.com>
2017-12-22 10:10:47 +0000

    made all messages use getters only
    - This supports a consistent approach and any issues around json de-serialising will be handled by custom json settings code

--- src/SFA.DAS.EmployerAccounts.Events/Messages/AgreementSignedMessage.cs
+++ src/SFA.DAS.EmployerAccounts.Events/Messages/AgreementSignedMessage.cs
@@ -5,11 +5,17 @@ namespace SFA.DAS.EmployerAccounts.Events.Messages
 {
     [Serializable]
     [MessageGroup("agreement_signed")]
-    public class AgreementSignedMessage
+    public class AgreementSignedMessage : AccountMessageBase
     {
-        public long AccountId { get; set; }
-        public long LegalEntityId { get; set; }
-        public long AgreementId { get; set; }
-        public bool CohortCreated { get; set; }
+        public long LegalEntityId { get; }
+        public long AgreementId { get; }
+        public bool CohortCreated { get; }
+
+        public AgreementSignedMessage(long accountId, long agreementId, long legalEntityId, bool cohortCreated, string creatorName, string creatorUserRef) : base(accountId, creatorName, creatorUserRef)
+        {
+            AgreementId = agreementId;
+            LegalEntityId = legalEntityId;
+            CohortCreated = cohortCreated;
+        }
     }
 }
