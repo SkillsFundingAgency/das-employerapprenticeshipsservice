07548b47e9a7b0fcb172c0962e0de2bd7add8e8a
Elliot Ritchie <elliotritchie@hotmail.com>
2018-01-05 10:46:09 +0000

    AML-1972 Added unit test to verify orchestrator exceptions are not swallowed

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/BaseControllerTests/WhenReturningView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using NUnit.Framework;
+using SFA.DAS.EAS.Web.Controllers;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Controllers.BaseControllerTests
+{
+    public class WhenReturningView : ControllerTestBase
+    {
+        private TestController _controller;
+
+        [SetUp]
+        public void Arrange()
+        {
+            base.Arrange();
+
+            _controller = new TestController
+            {
+                ControllerContext = _controllerContext.Object
+            };
+        }
+
+        [Test]
+        public void ThenItShouldRethrowExceptionIfOrchestratorResponseContainsException()
+        {
+            Assert.Throws<Exception>(() => _controller.Test(), "Foobar");
+        }
+
+        internal class TestController : BaseController
+        {
+            public TestController() : base(null, null, null, null)
+            {
+            }
+
+            public ActionResult Test()
+            {
+                return View(new OrchestratorResponse
+                {
+                    Status = HttpStatusCode.InternalServerError,
+                    Exception = new Exception("Foobar")
+                });
+            }
+        }
+    }
+}
