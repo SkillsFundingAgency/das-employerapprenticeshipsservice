0fbed3d08a81744ca548c8b39679b6c551a77f0b
Ian Gledhill <ian@gledhills.mobi>
2018-01-29 11:58:34 +0000

    Added new intergartion project

---
+++ src/SFA.DAS.EAS.Web.IntegrationTests/EmployerAccountControllerTests/WhenICreateAnAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Http.Routing;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
+using SFA.DAS.EAS.Web.Authentication;
+using SFA.DAS.EAS.Web.Controllers;
+using SFA.DAS.EAS.Web.Orchestrators;
+using SFA.DAS.EAS.Web.ViewModels;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.EAS.Web.IntegrationTests.EmployerAccountControllerTests
+{
+    class WhenICreateAnAccount 
+    {
+        [SetUp]
+        public void Arrange()
+        {
+        }
+    }
+}
---
+++ src/SFA.DAS.EAS.Web.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFA.DAS.EAS.Web.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFA.DAS.EAS.Web.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25855d5f-8ae4-4ad5-9eed-18eebc2c8cc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
