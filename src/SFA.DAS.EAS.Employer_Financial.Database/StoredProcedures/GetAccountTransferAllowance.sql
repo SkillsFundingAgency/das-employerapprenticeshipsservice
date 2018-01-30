beb611601c87e00725bdb1cfa800486697b1a01f
Robin O'Connor <github@bytepointsoftware.com>
2018-01-30 14:42:29 +0000

    Added initial version of the script that will retrieve the allowance.
    - This code is too basic for release but is a starting point to then add more filtering to.

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferAllowance.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [employer_financial].[GetAccountTransferAllowance]
+	@accountId bigint = 0	
+AS
+	SELECT SUM(tl.Amount) FROM [employer_financial].[TransactionLine] tl
+	INNER JOIN [employer_financial].[PeriodEnd] pe ON tl.PeriodEnd = pe.PeriodEndId
+	WHERE pe.CalendarPeriodYear > Year(GETDATE()) - 2
+	AND pe.CalendarPeriodMonth = 4
+	AND tl.AccountId = @accountId
+
