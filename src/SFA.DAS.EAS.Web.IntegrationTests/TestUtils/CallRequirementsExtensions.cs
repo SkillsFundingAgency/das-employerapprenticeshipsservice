0c2f03d6ccb850039457b5d5b6b860c8d4f4e0aa
Ian Gledhill <ian@gledhills.mobi>
2018-01-31 14:34:05 +0000

    Added some comments

--- src/SFA.DAS.EAS.Web.IntegrationTests/TestUtils/CallRequirementsExtensions.cs
+++ src/SFA.DAS.EAS.Web.IntegrationTests/TestUtils/CallRequirementsExtensions.cs
@@ -6,6 +6,10 @@ using SFA.DAS.EAS.Application;
 
 namespace SFA.DAS.EAS.Account.API.IntegrationTests.TestUtils
 {
+    /// <summary>
+    ///     Extension methods to help build up the requirements to a call. A fluent syntax is used allowing the 
+    ///     builder methods to be chained together.
+    /// </summary>
     public static class CallRequirementsExtensions
     {
         /// <summary>
@@ -19,24 +23,45 @@ namespace SFA.DAS.EAS.Account.API.IntegrationTests.TestUtils
                 .IgnoreExceptionTypes(typeof(InvalidRequestException), typeof(ValidationException));
         }
 
+        /// <summary>
+        ///     Specifies the unhandled exception types that, should they occur, will not be treated as 
+        ///     an error. Any other unhandled exceptions will cause the call to be considered a failure.
+        ///     Note that the test uses a "is", so child exception types will also be accepted.
+        /// </summary>
+        /// <param name="call"></param>
+        /// <param name="exceptionTypes"></param>
+        /// <returns></returns>
         public static CallRequirements IgnoreExceptionTypes(this CallRequirements call, params Type[] exceptionTypes)
         {
             call.IgnoreExceptionTypes = exceptionTypes.ToArray();
             return call;
         }
 
+        /// <summary>
+        ///     Specifies the response codes that indicate a successful call. Any other status codes will
+        ///     cause the call to be considered a failure.
+        /// </summary>
         public static CallRequirements AllowStatusCodes(this CallRequirements call, params HttpStatusCode[] statuscodes)
         {
             call.AcceptableStatusCodes = statuscodes.ToArray();
             return call;
         }
 
+        /// <summary>
+        ///     Specify the controller class that is expected to be used in the call. If this class
+        ///     is not constructed (from DI) then the call will be considered a failure.
+        /// </summary>
         public static CallRequirements ExpectControllerType(this CallRequirements call, Type controllerType)
         {
             call.ExpectedControllerType = controllerType;
             return call;
         }
 
+        /// <summary>
+        ///     Specify that the call is expcted to return the specfied object type. If this is set 
+        ///     then call response body will be de-serialised into the specified type. If not set then
+        ///     the response body (if any) will be ignored.
+        /// </summary>
         public static CallRequirements ExpectResultType(this CallRequirements call, Type resultType)
         {
             call.ExpectResponseType = resultType;
