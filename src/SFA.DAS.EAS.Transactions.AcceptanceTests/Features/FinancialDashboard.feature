b995ccb4bc94235f5558b32b614b8dd0096963e5
Robin O'Connor <github@bytepointsoftware.com>
2018-02-07 14:27:38 +0000

    Added Acceptance test for the transfer balance show on Financial Dashboard

---
+++ src/SFA.DAS.EAS.Transactions.AcceptanceTests/Features/FinancialDashboard.feature
@@ -0,0 +1,16 @@
+﻿Feature: FinancialDashboard
+	In order to review financial details of my account
+	I want view a summary of my account's financial figures
+
+
+Scenario: Transfer Balance
+	Given I have an account
+	When I have the following submissions
+		| Paye_scheme | LevyDueYtd | Payroll_Year | Payroll_Month | English_Fraction | SubmissionDate | CreatedDate |
+		| 223/ABC     | 1000       | 16-17        | 11            | 1                | 2017-03-18     | 2017-03-23  |
+		| 223/ABC     | 2000       | 16-17        | 11            | 1                | 2017-03-20     | 2017-03-23  |
+		| 223/ABC     | 3000       | 16-17        | 12            | 1                | 2017-04-18     | 2017-04-23  |
+		| 223/ABC     | 1000       | 17-18        | 1             | 1                | 2017-05-18     | 2017-05-23  |
+		| 223/ABC     | 2000       | 17-18        | 2             | 1                | 2017-06-18     | 2017-06-23  |
+		| 223/ABC     | 3000       | 17-18        | 3             | 1                | 2017-07-18     | 2017-07-23  |
+	Then the transfer balance should be 3300 on the financial dashboard screen
---
+++ src/SFA.DAS.EAS.Transactions.AcceptanceTests/Steps/FinancialDashboardSteps/FinancialDashboardSteps.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.EAS.Domain.Interfaces;
+using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.TestCommon.DependencyResolution;
+using SFA.DAS.EAS.Web.Authentication;
+using SFA.DAS.EAS.Web.Orchestrators;
+using SFA.DAS.Events.Api.Client;
+using SFA.DAS.Messaging.Interfaces;
+using StructureMap;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EAS.Transactions.AcceptanceTests.Steps.FinancialDashboardSteps
+{
+    [Binding]
+    public class FinancialDashboardSteps : TechTalk.SpecFlow.Steps
+    {
+
+        private static IContainer _container;
+        private static Mock<IMessagePublisher> _messagePublisher;
+        private static Mock<IOwinWrapper> _owinWrapper;
+        private string _hashedAccountId;
+        private static Mock<ICookieStorageService<EmployerAccountData>> _cookieService;
+        private static Mock<IEventsApi> _eventsApi;
+        private static Mock<IEmployerCommitmentApi> _commitmentsApi;
+
+
+        [BeforeFeature]
+        public static void Arrange()
+        {
+            _messagePublisher = new Mock<IMessagePublisher>();
+            _owinWrapper = new Mock<IOwinWrapper>();
+            _cookieService = new Mock<ICookieStorageService<EmployerAccountData>>();
+            _eventsApi = new Mock<IEventsApi>();
+            _commitmentsApi = new Mock<IEmployerCommitmentApi>();
+
+            _container = IoC.CreateContainer(_messagePublisher, _owinWrapper, _cookieService, _eventsApi, _commitmentsApi);
+        }
+
+        [AfterFeature]
+        public static void TearDown()
+        {
+            _container.Dispose();
+        }
+
+        [Then(@"the transfer balance should be (.*) on the financial dashboard screen")]
+        public void ThenTheTransferBalanceShouldBeOnTheFinancialDashboardScreen(decimal expectedTransferBalance)
+        {
+            var hashedAccountId = ScenarioContext.Current["HashedAccountId"].ToString();
+            var userId = ScenarioContext.Current["AccountOwnerUserId"].ToString();
+
+            var orchestrator = _container.GetInstance<EmployerAccountTransactionsOrchestrator>();
+            var response = orchestrator.GetFinanceDashboardViewModel(hashedAccountId, 0, 0, userId).Result;
+
+            Assert.IsNotNull(response?.Data);
+
+            Assert.AreEqual(expectedTransferBalance, response.Data.CurrentTransferFunds);
+        }
+    }
+}
+
+
