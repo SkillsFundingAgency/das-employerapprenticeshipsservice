91e2d8c1ed34ed90bf5afdbd9350e6dca46b92dc
Robin O'Connor <github@bytepointsoftware.com>
2018-02-07 15:53:14 +0000

    Initial version of the get transfer balance stored proc.
    - This will likely change to clean up the statements and some logic may be move to code

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferBalance.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [employer_financial].[GetAccountTransferBalance]
+	@accountId BIGINT NOT NULL	
+AS
+	DECLARE @currentDate AS DATETIME = GETDATE()
+	DECLARE @currentYear AS INT = DATEPART(year, @currentDate)
+	DECLARE @financialYearStartDate AS DATETIME
+	DECLARE @financialYearEndDate AS DATETIME
+
+	BEGIN
+	IF DATEPART(month, @currentDate) > 4
+		BEGIN
+			SELECT @financialYearStartDate = CONVERT(datetime, CONCAT(@currentYear - 1, '-04-18 00:00:00')),
+			 @financialYearEndDate = CONVERT(datetime, CONCAT(@currentYear, '-03-18 23:59:59'))	
+		END
+	ELSE
+		BEGIN
+			SELECT @financialYearStartDate = CONVERT(datetime, CONCAT(@currentYear - 2, '-04-18 00:00:00')),
+			 @financialYearEndDate = CONVERT(datetime, CONCAT(@currentYear - 1, '-03-18 23:59:59'))	
+		END
+	END
+
+	SELECT SUM(Amount) * 0.1 FROM [employer_financial].[TransactionLine] 
+	WHERE TransactionDate >= @financialYearStartDate
+	AND TransactionDate <= @financialYearEndDate
+	AND TransactionType = 1
+	AND AccountId = @accountId
