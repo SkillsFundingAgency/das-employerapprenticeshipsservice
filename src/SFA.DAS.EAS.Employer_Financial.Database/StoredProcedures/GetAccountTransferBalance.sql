700a3c11a6c6bef336ffa64e5aa492060092620e
Robin O'Connor <github@bytepointsoftware.com>
2018-02-08 10:19:58 +0000

    Refactored SQL stored proc to use better date creation code

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferBalance.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferBalance.sql
@@ -1,8 +1,7 @@
 ﻿CREATE PROCEDURE [employer_financial].[GetAccountTransferBalance]
 	@accountId BIGINT NOT NULL	
 AS
-
-SET NOCOUNT ON
+	SET NOCOUNT ON
 
 	DECLARE @currentDate AS DATETIME = GETDATE()
 	DECLARE @currentYear AS INT = DATEPART(year, @currentDate)
@@ -10,16 +9,16 @@ SET NOCOUNT ON
 	DECLARE @financialYearEndDate AS DATETIME
 
 	BEGIN
-	IF DATEPART(month, @currentDate) > 4
-		BEGIN
-			SELECT @financialYearStartDate = CONVERT(datetime, CONCAT(@currentYear - 1, '-04-18 00:00:00')),
-			 @financialYearEndDate = CONVERT(datetime, CONCAT(@currentYear, '-03-18 23:59:59'))	
-		END
-	ELSE
-		BEGIN
-			SELECT @financialYearStartDate = CONVERT(datetime, CONCAT(@currentYear - 2, '-04-18 00:00:00')),
-			 @financialYearEndDate = CONVERT(datetime, CONCAT(@currentYear - 1, '-03-18 23:59:59'))	
-		END
+		IF DATEPART(month, @currentDate) > 4
+			BEGIN
+				SELECT @financialYearStartDate = DATEFROMPARTS(@currentYear - 1, 4, 18),
+				 @financialYearEndDate = DATEFROMPARTS(@currentYear, 3, 19)
+			END
+		ELSE
+			BEGIN
+				SELECT @financialYearStartDate = DATEFROMPARTS(@currentYear - 2, 4, 18),
+				 @financialYearEndDate = DATEFROMPARTS(@currentYear - 1, 3, 19)
+			END
 	END
 
 	SELECT SUM(Amount) * 0.1 FROM [employer_financial].[TransactionLine] 
@@ -27,3 +26,5 @@ SET NOCOUNT ON
 	AND TransactionDate <= @financialYearEndDate
 	AND TransactionType = 1
 	AND AccountId = @accountId
+
+	GO
