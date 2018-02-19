48fe15a0fa0c7780e1415d0e093df34d94028369
Lee Wadhams <lee.wadhams@valtech.co.uk>
2018-02-19 17:08:25 +0000

    ER 217 Add Recruit links (#1141)
    * Add config for Employer recruit site url
    
    * Add top level menu item

--- src/SFA.DAS.EAS.Web/Extensions/UrlHelperExtensions.cs
+++ src/SFA.DAS.EAS.Web/Extensions/UrlHelperExtensions.cs
@@ -6,11 +6,22 @@ namespace SFA.DAS.EAS.Web.Extensions
 {
     public static class UrlHelperExtensions
     {
-        public static string ExternalAction(this UrlHelper helper, string controllerName, string actionName = "")
+        public static string EmployerCommitmentsAction(this UrlHelper helper, string controllerName, string actionName = "")
         {
             var baseUrl = CloudConfigurationManager.GetSetting(ControllerConstants.EmployerCommitmentsBaseUrlKeyName).EndsWith("/")
                 ? CloudConfigurationManager.GetSetting(ControllerConstants.EmployerCommitmentsBaseUrlKeyName)
                 : CloudConfigurationManager.GetSetting(ControllerConstants.EmployerCommitmentsBaseUrlKeyName) + "/";
+
+            var accountId = helper.RequestContext.RouteData.Values[ControllerConstants.HashedAccountIdKeyName];
+
+            return $"{baseUrl}accounts/{accountId}/{controllerName}/{actionName}";
+        }
+
+        public static string EmployerRecruitAction(this UrlHelper helper, string controllerName, string actionName = "")
+        {
+            var baseUrl = CloudConfigurationManager.GetSetting(ControllerConstants.EmployerRecruitControllerName).EndsWith("/")
+                ? CloudConfigurationManager.GetSetting(ControllerConstants.EmployerRecruitControllerName)
+                : CloudConfigurationManager.GetSetting(ControllerConstants.EmployerRecruitControllerName) + "/";
 
             var accountId = helper.RequestContext.RouteData.Values[ControllerConstants.HashedAccountIdKeyName];
 
