275487a3da06600cf796d28792ec1f1190e64366
abroodie <abroodie@gmail.com>
2018-02-21 14:40:08 +0000

    CI-527 Added new LevyDeclaration event message

---
+++ src/SFA.DAS.EmployerAccounts.Events/Messages/LevyDeclarationUpdatedMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.EmployerAccounts.Events.Messages
+{
+    public class LevyDeclarationUpdatedMessage : AccountMessageBase
+    {
+        public string PayrollYear { get; set; }
+        public short? PayrollMonth { get; set; }
+        public decimal LevyDeclaredInMonth { get; set; }
+
+        public LevyDeclarationUpdatedMessage() : base(0, null, null) { }
+        public LevyDeclarationUpdatedMessage(long accountId, string creatorName, string creatorUserRef)
+            : base(accountId, creatorName, creatorUserRef) { }
+    }
+}
