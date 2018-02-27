454308c63782fe8dc590e990fc48e1d00b9a3693
Ian Gledhill <ian@gledhills.mobi>
2018-02-27 16:47:39 +0000

    Add code to deal with new structure of feature config. Add code to cache the feature toggle information to optimise lookups.

---
+++ src/SFA.DAS.EAS.Infrastructure.UnitTests/Services/FeatureToggle/FeatureToggleCacheTests/WhenIProvideNoFeatures.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using SFA.DAS.EAS.Domain.Models.FeatureToggles;
+
+namespace SFA.DAS.EAS.Infrastructure.UnitTests.Services.FeatureToggle.FeatureToggleCacheTests
+{
+    [TestFixture]
+    public class WhenIProvideNoFeatures
+    {
+        [Test]
+        public void ThenNoFeaturesShouldBeSubjectToAToggle()
+        {
+            // arrange
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(FeatureToggleCollectionBuilder.Create());
+
+            // act
+            var isControllerSubjectToToggle = ftc.IsControllerSubjectToFeatureToggle("foo");
+
+            // assert
+            Assert.IsFalse(isControllerSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenNoActionShouldBeSubjectToAToggle()
+        {
+            // arrange
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(FeatureToggleCollectionBuilder.Create());
+
+            // act
+            var isActionSubjectToToggle = ftc.TryGetControllerActionSubjectToToggle("foo", "action", out _);
+
+            // assert
+            Assert.IsFalse(isActionSubjectToToggle);
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static string InvertCase(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return s;
+            }
+
+            var newString = new char[s.Length];
+
+            for (int i = 0; i < newString.Length; i++)
+            {
+                var c = s[i];
+                if (char.IsLetter(c))
+                {
+                    c = char.IsLower(c) ? char.ToUpper(c) : char.ToLower(c);
+                }
+
+                newString[i] = c;
+            }
+
+            return new string(newString);
+        }
+    }
+
+    internal static class FeatureToggleCollectionBuilder
+    {
+        public static FeatureToggleCollection Create()
+        {
+            return new FeatureToggleCollection();
+        }
+
+        public static FeatureToggleCollection WithFeature(this FeatureToggleCollection featureToggleCollection,
+            Domain.Models.FeatureToggles.FeatureToggle featureToggle)
+        {
+            featureToggleCollection.Features.Add(featureToggle);
+            return featureToggleCollection;
+        }
+    }
+
+    internal static class FeatureToggleBuilder
+    { 
+        public static Domain.Models.FeatureToggles.FeatureToggle Create(string name)
+        {
+            var newFeature = new Domain.Models.FeatureToggles.FeatureToggle { Name = name };
+            return newFeature;
+        }
+
+        public static Domain.Models.FeatureToggles.FeatureToggle WithControllerAction(this Domain.Models.FeatureToggles.FeatureToggle featureToggle, string controller, string action)
+        {
+            featureToggle.Actions.Add(new ControllerAction {Controller = controller, Action = action});
+            return featureToggle;
+        }
+
+
+        public static Domain.Models.FeatureToggles.FeatureToggle WithWhitelistedEmail(this Domain.Models.FeatureToggles.FeatureToggle featureToggle, string whitelistedEmail)
+        {
+            featureToggle.Whitelist.Emails.Add(whitelistedEmail);
+
+            return featureToggle;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Services/FeatureToggle/FeatureToggleCache.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.EAS.Domain.Models.FeatureToggles;
+
+namespace SFA.DAS.EAS.Infrastructure.Services.FeatureToggle
+{
+    public class FeatureToggleCache
+    {
+        private readonly HashSet<string> _toggledControllers;
+        private readonly Dictionary<string, ControllerActionCacheItem> _controllerActions;
+
+
+        public FeatureToggleCache(FeatureToggleCollection featureToggleCollection)
+        {
+            _toggledControllers = BuildControllerNamesCache(featureToggleCollection);
+            _controllerActions = BuildControllerActionsCache(featureToggleCollection);
+
+            IsAvailable = featureToggleCollection?.Features?.Count != null;
+        }
+
+        public bool IsAvailable { get; private set; }
+
+        public bool IsControllerSubjectToFeatureToggle(string controllerName)
+        {
+            return _toggledControllers.Contains(controllerName);
+        }
+
+        public bool TryGetControllerActionSubjectToToggle(string controllerName, string actionName, out ControllerActionCacheItem controllerAction)
+        {
+            controllerAction = null;
+
+            if (!IsControllerSubjectToFeatureToggle(controllerName))
+            {
+                return false;
+            }
+
+            return _controllerActions.TryGetValue($"{controllerName}.{actionName}", out controllerAction);
+        }
+
+        private HashSet<string> BuildControllerNamesCache(FeatureToggleCollection featureToggleCollection)
+        {
+            var result = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+
+            if (featureToggleCollection?.Features != null)
+            {
+                var controllerNames = featureToggleCollection
+                                        .Features
+                                        .SelectMany(feature => feature.Actions)
+                                        .Select(a => a.Controller).Distinct();
+                foreach (var controllerName in controllerNames)
+                {
+                    result.Add(controllerName);
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<string, ControllerActionCacheItem> BuildControllerActionsCache(FeatureToggleCollection featureToggleCollection)
+        {
+            var temp = new Dictionary<string, ControllerActionCacheItem>(StringComparer.InvariantCultureIgnoreCase);
+
+            if (featureToggleCollection?.Features != null)
+            {
+                for (int i = 0; i < featureToggleCollection.Features.Count; i++)
+                {
+                    var feature = featureToggleCollection.Features[i];
+                    for (int j = 0; j < feature.Actions.Count; j++)
+                    {
+                        var action = feature.Actions[j];
+                        var cachedItem = Ensure(temp, action);
+                        cachedItem.WhiteLists.Add(feature.Whitelist);
+                    }
+                }
+            }
+            return temp;
+        }
+
+        private ControllerActionCacheItem Ensure(Dictionary<string, ControllerActionCacheItem> cache, ControllerAction action)
+        {
+            ControllerActionCacheItem result;
+
+            if (!cache.TryGetValue(action.QualifiedName, out result))
+            {
+                result = new ControllerActionCacheItem(action.Controller, action.Action);
+                cache.Add(action.QualifiedName, result);
+            }
+
+            return result;
+        }
+    }
+}
