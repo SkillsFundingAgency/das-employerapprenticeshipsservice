9811c279ebff9fe55b5f3d0a912cc938399800af
Ian Gledhill <ian@gledhills.mobi>
2018-02-27 16:47:39 +0000

    Add code to deal with new structure of feature config. Add code to cache the feature toggle information to optimise lookups.

---
+++ src/SFA.DAS.EAS.Infrastructure.UnitTests/Services/FeatureToggle/FeatureToggleCacheTests/WhenIProvideSingleFeature.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.EAS.Infrastructure.UnitTests.Services.FeatureToggle.FeatureToggleCacheTests
+{
+    [TestFixture]
+    public class WhenIProvideSingleFeature
+    {
+        const string TestControllerName = "foo";
+        const string TestActionName = "index";
+
+        [Test]
+        public void ThenFeatureShouldBeSubjectToAToggle()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, "index");
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isControllerSubjectToToggle = ftc.IsControllerSubjectToFeatureToggle(TestControllerName);
+
+            // assert
+            Assert.IsTrue(isControllerSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenActionShouldBeSubjectToAToggle()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, TestActionName);
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isActionSubjectToToggle = ftc.TryGetControllerActionSubjectToToggle(TestControllerName, TestActionName, out _);
+
+            // assert
+            Assert.IsTrue(isActionSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenFeatureToggleShouldBeCaseInsensitive()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, "index");
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isControllerSubjectToToggle = ftc.IsControllerSubjectToFeatureToggle(TestControllerName.InvertCase());
+
+            // assert
+            Assert.IsTrue(isControllerSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenActionToggleShouldBeCaseInsenstive()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, TestActionName);
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isActionSubjectToToggle = ftc.TryGetControllerActionSubjectToToggle(TestControllerName.InvertCase(), TestActionName.InvertCase(), out _);
+
+            // assert
+            Assert.IsTrue(isActionSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenNoneToggeledControllerShouldNotBeSubjectToToggle()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, TestActionName);
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isActionSubjectToToggle = ftc.IsControllerSubjectToFeatureToggle("NonToggledController");
+
+            // assert
+            Assert.IsFalse(isActionSubjectToToggle);
+        }
+
+        [Test]
+        public void ThenNoneToggeledActionShouldNotBeSubjectToToggle()
+        {
+            // arrange
+            var feature = FeatureToggleBuilder.Create("transfers").WithControllerAction(TestControllerName, TestActionName);
+            var featureConfig = FeatureToggleCollectionBuilder.Create().WithFeature(feature);
+            var ftc = new Infrastructure.Services.FeatureToggle.FeatureToggleCache(featureConfig);
+
+            // act
+            var isActionSubjectToToggle = ftc.TryGetControllerActionSubjectToToggle("NonToggledController", "Index", out _);
+
+            // assert
+            Assert.IsFalse(isActionSubjectToToggle);
+        }
+    }
+}
