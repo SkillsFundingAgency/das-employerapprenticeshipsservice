b39433f45fc3b4c5769a783d0c82d9b47b724a04
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-03-13 10:20:01 +0000

    Added AccountTransfer Table

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Tables/AccountTransfers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [employer_financial].[AccountTransfers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SenderAccountId] BIGINT NOT NULL, 
+    [RecieverAccountId] BIGINT NOT NULL, 
+    [CommitmentId] BIGINT NOT NULL, 
+    [Amount] DECIMAL NOT NULL, 
+    [Type] SMALLINT NOT NULL, 
+    [TransferDate] DATETIME NOT NULL
+)
