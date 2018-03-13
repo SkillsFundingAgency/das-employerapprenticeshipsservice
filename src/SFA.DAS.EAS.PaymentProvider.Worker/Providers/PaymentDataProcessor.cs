8c03844442ec24f78a156f4176b231f08f7f9c9a
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-03-13 16:38:51 +0000

    Updated the message processor code to take advantage of generic message processor logic.
    - Also added some code to run multiple messahe handlers at once

--- src/SFA.DAS.EAS.PaymentProvider.Worker/Providers/PaymentDataProcessor.cs
+++ src/SFA.DAS.EAS.PaymentProvider.Worker/Providers/PaymentDataProcessor.cs
@@ -1,83 +1,37 @@
 ﻿using MediatR;
 using SFA.DAS.EAS.Application.Commands.Payments.RefreshPaymentData;
-using SFA.DAS.EAS.Application.Commands.RefreshAccountTransfers;
 using SFA.DAS.EAS.Application.Messages;
+using SFA.DAS.Messaging;
 using SFA.DAS.Messaging.AzureServiceBus.Attributes;
 using SFA.DAS.Messaging.Interfaces;
 using SFA.DAS.NLog.Logger;
-using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EAS.PaymentProvider.Worker.Providers
 {
     [TopicSubscription("MA_PaymentDataProcessor")]
-    public class PaymentDataProcessor : IPaymentDataProcessor
+    public class PaymentDataProcessor : MessageProcessor<PaymentProcessorQueueMessage>
     {
-        private readonly IMessageSubscriberFactory _subscriberFactory;
         private readonly IMediator _mediator;
         private readonly ILog _logger;
 
         public PaymentDataProcessor(IMessageSubscriberFactory subscriberFactory, IMediator mediator, ILog logger)
+            : base(subscriberFactory, logger)
         {
-            _subscriberFactory = subscriberFactory;
             _mediator = mediator;
             _logger = logger;
         }
 
-        public async Task RunAsync(CancellationToken cancellationToken)
+        protected override async Task ProcessMessage(PaymentProcessorQueueMessage message)
         {
-            using (var subscriber = _subscriberFactory.GetSubscriber<PaymentProcessorQueueMessage>())
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    var message = await subscriber.ReceiveAsAsync();
-
-                    try
-                    {
-                        await ProcessMessage(message);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.Fatal(ex,
-                            $"Refresh payment message processing failed for account with ID [{message?.Content?.AccountId}]");
-                        break; //Stop processing anymore messages as this failure needs to be investigated
-                    }
-                }
-            }
-        }
-
-        private async Task ProcessMessage(IMessage<PaymentProcessorQueueMessage> message)
-        {
-            if (message?.Content?.AccountId == null)
-            {
-                if (message != null)
-                {
-                    await message.CompleteAsync();
-                }
-
-                return;
-            }
-
-            _logger.Info($"Processing refresh payment command for AccountId='{message.Content.AccountId}' and  PeriodEnd = '{message.Content.PeriodEndId}'");
+            _logger.Info($"Processing refresh payment command for AccountId:{message.AccountId} PeriodEnd:{message.PeriodEndId}");
 
             await _mediator.SendAsync(new RefreshPaymentDataCommand
             {
-                AccountId = message.Content.AccountId,
-                PeriodEnd = message.Content.PeriodEndId,
-                PaymentUrl = message.Content.AccountPaymentUrl
+                AccountId = message.AccountId,
+                PeriodEnd = message.PeriodEndId,
+                PaymentUrl = message.AccountPaymentUrl
             });
-
-            _logger.Info($"Processing refresh account transfers command for AccountId:{message.Content.AccountId} PeriodEnd:{message.Content.PeriodEndId}");
-
-
-            await _mediator.SendAsync(new RefreshAccountTransfersCommand
-            {
-                AccountId = message.Content.AccountId,
-                PeriodEnd = message.Content.PeriodEndId
-            });
-
-            await message.CompleteAsync();
         }
     }
 }
---
+++ src/SFA.DAS.EAS.PaymentProvider.Worker/Providers/TransferDataProcessor.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SFA.DAS.EAS.Application.Commands.RefreshAccountTransfers;
+using SFA.DAS.EAS.Application.Messages;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.PaymentProvider.Worker.Providers
+{
+    public class TransferDataProcessor : MessageProcessor<PaymentProcessorQueueMessage>
+    {
+        private readonly ILog _logger;
+        private readonly IMediator _mediator;
+
+        public TransferDataProcessor(IMessageSubscriberFactory subscriberFactory, ILog logger, IMediator mediator) 
+            : base(subscriberFactory, logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        protected override async Task ProcessMessage(PaymentProcessorQueueMessage message)
+        {
+            _logger.Info($"Processing refresh account transfers command for AccountId:{message.AccountId} PeriodEnd:{message.PeriodEndId}");
+
+            await _mediator.SendAsync(new RefreshAccountTransfersCommand
+            {
+                AccountId = message.AccountId,
+                PeriodEnd = message.PeriodEndId
+            });
+        }
+    }
+}
