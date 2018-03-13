60b160235797736d21850566dc6d801a3462fcb0
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-03-13 14:12:13 +0000

    Implemented the repository code for creating account transfers

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transfers/AccountTransfer.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transfers/AccountTransfer.cs
@@ -2,13 +2,19 @@
 
 namespace SFA.DAS.EAS.Domain.Models.Transfers
 {
+    //TODO: Remove this once the enum is present in the payment events api types nuget package
+    public enum AccountTransferType
+    {
+        None = 0
+    }
+
     public class AccountTransfer
     {
         public long SenderAccountId { get; set; }
         public long RecieverAccountId { get; set; }
         public long CommitmentId { get; set; }
         public decimal Amount { get; set; }
-        public string Type { get; set; }
+        public AccountTransferType Type { get; set; }
         public DateTime TransferDate { get; set; }
     }
 }
--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/TransferRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/TransferRepository.cs
@@ -4,19 +4,23 @@ using SFA.DAS.EAS.Domain.Data.Repositories;
 using SFA.DAS.EAS.Domain.Models.Transfers;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Sql.Client;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EAS.Infrastructure.Data
 {
     public class TransferRepository : BaseRepository, ITransferRepository
     {
+        private readonly ILog _logger;
         private readonly float _allowancePercentage;
 
         public TransferRepository(LevyDeclarationProviderConfiguration configuration, ILog logger)
             : base(configuration.DatabaseConnectionString, logger)
         {
+            _logger = logger;
             _allowancePercentage = configuration.TransferAllowancePercentage;
         }
 
@@ -37,9 +41,43 @@ namespace SFA.DAS.EAS.Infrastructure.Data
             return result ?? 0;
         }
 
-        public Task CreateAccountTransfers(IEnumerable<AccountTransfer> transfers)
+        public async Task CreateAccountTransfers(IEnumerable<AccountTransfer> transfers)
         {
-            throw new System.NotImplementedException();
+            await WithTransaction(async (connection, transaction) =>
+            {
+                var accountTransfers = transfers as AccountTransfer[] ?? transfers.ToArray();
+
+                try
+                {
+                    foreach (var transfer in accountTransfers)
+                    {
+                        var parameters = new DynamicParameters();
+                        parameters.Add("@senderAccountId", transfer.SenderAccountId, DbType.Int64);
+                        parameters.Add("@recieverAccountId", transfer.RecieverAccountId, DbType.Int64);
+                        parameters.Add("@commitmentId", transfer.CommitmentId, DbType.Int64);
+                        parameters.Add("@amount", transfer.Amount, DbType.Decimal);
+                        parameters.Add("@type", transfer.Type, DbType.Int16);
+                        parameters.Add("@transferDate", transfer.TransferDate, DbType.DateTime);
+
+                        await connection.ExecuteAsync(
+                            sql: "[employer_financial].[CreateAccountTransfer]",
+                            param: parameters,
+                            commandType: CommandType.StoredProcedure);
+                    }
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+
+                    var transfer = accountTransfers.FirstOrDefault();
+
+                    _logger.Error(ex, $"Failed to save transfers for account id {transfer?.SenderAccountId}");
+
+                    throw;
+                }
+            });
         }
     }
 }
