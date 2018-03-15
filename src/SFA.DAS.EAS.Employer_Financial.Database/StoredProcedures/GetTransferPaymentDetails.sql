3341d741a93c38114f28ac006ddf5f3a16e9f58a
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-03-15 15:35:11 +0000

    Added code to get the transfer details related to payments from the database

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetTransferPaymentDetails.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [employer_financial].[GetTransferPaymentDetails]
+	@receiverAccountId BIGINT,
+	@periodEnd NVARCHAR(20),
+	@apprenticeshipId BIGINT
+AS
+	SELECT 
+		meta.ApprenticeshipCourseName as CourseName
+		,COUNT(DISTINCT Uln) as ApprenticeCount
+		,SUM(p.Amount) as Total		
+	FROM [employer_financial].[Payment] p
+	INNER JOIN [employer_financial].[PaymentMetaData] meta ON p.PaymentMetaDataId = meta.Id
+	WHERE p.AccountId = @receiverAccountId
+	AND PeriodEnd = @periodEnd
+	AND ApprenticeshipId = @apprenticeshipId
+	GROUP BY meta.ApprenticeshipCourseName
---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transfers/AccountTransferPaymentDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.EAS.Domain.Models.Transfers
+{
+    public class AccountTransferPaymentDetails
+    {
+        public string CourseName { get; set; }
+        public uint ApprenticeCount { get; set; }
+        public decimal PaymentTotal { get; set; }
+    }
+}
