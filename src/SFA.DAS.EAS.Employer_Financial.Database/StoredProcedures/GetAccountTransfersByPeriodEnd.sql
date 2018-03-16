ff8a5430ca992e163c2b31b3c38b832e56376461
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-03-16 16:12:16 +0000

    Added repository code for getting transfers
    - Also fixed account repository incorrect query type

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransfersByPeriodEnd.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [employer_financial].[GetAccountTransfersByPeriodEnd]
+	@senderAccountId BIGINT,
+	@periodEnd NVARCHAR(20)
+AS
+	SELECT * FROM [employer_financial].[AccountTransfers]
+	WHERE SenderAccountId = @senderAccountId
+	AND PeriodEnd = @periodEnd
+
