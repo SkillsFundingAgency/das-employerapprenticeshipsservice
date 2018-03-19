7aae864d75317d820aaaba0c1279b577605dc023
Ewan Noble <ewan.noble@education.gov.uk>
2018-03-19 14:06:20 +0000

    Attempt to fix build failures

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -2,6 +2,9 @@ open Fake
 
 Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
-            { p with
-                Project = ""})
+        { p with
+                Project = ".\\SFA.DAS.Account.Api.Client" })
+    DotNetCli.Restore(fun p ->
+        { p with
+                Project = ".\\SFA.DAS.EAS.Account.Api.Types" })
 )
