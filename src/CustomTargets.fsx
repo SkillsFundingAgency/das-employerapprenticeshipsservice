0b23878a66b72a6c069412701be87eb7ae832a91
Noble <e.noble@live.co.uk>
2018-03-20 14:43:33 +0000

    Add custom test runner target

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -1,4 +1,11 @@
 open Fake
+
+let testDirectory = getBuildParamOrDefault "buildMode" "Debug"
+let nUnitRunner = "nunit3-console.exe"
+let mutable nUnitToolPath = @"tools\NUnit.ConsoleRunner\"
+let acceptanceTestPlayList = getBuildParamOrDefault "playList" ""
+let nunitTestFormat = getBuildParamOrDefault "nunitTestFormat" "nunit2"
+
 Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
@@ -6,4 +13,26 @@ Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
                 Project = ".\\SFA.DAS.EAS.Account.Api.Types" })
+)
+
+Target "Run EAS Acceptance Tests" (fun _ ->
+
+    trace "Run EAS Acceptance Tests"
+
+    let mutable shouldRunTests = false
+
+    let testDlls = !! ("./**/bin/" + testDirectory + "/*.AcceptanceTests.dll")
+
+    for testDll in testDlls do
+        shouldRunTests <- true
+
+    if shouldRunTests then
+        testDlls |> Fake.Testing.NUnit3.NUnit3 (fun p ->
+            {p with
+                ToolPath = (nUnitToolPath + @"tools\" + nUnitRunner);
+                StopOnError = false;
+                Agents = Some 1;
+                Testlist = acceptanceTestPlayList;
+                ResultSpecs = [("TestResult.xml;format=" + nunitTestFormat)];
+                })
 )
