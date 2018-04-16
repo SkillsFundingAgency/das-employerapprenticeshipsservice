a8b34d0d57b1cbbf41bdeb855c7687fd0bee36a6
Elliot Ritchie <elliotritchie@hotmail.com>
2018-04-16 16:23:40 +0100

    Updated agreement table to have a default version value of 1

--- src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreementTemplate.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreementTemplate.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
 	[PartialViewName] NVARCHAR(50) NOT NULL DEFAULT('_Agreement_v1'),
     [CreatedDate] DATETIME NOT NULL DEFAULT GETDATE(), 
-    [VersionNumber] INT NOT NULL DEFAULT 0
+    [VersionNumber] INT NOT NULL DEFAULT 1
 )
