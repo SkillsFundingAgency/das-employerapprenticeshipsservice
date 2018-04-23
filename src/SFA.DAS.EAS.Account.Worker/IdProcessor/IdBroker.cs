c4713f152121103385e58e289240045f68ebfbe8
Ian Gledhill <ian@gledhills.mobi>
2018-04-23 09:12:12 +0100

    Fixed some PR issues

--- src/SFA.DAS.EAS.Account.Worker/IdProcessor/IdBroker.cs
+++ src/SFA.DAS.EAS.Account.Worker/IdProcessor/IdBroker.cs
@@ -45,13 +45,13 @@ namespace SFA.DAS.EAS.Account.Worker.IdProcessor
 
         private async Task<ProcessingInfo> CreateProcessingInfoAsync(IIdProvider idProvider)
         {
-            var lastSuccessfulAccountProcessed = await _idCheckpoint.GetLastCheckpointAsync(idProvider);
+            var lastSuccessfullyProcessedId = await _idCheckpoint.GetLastCheckpointAsync(idProvider);
 
             var result = new ProcessingInfo
             {
                 BatchSize = _idProcessorConfiguration.BatchSize,
                 StartTime = DateTime.UtcNow,
-                LastProcessedId = lastSuccessfulAccountProcessed,
+                LastProcessedId = lastSuccessfullyProcessedId,
                 State = ProcessingState.InProgress
             };
 
@@ -64,17 +64,17 @@ namespace SFA.DAS.EAS.Account.Worker.IdProcessor
             IProcessor processor,
             ProcessingContext processingContext)
         {
-            var accountIds = await idProvider.GetIdsAsync(processingInfo.LastProcessedId, processingInfo.BatchSize, processingContext);
+            var ids = await idProvider.GetIdsAsync(processingInfo.LastProcessedId, processingInfo.BatchSize, processingContext);
                 
             var processedBatchSize = 0;
             long? previousId = null; 
 
             // the link stuff is here in case the provider did not respect the parameters that were passed to it.
-            foreach (var id in accountIds.Where(i => i > processingInfo.LastProcessedId).Take(processingInfo.BatchSize))
+            foreach (var id in ids.Where(i => i > processingInfo.LastProcessedId).Take(processingInfo.BatchSize))
             {
                 AssertIdsInCorrectOrder(id, ref previousId, processingInfo);
 
-                if (!await ProcessNextAccountIdAsync(processingInfo, id, processor, processingContext))
+                if (!await ProcessNextIdAsync(processingInfo, id, processor, processingContext))
                 {
                     break;
                 }
@@ -104,7 +104,7 @@ namespace SFA.DAS.EAS.Account.Worker.IdProcessor
             previousId = id;
         }
 
-        private async Task<bool> ProcessNextAccountIdAsync(ProcessingInfo processingInfo, long id, IProcessor processor, ProcessingContext processingContext)
+        private async Task<bool> ProcessNextIdAsync(ProcessingInfo processingInfo, long id, IProcessor processor, ProcessingContext processingContext)
         {
             bool shouldContinue;
             try
