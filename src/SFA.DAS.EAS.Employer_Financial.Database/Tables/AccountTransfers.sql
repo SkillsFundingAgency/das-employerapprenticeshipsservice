a6f50d5cb92a04b4775b2451d33d2b59d99b7949
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-23 13:22:19 +0100

    Added unique database index to stop duplicates being added

--- src/SFA.DAS.EAS.Employer_Financial.Database/Tables/AccountTransfers.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Tables/AccountTransfers.sql
@@ -1,15 +1,20 @@
 ﻿CREATE TABLE [employer_financial].[AccountTransfers]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SenderAccountId] BIGINT NOT NULL, 
-	[SenderAccountName] NVARCHAR(100) NOT NULL, 
+    [SenderAccountName] NVARCHAR(100) NOT NULL, 
     [ReceiverAccountId] BIGINT NOT NULL, 
-	[ReceiverAccountName] NVARCHAR(100) NOT NULL, 
+    [ReceiverAccountName] NVARCHAR(100) NOT NULL, 
     [ApprenticeshipId] BIGINT NOT NULL, 	
-	[CourseName] VARCHAR(MAX) NOT NULL, 
-	[PeriodEnd] nvarchar(20) NOT NULL,
+    [CourseName] VARCHAR(MAX) NOT NULL, 
+    [PeriodEnd] nvarchar(20) NOT NULL,
     [Amount] DECIMAL(18, 5) NOT NULL, 
     [Type] SMALLINT NOT NULL, 
     [TransferDate] DATETIME NOT NULL, 
     [CreatedDate] DATETIME NOT NULL
 )
+GO
+
+CREATE UNIQUE INDEX [IX_PeriodEndAccountTransfer]
+ON [employer_financial].[AccountTransfers]([ApprenticeshipId] ASC, [PeriodEnd] ASC)
+GO
