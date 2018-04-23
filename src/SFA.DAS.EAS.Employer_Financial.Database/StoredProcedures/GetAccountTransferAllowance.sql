618d72bc39682ec674da6c457cccb1166ce59dcd
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-23 10:48:45 +0100

    Fixed the SQL to return a correct figure.
    - Broke the SQL into multiple calls to make it easier to understand

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferAllowance.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAccountTransferAllowance.sql
@@ -4,13 +4,27 @@
 AS
 	SET NOCOUNT ON
 
-	SELECT	(SUM(lines.Amount) * @allowancePercentage) - SUM(transfers.Amount)
-	FROM		[employer_financial].[TransactionLine] as lines
-			INNER JOIN [employer_financial].[AccountTransfers] transfers ON lines.AccountId = transfers.SenderAccountId
+	DECLARE @previousYearLevyTotal DECIMAL(18,4)
+	DECLARE @transferAllowance DECIMAL(18,4)
+	DECLARE @transferSpent DECIMAL(18,5)
+
+	-- Get last years total levy
+	SELECT	@previousYearLevyTotal = SUM(lines.Amount)
+	FROM		[employer_financial].[TransactionLine] as lines			
 			CROSS JOIN employer_financial.GetPreviousFinancialYearDates(DEFAULT) as previousFinancialYear
 	WHERE	lines.TransactionDate >= previousFinancialYear.YearStart
 			AND lines.TransactionDate < previousFinancialYear.YearEnd
 			AND lines.TransactionType = 1			
-			AND lines.AccountId = @AccountId
-			AND transfers.TransferDate >= previousFinancialYear.YearEnd
+			AND lines.AccountId = @AccountId	
+
+	-- Get the transfer allowance for the current year
+	SET @transferAllowance = @previousYearLevyTotal * @allowancePercentage
+
+	-- Get total transfer allowance spent this year
+	SELECT @transferSpent = SUM(transfers.Amount) FROM [employer_financial].[AccountTransfers] transfers
+	CROSS JOIN employer_financial.GetPreviousFinancialYearDates(DEFAULT) as previousFinancialYear
+	WHERE transfers.TransferDate >= previousFinancialYear.YearEnd
+
+	-- Return the current available transfer allowance	
+	SELECT @transferAllowance - @transferSpent
 GO
