b36d18107480e37e2ea19f14f6f9e714520b442a
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-24 14:05:00 +0100

    Implemented draft agreement changes sections using stubs
    - The content and view logic has been done
    - We need to wire up some of the backend code with code that is being finished on a different branch
    - Stubs have been used to allow for the front end code to be tested. These will be removed when backend is wired up

--- src/SFA.DAS.EAS.Web/ViewModels/EmployerAgreementViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/EmployerAgreementViewModel.cs
@@ -1,9 +1,20 @@
 ﻿using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
+using System;
 
 namespace SFA.DAS.EAS.Web.ViewModels
 {
     public class EmployerAgreementViewModel
     {
         public EmployerAgreementView EmployerAgreement { get; set; }
+
+        public EmployerAgreementView PreviouslySignedEmployerAgreement => new EmployerAgreementView()
+        {
+            HashedAccountId = "ABC123",
+            HashedAgreementId = "DEF456",
+            SignedByName = "Test Person",
+            SignedDate = DateTime.Now,
+            LegalEntityName = "Test Corp"
+        };
     }
+
 }
