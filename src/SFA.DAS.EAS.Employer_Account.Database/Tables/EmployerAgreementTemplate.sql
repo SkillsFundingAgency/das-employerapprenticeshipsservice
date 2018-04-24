524b6f5bb6c13fb0d05fdc57f99fdb38060b166e
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-24 14:21:39 +0100

    Added indexes to the template table
    - Now version number must be unique
    - Removed unneeded default values
    - Added Id index to speed up lookups

--- src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreementTemplate.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreementTemplate.sql
@@ -1,7 +1,16 @@
 ﻿CREATE TABLE [employer_account].[EmployerAgreementTemplate]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-	[PartialViewName] NVARCHAR(50) NOT NULL DEFAULT('_Agreement_v1'),
-    [CreatedDate] DATETIME NOT NULL DEFAULT GETDATE(), 
-    [VersionNumber] INT NOT NULL DEFAULT 1
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PartialViewName] NVARCHAR(50) NOT NULL,
+    [CreatedDate] DATETIME NOT NULL, 
+    [VersionNumber] INT NOT NULL
 )
+GO
+
+CREATE INDEX [IX_TemplateId]
+ON [employer_account].[EmployerAgreementTemplate] (Id ASC)
+GO
+
+CREATE UNIQUE INDEX [IX_VersionNumber]
+ON [employer_account].[EmployerAgreementTemplate] ([VersionNumber] DESC)
+GO
