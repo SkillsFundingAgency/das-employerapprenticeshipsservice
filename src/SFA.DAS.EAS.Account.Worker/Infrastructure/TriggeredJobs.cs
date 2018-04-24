df376039f238d0240ba34cd95a360dcbcad2ae25
Ian Gledhill <ian@gledhills.mobi>
2018-04-24 08:49:03 +0100

    PR changes

--- src/SFA.DAS.EAS.Account.Worker/Infrastructure/TriggeredJobs.cs
+++ src/SFA.DAS.EAS.Account.Worker/Infrastructure/TriggeredJobs.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
 using SFA.DAS.EAS.Account.Worker.Infrastructure.Interfaces;
 using SFA.DAS.EAS.Account.Worker.Jobs;
 
@@ -11,10 +12,10 @@ namespace SFA.DAS.EAS.Account.Worker.Infrastructure
 {
 	public class TriggeredJobs
 	{
-		public void ProcessQueueMessage([QueueTrigger(Constants.AzureQueueNames.AdHocJobQueue)] AdHocJobParams jobParams, TextWriter logger)
+		public void ProcessQueueMessage([QueueTrigger(Constants.AzureQueueNames.AdHocJobQueue)] AdHocJobParams jobParams, TraceWriter logger)
 		{
 			// find implementation of IJob named jobParams.JobName
-			JobLogger.WriteLine(logger, $"Received request to process job type {jobParams.JobName}");
+			logger.Info($"Received request to process job type {jobParams.JobName}");
 			var jobtype = GetAllPossibleJobs()
 				.FirstOrDefault(jobType => string.Equals(jobType.Name, jobParams.JobName, StringComparison.InvariantCultureIgnoreCase));
 
@@ -25,33 +26,32 @@ namespace SFA.DAS.EAS.Account.Worker.Infrastructure
 		}
 
 		//                                    {second} {minute} {hour} {day} {month} {day-of-week}
-		public void PaymentCheck([TimerTrigger("0 30 5 1 * *")] TimerInfo timer, TextWriter logger)
+		public void PaymentCheck([TimerTrigger("0 30 5 1 * *")] TimerInfo timer, TraceWriter logger)
 		{
 			ScheduleJob<PaymentIntegrityCheckerJob>();	
 		}
 
-		private void RunJob(Type jobtype, TextWriter logger)
+		private void RunJob(Type jobtype, TraceWriter logger)
 		{
 		    using (var thisContainer = ServiceLocator.CreateNestedContainer())
 		    {
-		        ServiceLocator.Register<TextWriter>(thisContainer, logger);
+		        ServiceLocator.Register<TraceWriter>(thisContainer, logger);
 		        IJob job;
-		        logger.WriteLine($"found job type {jobtype.FullName}... resolving from IoC...");
+		        logger.Info($"found job type {jobtype.FullName}");
 		        try
 		        {
 		            job = (IJob) thisContainer.GetInstance(jobtype);
-		            JobLogger.WriteLine(logger, $"Obtained instance of type {jobtype.FullName} from IoC");
+		            logger.Verbose($"Obtained instance of type {jobtype.FullName} from IoC");
 		        }
 		        catch (Exception e)
 		        {
-		            JobLogger.WriteLine(logger,
-		                $"Failed to fetch instance of type {jobtype.FullName} from IoC - error: {e.GetType()} - {e.Message}");
+		            logger.Error($"Failed to fetch instance of type {jobtype.FullName} from IoC - error: {e.GetType()} - {e.Message}");
 		            throw;
 		        }
 
 		        job.Run().ContinueWith(task =>
 		        {
-		            JobLogger.WriteLine(logger, $"Job has ended. Canceled?:{task.IsCanceled} Faulted?:{task.IsFaulted}");
+		            logger.Info($"Job has ended. Canceled?:{task.IsCanceled} Faulted?:{task.IsFaulted}");
 		        });
 		    }
 		}
