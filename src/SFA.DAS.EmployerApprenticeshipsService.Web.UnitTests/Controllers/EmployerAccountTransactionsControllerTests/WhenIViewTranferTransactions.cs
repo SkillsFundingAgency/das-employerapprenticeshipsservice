93df5ec1f0e1f68fd75ac141e6221ffc93095b97
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-25 12:43:45 +0100

    Fixed merge issue

--- src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountTransactionsControllerTests/WhenIViewTranferTransactions.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Controllers/EmployerAccountTransactionsControllerTests/WhenIViewTranferTransactions.cs
@@ -2,12 +2,12 @@
 using MediatR;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.EAS.Application.Queries.GetAccountTransferTransactionDetails;
 using SFA.DAS.EAS.Application.Queries.GetTransactionsDownloadResultViewModel;
 using SFA.DAS.EAS.Application.Queries.GetTransferTransactionDetails;
 using SFA.DAS.EAS.Domain.Interfaces;
 using SFA.DAS.EAS.Domain.Models.Transfers;
-using SFA.DAS.EAS.Web.Authentication;
+using SFA.DAS.EAS.Infrastructure.Authentication;
+using SFA.DAS.EAS.Infrastructure.Authorization;
 using SFA.DAS.EAS.Web.Orchestrators;
 using SFA.DAS.EAS.Web.ViewModels;
 using SFA.DAS.EAS.Web.ViewModels.Transactions;
@@ -27,7 +27,7 @@ namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountTransactionsContr
         private Web.Controllers.EmployerAccountTransactionsController _controller;
         private Mock<EmployerAccountTransactionsOrchestrator> _orchestrator;
         private Mock<IAuthenticationService> _owinWrapper;
-        private Mock<IFeatureToggleService> _featureToggle;
+        private Mock<IAuthorizationService> _authorizationService;
         private Mock<IMultiVariantTestingService> _userViewTestingService;
         private Mock<ICookieStorageService<FlashMessageViewModel>> _flashMessage;
         private Mock<IHashingService> _hashingService;
@@ -40,7 +40,7 @@ namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountTransactionsContr
         {
             _orchestrator = new Mock<EmployerAccountTransactionsOrchestrator>();
             _owinWrapper = new Mock<IAuthenticationService>();
-            _featureToggle = new Mock<IFeatureToggleService>();
+            _authorizationService = new Mock<IAuthorizationService>();
             _userViewTestingService = new Mock<IMultiVariantTestingService>();
             _flashMessage = new Mock<ICookieStorageService<FlashMessageViewModel>>();
             _mapper = new Mock<IMapper>();
@@ -49,7 +49,7 @@ namespace SFA.DAS.EAS.Web.UnitTests.Controllers.EmployerAccountTransactionsContr
             _mediator = new Mock<IMediator>();
 
             _controller = new Web.Controllers.EmployerAccountTransactionsController(_owinWrapper.Object,
-                _featureToggle.Object, _hashingService.Object, _mediator.Object,
+                _authorizationService.Object, _hashingService.Object, _mediator.Object,
                 _orchestrator.Object, _userViewTestingService.Object, _flashMessage.Object,
                 Mock.Of<ITransactionFormatterFactory>(), _mapper.Object);
 
