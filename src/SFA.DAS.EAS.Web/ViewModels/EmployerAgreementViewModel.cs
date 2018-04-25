9d5bd346aa29d20dedb81f37b7bf479bf7e6312a
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-25 15:24:24 +0100

    Updated agreement action to use new controller work flow
    - Now controller take a request model
    - Maps mediator response to view model
    - Returns view model and not Orchestrator response
    - Updated tests to new approach as well.
    - Some code is commented out for updating later on in this story

--- src/SFA.DAS.EAS.Web/ViewModels/EmployerAgreementViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/EmployerAgreementViewModel.cs
@@ -1,20 +1,10 @@
 ﻿using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
-using System;
 
 namespace SFA.DAS.EAS.Web.ViewModels
 {
     public class EmployerAgreementViewModel
     {
         public EmployerAgreementView EmployerAgreement { get; set; }
-
-        public EmployerAgreementView PreviouslySignedEmployerAgreement => new EmployerAgreementView()
-        {
-            HashedAccountId = "ABC123",
-            HashedAgreementId = "DEF456",
-            SignedByName = "Test Person",
-            SignedDate = DateTime.Now,
-            LegalEntityName = "Test Corp"
-        };
+        public EmployerAgreementView PreviouslySignedEmployerAgreement { get; set; }
     }
-
 }
