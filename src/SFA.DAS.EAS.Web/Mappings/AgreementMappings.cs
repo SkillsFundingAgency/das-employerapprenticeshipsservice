09f16c83075e8886da750d1fc4acd34de43e7497
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-26 17:39:48 +0100

    Added mapping for agreements

---
+++ src/SFA.DAS.EAS.Web/Mappings/AgreementMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SFA.DAS.EAS.Application.Queries.GetEmployerAgreement;
+using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.Domain.Models.EmployerAgreement;
+using SFA.DAS.EAS.Web.ViewModels;
+
+namespace SFA.DAS.EAS.Web.Mappings
+{
+    public class AgreementMappings : Profile
+    {
+        public AgreementMappings()
+        {
+            CreateMap<EmployerAgreement, EmployerAgreementView>();
+            CreateMap<GetEmployerAgreementResponse, EmployerAgreementViewModel>();
+        }
+    }
+}
