6cfe49ece104cdfff1e8fb4a2a4a2bc3d640a1de
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-04-30 13:24:53 +0100

    Changes that came from code review. Mainly clean up.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Dtos/EmployerAgreement/EmployerAgreementDto.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Dtos/EmployerAgreement/EmployerAgreementDto.cs
@@ -5,17 +5,17 @@ namespace SFA.DAS.EAS.Application.Dtos.EmployerAgreement
 {
     public class EmployerAgreementDto
     {
-        public virtual long Id { get; set; }
-        public virtual string HashedAgreementId { get; set; }
-        public virtual long LegalEntityId { get; set; }
-        public virtual long AccountId { get; set; }
-        public virtual string HashedAccountId { get; set; }
-        public virtual int TemplateId { get; set; }
-        public virtual EmployerAgreementStatus StatusId { get; set; }
-        public virtual string SignedByName { get; set; }
-        public virtual DateTime? SignedDate { get; set; }
-        public virtual DateTime? ExpiredDate { get; set; }
-        public virtual long? SignedById { get; set; }
+        public long Id { get; set; }
+        public string HashedAgreementId { get; set; }
+        public long LegalEntityId { get; set; }
+        public long AccountId { get; set; }
+        public string HashedAccountId { get; set; }
+        public int TemplateId { get; set; }
+        public EmployerAgreementStatus StatusId { get; set; }
+        public string SignedByName { get; set; }
+        public DateTime? SignedDate { get; set; }
+        public DateTime? ExpiredDate { get; set; }
+        public long? SignedById { get; set; }
 
         public AccountDto Account { get; set; }
         public LegalEntityDto LegalEntity { get; set; }
