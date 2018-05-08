1976f88a64806f125242163b3c22d9e62bca6399
Elliot Ritchie <elliotritchie@hotmail.com>
2018-05-08 14:08:51 +0100

    Fixed a bug where the latest signed agreement version was incorrect for accounts where a new agreement was signed but not a previous one

--- src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLatestSignedAgreementVersionForAccount.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLatestSignedAgreementVersionForAccount.sql
@@ -5,18 +5,23 @@ AS
 
 	SELECT
 		CASE
-			WHEN MIN(v.VersionNumber) = 0 THEN NULL
-			ELSE MIN(v.VersionNumber)
+			WHEN MIN(m.VersionNumber) = 0 THEN NULL
+			ELSE MIN(m.VersionNumber)
 		END AS LatestSignedAgreementVersionNumber
 	FROM (
-		SELECT
-			CASE
-				WHEN a.StatusId = 1 THEN 0
-				WHEN a.StatusId = 2 THEN MAX(t.VersionNumber)
-			END AS VersionNumber
-		FROM employer_account.EmployerAgreement as a
-		JOIN employer_account.EmployerAgreementTemplate AS t ON t.Id = a.TemplateId 
-		WHERE a.AccountId = @accountId
-		AND a.StatusId IN (1, 2)
-		GROUP BY a.AccountId, a.LegalEntityId, a.StatusId
-	) AS v
+		SELECT MAX(v.VersionNumber) AS VersionNumber
+		FROM (
+			SELECT
+				a.AccountId,
+				a.LegalEntityId,
+				CASE
+					WHEN a.StatusId = 1 THEN 0
+					WHEN a.StatusId = 2 THEN t.VersionNumber
+				END AS VersionNumber
+			FROM [employer_account].[EmployerAgreement] a
+			JOIN [employer_account].[EmployerAgreementTemplate] t ON t.Id = a.TemplateId 
+			WHERE a.AccountId = @accountId
+			AND a.StatusId IN (1, 2)
+		) v
+		GROUP BY v.AccountId, v.LegalEntityId
+	) m
