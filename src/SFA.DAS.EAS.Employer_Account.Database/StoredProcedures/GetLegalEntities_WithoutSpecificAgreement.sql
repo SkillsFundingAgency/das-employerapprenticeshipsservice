1df0008a33ef99f581452dd5bffe7f4994366549
Ian Gledhill <ian@gledhills.mobi>
2018-05-08 22:01:50 +0100

    AML-2239 Fix query for returning legal entities that need an agreement creating

--- src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLegalEntities_WithoutSpecificAgreement.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetLegalEntities_WithoutSpecificAgreement.sql
@@ -7,12 +7,21 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT	TOP(@count) le.Id 
-	  FROM	employer_account.LegalEntity AS le
-			LEFT JOIN employer_account.EmployerAgreement AS ea
-				ON ea.LegalEntityId = le.Id
-					AND ea.TemplateId = @agreementId
-	WHERE	ea.Id IS NULL
-			AND le.id >= @firstId
-	ORDER BY le.Id
+	SELECT	TOP(@count) LegalEntityId
+	FROM		(SELECT		DISTINCT EA.LegalEntityId
+				FROM		employer_account.EmployerAgreement AS EA
+				WHERE	EA.LegalEntityId > @firstId
+						AND EXISTS(	SELECT	1 
+									FROM		employer_account.EmployerAgreement 
+									WHERE	LegalEntityId = EA.LegalEntityId 
+											AND AccountId = EA.AccountId 
+											AND StatusId != 5)
+						AND EXISTS(	SELECT	1 
+									FROM		employer_account.EmployerAgreement 
+									WHERE	LegalEntityId = EA.LegalEntityId 
+									GROUP BY AccountId 
+									HAVING MAX(TemplateId) < @agreementId)
+			) AS AffectedLegalEntities
+	ORDER BY AffectedLegalEntities.LegalEntityId 
+
 END
