9110449c1f1de0b32923b43db92bfce2f4a13bb3
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-05-08 09:30:14 +0100

    Removed commented out code

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Extensions/ExceptionExtensions.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Extensions/ExceptionExtensions.cs
@@ -7,31 +7,6 @@ namespace SFA.DAS.EAS.Infrastructure.Extensions
 {
     public static class ExceptionExtensions
     {
-        //public class ExceptionTypeFormatter
-        //{
-        //    public ExceptionTypeFormatter(Type exceptionType, Action<Exception, StringBuilder> build)
-        //    {
-        //        Build = build;
-        //        SupportedException = exceptionType;
-        //    }
-
-        //    public Type SupportedException { get; }
-        //    public Action<Exception, StringBuilder> Build { get; }
-        //}
-
-        //private static readonly ConcurrentDictionary<Type, ExceptionTypeFormatter> ExceptionTypeFormatterCache = new ConcurrentDictionary<Type, ExceptionTypeFormatter>();
-
-        //private static readonly ExceptionTypeFormatter LastChanceFormatter = new ExceptionTypeFormatter(typeof(Exception), BuildLastChanceMessage);
-
-        //private static readonly ExceptionTypeFormatter[] ExceptionFormatters = new ExceptionTypeFormatter[]
-        //{
-        //    // This will catch all exceptions
-        //    new ExceptionTypeFormatter(typeof(Exception), BuildExceptionMessage),
-        //    new ExceptionTypeFormatter(typeof(AggregateException), BuildAggregateExceptionMessage),
-        //    new ExceptionTypeFormatter(typeof(HttpException), BuildHttpExceptionMessage)
-        //};
-
-
         public static string GetMessage(this Exception exception)
         {
             const int reasonableInitialMessageSize = 200;
@@ -41,8 +16,6 @@ namespace SFA.DAS.EAS.Infrastructure.Extensions
 
             messageFormatter.AppendFormattedMessage(exception, messageBuilder);
 
-            //BuildDetailedExceptionMessage(exception, messageBuilder);
-
             return messageBuilder.ToString();
         }
 
@@ -50,99 +23,5 @@ namespace SFA.DAS.EAS.Infrastructure.Extensions
         {
             return ExceptionMessageFormatterFactory.GetFormatter(exception);
         }
-
-        //private static void BuildDetailedExceptionMessage(Exception exception, StringBuilder messageBuilder)
-        //{
-        //    if (exception == null)
-        //    {
-        //        return;
-        //    }
-
-        //    var handler = ExceptionTypeFormatterCache.GetOrAdd(exception.GetType(), GetAppropriateExceptionFormatter(exception));
-
-        //    handler.Build(exception, messageBuilder);
-
-        //    BuildDetailedExceptionMessage(exception.InnerException, messageBuilder);
-        //}
-
-        //public static ExceptionTypeFormatter GetAppropriateExceptionFormatter(Exception exception)
-        //{
-        //    ExceptionTypeFormatter result = null;
-
-        //    if (exception == null)
-        //    {
-        //        result = LastChanceFormatter;
-        //    }
-        //    else
-        //    {
-        //        var exceptionType = exception.GetType();
-
-        //        while (result == null)
-        //        {
-        //            // convert to dictionary
-        //            result = ExceptionFormatters.FirstOrDefault(ef => ef.SupportedException == exceptionType);
-        //            if (result == null)
-        //            {
-        //                if (exceptionType.BaseType == null)
-        //                {
-        //                    return LastChanceFormatter;
-        //                }
-
-        //                exceptionType = exceptionType.BaseType;
-        //            }
-        //        }
-        //    }
-
-        //    return result;
-        //}
-
-        //private static void BuildLastChanceMessage(Exception exception, StringBuilder messageBuilder)
-        //{
-        //    if (exception == null)
-        //    {
-        //        messageBuilder.Append("Exception is null");
-        //    }
-        //    else
-        //    {
-        //        messageBuilder.Append("Exception:");
-        //        messageBuilder.Append(exception.GetType().Name);
-        //        messageBuilder.Append(" Message:");
-        //        messageBuilder.Append(exception.Message);
-        //    }
-        //}
-
-        //private static void BuildExceptionMessage(Exception exception, StringBuilder messageBuilder)
-        //{
-        //    messageBuilder.Append("Exception:");
-        //    messageBuilder.Append(exception.GetType().Name);
-        //    messageBuilder.Append(" Message:");
-        //    messageBuilder.Append(exception.Message);
-        //}
-
-        //private static void BuildAggregateExceptionMessage(Exception exception, StringBuilder messageBuilder)
-        //{
-        //    var exceptions = ((AggregateException)exception).Flatten();
-
-        //    messageBuilder.AppendLine($"Aggregate exception has {exceptions.InnerExceptions.Count} inner exception.");
-        //    for (int i = 0; i < exceptions.InnerExceptions.Count; i++)
-        //    {
-        //        messageBuilder.Append("Exception:");
-        //        messageBuilder.Append(i);
-        //        BuildDetailedExceptionMessage(exceptions.InnerExceptions[i], messageBuilder);
-        //    }
-        //}
-
-        //private static void BuildHttpExceptionMessage(Exception exception, StringBuilder messageBuilder)
-        //{
-        //    var ex = (HttpException)exception;
-        //    messageBuilder.Append("Exception: ");
-        //    messageBuilder.Append(ex.GetType().Name);
-        //    messageBuilder.Append(" Message: ");
-        //    messageBuilder.Append(ex.Message);
-        //    messageBuilder.Append(" HTTP-StatusCode:");
-        //    messageBuilder.Append(ex.GetHttpCode());
-        //    messageBuilder.Append(" HTTP-Message:");
-        //    messageBuilder.Append(ex.GetHtmlErrorMessage());
-        //}
     }
 }
