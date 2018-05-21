dfd3ebd6389e643fd1508563e7eedead5e020067
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-05-21 10:38:34 +0100

    Added constraints for duplicate legal entities in agreements table and refactor some transfer sql

---
+++ src/SFA.DAS.EAS.Employer_Account.Database/Scripts/PreDeployment/RemoveDuplicateAccountLegalEntities.sql
@@ -0,0 +1,11 @@
+﻿ WITH duplicateRow as (SELECT Id, LegalEntityId, AccountId,  TemplateId, StatusId, row_number() 
+ OVER(PARTITION BY LegalEntityId, AccountId,TemplateId ORDER BY SignedDate DESC) AS [rn]
+ FROM [employer_account].[EmployerAgreement])
+
+ DELETE FROM [employer_account].[EmployerAgreement]
+ WHERE Id IN 
+ ( 
+	 SELECT Id FROM duplicateRow 
+	 WHERE rn > 1
+	 AND StatusId <> 5
+ )
--- src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreement.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Tables/EmployerAgreement.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [employer_account].[EmployerAgreement]
 (
-	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [LegalEntityId] BIGINT NOT NULL, 
-	[AccountId] BIGINT NULL,
+    [AccountId] BIGINT NULL,
     [TemplateId] INT NOT NULL, 
     [StatusId] TINYINT NOT NULL DEFAULT 1, 
     [SignedByName] NVARCHAR(100) NULL, 
@@ -11,10 +11,15 @@
     [SignedById] BIGINT NULL, 
     CONSTRAINT [FK_EmployerAgreement_LegalEntity] FOREIGN KEY ([LegalEntityId]) REFERENCES [employer_account].[LegalEntity]([Id]), 
     CONSTRAINT [FK_EmployerAgreement_Account] FOREIGN KEY ([AccountId]) REFERENCES [employer_account].[Account]([Id]), 
-    CONSTRAINT [FK_EmployerAgreement_SignedBy] FOREIGN KEY ([SignedById]) REFERENCES [employer_account].[User]([Id]),
+    CONSTRAINT [FK_EmployerAgreement_SignedBy] FOREIGN KEY ([SignedById]) REFERENCES [employer_account].[User]([Id])
 )
 GO
 
 CREATE INDEX [IX_EmployerAgreement_AccountId_StatusId]
 ON [employer_account].[EmployerAgreement] ([AccountId], [StatusId])
+GO
+
+CREATE UNIQUE INDEX [IX_EmployerAgreement_LegalEntity]
+ON [employer_account].[EmployerAgreement] (LegalEntityId, AccountId, TemplateId)
+WHERE StatusId <> 5
 GO
