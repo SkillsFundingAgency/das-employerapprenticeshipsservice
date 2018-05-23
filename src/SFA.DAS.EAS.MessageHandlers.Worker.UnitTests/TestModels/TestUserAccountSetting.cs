e9c40046d08d4ebc8491032bba786a354c63b239
DScuffle <david.scuffle@accomplish-it.co.uk>
2018-05-23 14:28:24 +0100

    AML-1946-2107-Transfer-Invitation-Response-Notification

---
+++ src/SFA.DAS.EAS.MessageHandlers.Worker.UnitTests/TestModels/TestUserAccountSetting.cs
@@ -0,0 +1,19 @@
+﻿using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.Domain.Models.UserProfile;
+
+namespace SFA.DAS.EAS.MessageHandlers.Worker.UnitTests.TestModels
+{
+    internal sealed class TestUserAccountSetting : UserAccountSetting
+    {
+        public TestUserAccountSetting() { }
+        public TestUserAccountSetting(Account account, User user,
+            bool receiveNotifications)
+        {
+            Account = account;
+            AccountId = account.Id;
+            User = user;
+            UserId = user.Id;
+            ReceiveNotifications = receiveNotifications;
+        }
+    }
+}
---
+++ src/SFA.DAS.EAS.MessageHandlers.Worker/EventHandlers/ApprovedTransferConnectionInvitationEventHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.EAS.Domain.Configuration;
+using SFA.DAS.EAS.Domain.Extensions;
+using SFA.DAS.EAS.Domain.Models.UserProfile;
+using SFA.DAS.EAS.Infrastructure.Data;
+using SFA.DAS.EmployerAccounts.Events.Messages;
+using SFA.DAS.Messaging;
+using SFA.DAS.Messaging.AzureServiceBus.Attributes;
+using SFA.DAS.Messaging.Interfaces;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Notifications.Api.Client;
+using SFA.DAS.Notifications.Api.Types;
+
+namespace SFA.DAS.EAS.MessageHandlers.Worker.EventHandlers
+{
+    [TopicSubscription("Task_ApprovedTransferConnectionInvitation")]
+    public class ApprovedTransferConnectionInvitationEventHandler : MessageProcessor<ApprovedTransferConnectionInvitationEvent>
+    {
+        private readonly EmployerApprenticeshipsServiceConfiguration _employerApprenticeshipsServiceConfiguration;
+
+        public const string UrlFormat = "/accounts/{0}/transfers";
+
+        private readonly EmployerAccountDbContext _db;
+        private readonly ILog _log;
+        private readonly INotificationsApi _notificationsApi;
+
+        public ApprovedTransferConnectionInvitationEventHandler(IMessageSubscriberFactory subscriberFactory, ILog log, EmployerAccountDbContext dbContext, INotificationsApi notificationsApi, EmployerApprenticeshipsServiceConfiguration employerApprenticeshipsServiceConfiguration) : base(subscriberFactory, log)
+        {
+            _db = dbContext;
+            _notificationsApi = notificationsApi;
+            _employerApprenticeshipsServiceConfiguration = employerApprenticeshipsServiceConfiguration;
+            _log = log;
+        }
+
+        protected override async Task ProcessMessage(ApprovedTransferConnectionInvitationEvent messageContent)
+        {
+            IQueryable<User> users;
+            try
+            {
+                users = _db.Users.UsersThatReceiveNotifications(messageContent.SenderAccountId);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex, $"Unable to find users that receive notifications for SenderAccountId '{messageContent.SenderAccountId}'");
+
+                return;
+            }
+
+            foreach (var owner in users)
+            {
+                try
+                {
+                    await _notificationsApi.SendEmail(CreateEmail(owner, messageContent.ReceiverAccountName,
+                        messageContent.SenderAccountHashedId));
+                }
+                catch (Exception ex)
+                {
+                    _log.Error(ex, $"Unable to send approved transfer invitation notification to userId {owner.Id} for Receiver Account Id {messageContent.ReceiverAccountId} ");
+                }
+            }
+        }
+
+        private Email CreateEmail(User user, string accountName, string senderExternalId)
+        {
+            return new Email
+            {
+                RecipientsAddress = user.Email,
+                TemplateId = "TransferConnectionRequestApproved",
+                ReplyToAddress = "noreply@sfa.gov.uk",
+                Subject = "x",
+                SystemId = "x",
+                Tokens = new Dictionary<string, string>
+                {
+                    {"name", user.FirstName},
+                    {"account_name", accountName},
+                    {
+                        "link_notification_page",
+                        $"\"{_employerApprenticeshipsServiceConfiguration.DashboardUrl}{string.Format(UrlFormat, senderExternalId)}\""
+                    }
+                }
+            };
+        }
+    }
+}
