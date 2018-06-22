eb2d2b8e392d831542d922025cbb62acb9aa0333
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-06-22 13:25:12 +0100

    Added Import Levy Declaration process

---
+++ src/SFA.DAS.EAS.Messages/Commands/IImportAccountLevyDeclarationsCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.EAS.Messages.Commands
+{
+    public interface IImportAccountLevyDeclarationsCommand
+    {
+        long AccountId { get; set; }
+        string PayeRef { get; set; }
+    }
+}
