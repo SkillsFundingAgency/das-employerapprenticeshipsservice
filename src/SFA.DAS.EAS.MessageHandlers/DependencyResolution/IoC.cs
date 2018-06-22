48cbd18274e516a52023e3aa63f9528800d4c5a3
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-06-22 12:08:00 +0100

    Added NserviceBus Endpoints

---
+++ src/SFA.DAS.EAS.MessageHandlers/DependencyResolution/IoC.cs
@@ -0,0 +1,15 @@
+﻿using StructureMap;
+
+namespace SFA.DAS.EAS.MessageHandlers.DependencyResolution
+{
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
+            return new Container(c =>
+            {
+                c.AddRegistry<DefaultRegistry>();
+            });
+        }
+    }
+}
