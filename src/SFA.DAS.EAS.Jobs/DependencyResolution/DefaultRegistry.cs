c72c5ccd3511ea5ec3e007ed09643e7e74b79746
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-06-22 13:25:12 +0100

    Added Import Levy Declaration process

--- src/SFA.DAS.EAS.Jobs/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.EAS.Jobs/DependencyResolution/DefaultRegistry.cs
@@ -6,7 +6,11 @@ namespace SFA.DAS.EAS.Jobs.DependencyResolution
     {
         public DefaultRegistry()
         {
-
+            Scan(s =>
+            {
+                s.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
+                s.RegisterConcreteTypesAgainstTheFirstInterface();
+            });
         }
     }
 }
---
+++ src/SFA.DAS.EAS.Jobs/Jobs.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using NServiceBus;
+using SFA.DAS.EAS.Jobs.DependencyResolution;
+using SFA.DAS.EAS.Messages.Commands;
+
+namespace SFA.DAS.EAS.Jobs
+{
+    public class Jobs
+    {
+        public void ImportLevyDeclarations([TimerTrigger("0 0 15 20 * *")] TimerInfo timer, TraceWriter logger)
+        {
+            var endpoint = ServiceLocator.Get<IEndpointInstance>();
+
+            endpoint.Send<IImportLevyDeclarationsCommand>(c => { });
+        }
+    }
+}
