278c8f2e0b98999db1d7bc8a11cbe101722be0b5
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-06-22 14:48:52 +0100

    Removed worker projects and tests which will be re-add later to fit the new infrastructure

--- src/SFA.DAS.EAS.Account.Worker/Jobs/IJob.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace SFA.DAS.EAS.Account.Worker.Jobs
-{
-    public interface IJob
-    {
-        Task Run();
-    }
-}
--- src/SFA.DAS.LevyDeclarationProvider.Worker/StartUp/IIS.bat
+++
@@ -1,4 +0,0 @@
-
-PowerShell.exe -NonInteractive -ExecutionPolicy Unrestricted "StartUp\TokenServiceCertInstall.PS1" >> log.txt
-
-EXIT /B 0
