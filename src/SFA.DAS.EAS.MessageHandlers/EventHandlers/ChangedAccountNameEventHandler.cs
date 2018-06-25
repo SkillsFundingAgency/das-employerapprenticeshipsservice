b01741f8693bd389e3b4c113fb7f2337d643a6a5
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-06-25 10:09:35 +0100

    Added NServiceBus account name changed event

---
+++ src/SFA.DAS.EAS.MessageHandlers/EventHandlers/ChangedAccountNameEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using NServiceBus;
+using SFA.DAS.EAS.Messages.Events;
+using SFA.DAS.EmployerAccounts.Events.Messages;
+using SFA.DAS.Messaging.Interfaces;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EAS.MessageHandlers.EventHandlers
+{
+    public class ChangedAccountNameEventHandler : IHandleMessages<IChangedAccountNameEvent>
+    {
+        private readonly IMessagePublisher _messagePublisher;
+
+        public ChangedAccountNameEventHandler(IMessagePublisher messagePublisher)
+        {
+            _messagePublisher = messagePublisher;
+        }
+
+        public async Task Handle(IChangedAccountNameEvent message, IMessageHandlerContext context)
+        {
+            await _messagePublisher.PublishAsync(new AccountNameChangedMessage(
+                message.PreviousName,
+                message.CurrentName,
+                message.AccountId,
+                message.UserName,
+                message.UserRef.ToString()));
+        }
+    }
+}
