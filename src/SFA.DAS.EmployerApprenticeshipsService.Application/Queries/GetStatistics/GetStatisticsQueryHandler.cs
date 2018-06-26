6c641b2dd11c4abdd2bd7bd632f99c6b1f1fda97
Mahinder Suniara <mahinder.suniara@btinternet.com>
2018-06-26 14:03:03 +0100

    Removed filter on TotalLegalEntities and TotalPaymentsThisYear

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetStatistics/GetStatisticsQueryHandler.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetStatistics/GetStatisticsQueryHandler.cs
@@ -23,18 +23,18 @@ namespace SFA.DAS.EAS.Application.Queries.GetStatistics
         public async Task<GetStatisticsResponse> Handle(GetStatisticsQuery message)
         {
             var accountsQuery = _accountDb.Accounts.FutureCount();
-            var legalEntitiesQuery = _accountDb.LegalEntities.Where(l => l.Status == "active").FutureCount();
+            var legalEntitiesQuery = _accountDb.LegalEntities.FutureCount();
             var payeSchemesQuery = _accountDb.Payees.FutureCount();
             var agreementsQuery = _accountDb.Agreements.Where(a => a.StatusId == EmployerAgreementStatus.Signed).FutureCount();
-            var paymentsQuery = _financialDb.Payments.Where(p => p.CollectionPeriodYear == DateTime.Now.Year).FutureCount();
+            var paymentsQuery = _financialDb.Payments.FutureCount();
 
             var statistics = new StatisticsViewModel
             {
                 TotalAccounts = await accountsQuery.ValueAsync(),
-                TotalActiveLegalEntities = await legalEntitiesQuery.ValueAsync(),
+                TotalLegalEntities = await legalEntitiesQuery.ValueAsync(),
                 TotalPayeSchemes = await payeSchemesQuery.ValueAsync(),
-                TotalSignedAgreements = await agreementsQuery.ValueAsync(),
-                TotalPaymentsThisYear = await paymentsQuery.ValueAsync()
+                TotalAgreements = await agreementsQuery.ValueAsync(),
+                TotalPayments = await paymentsQuery.ValueAsync()
             };
 
             return new GetStatisticsResponse
