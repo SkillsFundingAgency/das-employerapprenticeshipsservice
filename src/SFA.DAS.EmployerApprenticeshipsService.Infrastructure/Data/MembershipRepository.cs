a145dbc4999ae07672b761843b90b4417b671644
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-02 15:19:30 +0100

    Reworked account repositories to enlist in the current transaction

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/MembershipRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/MembershipRepository.cs
@@ -13,140 +13,136 @@ namespace SFA.DAS.EAS.Infrastructure.Data
 {
     public class MembershipRepository : BaseRepository, IMembershipRepository
     {
-        public MembershipRepository(EmployerApprenticeshipsServiceConfiguration configuration, ILog logger) : base(configuration.DatabaseConnectionString, logger)
+        private readonly Lazy<EmployerAccountDbContext> _db;
+
+        public MembershipRepository(EmployerApprenticeshipsServiceConfiguration configuration, ILog logger, Lazy<EmployerAccountDbContext> db)
+            : base(configuration.DatabaseConnectionString, logger)
         {
+            _db = db;
         }
 
         public async Task<TeamMember> Get(long accountId, string email)
         {
-            var result = await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@accountId", accountId, DbType.Int64);
-                parameters.Add("@email", email, DbType.String);
+            var parameters = new DynamicParameters();
 
-                return await c.QueryAsync<TeamMember>(
-                    sql: "SELECT * FROM [employer_account].[GetTeamMembers] WHERE AccountId = @accountId AND Email = @email;",
-                    param: parameters,
-                    commandType: CommandType.Text);
-            });
+            parameters.Add("@accountId", accountId, DbType.Int64);
+            parameters.Add("@email", email, DbType.String);
+
+            var result = await _db.Value.Database.Connection.QueryAsync<TeamMember>(
+                sql: "SELECT * FROM [employer_account].[GetTeamMembers] WHERE AccountId = @accountId AND Email = @email;",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.Text);
 
             return result.SingleOrDefault();
         }
 
         public async Task<Membership> Get(long userId, long accountId)
         {
-            var result = await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@accountId", accountId, DbType.Int64);
-                parameters.Add("@userId", userId, DbType.Int64);
+            var parameters = new DynamicParameters();
 
-                return await c.QueryAsync<Membership>(
-                    sql: "SELECT * FROM [employer_account].[Membership] WHERE AccountId = @accountId AND UserId = @userId;",
-                    param: parameters,
-                    commandType: CommandType.Text);
-            });
+            parameters.Add("@accountId", accountId, DbType.Int64);
+            parameters.Add("@userId", userId, DbType.Int64);
+
+            var result = await _db.Value.Database.Connection.QueryAsync<Membership>(
+                sql: "SELECT * FROM [employer_account].[Membership] WHERE AccountId = @accountId AND UserId = @userId;",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.Text);
 
             return result.SingleOrDefault();
         }
 
-        public async Task Remove(long userId, long accountId)
+        public Task Remove(long userId, long accountId)
         {
-            await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@UserId", userId, DbType.Int64);
-                parameters.Add("@AccountId", accountId, DbType.Int64);
+            var parameters = new DynamicParameters();
 
-                return await c.ExecuteAsync(
-                    sql: "[employer_account].[RemoveMembership]",
-                    param: parameters,
-                    commandType: CommandType.StoredProcedure);
-            });
+            parameters.Add("@UserId", userId, DbType.Int64);
+            parameters.Add("@AccountId", accountId, DbType.Int64);
+
+            return _db.Value.Database.Connection.ExecuteAsync(
+                sql: "[employer_account].[RemoveMembership]",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.StoredProcedure);
         }
 
-        public async Task ChangeRole(long userId, long accountId, short roleId)
+        public Task ChangeRole(long userId, long accountId, short roleId)
         {
-            await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@userId", userId, DbType.Int64);
-                parameters.Add("@accountId", accountId, DbType.Int64);
-                parameters.Add("@roleId", roleId, DbType.Int16);
+            var parameters = new DynamicParameters();
 
-                return await c.ExecuteAsync(
-                    sql: "UPDATE [employer_account].[Membership] SET RoleId = @roleId WHERE AccountId = @accountId AND UserId = @userId;",
-                    param: parameters,
-                    commandType: CommandType.Text);
-            });
+            parameters.Add("@userId", userId, DbType.Int64);
+            parameters.Add("@accountId", accountId, DbType.Int64);
+            parameters.Add("@roleId", roleId, DbType.Int16);
+
+            return _db.Value.Database.Connection.ExecuteAsync(
+                sql: "UPDATE [employer_account].[Membership] SET RoleId = @roleId WHERE AccountId = @accountId AND UserId = @userId;",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.Text);
         }
 
         public async Task<MembershipView> GetCaller(long accountId, string externalUserId)
         {
-            var result = await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@AccountId", accountId, DbType.Int64);
-                parameters.Add("@externalUserId", externalUserId, DbType.String);
+            var parameters = new DynamicParameters();
 
-                return await c.QueryAsync<MembershipView>(
-                    sql: "SELECT * FROM [employer_account].[MembershipView] m inner join [employer_account].account a on a.id=m.accountid WHERE a.Id = @AccountId AND UserRef = @externalUserId;",
-                    param: parameters,
-                    commandType: CommandType.Text);
-            });
+            parameters.Add("@AccountId", accountId, DbType.Int64);
+            parameters.Add("@externalUserId", externalUserId, DbType.String);
+
+            var result = await _db.Value.Database.Connection.QueryAsync<MembershipView>(
+                sql: "SELECT * FROM [employer_account].[MembershipView] m inner join [employer_account].account a on a.id=m.accountid WHERE a.Id = @AccountId AND UserRef = @externalUserId;",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.Text);
 
             return result.SingleOrDefault();
         }
 
         public async Task<MembershipView> GetCaller(string hashedAccountId, string externalUserId)
         {
-            var result = await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@hashedAccountId", hashedAccountId, DbType.String);
-                parameters.Add("@externalUserId", externalUserId, DbType.String);
+            var parameters = new DynamicParameters();
 
-                return await c.QueryAsync<MembershipView>(
-                    sql: "[employer_account].[GetTeamMember]",
-                    param: parameters,
-                    commandType: CommandType.StoredProcedure);
-            });
+            parameters.Add("@hashedAccountId", hashedAccountId, DbType.String);
+            parameters.Add("@externalUserId", externalUserId, DbType.String);
+
+            var result = await _db.Value.Database.Connection.QueryAsync<MembershipView>(
+                sql: "[employer_account].[GetTeamMember]",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.StoredProcedure);
 
             return result.SingleOrDefault();
         }
 
-        public async Task Create(long userId, long accountId, short roleId)
+        public Task Create(long userId, long accountId, short roleId)
         {
-            await WithConnection(async c =>
-            {
-                var parameters = new DynamicParameters();
-                parameters.Add("@userId", userId, DbType.Int64);
-                parameters.Add("@accountId", accountId, DbType.Int64);
-                parameters.Add("@roleId", roleId, DbType.Int16);
-                parameters.Add("@createdDate",DateTime.UtcNow, DbType.DateTime);
+            var parameters = new DynamicParameters();
 
-                return await c.ExecuteAsync(
-                    sql: "INSERT INTO [employer_account].[Membership] ([AccountId], [UserId], [RoleId], [CreatedDate]) VALUES(@accountId, @userId, @roleId, @createdDate); ",
-                    param: parameters,
-                    commandType: CommandType.Text);
-            });
+            parameters.Add("@userId", userId, DbType.Int64);
+            parameters.Add("@accountId", accountId, DbType.Int64);
+            parameters.Add("@roleId", roleId, DbType.Int16);
+            parameters.Add("@createdDate",DateTime.UtcNow, DbType.DateTime);
+
+            return _db.Value.Database.Connection.ExecuteAsync(
+                sql: "INSERT INTO [employer_account].[Membership] ([AccountId], [UserId], [RoleId], [CreatedDate]) VALUES(@accountId, @userId, @roleId, @createdDate); ",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.Text);
         }
 
-        public async Task SetShowAccountWizard(string hashedAccountId, string externalUserId, bool showWizard)
+        public Task SetShowAccountWizard(string hashedAccountId, string externalUserId, bool showWizard)
         {
-           await WithConnection(async c =>
-           {
-               var parameters = new DynamicParameters();
-               parameters.Add("@externalUserId", Guid.Parse(externalUserId), DbType.Guid);
-               parameters.Add("@hashedAccountId", hashedAccountId, DbType.String);
-               parameters.Add("@showWizard", showWizard, DbType.Boolean);
+            var parameters = new DynamicParameters();
 
-               return await c.ExecuteAsync(
-                   sql: "[employer_account].[UpdateShowWizard]",
-                   param: parameters,
-                   commandType: CommandType.StoredProcedure);
-           });
+            parameters.Add("@externalUserId", Guid.Parse(externalUserId), DbType.Guid);
+            parameters.Add("@hashedAccountId", hashedAccountId, DbType.String);
+            parameters.Add("@showWizard", showWizard, DbType.Boolean);
+
+            return _db.Value.Database.Connection.ExecuteAsync(
+                sql: "[employer_account].[UpdateShowWizard]",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.StoredProcedure);
         }
     }
 }
