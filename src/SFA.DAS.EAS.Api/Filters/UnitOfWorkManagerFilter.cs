3032b606c4102518397e01b233915a1046ef0b9a
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-04 16:59:53 +0100

    Added unit of work manager filter to api and mvc projects to manage employer finance db context transaction for now

---
+++ src/SFA.DAS.EAS.Api/Filters/UnitOfWorkManagerFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using SFA.DAS.EAS.Infrastructure.Data;
+using WebApi.StructureMap;
+
+namespace SFA.DAS.EAS.Account.Api.Filters
+{
+    public class UnitOfWorkManagerFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            actionContext.GetService<IUnitOfWorkManager>().Begin();
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Exception == null)
+            {
+                actionExecutedContext.GetService<IUnitOfWorkManager>().End();
+            }
+        }
+    }
+}
