60c59a7a61828f69be2550a2d1300f7f2847ccdb
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-04 16:59:53 +0100

    Added unit of work manager filter to api and mvc projects to manage employer finance db context transaction for now

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/IUnitOfWorkManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.EAS.Infrastructure.Data
+{
+    public interface IUnitOfWorkManager
+    {
+        void Begin();
+        void End(Exception ex = null);
+    }
+}
