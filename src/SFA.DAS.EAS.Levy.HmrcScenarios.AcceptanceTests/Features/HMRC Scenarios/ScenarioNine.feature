70009a8bfaf2fee176565d495754f3d70ab53afe
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-05 11:48:29 +0100

    Refactored NServiceBus infrastructure to be DB engine agnostic

--- src/SFA.DAS.EAS.Levy.HmrcScenarios.AcceptanceTests/Features/HMRC Scenarios/ScenarioNine.feature
+++
@@ -1,3 +0,0 @@
-﻿Feature: Scenario Nine - PAYE scheme moved to another account and subsequent adjustment
-
-# TODO - Worker based tests to be added when worker testing is created
---
+++ src/SFA.DAS.NServiceBus.UnitTests/OutboxMessageBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Moq;
+
+namespace SFA.DAS.NServiceBus.UnitTests
+{
+    public class OutboxMessageBuilder
+    {
+        private readonly Mock<OutboxMessage> _outboxMessage = new Mock<OutboxMessage>();
+
+        public OutboxMessageBuilder WithId(Guid id)
+        {
+            _outboxMessage.Setup(m => m.Id).Returns(id);
+
+            return this;
+        }
+
+        public OutboxMessageBuilder WithSent(DateTime sent)
+        {
+            _outboxMessage.Setup(m => m.Sent).Returns(sent);
+
+            return this;
+        }
+
+        public OutboxMessageBuilder WithPublished(DateTime? published)
+        {
+            _outboxMessage.Setup(m => m.Published).Returns(published);
+
+            return this;
+        }
+
+        public OutboxMessage Build()
+        {
+            return _outboxMessage.Object;
+        }
+    }
+}
