a79cdfbd60a9a6e905f9f041a4d7d92c5f93cd9d
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-07 11:43:34 +0100

    Added common testing infrastructure

--- src/SFA.DAS.NServiceBus/IUnitOfWorkContext.cs
+++ src/SFA.DAS.NServiceBus/IUnitOfWorkContext.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.NServiceBus
 {
     public interface IUnitOfWorkContext
     {
+        void AddEvent<T>(T message) where T : Event;
         void AddEvent<T>(Action<T> action) where T : Event, new();
         T Get<T>();
         IEnumerable<Event> GetEvents();
