062a224c64dd7506465207c742e788e3894ccef0
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-10 17:05:57 +0100

    Updated NServiceBus infrastructure ClientOutboxData table schema to be consistent with OutboxData table

--- src/SFA.DAS.EAS.Employer_Account.Database/Tables/ClientOutboxData.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Tables/ClientOutboxData.sql
@@ -2,12 +2,15 @@
 (
 	[MessageId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY NONCLUSTERED,
 	[EndpointName] NVARCHAR(150) NOT NULL,
-	[Created] DATETIME NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
 	[Dispatched] BIT NOT NULL DEFAULT(0),
 	[DispatchedAt] DATETIME NULL,
 	[Operations] NVARCHAR(MAX) NOT NULL
 )
 GO
 
-CREATE INDEX [IX_ClientOutboxData] ON [dbo].[ClientOutboxData] ([Created] ASC) WHERE [Dispatched] = 0
+CREATE INDEX [IX_CreatedAt] ON [dbo].[ClientOutboxData] ([CreatedAt] ASC) WHERE [Dispatched] = 0
+GO
+
+CREATE INDEX [IX_DispatchedAt] ON [dbo].[ClientOutboxData] ([DispatchedAt] ASC) WHERE [Dispatched] = 1
 GO
