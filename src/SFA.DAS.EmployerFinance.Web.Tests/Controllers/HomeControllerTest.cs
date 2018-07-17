473c379d921c2710a56c78aa8acc11785ae372fa
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-07-17 11:05:25 +0100

    Created new Accounts and Finance website for decoupling

---
+++ src/SFA.DAS.EmployerFinance.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFA.DAS.EmployerFinance.Web.Controllers;
+
+namespace SFA.DAS.EmployerFinance.Web.UnitTests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
