b0cbc996af8f6faed3f646d22cae02322c1cb1f5
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-07-17 11:05:25 +0100

    Created new Accounts and Finance website for decoupling

---
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerAccounts.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
