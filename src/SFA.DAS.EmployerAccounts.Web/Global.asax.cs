7ca5f0d5fcfefbf01ca3f0f2085743c7d422266d
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-07-19 09:42:26 +0100

    Added authentication to accounts and finance websites

--- src/SFA.DAS.EmployerAccounts.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Global.asax.cs
@@ -1,13 +1,10 @@
-﻿using System.Data.Common;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-using NServiceBus;
+﻿using NServiceBus;
 using SFA.DAS.EmployerAccounts.Configuration;
 using SFA.DAS.EmployerAccounts.Data;
 using SFA.DAS.EmployerAccounts.Extensions;
 using SFA.DAS.EmployerAccounts.Web.App_Start;
+using SFA.DAS.EmployerAccounts.Web.Logging;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
 using SFA.DAS.NServiceBus;
 using SFA.DAS.NServiceBus.EntityFramework;
 using SFA.DAS.NServiceBus.MsSqlServer;
@@ -15,26 +12,91 @@ using SFA.DAS.NServiceBus.Mvc;
 using SFA.DAS.NServiceBus.NewtonsoftSerializer;
 using SFA.DAS.NServiceBus.NLog;
 using SFA.DAS.NServiceBus.StructureMap;
+using SFA.DAS.Web.Policy;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Net;
+using System.Reflection;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.ApplicationInsights;
+using NLog;
+using SFA.DAS.Exceptions;
+using ConfigurationHelper = SFA.DAS.EmployerAccounts.Configuration.ConfigurationHelper;
+using Environment = SFA.DAS.EmployerAccounts.Configuration.Environment;
 
 namespace SFA.DAS.EmployerAccounts.Web
 {
     public class MvcApplication : HttpApplication
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
         private IEndpointInstance _endpoint;
 
         protected void Application_Start()
         {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            LoggingConfig.ConfigureLogging();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            if (ConfigurationHelper.IsEnvironmentAnyOf(Environment.Local, Environment.At, Environment.Test))
+            {
+                SystemDetailsViewModel.EnvironmentName = ConfigurationHelper.CurrentEnvironmentName;
+                SystemDetailsViewModel.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
 
             StartServiceBusEndpoint();
+        }
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            new HttpContextPolicyProvider(new List<IHttpContextPolicy> { new ResponseHeaderRestrictionPolicy() })
+                .Apply(new HttpContextWrapper(HttpContext.Current), PolicyConcern.HttpResponse);
         }
 
         protected void Application_End()
         {
             StopServiceBusEndpoint();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            if (exception is HttpException httpException && httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
+            {
+                return;
+            }
+
+            Dictionary<string, object> properties = null;
+
+            try
+            {
+                properties = new Dictionary<string, object>
+                {
+                    ["HttpMethod"] = Request.HttpMethod,
+                    ["IsAuthenticated"] = Request.IsAuthenticated,
+                    ["Url"] = Request.Url.PathAndQuery,
+                    ["UrlReferrer"] = Request.UrlReferrer?.PathAndQuery
+                };
+            }
+            catch (Exception)
+            {
+                // Request not available
+            }
+
+            var message = exception.GetMessage();
+            var telemetryClient = new TelemetryClient();
+
+            Logger.Error(exception, message, properties);
+            telemetryClient.TrackException(exception);
         }
 
         private void StartServiceBusEndpoint()
--- src/SFA.DAS.EmployerAccounts/Configuration/EmployerAccountsConfiguration.cs
+++ src/SFA.DAS.EmployerAccounts/Configuration/EmployerAccountsConfiguration.cs
@@ -1,8 +1,14 @@
-﻿namespace SFA.DAS.EmployerAccounts.Configuration
+﻿using SFA.DAS.Authenication;
+using SFA.DAS.Configuration;
+
+namespace SFA.DAS.EmployerAccounts.Configuration
 {
     public class EmployerAccountsConfiguration
     {
         public string DatabaseConnectionString { get; set; }
         public string ServiceBusConnectionString { get; set; }
+
+        public IdentityServerConfiguration Identity { get; set; }
+        public string DashboardUrl { get; set; }
     }
 }
