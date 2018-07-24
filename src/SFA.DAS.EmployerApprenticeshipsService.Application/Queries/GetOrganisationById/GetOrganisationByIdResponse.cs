fb6a10b3ef8c5b37608de15c773d0217cc596ab3
Ian Gledhill <ian@gledhills.mobi>
2018-07-24 09:27:53 +0100

    Add get id from reference data#

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetOrganisationById/GetOrganisationByIdResponse.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.EAS.Domain.Models.ReferenceData;
+using SFA.DAS.ReferenceData.Types.DTO;
+
+namespace SFA.DAS.EAS.Application.Queries.GetOrganisationById
+{
+    public class GetOrganisationByIdResponse
+    {
+        public Organisation Organisation { get; set; }
+    }
+}
