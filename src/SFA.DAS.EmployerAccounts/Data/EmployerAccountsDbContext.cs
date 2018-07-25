b8ca3572c163f803e358d4ed3d288d3dbb0388ef
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-25 10:32:12 +0100

    Added NServiceBus endpoint to employer accounts web

---
+++ src/SFA.DAS.EmployerAccounts/Data/EmployerAccountsDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Threading.Tasks;
+using SFA.DAS.NServiceBus;
+
+namespace SFA.DAS.EmployerAccounts.Data
+{
+    [DbConfigurationType(typeof(SqlAzureDbConfiguration))]
+    public class EmployerAccountsDbContext : DbContext
+    {
+        static EmployerAccountsDbContext()
+        {
+            Database.SetInitializer<EmployerAccountsDbContext>(null);
+        }
+
+        public EmployerAccountsDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        public EmployerAccountsDbContext(IUnitOfWorkContext unitOfWorkContext)
+            : base(unitOfWorkContext.Get<DbConnection>(), false)
+        {
+            Database.UseTransaction(unitOfWorkContext.Get<DbTransaction>());
+        }
+
+        protected EmployerAccountsDbContext()
+        {
+        }
+
+        public virtual Task<List<T>> SqlQueryAsync<T>(string query, params object[] parameters)
+        {
+            return Database.SqlQuery<T>(query, parameters).ToListAsync();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.HasDefaultSchema("employer_account");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts/DependencyResolution/DataRegistry.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using SFA.DAS.EmployerAccounts.Configuration;
+using SFA.DAS.EmployerAccounts.Data;
+using StructureMap;
+
+namespace SFA.DAS.EmployerAccounts.DependencyResolution
+{
+    public class DataRegistry : Registry
+    {
+        public DataRegistry()
+        {
+            For<DbConnection>().Use(c => new SqlConnection(c.GetInstance<EmployerAccountsConfiguration>().DatabaseConnectionString));
+            ForConcreteType<EmployerAccountsDbContext>();
+        }
+    }
+}
