daf1c5ad88c98e58c6ef80f59dbcf438d950057e
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-25 10:32:12 +0100

    Added NServiceBus endpoint to employer accounts web

--- src/SFA.DAS.EmployerAccounts.Web/Global.asax.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Global.asax.cs
@@ -1,21 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data.Common;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NServiceBus;
+using SFA.DAS.EmployerAccounts.Configuration;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.EmployerAccounts.Extensions;
+using SFA.DAS.EmployerAccounts.Web.App_Start;
+using SFA.DAS.NServiceBus;
+using SFA.DAS.NServiceBus.EntityFramework;
+using SFA.DAS.NServiceBus.MsSqlServer;
+using SFA.DAS.NServiceBus.Mvc;
+using SFA.DAS.NServiceBus.NewtonsoftSerializer;
+using SFA.DAS.NServiceBus.NLog;
+using SFA.DAS.NServiceBus.StructureMap;
 
 namespace SFA.DAS.EmployerAccounts.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private IEndpointInstance _endpoint;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            StartServiceBusEndpoint();
+        }
+
+        protected void Application_End()
+        {
+            StopServiceBusEndpoint();
+        }
+
+        private void StartServiceBusEndpoint()
+        {
+            var container = StructuremapMvc.StructureMapDependencyScope.Container;
+
+            var endpointConfiguration = new EndpointConfiguration("SFA.DAS.EmployerAccounts.Web")
+                .SetupAzureServiceBusTransport(() => container.GetInstance<EmployerAccountsConfiguration>().ServiceBusConnectionString)
+                .SetupEntityFrameworkUnitOfWork<EmployerAccountsDbContext>()
+                .SetupErrorQueue()
+                .SetupInstallers()
+                //.SetupLicense(container.GetInstance<EmployerApprenticeshipsServiceConfiguration>().NServiceBusLicense)
+                .SetupMsSqlServerPersistence(() => container.GetInstance<DbConnection>())
+                .SetupNewtonsoftSerializer()
+                .SetupNLogFactory()
+                .SetupOutbox(GlobalFilters.Filters)
+                .SetupStructureMapBuilder(container);
+
+            _endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            container.Configure(c =>
+            {
+                c.For<IMessageSession>().Use(_endpoint);
+            });
+        }
+
+        private void StopServiceBusEndpoint()
+        {
+            _endpoint?.Stop().GetAwaiter().GetResult();
         }
     }
 }
---
+++ src/SFA.DAS.EmployerAccounts/Configuration/Environment.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.EmployerAccounts.Configuration
+{
+    public enum Environment
+    {
+        Unknown,
+        Local,
+        At,
+        Test,
+        Prod,
+        Mo,
+        Demo
+    }
+}
