421a6a9cc681a0f596265496185a061b2222d78f
Elliot Ritchie <elliotritchie@hotmail.com>
2018-07-31 19:01:28 +0100

    Fixed broken unit tests

--- src/SFA.DAS.EAS.Infrastructure.UnitTests/Extensions/ExceptionExtensionTests.cs
+++ src/SFA.DAS.EAS.Infrastructure.UnitTests/Extensions/ExceptionExtensionTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Net;
+using System.Net.Http;
 using System.Web;
 using NUnit.Framework;
 using SFA.DAS.EAS.Infrastructure.Extensions;
@@ -26,7 +27,7 @@ namespace SFA.DAS.EAS.Infrastructure.UnitTests.Extensions
         [Test]
         public void GetAppropriateExceptionFormatter_WhenCalledWithHttpException_GetsHttpExceptionHandler()
         {
-            TestExpectedExceptionHandler(new HttpException("outermessage"), typeof(HttpException));
+            TestExpectedExceptionHandler(new HttpRequestException("outermessage"), typeof(HttpRequestException));
         }
 
         [Test]
@@ -116,7 +117,7 @@ namespace SFA.DAS.EAS.Infrastructure.UnitTests.Extensions
         [Test]
         public void GetDetailedMessage_WhenCalledWithHttpExceptions_ContainsHttpErrorCode()
         {
-            TestDetailedMessage(new HttpException((int)HttpStatusCode.Conflict, ""),
+            TestDetailedMessage(new HttpRequestException((int)HttpStatusCode.Conflict + ""),
                 ((int)HttpStatusCode.Conflict).ToString(CultureInfo.InvariantCulture));
         }
 
