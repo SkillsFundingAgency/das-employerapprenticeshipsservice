bcfa8fac8d2be8e999a8c68f386c47705acbd2b3
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-08-02 09:28:09 +0100

    Added code to support users getting saved to database in employer accounts project

--- src/SFA.DAS.EmployerAccounts.Web/DependencyResolution/IoC.cs
+++ src/SFA.DAS.EmployerAccounts.Web/DependencyResolution/IoC.cs
@@ -9,6 +9,9 @@ namespace SFA.DAS.EmployerAccounts.Web.DependencyResolution
         {
             return new Container(c =>
             {
+                c.AddRegistry<RepositoriesRegistry>();
+                c.AddRegistry<MediatorRegistry>();
+                c.AddRegistry<LoggerRegistry>();
                 c.AddRegistry<ConfigurationRegistry>();
                 c.AddRegistry<TokenServiceRegistry>();
                 c.AddRegistry<DataRegistry>();
---
+++ src/SFA.DAS.EmployerAccounts/DependencyResolution/RepositoriesRegistry.cs
@@ -0,0 +1,13 @@
+﻿using SFA.DAS.EmployerAccounts.Data;
+using StructureMap;
+
+namespace SFA.DAS.EmployerAccounts.DependencyResolution
+{
+    public class RepositoriesRegistry : Registry
+    {
+        public RepositoriesRegistry()
+        {
+            For<IUserRepository>().Use<UserRepository>();
+        }
+    }
+}
