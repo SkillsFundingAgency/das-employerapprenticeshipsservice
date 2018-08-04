5275000a983e5600794b6dadea4e28eedebdbd1a
Elliot Ritchie <elliotritchie@hotmail.com>
2018-08-04 13:40:54 +0100

    Fixed broken unit test project

---
+++ src/SFA.DAS.UnitTests/NoopTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.UnitTests
+{
+    [TestFixture]
+    public class NoopTests
+    {
+        [Test]
+        public void Noop()
+        {
+        }
+    }
+}
