d8b54ffa03cd7e1d92e632f3fbbdcf6b0ade4dd0
Elliot Ritchie <elliotritchie@hotmail.com>
2018-08-04 12:38:42 +0100

    Enabled NServiceBus license setup

---
+++ src/SFA.DAS/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace SFA.DAS.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string HtmlDecode(this string input)
+        {
+            var output = WebUtility.HtmlDecode(input);
+            return output;
+        }
+
+        public static string HtmlEncode(this string input)
+        {
+            var output = WebUtility.HtmlEncode(input);
+            return output;
+        }
+    }
+}
