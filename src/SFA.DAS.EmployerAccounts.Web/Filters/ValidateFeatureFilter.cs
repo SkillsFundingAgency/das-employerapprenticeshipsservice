4c2187694820001db5535a53d33fbd6c08ebb900
EwanNoble <ewan.noble@education.gov.uk>
2018-08-07 09:17:12 +0100

    Add Filters, Filter config and dependencies

---
+++ src/SFA.DAS.EmployerAccounts.Web/Filters/ValidateFeatureFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using SFA.DAS.EmployerAccounts.Authorization;
+using SFA.DAS.EmployerAccounts.Features;
+using SFA.DAS.EmployerAccounts.Web.Extensions;
+
+namespace SFA.DAS.EmployerAccounts.Web.Filters
+{
+    public class ValidateFeatureFilter : ActionFilterAttribute
+    {
+        private readonly Func<IAuthorizationService> _authorizationService;
+
+        public ValidateFeatureFilter(Func<IAuthorizationService> authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var featureAttribute = filterContext.ActionDescriptor.GetCustomAttribute<FeatureAttribute>();
+
+            if (featureAttribute != null)
+            {
+                var featureType = featureAttribute.FeatureType;
+                var isAuthorized = _authorizationService().IsAuthorized(featureType);
+
+                if (!isAuthorized)
+                {
+                    filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+            }
+        }
+    }
+}
