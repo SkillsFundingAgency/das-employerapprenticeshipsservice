6e53c8cc29197a094d8233e3090b4c06fca6befc
EwanNoble <ewan.noble@education.gov.uk>
2018-08-07 09:17:12 +0100

    Add Filters, Filter config and dependencies

---
+++ src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerAccounts.Web.ViewModels
+{
+    public abstract class AccountViewModel : IAccountViewModel
+    {
+        [IgnoreMap]
+        public long AccountId { get; set; }
+
+        [IgnoreMap]
+        public string AccountHashedId { get; set; }
+    }
+}
