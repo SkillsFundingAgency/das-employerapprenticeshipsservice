d19604c7f6ea7014bccbc61876b5e5f707587fe4
Elliot Ritchie <elliotritchie@hotmail.com>
2018-08-08 08:29:35 +0100

    Reverted accounts and finance web unit test csproj files to default .NET Framework format

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFA.DAS.EmployerAccounts.Web.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFA.DAS.EmployerAccounts.Web.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f16390b-e824-4ef9-86a1-7533235035cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
