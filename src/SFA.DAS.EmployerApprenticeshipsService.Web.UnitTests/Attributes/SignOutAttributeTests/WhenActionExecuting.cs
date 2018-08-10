a51b3f35fd8c9aca6f1b574abc2ac9b6e6753d73
DScuffle <david.scuffle@accomplish-it.co.uk>
2018-08-10 11:55:49 +0100

    AML-2660 Updated the invitations page to automatically logout the user if theyre already logged in

---
+++ src/SFA.DAS.EmployerApprenticeshipsService.Web.UnitTests/Attributes/SignOutAttributeTests/WhenActionExecuting.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Infrastructure.Authentication;
+using SFA.DAS.EAS.Web.Attributes;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Attributes.SignOutAttributeTests
+{
+    [TestFixture]
+    public class WhenActionExecuting
+    {
+        private Web.Attributes.SignOutAttribute _filter;
+        private Mock<IAuthenticationService> _authorizationService;
+        private ActionExecutingContext _filterContext;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _authorizationService = new Mock<IAuthenticationService>();
+            _filterContext = new ActionExecutingContext();
+
+            _filter = new SignOutAttribute (() => _authorizationService.Object);
+        }
+
+        [Test]
+        public void SignOutUserIsCalled()
+        {
+            _filter.OnActionExecuting(_filterContext);
+
+            _authorizationService.Verify(a => a.SignOutUser(), Times.Once);
+        }
+    }
+}
