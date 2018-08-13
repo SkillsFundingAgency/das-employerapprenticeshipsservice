719a9ec2eed06778a70ef9662dc5da2694434e80
Elliot Ritchie <elliotritchie@hotmail.com>
2018-08-13 15:26:34 +0100

    Added error handling to accounts and finance web projects

---
+++ src/SFA.DAS.EmployerFinance.Web/Filters/HandleErrorFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerFinance.Web.Filters
+{
+    public class HandleErrorFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.Exception is UnauthorizedAccessException)
+            {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
