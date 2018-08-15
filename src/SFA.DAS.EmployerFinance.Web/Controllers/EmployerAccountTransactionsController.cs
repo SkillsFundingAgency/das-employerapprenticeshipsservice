56cf60ba41ac1f20b8a1bce44c0152dcd3fd5b8f
Iain Stevenson <IainStevenson@live.co.uk>
2018-08-15 11:51:53 +0100

    Fulfilled links in ProviderPaymentSummaryView added dummy controller, constants  and methods.

--- src/SFA.DAS.EmployerFinance.Web/Controllers/EmployerAccountTransactionsController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/EmployerAccountTransactionsController.cs
@@ -2,10 +2,7 @@
 using SFA.DAS.EmployerFinance.Web.Helpers;
 using SFA.DAS.EmployerFinance.Web.Orchestrators;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SFA.DAS.EmployerFinance.Web.Controllers
@@ -45,6 +42,33 @@ namespace SFA.DAS.EmployerFinance.Web.Controllers
             var viewModel = await _accountTransactionsOrchestrator.GetProviderPaymentSummary(hashedAccountId, ukprn, fromDate, toDate, OwinWrapper.GetClaimValue(ControllerConstants.UserRefClaimKeyName));
 
             return View(ControllerConstants.ProviderPaymentSummaryViewName, viewModel);
+        }
+
+        [Route("finance/course/standard/summary")]
+        [Route("balance/course/standard/summary")]
+        public async Task<ActionResult> CourseStandardPaymentSummary(string hashedAccountId, long ukprn, string courseName,
+            int? courseLevel, DateTime fromDate, DateTime toDate)
+        {
+            return await CourseFrameworkPaymentSummary(hashedAccountId, ukprn, courseName, courseLevel, null, fromDate, toDate);
+        }
+
+        [Route("finance/course/framework/summary")]
+        [Route("balance/course/framework/summary")]
+        public async Task<ActionResult> CourseFrameworkPaymentSummary(string hashedAccountId, long ukprn, string courseName,
+            int? courseLevel, int? pathwayCode, DateTime fromDate, DateTime toDate)
+        {
+            var viewModel = await _accountTransactionsOrchestrator.GetCoursePaymentSummary(
+                hashedAccountId, ukprn, courseName, courseLevel, pathwayCode,
+                fromDate, toDate, OwinWrapper.GetClaimValue(ControllerConstants.UserRefClaimKeyName));
+
+            return View(ControllerConstants.CoursePaymentSummaryViewName, viewModel);
+        }
+
+       
+        public ActionResult Index()
+        {
+            // Dummy method for replacement by future merge
+            throw new NotImplementedException();
         }
     }
 }
--- src/SFA.DAS.EmployerFinance.Web/Helpers/ControllerConstants.cs
+++ src/SFA.DAS.EmployerFinance.Web/Helpers/ControllerConstants.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SFA.DAS.EmployerFinance.Web.Helpers
+﻿namespace SFA.DAS.EmployerFinance.Web.Helpers
 {
     public static class ControllerConstants
     {
         public const string ProviderPaymentSummaryViewName = "ProviderPaymentSummary";
         public const string UserRefClaimKeyName = "sub";
+        public const string CoursePaymentSummaryViewName = "CoursePaymentSummary";
     }
 }
