d8b0927bb0c30367146161027613acfc1cb1bd8c
Iain Stevenson <IainStevenson@live.co.uk>
2018-08-22 15:14:26 +0100

    Physically migrated CoursePaymentSummary view and controler methods, models and orchestration support classes

--- src/SFA.DAS.EmployerFinance/Services/DasLevyService.cs
+++ src/SFA.DAS.EmployerFinance/Services/DasLevyService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFA.DAS.EmployerFinance.Queries.AccountTransactions.GetAccountCoursePayments;
 
 namespace SFA.DAS.EmployerFinance.Services
 {
@@ -25,6 +26,23 @@ namespace SFA.DAS.EmployerFinance.Services
             {
                 AccountId = accountId,
                 UkPrn = ukprn,
+                FromDate = fromDate,
+                ToDate = toDate
+            });
+
+            return result?.Transactions?.OfType<T>().ToList() ?? new List<T>();
+        }
+        public async Task<ICollection<T>> GetAccountCoursePaymentsByDateRange<T>(
+            long accountId, long ukprn, string courseName, int? courseLevel, int? pathwayCode, DateTime fromDate,
+            DateTime toDate) where T : TransactionLine
+        {
+            var result = await _mediator.SendAsync(new GetAccountCoursePaymentsQuery
+            {
+                AccountId = accountId,
+                UkPrn = ukprn,
+                CourseName = courseName,
+                CourseLevel = courseLevel,
+                PathwayCode = pathwayCode,
                 FromDate = fromDate,
                 ToDate = toDate
             });
