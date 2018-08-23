4f57017c9be9b5f0c09117000a0ccd0ede16cbf5
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-08-23 15:37:12 +0100

    Added unit tests for code added to finance project

--- src/SFA.DAS.EmployerFinance/Models/Account/Account.cs
+++ src/SFA.DAS.EmployerFinance/Models/Account/Account.cs
@@ -3,6 +3,7 @@ namespace SFA.DAS.EmployerFinance.Models.Account
     public class Account
     {
         public virtual long Id { get; set; }
+        public virtual string HashedId { get; set; }
         public virtual string Name { get; set; }
     }
 }
