45c28e0fe267a5d4a36bbf03b85e0b4bb017a761
Ian Gledhill <ian@gledhills.mobi>
2018-09-06 12:39:20 +0100

    Add bare-bones accounts and finance API projects

---
+++ src/SFA.DAS.EmployerAccounts.Api/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SFA.DAS.EmployerAccounts.Api;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SFA.DAS.EmployerAccounts.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+            //{
+            //    Tenant = CloudConfigurationManager.GetSetting("idaTenant"),
+            //    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
+            //    {
+            //        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+            //        ValidAudience = CloudConfigurationManager.GetSetting("idaAudience")
+            //    }
+            //});
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+[assembly: OwinStartup(typeof(SFA.DAS.EAS.Account.Api.Startup))]
+
+namespace SFA.DAS.EAS.Account.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+            //{
+            //    Tenant = CloudConfigurationManager.GetSetting("idaTenant"),
+            //    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
+            //    {
+            //        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+            //        ValidAudience = CloudConfigurationManager.GetSetting("idaAudience")
+            //    }
+            //});
+        }
+    }
+}
