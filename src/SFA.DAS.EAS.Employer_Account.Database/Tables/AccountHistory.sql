0f92e0f5177b03cfc437cd4f6153319e659389c9
Elliot Ritchie <elliotritchie@hotmail.com>
2018-09-14 10:43:11 +0100

    Fixed a bug where a race condition would allow a paye scheme to be added to multiple accounts

--- src/SFA.DAS.EAS.Employer_Account.Database/Tables/AccountHistory.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Tables/AccountHistory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [employer_account].[AccountHistory]
 (
-	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
 	[AccountId] BIGINT NOT NULL,
 	[PayeRef] VARCHAR(20) NOT NULL,
 	[AddedDate] DATETIME NOT NULL,
@@ -8,7 +8,8 @@
 	CONSTRAINT [FK_AccountHistory_Account] FOREIGN KEY (AccountId) REFERENCES [employer_account].[Account]([Id])
 )
 GO
-CREATE INDEX [IDX_AccountHistory_Account] on [employer_account].[AccountHistory] (AccountId, PayeRef)
+
+CREATE INDEX [IX_AccountId_PayeRef] ON [employer_account].[AccountHistory] (AccountId, PayeRef)
 GO
-CREATE UNIQUE NONCLUSTERED INDEX [IDX_UNIQUE_AccountHistory_Account_Paye] on [employer_account].[AccountHistory] (AccountId, PayeRef)
-WHERE RemovedDate IS NULL
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_PayeRef_RemovedDate] ON [employer_account].[AccountHistory] (PayeRef) WHERE RemovedDate IS NULL
