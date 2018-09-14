d218642f488fc4b5576479dfb8d71379a1deb14c
Elliot Ritchie <elliotritchie@hotmail.com>
2018-09-14 18:29:36 +0100

    Fxed broken transfer journey redirects

--- src/SFA.DAS.EAS.Web/Controllers/TransferConnectionInvitationsController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/TransferConnectionInvitationsController.cs
@@ -22,14 +22,14 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route]
         public ActionResult Index()
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests"));
         }
 
         [ImportModelStateFromTempData]
         [Route("start")]
         public ActionResult Start()
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/start?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/start"));
         }
 
         [HttpNotFoundForNullModel]
@@ -45,7 +45,7 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route("{transferConnectionInvitationId}/sent")]
         public ActionResult Sent(GetSentTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/sent?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/sent"));
         }
 
         [HttpNotFoundForNullModel]
@@ -53,7 +53,7 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route("{transferConnectionInvitationId}/receive")]
         public ActionResult Receive(GetReceivedTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/receive?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/receive"));
         }
 
 
@@ -62,7 +62,7 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route("{transferConnectionInvitationId}/approved")]
         public ActionResult Approved(GetApprovedTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/approved?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/approved"));
         }
 
         [HttpNotFoundForNullModel]
@@ -70,28 +70,28 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route("{transferConnectionInvitationId}/rejected")]
         public ActionResult Rejected(GetRejectedTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/rejected?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/rejected"));
         }
 
         [HttpNotFoundForNullModel]
         [Route("{transferConnectionInvitationId}/details")]
         public ActionResult Details(GetTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/details?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{query.TransferConnectionInvitationId}/details"));
         }
 
         [ImportModelStateFromTempData]
         [Route("{transferConnectionInvitationId}/deleted")]
         public ActionResult Deleted(string transferConnectionInvitationId)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{transferConnectionInvitationId}/deleted?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/{transferConnectionInvitationId}/deleted"));
         }
 
         [HttpGet]
         [Route("outstanding")]
         public ActionResult Outstanding(GetLatestPendingReceivedTransferConnectionInvitationQuery query)
         {
-            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/outstanding?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"transfers/connections/requests/outstanding"));
         }
     }
 }
