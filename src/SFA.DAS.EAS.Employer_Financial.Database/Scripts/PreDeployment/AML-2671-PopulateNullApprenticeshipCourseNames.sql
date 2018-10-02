73b9945316ef6fcee1bd749c3e0c2a8fba577b92
Peter Williams <peter.david.williams.1991@gmail.com>
2018-10-02 16:24:07 +0100

    Added data fix to pre-deployment folder

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/PreDeployment/AML-2671-PopulateNullApprenticeshipCourseNames.sql
@@ -0,0 +1,50 @@
+﻿BEGIN TRY
+BEGIN TRANSACTION
+
+DECLARE @Frameworks as table(
+        FrameworkCode int,
+        ProgrammeType int,
+        PathwayCode int,
+        PathwayName varchar(max),
+        ApprenticeshipCourseName varchar(max));
+
+INSERT INTO @Frameworks (FrameworkCode, ProgrammeType, PathwayCode, PathwayName, ApprenticeshipCourseName)
+SELECT FrameworkCode, ProgrammeType, PathwayCode, PathwayName, ApprenticeshipCourseName
+FROM (
+    SELECT    FrameworkCode, ProgrammeType, PathwayCode, PathwayName, ApprenticeshipCourseName,
+            ROW_NUMBER() OVER (
+                PARTITION BY FrameworkCode,ProgrammeType, PathwayCode
+                ORDER BY FrameworkCode,ProgrammeType, PathwayCode)  AS RowNumber
+    FROM    [employer_financial].PaymentMetaData
+    WHERE    PathwayName IS NOT NULL
+            AND    ApprenticeshipCourseName IS NOT NULL) AS T1
+WHERE T1.RowNumber = 1
+ORDER BY 1, 2, 3
+
+		UPDATE Bad
+		SET Bad.ApprenticeshipCourseName = FW.ApprenticeshipCourseName,
+		Bad.PathwayName = FW.PathwayName
+		FROM [employer_financial].[PaymentMetaData] AS Bad
+		JOIN @Frameworks AS FW
+		ON FW.FrameworkCode = Bad.FrameworkCode
+		AND FW.ProgrammeType = Bad.ProgrammeType
+		AND FW.PathwayCode = Bad.PathwayCode
+		WHERE Bad.PathwayName IS NULL
+		AND Bad.ApprenticeshipCourseName IS NULL
+
+END TRY
+BEGIN CATCH
+DECLARE @ErrorMsg nvarchar(max)
+DECLARE @ErrorSeverity INT;
+DECLARE @ErrorState INT;
+SET @ErrorMsg = ERROR_NUMBER() + ERROR_LINE() + ERROR_MESSAGE()
+SET @ErrorSeverity = ERROR_SEVERITY()
+SET @ErrorState = ERROR_STATE()
+
+IF @@TRANCOUNT > 0
+    ROLLBACK TRANSACTION
+
+RAISERROR(@ErrorMsg, @ErrorSeverity, @ErrorState)
+END CATCH
+IF @@TRANCOUNT > 0
+ COMMIT TRANSACTION
