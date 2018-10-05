fb64679abdbf4514c037a5d5518bf51e3468d3f0
DScuffle <david.scuffle@accomplish-it.co.uk>
2018-10-05 13:42:02 +0100

    CShtml now compiles

--- src/SFA.DAS.EAS.Web/ViewModels/PaymentTransactionViewModel.cs
+++ src/SFA.DAS.EAS.Web/ViewModels/PaymentTransactionViewModel.cs
@@ -1,13 +1,13 @@
-﻿//using System.Collections.Generic;
-//using SFA.DAS.EAS.Domain.Models.Payments;
-//using SFA.DAS.EAS.Web.Models;
+﻿using System.Collections.Generic;
+using SFA.DAS.EAS.Domain.Models.Payments;
+using SFA.DAS.EAS.Web.Models;
 
-//namespace SFA.DAS.EAS.Web.ViewModels
-//{
-//    public class PaymentTransactionViewModel : TransactionLineViewModel<PaymentTransactionLine>
-//    {
-//        public string ProviderName { get; set; }
+namespace SFA.DAS.EAS.Web.ViewModels
+{
+    public class PaymentTransactionViewModel : TransactionLineViewModel<PaymentTransactionLine>
+    {
+        public string ProviderName { get; set; }
 
-//        public ICollection<ApprenticeshipPaymentGroup> CoursePaymentGroups { get; set; }
-//    }
-//}
+        public ICollection<ApprenticeshipPaymentGroup> CoursePaymentGroups { get; set; }
+    }
+}
