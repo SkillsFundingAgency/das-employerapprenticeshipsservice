2ffb7b36461165079fd5dffeb4c31b38f5cef1a0
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-10-10 16:02:48 +0100

    Updated script and added to project

--- src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GetAccountTransactionTotalsByMonth.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GetAccountTransactionTotalsByMonth.sql
@@ -1,11 +1,13 @@
-﻿SELECT
+﻿DECLARE @AccountId BIGINT = 1
+
+SELECT 
   DATEPART(year, DateCreated) AS Year, DATEPART(month, DateCreated) AS Month,
-	SUM(CASE WHEN TransactionType = 1 THEN Amount ELSE 0 END) AS 'Levy' ,
+  SUM(CASE WHEN TransactionType = 1 THEN Amount ELSE 0 END) AS 'Levy' ,
   SUM(CASE WHEN TransactionType = 2 THEN Amount ELSE 0 END) AS 'TopUp' ,
   SUM(CASE WHEN TransactionType = 3 THEN Amount ELSE 0 END) AS 'Payment' ,
   SUM(CASE WHEN TransactionType = 4 THEN Amount ELSE 0 END) AS 'Transfer'
 FROM employer_financial.TransactionLine
-WHERE AccountId = 1
+WHERE AccountId = @AccountId
 AND DateCreated < '2018-11-01 0:00:00'
 GROUP BY DATEPART(year, DateCreated), DATEPART(month, DateCreated)
 ORDER BY 1,2
