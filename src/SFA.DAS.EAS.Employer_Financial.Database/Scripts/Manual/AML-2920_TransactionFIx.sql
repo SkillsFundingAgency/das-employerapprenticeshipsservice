613199eeb33c9e67dd4f033109c8ea3480216c1a
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-10-10 10:28:53 +0100

    Added SQL script to use when correct data is available

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/AML-2920_TransactionFIx.sql
@@ -0,0 +1,36 @@
+﻿
+-- Replace values below with correct values and then run the script on a per tranaction basis
+DECLARE @SubmissionId BIGINT = 1000
+DECLARE @TransactionAmount DECIMAL = 1000
+DECLARE @AdjustmentAmount DECIMAL = 1000
+
+
+BEGIN TRY
+BEGIN TRANSACTION
+
+UPDATE employer_financial.TransactionLine
+SET Amount = @TransactionAmount
+WHERE SubmissionId = @SubmissionId
+
+UPDATE employer_financial.LevyDeclaration
+SET EndOfYearAdjustmentAmount = @AdjustmentAmount
+WHERE SubmissionId = @SubmissionId
+
+END TRY
+BEGIN CATCH
+	DECLARE @ErrorMsg nvarchar(max)
+	DECLARE @ErrorSeverity INT;
+	DECLARE @ErrorState INT;
+
+	SET @ErrorMsg = ERROR_NUMBER() + ERROR_LINE() + ERROR_MESSAGE()
+	SET @ErrorSeverity = ERROR_SEVERITY()
+	SET @ErrorState = ERROR_STATE()
+
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRANSACTION
+
+	RAISERROR(@ErrorMsg, @ErrorSeverity, @ErrorState)
+END CATCH
+
+IF @@TRANCOUNT > 0
+	COMMIT TRANSACTION
