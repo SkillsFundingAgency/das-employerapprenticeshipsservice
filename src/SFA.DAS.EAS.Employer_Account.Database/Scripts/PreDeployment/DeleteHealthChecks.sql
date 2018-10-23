c68d552e41a3515f3cd062485b9d2954670028d5
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-10-23 14:21:00 +0100

    Added table exists check

--- src/SFA.DAS.EAS.Employer_Account.Database/Scripts/PreDeployment/DeleteHealthChecks.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Scripts/PreDeployment/DeleteHealthChecks.sql
@@ -1,1 +1,7 @@
-﻿DELETE FROM HealthChecks
+﻿IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'employer_account' 
+                 AND  TABLE_NAME = 'HealthChecks'))
+BEGIN
+DELETE FROM HealthChecks
+END
