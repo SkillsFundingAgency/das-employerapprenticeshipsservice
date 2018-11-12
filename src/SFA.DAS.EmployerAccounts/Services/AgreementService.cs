41ef21aea5d337c9aebfc67dce6b27b3a23c0bae
Iain Stevenson <IainStevenson@live.co.uk>
2018-11-12 08:44:21 +0000

    Working Build

---
+++ src/SFA.DAS.EmployerAccounts/Services/AgreementService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.Entity;
+using System.Globalization;
+using System.Threading.Tasks;
+using SFA.DAS.Caches;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.EmployerAccounts.Features;
+
+namespace SFA.DAS.EmployerAccounts.Services
+{
+    public class AgreementService : IAgreementService
+    {
+        private const int NullCacheValue = -1;
+
+        private readonly Lazy<EmployerAccountsDbContext> _db;
+        private readonly IDistributedCache _cache;
+
+        public AgreementService(Lazy<EmployerAccountsDbContext> db, IDistributedCache cache)
+        {
+            _db = db;
+            _cache = cache;
+        }
+
+        public async Task<int?> GetAgreementVersionAsync(long accountId)
+        {
+            var version = await _cache.GetOrAddAsync(GetCacheKeyForAccount(accountId), k => GetMinAgreementVersionAsync(accountId)).ConfigureAwait(false);
+
+            if (version == NullCacheValue)
+            {
+                return null;
+            }
+
+            return version;
+        }
+
+        private async Task<int> GetMinAgreementVersionAsync(long accountId)
+        {
+            var versionNumber = await _db.Value.AccountLegalEntities
+                .WithSignedOrPendingAgreementsForAccount(accountId)
+                .MinAsync(ale => ale.SignedAgreementId == null ? 0 : (int)ale.SignedAgreementVersion)
+                .ConfigureAwait(false);
+
+            return versionNumber > 0 ? versionNumber : NullCacheValue;
+        }
+
+        public Task RemoveFromCacheAsync(long accountId)
+        {
+            return _cache.RemoveFromCache(GetCacheKeyForAccount(accountId));
+        }
+
+        private string GetCacheKeyForAccount(long accountId)
+        {
+            return "AccountId:" + accountId.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
