7dbb59f455a3b768214d2204edde9ab0d2db373b
Paul Graham <paul.graham@amido.com>
2018-11-15 10:52:57 +0000

    Added code to create the database and collection via the start up job

---
+++ src/SFA.DAS.EmployerAccounts.Jobs.UnitTests/StartupJobs/CreateReadStoreDatabaseJobTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Jobs.StartupJobs;
+using SFA.DAS.EmployerAccounts.ReadStore.Data;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.EmployerAccounts.Jobs.UnitTests.StartupJobs
+{
+    [TestFixture]
+    [Parallelizable]
+    public class CreateReadStoreDatabaseJobTests : FluentTest<CreateReadStoreDatabaseJobTestsFixture>
+    {
+        [Test]
+        public Task Run_WhenRunningCreateReadStoreDatabaseJob_ThenShouldCreateReadStoreDatabase()
+        {
+            return RunAsync(f => f.Run(), f => f.DocumentClient.Verify(c => c.CreateDatabaseIfNotExistsAsync(It.Is<Database>(d => d.Id == DocumentSettings.DatabaseName), null), Times.Once));
+        }
+
+        [Test]
+        public Task Run_WhenRunningCreateReadStoreDatabaseJob_ThenShouldCreateReadStoreUsersCollection()
+        {
+            return RunAsync(f => f.Run(), f => f.DocumentClient.Verify(c => c.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(DocumentSettings.DatabaseName),
+                It.Is<DocumentCollection>(d =>
+                    d.Id == DocumentSettings.UsersCollectionName &&
+                    d.PartitionKey.Paths.Contains("/userRef") &&
+                    d.UniqueKeyPolicy.UniqueKeys[0].Paths.Contains("/accountId")
+                ), null), Times.Once));
+        }
+    }
+
+    public class CreateReadStoreDatabaseJobTestsFixture
+    {
+        public Mock<IDocumentClient> DocumentClient { get; set; }
+        public CreateReadStoreDatabaseJob CreateReadStoreDatabaseJob { get; set; }
+
+        public CreateReadStoreDatabaseJobTestsFixture()
+        {
+            DocumentClient = new Mock<IDocumentClient>();
+            CreateReadStoreDatabaseJob = new CreateReadStoreDatabaseJob(DocumentClient.Object);
+        }
+
+        public Task Run()
+        {
+            return CreateReadStoreDatabaseJob.Run();
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Jobs/StartupJobs/CreateReadStoreDatabaseJob.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.WebJobs;
+using SFA.DAS.EmployerAccounts.ReadStore.Data;
+
+namespace SFA.DAS.EmployerAccounts.Jobs.StartupJobs
+{
+    public class CreateReadStoreDatabaseJob
+    {
+        private readonly IDocumentClient _documentClient;
+
+        public CreateReadStoreDatabaseJob(IDocumentClient documentClient)
+        {
+            _documentClient = documentClient; 
+        }
+
+        [NoAutomaticTrigger]
+        public async Task Run()
+        {
+            var database = new Database
+            {
+                Id = DocumentSettings.DatabaseName
+            };
+
+            var documentCollection = new DocumentCollection
+            {
+                Id = DocumentSettings.UsersCollectionName,
+                PartitionKey = new PartitionKeyDefinition
+                {
+                    Paths = new Collection<string>
+                        {
+                            "/userRef"
+                        }
+                },
+                UniqueKeyPolicy = new UniqueKeyPolicy
+                {
+                    UniqueKeys = new Collection<UniqueKey>
+                        {
+                            new UniqueKey
+                            {
+                                Paths = new Collection<string> { "/accountId" }
+                            }
+                        }
+                }
+            };
+
+            await _documentClient.CreateDatabaseIfNotExistsAsync(database);
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(database.Id), documentCollection);
+        }
+    }
+}
