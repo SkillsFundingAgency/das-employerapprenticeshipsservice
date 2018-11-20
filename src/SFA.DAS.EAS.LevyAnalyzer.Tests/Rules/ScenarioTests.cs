6e1a383920aabf2ebc00320af303bdcefc67475e
Ian Gledhill <ian@gledhills.mobi>
2018-11-20 10:25:47 +0000

    Scenarios 1-18 working

--- src/SFA.DAS.EAS.LevyAnalyzer.Tests/Rules/ScenarioTests.cs
+++ src/SFA.DAS.EAS.LevyAnalyzer.Tests/Rules/ScenarioTests.cs
@@ -11,7 +11,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
     public class ScenarioTests
     {
         [Test]
-        public void Scenario1_P1toP11AllOnTime()
+        public void Scenario01_P1toP11AllOnTime()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -43,7 +43,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
         }
 
         [Test]
-        public void Scenario2_P12OnTime()
+        public void Scenario02_P12OnTime()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -55,7 +55,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
         }
 
         [Test]
-        public void Scenario3_P1toP12AllOnTime()
+        public void Scenario03_P1toP12AllOnTime()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -89,7 +89,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
         }
 
         [Test]
-        public void Scenario4_P1toP11SomeLate()
+        public void Scenario04_P1toP11SomeLate()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -119,7 +119,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
         }
 
         [Test]
-        public void Scenario5_P1toP11AllLate()
+        public void Scenario05_P1toP11AllLate()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -140,7 +140,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
 
 
         [Test]
-        public void Scenario6_P1toP11WithSuperseded()
+        public void Scenario06_P1toP11WithSuperseded()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -153,7 +153,7 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
         }
 
         [Test]
-        public void Scenario7_P1toP12WithSupersededPeriod12()
+        public void Scenario07_P1toP12WithSupersededP12()
         {
             var fixtures = new LevyAnalyzerTestFixtures()
                 .WithEmpRef("ABC/012345")
@@ -163,6 +163,150 @@ namespace SFA.DAS.EAS.LevyAnalyser.Tests.Rules
 
                 .WithTransaction(121, 100)
                 .WithTransaction(123, 300);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario08_P12WithSupersededP12()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithEmpRef("ABC/012345")
+                .WithOntimeLevy(122, "18-19", 12, new DateTime(2019, 04, 11), 200)
+                .WithOntimeLevy(123, "18-19", 12, new DateTime(2019, 04, 10), 400)
+
+                .WithTransaction(123, 400);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario09_PostiveYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(122, "18-19", 12, new DateTime(2019, 04, 11), 200)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 400)
+
+                .WithTransaction(122, 200)
+                .WithTransaction(123, 200);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario10_NegativeYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(122, "18-19", 12, new DateTime(2019, 04, 11), 200)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 100)
+
+                .WithTransaction(122, 200)
+                .WithTransaction(123, -100);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario11_OnTimePeriodP1toP11AndNegativeYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(122, "18-19", 2, new DateTime(2018, 06, 11), 200)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 100)
+
+                .WithTransaction(122, 200)
+                .WithTransaction(123, -100);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario13_OnTimePeriodP1toP11AndYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithLateLevy(122, "18-19", 2, new DateTime(2018, 06, 11), 200)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 100)
+
+                .WithTransaction(123, 100);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario14_OnTimeP1toP11WithSupersededAndYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(121, "18-19", 2, new DateTime(2018, 06, 11), 200)
+                .WithOntimeLevy(122, "18-19", 2, new DateTime(2018, 06, 12), 300)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 500)
+
+                .WithTransaction(122, 300)
+                .WithTransaction(123, 200);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario15_OnTimeP12WithSupersededP12AndYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(121, "18-19", 12, new DateTime(2019, 04, 11), 200)
+                .WithOntimeLevy(122, "18-19", 12, new DateTime(2019, 04, 12), 300)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 500)
+
+                .WithTransaction(122, 300)
+                .WithTransaction(123, 200);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario16_OnTimeP1toP11WithMultipleAdjacentYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(121, "18-19", 2, new DateTime(2018, 06, 11), 200)
+                .WithOntimeLevy(122, "18-19", 3, new DateTime(2018, 07, 12), 300)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 05, 21), 500)
+                .WithYearEndAdjustment(124, "18-19", new DateTime(2019, 05, 22), 800)
+                .WithYearEndAdjustment(125, "18-19", new DateTime(2019, 05, 23), 1200)
+
+                .WithTransaction(121, 200)
+                .WithTransaction(122, 100)
+                .WithTransaction(123, 200)
+                .WithTransaction(124, 300)
+                .WithTransaction(125, 400);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario17_OnTimeP12WithMultipleIsolatedYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithOntimeLevy(121, "18-19", 12, new DateTime(2019, 04, 10), 200)
+                .WithYearEndAdjustment(122, "18-19", new DateTime(2019, 05, 21), 500)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 06, 22), 900)
+                .WithYearEndAdjustment(124, "18-19", new DateTime(2019, 07, 23), 1400)
+
+                .WithTransaction(121, 200)
+                .WithTransaction(122, 300)
+                .WithTransaction(123, 400)
+                .WithTransaction(124, 500);
+
+            fixtures.AssertAllRulesAreValid();
+        }
+
+        [Test]
+        public void Scenario18_LateP1toP11MultipleIsolatedYearEndAdjustments()
+        {
+            var fixtures = new LevyAnalyzerTestFixtures()
+                .WithLateLevy(121, "18-19", 2, new DateTime(2018, 05, 10), 200)
+                .WithYearEndAdjustment(122, "18-19", new DateTime(2019, 05, 21), 500)
+                .WithYearEndAdjustment(123, "18-19", new DateTime(2019, 06, 22), 900)
+                .WithYearEndAdjustment(124, "18-19", new DateTime(2019, 07, 23), 1200)
+
+                .WithTransaction(122, 500)
+                .WithTransaction(123, 400)
+                .WithTransaction(124, 300);
 
             fixtures.AssertAllRulesAreValid();
         }
