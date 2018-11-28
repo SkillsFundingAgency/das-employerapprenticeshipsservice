4cef4702061a6b54d43d2cfd30e4411b77c710a3
Paul Graham <paul.graham@amido.com>
2018-11-28 15:19:39 +0000

    MessageHandler Test Harness added

---
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness.DependencyResolution;
+using SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness.Scenarios;
+
+namespace SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            using (var container = IoC.Initialize())
+            {
+                var startup = container.GetInstance<NServiceBusStartup>();
+
+                await startup.StartAsync();
+
+                var scenario = container.GetInstance<PublishUserRolesUpdatedEvents>();
+
+                try
+                {
+                    await scenario.Run();
+                }
+                finally
+                {
+                    await startup.StopAsync();
+                }
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness/Scenarios/PublishUserRolesUpdatedEvents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.EmployerAccounts.Messages.Events;
+using SFA.DAS.EmployerAccounts.Types.Models;
+
+namespace SFA.DAS.EmployerAccounts.MessageHandlers.TestHarness.Scenarios
+{
+    public class PublishUserRolesUpdatedEvents
+    {
+        private readonly IMessageSession _messageSession;
+
+        public PublishUserRolesUpdatedEvents(IMessageSession messageSession)
+        {
+            _messageSession = messageSession;
+        }
+
+        public async Task Run()
+        {
+            Guid userRef = Guid.NewGuid(); // Guid.Parse("3FCDF9A5-695B-4B16-A710-CBA708E2AA30");
+            long accountId = 2134;
+            var roles = new HashSet<UserRole> {UserRole.Viewer};
+
+            var newUserEvent = new UserRolesUpdatedEvent(accountId, userRef.ToString(), roles, DateTime.Now) ;
+
+            await _messageSession.Publish(newUserEvent);
+
+            var updateUserEvent = new UserRolesUpdatedEvent(accountId, userRef.ToString(), roles, DateTime.Now);
+
+            await _messageSession.Publish(updateUserEvent);
+        }
+    }
+}
