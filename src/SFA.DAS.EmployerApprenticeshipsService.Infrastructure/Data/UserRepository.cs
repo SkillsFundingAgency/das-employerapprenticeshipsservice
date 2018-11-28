16328d758247e0ad884f3fd9fa492fe59e0907de
Scott Wakefield <scott@expertcc.co.uk>
2018-11-28 16:32:42 +0000

    AML-3061 reset files back to master.

--- src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/UserRepository.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Infrastructure/Data/UserRepository.cs
@@ -37,13 +37,11 @@ namespace SFA.DAS.EAS.Infrastructure.Data
             var parameters = new DynamicParameters();
 
             parameters.Add("@userRef", new Guid(id), DbType.Guid);
-            var connection = _db.Value.Database.Connection;
-            var transaction = _db.Value.Database.CurrentTransaction;
 
-            var result = await connection.QueryAsync<User>(
+            var result = await _db.Value.Database.Connection.QueryAsync<User>(
                 sql: "SELECT Id, CONVERT(varchar(64), UserRef) as UserRef, Email, FirstName, LastName FROM [employer_account].[User] WHERE UserRef = @userRef",
                 param: parameters,
-                transaction: transaction.UnderlyingTransaction,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
                 commandType: CommandType.Text);
 
             return result.SingleOrDefault();
