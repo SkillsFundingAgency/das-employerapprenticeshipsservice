a60c6f07094ab2fa7cddf63efbf113e9d2002e29
Scott Wakefield <scott@expertcc.co.uk>
2018-12-05 10:12:10 +0000

    parking story for now, still WIP.

--- src/SFA.DAS.EAS.Web.IntegrationTests/StatisticsControllerTests/WhenIGetTheStatistics.cs
+++ src/SFA.DAS.EAS.Web.IntegrationTests/StatisticsControllerTests/WhenIGetTheStatistics.cs
@@ -1,10 +1,19 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.EAS.Account.Api.Types;
 using SFA.DAS.EAS.Account.API.IntegrationTests.TestUtils.ApiTester;
 using SFA.DAS.EAS.Account.API.IntegrationTests.TestUtils.DataAccess.DataHelpers;
+using SFA.DAS.EAS.Domain.Models.Payments;
+using SFA.DAS.EAS.Infrastructure.Data;
+using SFA.DAS.Hashing;
+using SFA.DAS.NLog.Logger;
+using SFA.DAS.Provider.Events.Api.Types;
 
 namespace SFA.DAS.EAS.Account.API.IntegrationTests.StatisticsControllerTests
 {
@@ -23,9 +32,78 @@ namespace SFA.DAS.EAS.Account.API.IntegrationTests.StatisticsControllerTests
             _apiTester = new ApiIntegrationTester();
             var accountStatisticsDataHelper = new AccountStatisticsDataHelper(_apiTester.EmployerApprenticeshipsServiceConfiguration.DatabaseConnectionString);
             var financeStatisticsDataHelper = new FinanceStatisticsDataHelper(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString);
-
+            
             _expectedStatisticsViewModel = await accountStatisticsDataHelper.GetStatistics();
+            if (_expectedStatisticsViewModel.TotalAccounts == 0
+                || _expectedStatisticsViewModel.TotalAgreements == 0
+                || _expectedStatisticsViewModel.TotalLegalEntities == 0
+                || _expectedStatisticsViewModel.TotalPayeSchemes == 0)
+            {
+                using (var connection = new SqlConnection(_apiTester.EmployerApprenticeshipsServiceConfiguration.DatabaseConnectionString))
+                {
+                    connection.Open();
+                    var transaction = connection.BeginTransaction();
+                    var accountDbContext = new EmployerAccountsDbContext(connection, transaction);
+                    var lazyDb = new Lazy<EmployerAccountsDbContext>(() => accountDbContext);
+                    var accountRepo = new AccountRepository(_apiTester.EmployerApprenticeshipsServiceConfiguration,
+                        Mock.Of<ILog>(), lazyDb, Mock.Of<IAccountLegalEntityPublicHashingService>());
+                    await accountRepo.CreateAccount(10003, "234", "integration-tests", "address", DateTime.Today, "emp-ref", 
+                        "access-token", "refresh-token", "company-status", "emp ref name", 2, 1, "sector");
+                    transaction.Commit();
+                }
+                
+                _expectedStatisticsViewModel = await accountStatisticsDataHelper.GetStatistics();
+            }
+
             var financialStatistics = await financeStatisticsDataHelper.GetStatistics();
+            if (financialStatistics.TotalPayments == 0)
+            {
+                //todo: this does not need the using as it currently stands.
+                using (var connection = new SqlConnection(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString))
+                {
+                    connection.Open();
+
+                    var financeDbContext = new EmployerFinanceDbContext(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString);
+                    var lazyDb = new Lazy<EmployerFinanceDbContext>(() => financeDbContext);
+                    var levyRepository = new DasLevyRepository(_apiTester.LevyDeclarationProviderConfiguration,
+                        Mock.Of<ILog>(), lazyDb);
+                    await levyRepository.CreatePayments(new List<PaymentDetails>
+                    {
+                        new PaymentDetails
+                        {
+                            ProviderName = "prov name",
+                            StandardCode = 31,
+                            ProgrammeType = 3,
+                            CourseName = "course name",
+                            ApprenticeName = "apprentice name",
+                            ApprenticeNINumber = "nin",
+                            CourseLevel = 3,
+                            CourseStartDate = DateTime.Today,
+
+                            Ukprn = 134234,
+                            Uln   = 323423,
+                            EmployerAccountId = 342,
+                            ApprenticeshipId = 1,
+                            DeliveryPeriodMonth = 5,
+                            DeliveryPeriodYear = 2018,
+                            CollectionPeriodId = "R05",
+                            CollectionPeriodMonth = 4,
+                            CollectionPeriodYear = 2018,
+                            EvidenceSubmittedOn = DateTime.Today,
+                            EmployerAccountVersion = "emp acct ver",
+                            ApprenticeshipVersion = "appr ver",
+                            FundingSource = FundingSource.Levy,
+                            TransactionType = TransactionType.Learning,
+                            Amount = 234,
+                            PeriodEnd = "R06"
+                        }
+                    });
+                    // todo: get hold of the trans and commit it.
+                }
+
+                financialStatistics = await financeStatisticsDataHelper.GetStatistics();
+            }
+
             _expectedStatisticsViewModel.TotalPayments = financialStatistics.TotalPayments;
 
             _actualResponse = await _apiTester.InvokeGetAsync<StatisticsViewModel>(call);
--- src/SFA.DAS.EAS.Web.IntegrationTests/TestUtils/DataAccess/DataHelpers/AccountStatisticsDataHelper.cs
+++ src/SFA.DAS.EAS.Web.IntegrationTests/TestUtils/DataAccess/DataHelpers/AccountStatisticsDataHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
 using SFA.DAS.EAS.Account.Api.Types;
@@ -17,13 +18,12 @@ namespace SFA.DAS.EAS.Account.API.IntegrationTests.TestUtils.DataAccess.DataHelp
         public async Task<StatisticsViewModel> GetStatistics()
         {
             using (var connection = new SqlConnection(_databaseConnectionString))
-            {
-                
-                return await connection.QuerySingleAsync<StatisticsViewModel>(Sql);
+            {   
+                return await connection.QuerySingleAsync<StatisticsViewModel>(GetStatisticsSql);
             }
         }
 
-        private const string Sql = @"
+        private const string GetStatisticsSql = @"
 select (
   select count(0)
   from employer_account.Account
@@ -38,5 +38,20 @@ select (
   from employer_account.EmployerAgreement a
   where a.StatusId = 2 -- signed
 ) as TotalAgreements;";
+
+        /*public async Task<int> AddAccount(CreateAccountModel model)
+        {
+            using (var connection = new SqlConnection(_databaseConnectionString))
+            {
+                return await connection.ExecuteAsync("[employer_account].[CreateAccount]", model, 
+                    connection., null, CommandType.StoredProcedure);
+            }
+        }*/
+    }
+
+    internal class CreateAccountModel
+    {
+        public long UserId { get; set; }
+        public string EmployerNumber { get; set; }
     }
 }
