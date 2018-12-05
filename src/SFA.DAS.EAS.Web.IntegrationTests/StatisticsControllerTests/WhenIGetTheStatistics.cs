5ab5a067ad2bfe8195c155644f918f94be5b95f5
Scott Wakefield <scott@expertcc.co.uk>
2018-12-05 16:51:22 +0000

    setting up finance data correctly now.

--- src/SFA.DAS.EAS.Web.IntegrationTests/StatisticsControllerTests/WhenIGetTheStatistics.cs
+++ src/SFA.DAS.EAS.Web.IntegrationTests/StatisticsControllerTests/WhenIGetTheStatistics.cs
@@ -47,7 +47,7 @@ namespace SFA.DAS.EAS.Account.API.IntegrationTests.StatisticsControllerTests
                     var lazyDb = new Lazy<EmployerAccountsDbContext>(() => accountDbContext);
                     var accountRepo = new AccountRepository(_apiTester.EmployerApprenticeshipsServiceConfiguration,
                         Mock.Of<ILog>(), lazyDb, Mock.Of<IAccountLegalEntityPublicHashingService>());
-                    await accountRepo.CreateAccount(10003, "234", "integration-tests", "address", DateTime.Today, "emp-ref", 
+                    await accountRepo.CreateAccount(10003/**/, "234", "integration-tests", "address", DateTime.Today, "emp-ref", 
                         "access-token", "refresh-token", "company-status", "emp ref name", 2, 1, "sector");
                     transaction.Commit();
                 }
@@ -58,48 +58,42 @@ namespace SFA.DAS.EAS.Account.API.IntegrationTests.StatisticsControllerTests
             var financialStatistics = await financeStatisticsDataHelper.GetStatistics();
             if (financialStatistics.TotalPayments == 0)
             {
-                //todo: this does not need the using as it currently stands.
-                using (var connection = new SqlConnection(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString))
+                var financeDbContext = new EmployerFinanceDbContext(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString);
+                var lazyDb = new Lazy<EmployerFinanceDbContext>(() => financeDbContext);
+                var levyRepository = new DasLevyRepository(_apiTester.LevyDeclarationProviderConfiguration,
+                    Mock.Of<ILog>(), lazyDb);
+                await levyRepository.CreatePayments(new List<PaymentDetails>
                 {
-                    connection.Open();
-
-                    var financeDbContext = new EmployerFinanceDbContext(_apiTester.LevyDeclarationProviderConfiguration.DatabaseConnectionString);
-                    var lazyDb = new Lazy<EmployerFinanceDbContext>(() => financeDbContext);
-                    var levyRepository = new DasLevyRepository(_apiTester.LevyDeclarationProviderConfiguration,
-                        Mock.Of<ILog>(), lazyDb);
-                    await levyRepository.CreatePayments(new List<PaymentDetails>
+                    new PaymentDetails
                     {
-                        new PaymentDetails
-                        {
-                            ProviderName = "prov name",
-                            StandardCode = 31,
-                            ProgrammeType = 3,
-                            CourseName = "course name",
-                            ApprenticeName = "apprentice name",
-                            ApprenticeNINumber = "nin",
-                            CourseLevel = 3,
-                            CourseStartDate = DateTime.Today,
+                        ProviderName = "prov name",
+                        StandardCode = 31,
+                        ProgrammeType = 3,
+                        CourseName = "course name",
+                        ApprenticeName = "apprentice name",
+                        ApprenticeNINumber = "nin",
+                        CourseLevel = 3,
+                        CourseStartDate = DateTime.Today,
 
-                            Ukprn = 134234,
-                            Uln   = 323423,
-                            EmployerAccountId = 342,
-                            ApprenticeshipId = 1,
-                            DeliveryPeriodMonth = 5,
-                            DeliveryPeriodYear = 2018,
-                            CollectionPeriodId = "R05",
-                            CollectionPeriodMonth = 4,
-                            CollectionPeriodYear = 2018,
-                            EvidenceSubmittedOn = DateTime.Today,
-                            EmployerAccountVersion = "emp acct ver",
-                            ApprenticeshipVersion = "appr ver",
-                            FundingSource = FundingSource.Levy,
-                            TransactionType = TransactionType.Learning,
-                            Amount = 234,
-                            PeriodEnd = "R06"
-                        }
-                    });
-                    // todo: get hold of the trans and commit it.
-                }
+                        Ukprn = 134234,
+                        Uln   = 323423,
+                        EmployerAccountId = 342,
+                        ApprenticeshipId = 1,
+                        DeliveryPeriodMonth = 5,
+                        DeliveryPeriodYear = 2018,
+                        CollectionPeriodId = "R05",
+                        CollectionPeriodMonth = 4,
+                        CollectionPeriodYear = 2018,
+                        EvidenceSubmittedOn = DateTime.Today,
+                        EmployerAccountVersion = "emp acct ver",
+                        ApprenticeshipVersion = "appr ver",
+                        FundingSource = FundingSource.Levy,
+                        TransactionType = TransactionType.Learning,
+                        Amount = 234,
+                        PeriodEnd = "R06"
+                    }
+                });
+                financeDbContext.Database.CurrentTransaction.Commit();
 
                 financialStatistics = await financeStatisticsDataHelper.GetStatistics();
             }
