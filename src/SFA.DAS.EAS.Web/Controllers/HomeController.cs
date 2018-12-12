0b429bb86c665c4cddad8308f15ee8c86dd38c1b
DScuffle <david.scuffle@accomplish-it.co.uk>
2018-12-12 15:42:00 +0000

    Simplified SignoutCleanup

--- src/SFA.DAS.EAS.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/HomeController.cs
@@ -102,13 +102,6 @@ namespace SFA.DAS.EAS.Web.Controllers
         public ActionResult SignOutCleanup()
         {
             OwinWrapper.SignOutUser();
-
-            var owinContext = HttpContext.GetOwinContext();
-            var authenticationManager = owinContext.Authentication;
-            var idToken = authenticationManager.User.FindFirst("id_token")?.Value;
-            var constants = new Constants(_configuration.Identity);
-
-            return new RedirectResult(string.Format(constants.LogoutEndpoint(), idToken));
         }
 
         [HttpGet]
