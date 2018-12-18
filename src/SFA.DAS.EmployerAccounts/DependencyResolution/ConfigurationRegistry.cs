81e2ff8ed39ca412aa6bde49997db81f5a732107
Paul Graham <paul.graham@amido.com>
2018-12-18 09:53:25 +0000

    Removed comments

--- src/SFA.DAS.EmployerAccounts/DependencyResolution/ConfigurationRegistry.cs
+++ src/SFA.DAS.EmployerAccounts/DependencyResolution/ConfigurationRegistry.cs
@@ -14,16 +14,6 @@ namespace SFA.DAS.EmployerAccounts.DependencyResolution
             For<EmployerAccountsConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<EmployerAccountsConfiguration>("SFA.DAS.EmployerAccounts")).Singleton();
             For<EmployerFinanceConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<EmployerFinanceConfiguration>("SFA.DAS.EmployerFinance")).Singleton();
             For<EmployerAccountsReadStoreConfiguration>().Use(c => c.GetInstance<ITableStorageConfigurationService>().Get<EmployerAccountsReadStoreConfiguration>("SFA.DAS.EmployerAccounts.ReadStore")).Singleton();
-
-            //For<EmployerAccountsConfiguration>().Use(() => ConfigurationHelper.GetConfiguration<EmployerAccountsConfiguration>("SFA.DAS.EmployerAccounts")).Singleton();
-            //For<EmployerFinanceConfiguration>().Use(() => ConfigurationHelper.GetConfiguration<EmployerFinanceConfiguration>("SFA.DAS.EmployerFinance")).Singleton();
-            ////For<EmployerAccountsReadStoreConfiguration>().Use(() => ConfigurationHelper.GetConfiguration<EmployerAccountsReadStoreConfiguration>("SFA.DAS.EmployerAccounts.ReadStore")).Singleton();
-            //For<EmployerAccountsReadStoreConfiguration>().Use(() => new EmployerAccountsReadStoreConfiguration
-            //{
-            //    Uri = "https://localhost:8081",
-            //    AuthKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
-            //}
-            //    ).Singleton();
         }
     }
 }
