554c6205ed3144d99d0aa9d66ad7dad9849dadbf
Robin O'Connor <github@rocketmanconsultancy.co.uk>
2018-12-19 10:42:16 +0000

    Changed url query string to correct value

--- src/SFA.DAS.EAS.Web/Controllers/EmployerAccountPayeController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/EmployerAccountPayeController.cs
@@ -57,7 +57,7 @@ namespace SFA.DAS.EAS.Web.Controllers
         [Route("schemes/confirm")]
         public ActionResult ConfirmPayeScheme(string hashedAccountId)
         {
-            return Redirect(Url.EmployerAccountsAction($"/schemes/confirm?{Request.QueryString}"));
+            return Redirect(Url.EmployerAccountsAction($"/schemes/confirm{Request?.Url?.Query}"));
         }
         
         [HttpGet]
