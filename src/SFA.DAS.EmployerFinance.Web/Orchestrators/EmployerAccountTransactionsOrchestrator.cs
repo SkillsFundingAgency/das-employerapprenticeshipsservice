fa7fd3ea42715e581eb6036b63c196303ed40eca
Ashley-Bass <ashley.bass@education.gov.uk>
2019-01-24 13:57:09 +0000

    CON-82 Fix - Using ULN instead of name for grouping
    Changed SQL to pull in ULN as well as name.
    Changed some data models to allow for ULN to be pulled through to the correct place.
    Changed grouping in the orchestrator to group by ULN

--- src/SFA.DAS.EmployerFinance.Web/Orchestrators/EmployerAccountTransactionsOrchestrator.cs
+++ src/SFA.DAS.EmployerFinance.Web/Orchestrators/EmployerAccountTransactionsOrchestrator.cs
@@ -199,7 +199,7 @@ namespace SFA.DAS.EmployerFinance.Web.Orchestrators
                     ExternalUserId = externalUserId
                 });
 
-                var apprenticePaymentGroups = data.Transactions.GroupBy(x => new { x.ApprenticeName, x.ApprenticeNINumber });
+                var apprenticePaymentGroups = data.Transactions.GroupBy(x => new { x.ApprenticeULN,x.ApprenticeName });
 
                 var paymentSummaries = apprenticePaymentGroups.Select(pg =>
                 {
@@ -207,7 +207,7 @@ namespace SFA.DAS.EmployerFinance.Web.Orchestrators
 
                     return new AprrenticeshipPaymentSummaryViewModel
                     {
-                        ApprenticeName = pg.Key.ApprenticeName,
+                        ApprenticeName = pg.Key.ApprenticeName + "-"+pg.Key.ApprenticeULN,
                         LevyPaymentAmount = payments.Sum(t => t.LineAmount),
                         SFACoInvestmentAmount = payments.Sum(p => p.SfaCoInvestmentAmount),
                         EmployerCoInvestmentAmount = payments.Sum(p => p.EmployerCoInvestmentAmount)
