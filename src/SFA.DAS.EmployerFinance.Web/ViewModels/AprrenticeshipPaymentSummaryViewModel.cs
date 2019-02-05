9b8a6f335adea24275a3c77d6dfa19f7be376477
Ashley-Bass <ashley.bass@education.gov.uk>
2019-02-05 10:06:11 +0000

    Revert "CON-82 UI Fix to display ULN to prevent confusion"
    This reverts commit 96ddcb5dd0891e541a0807c806c4ffbcdadc09c7.

--- src/SFA.DAS.EmployerFinance.Web/ViewModels/AprrenticeshipPaymentSummaryViewModel.cs
+++ src/SFA.DAS.EmployerFinance.Web/ViewModels/AprrenticeshipPaymentSummaryViewModel.cs
@@ -3,7 +3,6 @@
     public class AprrenticeshipPaymentSummaryViewModel
     {
         public string ApprenticeName { get; set; }
-        public int ApprenticeULN { get; set; }
         public decimal LevyPaymentAmount { get; set; }
         public decimal SFACoInvestmentAmount { get; set; }
         public decimal EmployerCoInvestmentAmount { get; set; }
