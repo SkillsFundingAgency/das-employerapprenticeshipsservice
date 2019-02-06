0084b30828a565857abc086e70d15884b7f4ce2c
Ashley-Bass <ashley.bass@education.gov.uk>
2019-02-06 17:07:13 +0000

    CON-91 Script Creation for data fix
    Script finds accountLegalEntities that have no agreements currently signed or unsigned and marks them as deleted.

---
+++ src/SFA.DAS.EAS.Employer_Account.Database/Scripts/Manual/CON-91-DeleteLegalEntitiesWithNoAgreements.sql
@@ -0,0 +1,10 @@
+﻿UPDATE ae
+SET StatusId = 5
+FROM [employer_account].[EmployerAgreement] AS ae
+JOIN [employer_account].[AccountLegalEntity] AS ale
+ON ae.AccountLegalEntityId = ale.Id
+WHERE (ale.SignedAgreementId IS NULL AND ale.PendingAgreementId IS NULL)
+
+UPDATE [employer_account].[AccountLegalEntity]
+SET Deleted = GetUtcDate()
+WHERE (SignedAgreementId IS NULL AND PendingAgreementId IS NULL)
