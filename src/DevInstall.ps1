59268c967162632b7be7ba0f7025b8a39f0fa7fe
Neil Winstanley <Winsto@users.noreply.github.com>
2019-02-06 14:06:50 +0000

    certificates are in same folder not sub-folder

--- src/DevInstall.ps1
+++ src/DevInstall.ps1
@@ -2,12 +2,12 @@
 
 $certpwd = ConvertTo-SecureString -String password -Force -AsPlainText
 
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\localhost.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\localhost.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\localhost.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $certpwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\DasAmlCert.pfx -CertStoreLocation cert://LocalMachine/Root -Password $certpwd -Exportable
 
 $idppwd = ConvertTo-SecureString -String idsrv3test -Force -AsPlainText
 
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\DasIDPCert.pfx -CertStoreLocation cert://CurrentUser/My -Password $idppwd -Exportable
-Import-PfxCertificate -FilePath $PSScriptRoot\Certificates\DasIDPCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $idppwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\DasIDPCert.pfx -CertStoreLocation cert://CurrentUser/My -Password $idppwd -Exportable
+Import-PfxCertificate -FilePath $PSScriptRoot\DasIDPCert.pfx -CertStoreLocation cert://LocalMachine/My -Password $idppwd -Exportable
