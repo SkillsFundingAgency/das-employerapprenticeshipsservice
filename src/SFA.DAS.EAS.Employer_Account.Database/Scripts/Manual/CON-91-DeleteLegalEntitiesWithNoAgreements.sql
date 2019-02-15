d3b3d2897c1de443fbf95ebe150039e15b9507ac
Ashley-Bass <ashley.bass@education.gov.uk>
2019-02-15 14:39:16 +0000

    Potential fix base on dev assumption

--- src/SFA.DAS.EAS.Employer_Account.Database/Scripts/Manual/CON-91-DeleteLegalEntitiesWithNoAgreements.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Scripts/Manual/CON-91-DeleteLegalEntitiesWithNoAgreements.sql
@@ -1,10 +1,6 @@
-﻿UPDATE ae
-SET StatusId = 5
-FROM [employer_account].[EmployerAgreement] AS ae
+﻿UPDATE ale
+SET Deleted = GETUTCDATE()
+FROM [employer_account].[EmployerAgreement] AS ea
 JOIN [employer_account].[AccountLegalEntity] AS ale
-ON ae.AccountLegalEntityId = ale.Id
-WHERE (ale.SignedAgreementId IS NULL AND ale.PendingAgreementId IS NULL)
-
-UPDATE [employer_account].[AccountLegalEntity]
-SET Deleted = GetUtcDate()
-WHERE (SignedAgreementId IS NULL AND PendingAgreementId IS NULL)
+ON ea.AccountLegalEntityId = ale.Id
+WHERE ea.StatusId = 5 AND ale.Deleted IS NULL AND ale.PendingAgreementId IS NULL AND ale.SignedAgreementId IS NULL
