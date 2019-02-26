e1b680c166d7ba90f8d9223c4d822af31f1dcd50
Kevin <kevin@stephek.co.uk>
2019-02-26 11:11:31 +0000

    Fix AssemblyInfo

--- src/SFA.DAS.StuctureMap.Extensions/Properties/AssemblyInfo.cs
+++ src/SFA.DAS.StuctureMap.Extensions/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SFA.DAS.StructureMap.WebExtensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SFA.DAS.StructureMap.WebExtensions")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
