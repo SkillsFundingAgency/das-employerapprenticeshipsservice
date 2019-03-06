2a6fcd5edddeec0d9d8e277050621131b4494cce
tom-gough <tom@tgsd.co.uk>
2019-03-06 11:14:53 +0000

    Added classes for bootstrapping

---
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/StartupRegistry.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.EmployerFinance.Startup;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.DependencyResolution
+{
+    public class StartupRegistry : Registry
+    {
+        public StartupRegistry()
+        {
+            Scan(s =>
+            {
+                s.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
+                s.Convention<CompositeDecorator<DefaultStartup, IStartup>>();
+            });
+        }
+    }
+}
