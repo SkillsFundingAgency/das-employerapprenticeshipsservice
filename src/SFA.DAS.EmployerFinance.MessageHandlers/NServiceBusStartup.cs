2bd27e2af173ade5ff497c0b46ec85c0752dc75b
tom-gough <tom@tgsd.co.uk>
2019-03-07 09:10:21 +0000

    Moved NServiceBus startup code to implementation of IStartup (for message handlers)

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/NServiceBusStartup.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.EmployerFinance.Configuration;
+using SFA.DAS.EmployerFinance.Extensions;
+using SFA.DAS.EmployerFinance.Startup;
+using SFA.DAS.Extensions;
+using SFA.DAS.NServiceBus;
+using SFA.DAS.NServiceBus.NewtonsoftJsonSerializer;
+using SFA.DAS.NServiceBus.NLog;
+using SFA.DAS.NServiceBus.SqlServer;
+using SFA.DAS.NServiceBus.StructureMap;
+using SFA.DAS.UnitOfWork.NServiceBus;
+using StructureMap;
+
+namespace SFA.DAS.EmployerFinance.MessageHandlers
+{
+    public class NServiceBusStartup : IStartup
+    {
+        private readonly IContainer _container;
+        private IEndpointInstance _endpoint;
+
+        public NServiceBusStartup(IContainer container)
+        {
+            _container = container;
+        }
+
+        public async Task StartAsync()
+        {
+            var endpointConfiguration = new EndpointConfiguration("SFA.DAS.EmployerFinance.MessageHandlers")
+                .UseAzureServiceBusTransport(() => _container.GetInstance<EmployerFinanceConfiguration>().ServiceBusConnectionString)
+                .UseErrorQueue()
+                .UseInstallers()
+                .UseLicense(_container.GetInstance<EmployerFinanceConfiguration>().NServiceBusLicense.HtmlDecode())
+                .UseSqlServerPersistence(() => _container.GetInstance<DbConnection>())
+                .UseNewtonsoftJsonSerializer()
+                .UseNLogFactory()
+                .UseOutbox()
+                .UseStructureMapBuilder(_container)
+                .UseUnitOfWork();
+
+            _endpoint = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+        }
+
+        public async Task StopAsync()
+        {
+            await _endpoint.Stop().ConfigureAwait(false);
+        }
+    }
+}
