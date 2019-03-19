0553eac009ec9f3c14782b644bfa807f328e0446
tom-gough <tom@tgsd.co.uk>
2019-03-19 15:01:43 +0000

    Added command and handler (not implemented) for ExpiredFunds

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/ExpireFundsCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.EmployerFinance.Messages.Commands;
+
+namespace SFA.DAS.EmployerFinance.MessageHandlers.CommandHandlers
+{
+    public class ExpireFundsCommandHandler : IHandleMessages<ExpireFundsCommand>
+    {
+        public Task Handle(ExpireFundsCommand message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance.Messages/Commands/ExpireFundsCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.NServiceBus;
+
+namespace SFA.DAS.EmployerFinance.Messages.Commands
+{
+    public class ExpireFundsCommand : Command
+    {
+    }
+}
