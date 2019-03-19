cb358eb497240fded19742411a4771d41d6f68dc
tom-gough <tom@tgsd.co.uk>
2019-03-19 09:30:01 +0000

    FundsOut sproc minus account filtering

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetFundsOut.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [employer_financial].[GetFundsOut]
+AS
+SELECT	periodEnd.CalendarPeriodMonth AS CalendarPeriodMonth,
+			periodEnd.CalendarPeriodYear AS CalendarPeriodYear,
+			SUM(transactionLine.[Amount]) AS FundsOut
+	FROM [employer_financial].[TransactionLine] transactionLine
+	INNER JOIN [employer_financial].[PeriodEnd] periodEnd
+	ON periodEnd.PeriodEndId = transactionLine.PeriodEnd
+	
+	GROUP BY CalendarPeriodYear, CalendarPeriodMonth
+	ORDER BY CalendarPeriodYear, CalendarPeriodMonth
