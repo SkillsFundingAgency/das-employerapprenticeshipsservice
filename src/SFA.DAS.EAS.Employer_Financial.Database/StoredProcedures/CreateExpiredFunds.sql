896c36dd0e122ec8fea0bd14f692ce56a8310483
Phil Davies <github@phildavies.co.uk>
2019-03-19 13:28:50 +0000

    write expired funds wip

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/CreateExpiredFunds.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [employer_financial].[CreateExpiredFunds]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
--- src/SFA.DAS.EmployerFinance/Data/ExpiredFundsRepository.cs
+++ src/SFA.DAS.EmployerFinance/Data/ExpiredFundsRepository.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using SFA.DAS.EmployerFinance.Extensions;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.Sql.Client;
+using SFA.DAS.EmployerFinance.Models.ExpiredFunds;
 
 namespace SFA.DAS.EmployerFinance.Data
 {
@@ -11,6 +17,21 @@ namespace SFA.DAS.EmployerFinance.Data
         public ExpiredFundsRepository(string connectionString, ILog logger, Lazy<EmployerFinanceDbContext> db) : base(connectionString, logger)
         {
             _db = db;
+        }
+
+        public async Task Create(IEnumerable<ExpiredFund> expiredFunds)
+        {
+            var expiredFundsTable = expiredFunds.ToExpiredFundsDataTable();
+
+            var parameters = new DynamicParameters();
+
+            parameters.Add("@expiredFunds", expiredFundsTable.AsTableValuedParameter("[employer_financial].[ExpiredFundsTable]"));
+
+            await _db.Value.Database.Connection.ExecuteAsync(
+                sql: "[employer_financial].[CreateExpiredFunds]",
+                param: parameters,
+                transaction: _db.Value.Database.CurrentTransaction.UnderlyingTransaction,
+                commandType: CommandType.StoredProcedure);
         }
     }
 }
