daa9ef37bef00f2b9fa293145d3b32c6ec270cbe
Elliot Ritchie <elliotritchie@hotmail.com>
2019-03-20 15:35:18 +0000

    Added ExpireFundsJob

---
+++ src/SFA.DAS.EmployerFinance.Jobs.UnitTests/ScheduledJobs/ExpireFundsJobTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NServiceBus;
+using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Jobs.ScheduledJobs;
+using SFA.DAS.EmployerFinance.Messages.Commands;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.EmployerFinance.Jobs.UnitTests.ScheduledJobs
+{
+    [TestFixture]
+    [Parallelizable]
+    public class ExpireFundsJobTests : FluentTest<ExpireFundsJobTestsFixture>
+    {
+        [Test]
+        public Task Run_WhenRunningJob_ThenShouldSendCommand()
+        {
+            return RunAsync(f => f.Run(), f => f.MessageSession.Verify(s => s.Send(It.IsAny<ExpireFundsCommand>(), It.IsAny<SendOptions>())));
+        }
+    }
+
+    public class ExpireFundsJobTestsFixture
+    {
+        public Mock<IMessageSession> MessageSession { get; set; }
+        public ExpireFundsJob Job { get; set; }
+
+        public ExpireFundsJobTestsFixture()
+        {
+            MessageSession = new Mock<IMessageSession>();
+            Job = new ExpireFundsJob(MessageSession.Object);
+        }
+
+        public Task Run()
+        {
+            return Job.Run(null, null);
+        }
+    }
+}
