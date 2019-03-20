14f18f677c8a6c090af86ec59e18f14c22203321
Phil Davies <github@phildavies.co.uk>
2019-03-20 13:42:23 +0000

    add Get to ExpiredFunds repo

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetExpiredFunds.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [employer_financial].[GetExpiredFunds]
+	@AccountId bigint
+AS
+
+SELECT	YEAR(TransactionDate) AS CalendarPeriodYear,
+		MONTH(TransactionDate) AS CalendarPeriodMonth,
+		Amount
+FROM	[employer_financial].[TransactionLine]
+WHERE	AccountId = @AccountId
+		AND TransactionType = /*ExpiredFund*/ 5
