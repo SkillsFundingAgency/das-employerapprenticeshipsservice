a9b2c3bf3e966f4996d451c481dba82ba52c423a
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-03-22 12:01:02 +0000

    [CON-329] Add separate command handler to process period end to avoid single huge outbox transaction for multiple periods

---
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/ProcessPeriodEndPaymentsCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using NServiceBus;
+using SFA.DAS.EmployerFinance.Messages.Commands;
+using SFA.DAS.EmployerFinance.Queries.GetAllEmployerAccounts;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.EmployerFinance.MessageHandlers.CommandHandlers
+{
+    public class ProcessPeriodPaymentsEndCommandHandler : IHandleMessages<ProcessPeriodEndPaymentsCommand>
+    {
+        private readonly IMediator _mediator;
+        private readonly ILog _logger;
+
+        public ProcessPeriodPaymentsEndCommandHandler(IMediator mediator, ILog logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Handle(ProcessPeriodEndPaymentsCommand message, IMessageHandlerContext context)
+        {
+            var response = await _mediator.SendAsync(new GetAllEmployerAccountsRequest());
+
+            var tasks = new List<Task>();
+
+            foreach (var account in response.Accounts)
+            {
+                _logger.Info($"Creating payment queue message for account ID: '{account.Id}' period end ref: '{message.PeriodEndRef}'");
+
+                tasks.Add(context.SendLocal<ImportAccountPaymentsCommand>(c =>
+                {
+                    c.AccountId = account.Id;
+                    c.PeriodEndRef = message.PeriodEndRef;
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance.Messages/Commands/ProcessPeriodEndPaymentsCommand.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.NServiceBus;
+
+namespace SFA.DAS.EmployerFinance.Messages.Commands
+{
+    public class ProcessPeriodEndPaymentsCommand : Command
+    {
+        public string PeriodEndRef { get; set; }
+    }
+}
