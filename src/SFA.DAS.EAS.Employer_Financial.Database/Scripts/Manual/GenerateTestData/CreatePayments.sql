1ad5237270f9588f4d86d14d6ca527477e25dc63
Phil Davies <github@phildavies.co.uk>
2019-03-28 14:37:13 +0000

    create a temp version of the payment processing sp in the create payments script, so can set DateCreated

--- src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/CreatePayments.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/CreatePayments.sql
@@ -17,6 +17,12 @@ BEGIN
 END
 GO
 
+IF OBJECT_ID('tempdb..#ProcessPaymentDataTransactionsGenerateDataEdition') IS NOT NULL
+BEGIN
+    DROP PROC #ProcessPaymentDataTransactionsGenerateDataEdition
+END
+GO
+
 -- CREATE THE STORED PROCEDURES
 CREATE PROCEDURE #createPayment
 (     
@@ -73,6 +79,53 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE #ProcessPaymentDataTransactionsGenerateDataEdition
+	@AccountId BIGINT
+AS
+
+--- Process Levy Payments ---
+INSERT INTO [employer_financial].[TransactionLine]
+select mainUpdate.* from
+    (
+    select 
+            x.AccountId as AccountId,
+            DATEFROMPARTS(DatePart(yyyy,GETDATE()),DatePart(MM,GETDATE()),DATEPART(dd,GETDATE())) as DateAdded,
+            null as SubmissionId,
+            Max(pe.CompletionDateTime) as TransactionDate,
+            3 as TransactionType,			
+            null as LevyDeclared,
+            Sum(ISNULL(p.Amount, 0)) * -1 Amount,
+            null as EmpRef,
+            x.PeriodEnd,
+            x.Ukprn,
+            Sum(ISNULL(pco.Amount, 0)) * -1 as SfaCoInvestmentAmount,
+            Sum(ISNULL(pci.Amount, 0)) * -1 as EmployerCoInvestmentAmount,
+			0 as EnglishFraction,
+			null as TransferSenderAccountId,
+			null as TransferSenderAccountName,
+			null as TransferReceiverAccountId,
+			null as TransferReceiverAccountName		
+        FROM 
+            employer_financial.[Payment] x
+		inner join [employer_financial].[PeriodEnd] pe 
+				on pe.PeriodEndId = x.PeriodEnd
+        left join [employer_financial].[Payment] p 
+				on p.PeriodEnd = pe.PeriodEndId and p.PaymentId = x.PaymentId and p.FundingSource IN (1, 5)
+        left join [employer_financial].[Payment] pco 
+				on pco.PeriodEnd = pe.PeriodEndId and pco.PaymentId = x.PaymentId and pco.FundingSource = x.FundingSource and pco.FundingSource = 2 
+        left join [employer_financial].[Payment] pci 
+				on pci.PeriodEnd = pe.PeriodEndId and pci.PaymentId = x.PaymentId  and pci.FundingSource = x.FundingSource and pci.FundingSource = 3 
+		WHERE x.AccountId = @AccountId
+        Group by
+            x.Ukprn,x.PeriodEnd,x.AccountId
+    ) mainUpdate
+    inner join (
+        select AccountId,Ukprn,PeriodEnd from [employer_financial].Payment where FundingSource IN (1,2,3,5)      
+    EXCEPT
+        select AccountId,Ukprn,PeriodEnd from [employer_financial].[TransactionLine] where TransactionType = 3
+    ) dervx on dervx.AccountId = mainUpdate.AccountId and dervx.PeriodEnd = mainUpdate.PeriodEnd and dervx.Ukprn = mainUpdate.Ukprn
+GO
+
 BEGIN TRANSACTION
     
     DECLARE @accountId BIGINT
@@ -108,12 +161,13 @@ BEGIN TRANSACTION
 		insert into employer_financial.periodend (periodendid, calendarperiodmonth, calendarperiodyear, accountdatavalidat, commitmentdatavalidat, completiondatetime, paymentsforperiod)
 		values
 		--todo: do month year etc. need to match @periodEnd? check real data
+		--looks like calendarperiod month|year aren't used, so no need to set correctly!?
 		(@periodEnd,5,2018,'2018-05-04 00:00:00.000','2018-05-04 09:07:34.457','2018-05-04 10:50:27.760','https://pp-payments.apprenticeships.sfa.bis.gov.uk/api/payments?periodId=1617-R10')
 	END
 
     EXEC #createAccountPayments @accountId, @accountName, 'CHESTERFIELD COLLEGE', 10001378, 'Accounting',  @periodEnd, @totalPaymentAmount	
         
-    exec employer_financial.processpaymentdatatransactions @accountId
+    exec #ProcessPaymentDataTransactionsGenerateDataEdition @accountId
 
 COMMIT TRANSACTION
 GO
