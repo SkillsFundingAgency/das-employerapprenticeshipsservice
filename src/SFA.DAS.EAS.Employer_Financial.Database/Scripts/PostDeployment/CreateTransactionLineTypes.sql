3a2e64c2464200a6fa4657ba2bb7053cfee28836
tom-gough <tom@tgsd.co.uk>
2019-04-02 15:46:31 +0100

    Renamed Levy Expired to Expired Levy as per story

--- src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/PostDeployment/CreateTransactionLineTypes.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/PostDeployment/CreateTransactionLineTypes.sql
@@ -8,5 +8,5 @@ BEGIN
 END
 IF (NOT EXISTS(SELECT TransactionType FROM [employer_financial].[TransactionLineTypes] WHERE TransactionType = 3))
 BEGIN
-	INSERT [employer_financial].[TransactionLineTypes] ([TransactionType], [Description]) VALUES (3, N'Levy Expired')
+	INSERT [employer_financial].[TransactionLineTypes] ([TransactionType], [Description]) VALUES (3, N'Expired Levy')
 END
