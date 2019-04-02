3a48ea51ea99874a1680489328b0d09205324754
Elliot Ritchie <elliotritchie@hotmail.com>
2019-04-02 14:24:54 +0100

    Updated expired funds process to use created date instead of transaction date for funds in and previously expired funds

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetLevyFundsIn.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetLevyFundsIn.sql
@@ -5,10 +5,10 @@ AS
 SELECT CalendarPeriodYear, CalendarPeriodMonth, SUM([Amount]) AS FundsIn
 FROM
 (
-	SELECT		(SELECT MONTH([TransactionDate])) AS CalendarPeriodMonth,
-			(SELECT YEAR([TransactionDate])) AS CalendarPeriodYear,
+	SELECT		(SELECT MONTH([DateCreated])) AS CalendarPeriodMonth,
+			(SELECT YEAR([DateCreated])) AS CalendarPeriodYear,
 			[Amount]
-	FROM [employer_financial].[TransactionLine]
+	FROM [employer_financial].[TransactionLine] 
 	WHERE AccountId = @AccountId
 	AND TransactionType = /*Declaration*/ 1
 ) AS ungroupedQuery
