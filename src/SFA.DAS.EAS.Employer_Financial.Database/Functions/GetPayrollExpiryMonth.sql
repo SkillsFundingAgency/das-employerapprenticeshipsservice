5cedfe476ca422485c78093deff9e3aeb8d7e947
tom-gough <tom@tgsd.co.uk>
2019-04-03 15:41:38 +0100

    Fixed capitalisation and removed spike script

--- src/SFA.DAS.EAS.Employer_Financial.Database/Functions/GetPayrollExpiryMonth.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Functions/GetPayrollExpiryMonth.sql
@@ -3,14 +3,14 @@
 	@currentDate DATETIME = NULL,
 	@expiryPeriod INT = NULL
 )
-RETURNS @monthAfterPayrollExpiry TABLE( monthPortion int, yearPortion int  )
+RETURNS @monthAfterPayrollExpiry TABLE( monthPortion INT, yearPortion INT  )
 AS
 BEGIN
 	--conversion of inputs
-	DECLARE @currentPayrollStartYear int = DATEPART(year, @currentDate)
-	DECLARE @currentPayrollMonth int = DATEPART(month, @currentDate)
-	DECLARE @expiryPeriodYearsPortion int = FLOOR(@expiryPeriod / 12)
-	DECLARE @expiryPeriodMonthsPortion int = @expiryPeriod % 12
+	DECLARE @currentPayrollStartYear INT = DATEPART(YEAR, @currentDate)
+	DECLARE @currentPayrollMonth INT = DATEPART(MONTH, @currentDate)
+	DECLARE @expiryPeriodYearsPortion INT = FLOOR(@expiryPeriod / 12)
+	DECLARE @expiryPeriodMonthsPortion INT = @expiryPeriod % 12
 
 	SET @currentPayrollMonth = @currentPayrollMonth - 4
 	IF(@currentPayrollMonth <= 0)
@@ -27,7 +27,7 @@ BEGIN
 	DECLARE @expiryPayrollMonth INT = @currentPayrollMonth - @expiryPeriodMonthsPortion
 
 	--if it's last year adjust it and the year to represent the correct month accordingly
-	if(@expiryPayrollMonth < -1)
+	IF(@expiryPayrollMonth < -1)
 	BEGIN
 		SET @expiryPayrollMonth = @expiryPayrollMonth + 12
 		SET @expiryPayrollStartYear = @expiryPayrollStartYear - 1
