9b6c829f740f410bf6953e618b2fd68831e2b98a
Phil Davies <github@phildavies.co.uk>
2019-04-04 11:35:22 +0100

    only delete the account's payment transaction lines!

--- src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/DeleteAccountPayments.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/DeleteAccountPayments.sql
@@ -2,4 +2,4 @@
 
 delete employer_financial.PaymentMetaData where Id in (select PaymentMetaDataId from employer_financial.Payment where AccountId = @accountId)
 delete employer_financial.Payment where AccountId = @accountId
-delete employer_financial.TransactionLine where AccountId = @accountId
+delete employer_financial.TransactionLine where AccountId = @accountId and TransactionType in (3)
