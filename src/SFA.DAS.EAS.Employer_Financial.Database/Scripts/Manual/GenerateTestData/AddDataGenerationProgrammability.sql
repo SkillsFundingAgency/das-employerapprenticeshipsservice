aa4a12b452217eebf693aef31c12289115319c14
Phil Davies <github@phildavies.co.uk>
2019-04-05 15:33:17 +0100

    add/remove data gen prog wip

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/AddDataGenerationProgrammability.sql
@@ -0,0 +1,92 @@
+﻿
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'DataGen')
+BEGIN
+	EXEC('CREATE SCHEMA DataGen')
+END
+GO
+
+CREATE OR ALTER FUNCTION DataGen.CalendarPeriodMonth (@date datetime)  
+RETURNS int 
+AS  
+BEGIN  
+  declare @month int = DATEPART(month,@date)
+  RETURN(@month);  
+END; 
+GO
+
+CREATE OR ALTER FUNCTION DataGen.CalendarPeriodYear (@date datetime)  
+RETURNS int
+AS  
+BEGIN  
+  declare @year int = DATEPART(year,@date)
+  return @year
+END; 
+GO
+
+CREATE OR ALTER FUNCTION DataGen.CollectionPeriodMonth (@date datetime)  
+RETURNS int 
+AS  
+BEGIN  
+  declare @collectionPeriodMonth int = (select dbo.CalendarPeriodMonth(@date))
+  return @collectionPeriodMonth
+END; 
+GO
+
+CREATE OR ALTER FUNCTION DataGen.CollectionPeriodYear (@date datetime)  
+RETURNS int
+AS  
+BEGIN  
+  declare @collectionPeriodYear int = (select dbo.CalendarPeriodYear(@date))
+  return @collectionPeriodYear
+END; 
+GO
+
+--todo R13, R14
+CREATE OR ALTER FUNCTION DataGen.PeriodEndMonth (@date datetime)  
+RETURNS VARCHAR(5)
+AS  
+BEGIN  
+  declare @month int = DATEPART(month,@date)
+
+  SET @month = @month - 7
+  IF @month < 1
+	SET @month = @month + 12
+
+  declare @periodEndMonth VARCHAR(5) = CONVERT(varchar(5), @month)
+  RETURN @periodEndMonth; 
+END; 
+GO
+
+--todo R13, R14
+CREATE OR ALTER FUNCTION DataGen.PeriodEndYear (@date datetime)  
+RETURNS VARCHAR(5)
+AS  
+BEGIN  
+  declare @month int = DATEPART(month,@date)
+  declare @year int = DATEPART(year,@date)
+
+  if @month < 8
+	SET @year = @year - 1
+	
+  DECLARE @periodEndYear VARCHAR(4) = (SELECT RIGHT(CONVERT(VARCHAR(5), @year, 1), 2)) + (SELECT RIGHT(CONVERT(VARCHAR(4), @year+1, 1), 2))
+  return @periodEndYear
+END; 
+GO
+
+CREATE OR ALTER FUNCTION DataGen.PeriodEnd (@date datetime)  
+RETURNS VARCHAR(8)
+AS  
+BEGIN  
+  declare @periodEnd varchar(8) = (SELECT dbo.PeriodEndYear(@date) + '-R' + right('0' + dbo.PeriodEndMonth(@date),2))
+  return @periodEnd
+END; 
+GO
+
+CREATE OR ALTER FUNCTION DataGen.CollectionPeriodId (@date datetime)  
+RETURNS VARCHAR(8)
+AS  
+BEGIN  
+  declare @periodEnd varchar(8) = (select dbo.PeriodEnd(@date))
+  return @periodEnd
+END; 
+GO
---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/RemoveDataGenerationProgrammability.sql
@@ -0,0 +1,38 @@
+﻿
+IF object_id(N'DataGen.CalendarPeriodMonth', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.CalendarPeriodMonth
+GO
+
+IF object_id(N'DataGen.CalendarPeriodYear', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.CalendarPeriodYear
+GO
+
+IF object_id(N'DataGen.CollectionPeriodMonth', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.CollectionPeriodMonth
+GO
+
+IF object_id(N'DataGen.CollectionPeriodYear', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.CollectionPeriodYear
+GO
+
+IF object_id(N'DataGen.PeriodEndMonth', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.PeriodEndMonth
+GO
+
+IF object_id(N'DataGen.PeriodEndYear', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.PeriodEndYear
+GO
+
+IF object_id(N'DataGen.PeriodEnd', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.PeriodEnd
+GO
+
+IF object_id(N'DataGen.CollectionPeriodId', N'FN') IS NOT NULL
+    DROP FUNCTION DataGen.CollectionPeriodId
+GO
+
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'DataGen')
+BEGIN
+	EXEC('DROP SCHEMA DataGen')
+END
+GO
