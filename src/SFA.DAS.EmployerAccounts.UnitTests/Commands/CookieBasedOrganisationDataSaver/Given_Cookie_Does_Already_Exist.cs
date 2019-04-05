0b4c4f82c110f6f372d37751146480e675094c80
Neil Winstanley <Winsto@users.noreply.github.com>
2019-04-05 15:16:12 +0100

    Add test to assert that PayeRef data is not changed when updating orgnisation data.

--- src/SFA.DAS.EmployerAccounts.UnitTests/Commands/CookieBasedOrganisationDataSaver/Given_Cookie_Does_Already_Exist.cs
+++ src/SFA.DAS.EmployerAccounts.UnitTests/Commands/CookieBasedOrganisationDataSaver/Given_Cookie_Does_Already_Exist.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerAccounts.Commands.OrganisationData;
@@ -13,9 +14,12 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.CookieBasedOrganisationDat
         : FluentTest<Given_Cookie_Does_Already_Exist>
     {
         private EmployerAccounts.Commands.OrganisationData.CookieBasedOrganisationDataSaver _sut;
+        private EmployerAccountPayeRefData _payeRefData;
 
         public Given_Cookie_Does_Already_Exist()
         {
+            setupPayeRefData();
+
             CookieRepository = new Mock<ICookieStorageService<EmployerAccountData>>();
 
             CookieRepository
@@ -26,7 +30,7 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.CookieBasedOrganisationDat
                     new EmployerAccountData
                     {
                         EmployerAccountOrganisationData = new EmployerAccountOrganisationData(),
-                        EmployerAccountPayeRefData = new EmployerAccountPayeRefData()
+                        EmployerAccountPayeRefData = _payeRefData
                     });
 
             _sut = new EmployerAccounts.Commands.OrganisationData.CookieBasedOrganisationDataSaver(CookieRepository.Object);
@@ -64,6 +68,32 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.CookieBasedOrganisationDat
                                         It.IsAny<EmployerAccountData>())));
         }
 
+        [Test]
+        public Task Then_Existing_PayeRef_Data_Is_Unchanged()
+        {
+            return
+                RunAsync(
+                    act: f => f.Handle(),
+                    assert: f =>
+                        f.CookieRepository
+                            .Verify(
+                                m =>
+                                    m.Update(
+                                        It.IsAny<string>(),
+                                        It.Is<EmployerAccountData>(data => payeRefDataIsUnchanged(data.EmployerAccountPayeRefData)))));
+        }
+
+        private bool payeRefDataIsUnchanged(EmployerAccountPayeRefData data)
+        {
+            data.AccessToken.Should().Be(_payeRefData.AccessToken);
+            data.EmpRefNotFound.Should().Be(_payeRefData.EmpRefNotFound);
+            data.EmployerRefName.Should().Be(_payeRefData.EmployerRefName);
+            data.PayeReference.Should().Be(_payeRefData.PayeReference);
+            data.RefreshToken.Should().Be(_payeRefData.RefreshToken);
+
+            return true;
+        }
+
         public Mock<ICookieStorageService<EmployerAccountData>> CookieRepository { get; set; }
 
         private Task Handle()
@@ -72,6 +102,18 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.CookieBasedOrganisationDat
                 _sut
                     .Handle(
                         new SaveOrganisationData(new EmployerAccountOrganisationData()));
+        }
+
+        private void setupPayeRefData()
+        {
+            _payeRefData = new EmployerAccountPayeRefData
+            {
+                AccessToken = @"{FBC24A46-AC40-4477-8D7E-9A514C0F4AAE}",
+                EmployerRefName = @"{64439F80-CE0A-4E7E-924A-57B4420EF4FF}",
+                EmpRefNotFound = false,
+                PayeReference = @"{74988389-DBED-4996-9576-7F61426585CB}",
+                RefreshToken = @"{0994787F-6890-41B5-9F67-FCF6D036FF7A}"
+            };
         }
     }
 }
