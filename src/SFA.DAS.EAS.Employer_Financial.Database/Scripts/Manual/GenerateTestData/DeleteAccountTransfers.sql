f8c1d40cfe922c6022a2873fb49d300eb38b9ea4
Phil Davies <github@phildavies.co.uk>
2019-04-08 11:07:25 +0100

    data gen scripts wip

--- src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/DeleteAccountTransfers.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/GenerateTestData/DeleteAccountTransfers.sql
@@ -1,9 +1,11 @@
-﻿--Warning: work in progress, DON'T USE YET!
+﻿--note: currently deletes transfer related rows, but currently leaves the corresponding payment transactionline for the receiver (that CreateTransferPayments generates)
+--todo: ^^ delete also
 
-declare @accountId bigint = 0
+declare @senderAccountId bigint = 0
+declare @receiverAccountId bigint = 1
 
-delete employer_financial.AccountTransfers where SenderAccountId = @accountId
-delete employer_financial.PaymentMetaData where Id in (select PaymentMetaDataId from employer_financial.Payment where AccountId = @accountId and FundingSource = /*LevyTransfer*/'5')
+delete employer_financial.AccountTransfers where SenderAccountId = @senderAccountId
+delete employer_financial.PaymentMetaData where Id in (select PaymentMetaDataId from employer_financial.Payment where AccountId = @receiverAccountId and FundingSource = /*LevyTransfer*/'5')
 --note: transactionType is /*Levy*/1, not /*Transfer*/4, so we pick up transfer rows by FundingSource
-delete employer_financial.Payment where AccountId = @accountId and FundingSource = /*LevyTransfer*/'5'
-delete employer_financial.TransactionLine where AccountId = @accountId and TransactionType in (/*Transfer*/4)
+delete employer_financial.Payment where AccountId = @receiverAccountId and FundingSource = /*LevyTransfer*/'5'
+delete employer_financial.TransactionLine where TransferSenderAccountId = @senderAccountId --and TransactionType in (/*Transfer*/4)
