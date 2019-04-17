b8c314bd5bd8e75a5d0e29f5efa406a526de10ba
tom-gough <tom@tgsd.co.uk>
2019-04-17 13:45:06 +0100

    Scenario 6 for late accounts working

--- src/SFA.DAS.EmployerFinance.AcceptanceTests/Features/LateAccounts/Scenario-01-Account-is-opened-on-the-23-MAY-2017-and-after-only-added-valid-PAYE-HMRC-submissions-no-older-than-12-months-old.feature
+++ src/SFA.DAS.EmployerFinance.AcceptanceTests/Features/LateAccounts/Scenario-01-Account-is-opened-on-the-23-MAY-2017-and-after-only-added-valid-PAYE-HMRC-submissions-no-older-than-12-months-old.feature
@@ -145,59 +145,41 @@ Scenario: 5 - A PAYE being used to create a new account has been in a different 
 	Then we should see a level 1 screen with a levy declared of 1100 on the 02/2019
 	And we should see a level 1 screen with a balance of 12650 on the 02/2019
 
-
-#
-#Scenario 6 A PAYE being used to create a new account has been in a different account longer ago than the 12 month limit and the new account includes 
-#end of year adjustments within that time limit and ignores one outside the limit. 
-#
-##Given An employer is adding a PAYE which has submissions older than the 12 month expiry rule limit
-#    
-##And Account 1 is opened
-#    
-##And Account 2 is opened
-#    
-##And Account 1 is associated with paye scheme 123/aaa
-#    
-##And Hmrc return the following submissions for paye scheme 123/aaa  
-#| Id        | LevyDueYtd | Payroll_Year | Payroll_Month | English_Fraction | SubmissionDate | CreatedDate | EndofYear Adjustment |
-#| 999000100 | 12000      | 16-17        | 12            | 1                | 2017-04-19     | 2017-04-20  | 0 			 | 
-#| 999000101 | 1000       | 17-18        | 1             | 1                | 2017-05-19     | 2017-05-20  | 0 			 |
-#| 999000102 | 2000       | 17-18        | 2             | 1                | 2017-06-19     | 2017-06-20  | 0			 |	
-#| 999000103 | 3000       | 17-18        | 3             | 1                | 2017-07-19     | 2017-07-20  | 0                    |
-#| 999000104 | 4000       | 17-18        | 4             | 1                | 2017-08-19     | 2017-08-20  | 0                    |
-#| 999000105 | 5000       | 17-18        | 5             | 1                | 2017-09-19     | 2017-09-20  | 0                    |
-#
-##And we refresh levy data for paye scheme 123/aaa
-#    
-##And account 2 is associated with paye scheme 123/aaa (paye scheme has been detached from account 1)
-#    
-##And Hmrc return the following submissions for paye scheme 123/aaa (with table)
-# 
-#
-#| Id        | LevyDueYtd | Payroll_Year | Payroll_Month | English_Fraction | SubmissionDate | CreatedDate | EndofYear Adjustment |  
-#| 999000106 | 6000       | 17-18        | 6             | 1                | 2017-10-19     | 2017-10-20  | 0                    |
-#| 999000107 | 7000       | 17-18        | 7             | 1                | 2017-11-19     | 2017-11-20  | 0                    |
-#| 999000108 | 8000       | 17-18        | 8             | 1                | 2017-12-19     | 2017-12-20  | 0                    |
-#| 999000109 | 9000       | 17-18        | 9             | 1                | 2018-01-19     | 2018-01-20  | 0                    |
-#| 999000110 | 10000      | 17-18        | 10            | 1                | 2018-02-19     | 2018-02-20  | 0                    |
-#| 999000111 | 11000      | 17-18        | 11            | 1                | 2018-03-19     | 2018-03-20  | 0                    |
-#| 999000112 | 12000      | 17-18        | 12            | 1                | 2018-04-19     | 2018-04-20  | 0                    |
-#| 999000113 | 1000       | 18-19        | 1             | 1                | 2018-05-19     | 2018-05-20  | 0                    |
-#| 999000114 | 2000       | 18-19        | 2             | 1                | 2018-06-19     | 2018-06-20  | 0                    |
-#| 999000115 | 3000       | 18-19        | 3             | 1                | 2018-07-19     | 2018-07-20  | 0                    |
-#| 999000116 | 4000       | 18-19        | 4             | 1                | 2018-08-19     | 2018-08-20  | 0                    |
-#| 999000117 | 5000       | 18-19        | 5             | 1                | 2018-09-19     | 2018-09-20  | 0                    |
-#| 999000118 | 6000       | 18-19        | 6             | 1                | 2018-10-19     | 2018-10-20  | 0                    |
-#| 999000119 | 12500      | 17-18        | 12            | 1                | 2018-10-19     | 2018-10-20  | 1                    |
-#| 999000120 | 7000       | 18-19        | 7             | 1                | 2018-11-19     | 2018-11-20  | 0                    |
-#| 999000121 | 8000       | 18-19        | 8             | 1                | 2018-12-19     | 2018-12-20  | 0                    |
-#| 999000122 | 9000       | 18-19        | 9             | 1                | 2019-01-19     | 2019-01-20  | 0                    |
-#| 999000123 | 10000      | 18-19        | 10            | 1                | 2019-02-19     | 2019-02-20  | 0                    |
-#| 999000124 | 11000      | 18-19        | 11            | 1                | 2019-03-19     | 2019-03-20  | 0                    |
-#| 999000125 | 13000      | 16-17        | 12            | 1                | 2019-03-19     | 2019-03-20  | 1                    |
-#
-##when we refresh levy data for paye scheme 123/aaa
-#    
-#WHEN we view the account transactions
-#THEN we should see a level 1 screen with an levy declared of 13750
-#AND we should see a level 1 screen with a balance of 13750 
+Scenario: 6 - A PAYE being used to create a new account has been in a different account longer ago than the 12 month limit and the new account includes end of year adjustments within that time limit and ignores one outside the limit. 
+	Given An employer is adding a PAYE which has submissions older than the 12 month expiry rule limit    
+	And Hmrc return the following submissions for paye scheme  
+	| Id        | LevyDueYtd | Payroll_Year | Payroll_Month | English_Fraction | SubmissionDate | CreatedDate | EndofYear Adjustment |
+	| 999000100 | 12000      | 16-17        | 12            | 1                | 2017-04-19     | 2017-04-20  | 0 			 | 
+	| 999000101 | 1000       | 17-18        | 1             | 1                | 2017-05-19     | 2017-05-20  | 0 			 |
+	| 999000102 | 2000       | 17-18        | 2             | 1                | 2017-06-19     | 2017-06-20  | 0			 |	
+	| 999000103 | 3000       | 17-18        | 3             | 1                | 2017-07-19     | 2017-07-20  | 0                    |
+	| 999000104 | 4000       | 17-18        | 4             | 1                | 2017-08-19     | 2017-08-20  | 0                    |
+	| 999000105 | 5000       | 17-18        | 5             | 1                | 2017-09-19     | 2017-09-20  | 0                    |
+	And we refresh levy data for paye scheme on the 09/2017
+    And Another account is opened and associated with the paye scheme
+    And Hmrc return the following submissions for paye scheme
+	| Id        | LevyDueYtd | Payroll_Year | Payroll_Month | English_Fraction | SubmissionDate | CreatedDate | EndofYear Adjustment |
+	| 999000106 | 6000       | 17-18        | 6             | 1                | 2017-10-19     | 2017-10-20  | 0                    |
+	| 999000107 | 7000       | 17-18        | 7             | 1                | 2017-11-19     | 2017-11-20  | 0                    |
+	| 999000108 | 8000       | 17-18        | 8             | 1                | 2017-12-19     | 2017-12-20  | 0                    |
+	| 999000109 | 9000       | 17-18        | 9             | 1                | 2018-01-19     | 2018-01-20  | 0                    |
+	| 999000110 | 10000      | 17-18        | 10            | 1                | 2018-02-19     | 2018-02-20  | 0                    |
+	| 999000111 | 11000      | 17-18        | 11            | 1                | 2018-03-19     | 2018-03-20  | 0                    |
+	| 999000112 | 12000      | 17-18        | 12            | 1                | 2018-04-19     | 2018-04-20  | 0                    |
+	| 999000113 | 1000       | 18-19        | 1             | 1                | 2018-05-19     | 2018-05-20  | 0                    |
+	| 999000114 | 2000       | 18-19        | 2             | 1                | 2018-06-19     | 2018-06-20  | 0                    |
+	| 999000115 | 3000       | 18-19        | 3             | 1                | 2018-07-19     | 2018-07-20  | 0                    |
+	| 999000116 | 4000       | 18-19        | 4             | 1                | 2018-08-19     | 2018-08-20  | 0                    |
+	| 999000117 | 5000       | 18-19        | 5             | 1                | 2018-09-19     | 2018-09-20  | 0                    |
+	| 999000118 | 6000       | 18-19        | 6             | 1                | 2018-10-19     | 2018-10-20  | 0                    |
+	| 999000119 | 12500      | 17-18        | 12            | 1                | 2018-10-19     | 2018-10-20  | 1                    |
+	| 999000120 | 7000       | 18-19        | 7             | 1                | 2018-11-19     | 2018-11-20  | 0                    |
+	| 999000121 | 8000       | 18-19        | 8             | 1                | 2018-12-19     | 2018-12-20  | 0                    |
+	| 999000122 | 9000       | 18-19        | 9             | 1                | 2019-01-19     | 2019-01-20  | 0                    |
+	| 999000123 | 10000      | 18-19        | 10            | 1                | 2019-02-19     | 2019-02-20  | 0                    |
+	| 999000124 | 11000      | 18-19        | 11            | 1                | 2019-03-19     | 2019-03-20  | 0                    |
+	| 999000125 | 13000      | 16-17        | 12            | 1                | 2019-03-19     | 2019-03-20  | 1                    |
+	When we refresh levy data for paye scheme on the 03/2019
+    And all the transaction lines in this scenario have had their transaction date updated to their created date
+	Then we should see a level 1 screen with a levy declared of 1100 on the 03/2019
+	And we should see a level 1 screen with a balance of 13750 on the 03/2019
--- src/SFA.DAS.EmployerFinance.AcceptanceTests/Features/LateAccounts/Scenario-01-Account-is-opened-on-the-23-MAY-2017-and-after-only-added-valid-PAYE-HMRC-submissions-no-older-than-12-months-old.feature.cs
+++ src/SFA.DAS.EmployerFinance.AcceptanceTests/Features/LateAccounts/Scenario-01-Account-is-opened-on-the-23-MAY-2017-and-after-only-added-valid-PAYE-HMRC-submissions-no-older-than-12-months-old.feature.cs
@@ -940,6 +940,296 @@ this.ScenarioInitialize(scenarioInfo);
 #line hidden
             this.ScenarioCleanup();
         }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("6 - A PAYE being used to create a new account has been in a different account lon" +
+            "ger ago than the 12 month limit and the new account includes end of year adjustm" +
+            "ents within that time limit and ignores one outside the limit.")]
+        public virtual void _6_APAYEBeingUsedToCreateANewAccountHasBeenInADifferentAccountLongerAgoThanThe12MonthLimitAndTheNewAccountIncludesEndOfYearAdjustmentsWithinThatTimeLimitAndIgnoresOneOutsideTheLimit_()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("6 - A PAYE being used to create a new account has been in a different account lon" +
+                    "ger ago than the 12 month limit and the new account includes end of year adjustm" +
+                    "ents within that time limit and ignores one outside the limit.", null, ((string[])(null)));
+#line 148
+this.ScenarioInitialize(scenarioInfo);
+            this.ScenarioStart();
+#line 149
+ testRunner.Given("An employer is adding a PAYE which has submissions older than the 12 month expiry" +
+                    " rule limit", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+            TechTalk.SpecFlow.Table table7 = new TechTalk.SpecFlow.Table(new string[] {
+                        "Id",
+                        "LevyDueYtd",
+                        "Payroll_Year",
+                        "Payroll_Month",
+                        "English_Fraction",
+                        "SubmissionDate",
+                        "CreatedDate",
+                        "EndofYear Adjustment"});
+            table7.AddRow(new string[] {
+                        "999000100",
+                        "12000",
+                        "16-17",
+                        "12",
+                        "1",
+                        "2017-04-19",
+                        "2017-04-20",
+                        "0"});
+            table7.AddRow(new string[] {
+                        "999000101",
+                        "1000",
+                        "17-18",
+                        "1",
+                        "1",
+                        "2017-05-19",
+                        "2017-05-20",
+                        "0"});
+            table7.AddRow(new string[] {
+                        "999000102",
+                        "2000",
+                        "17-18",
+                        "2",
+                        "1",
+                        "2017-06-19",
+                        "2017-06-20",
+                        "0"});
+            table7.AddRow(new string[] {
+                        "999000103",
+                        "3000",
+                        "17-18",
+                        "3",
+                        "1",
+                        "2017-07-19",
+                        "2017-07-20",
+                        "0"});
+            table7.AddRow(new string[] {
+                        "999000104",
+                        "4000",
+                        "17-18",
+                        "4",
+                        "1",
+                        "2017-08-19",
+                        "2017-08-20",
+                        "0"});
+            table7.AddRow(new string[] {
+                        "999000105",
+                        "5000",
+                        "17-18",
+                        "5",
+                        "1",
+                        "2017-09-19",
+                        "2017-09-20",
+                        "0"});
+#line 150
+ testRunner.And("Hmrc return the following submissions for paye scheme", ((string)(null)), table7, "And ");
+#line 158
+ testRunner.And("we refresh levy data for paye scheme on the 09/2017", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 159
+    testRunner.And("Another account is opened and associated with the paye scheme", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            TechTalk.SpecFlow.Table table8 = new TechTalk.SpecFlow.Table(new string[] {
+                        "Id",
+                        "LevyDueYtd",
+                        "Payroll_Year",
+                        "Payroll_Month",
+                        "English_Fraction",
+                        "SubmissionDate",
+                        "CreatedDate",
+                        "EndofYear Adjustment"});
+            table8.AddRow(new string[] {
+                        "999000106",
+                        "6000",
+                        "17-18",
+                        "6",
+                        "1",
+                        "2017-10-19",
+                        "2017-10-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000107",
+                        "7000",
+                        "17-18",
+                        "7",
+                        "1",
+                        "2017-11-19",
+                        "2017-11-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000108",
+                        "8000",
+                        "17-18",
+                        "8",
+                        "1",
+                        "2017-12-19",
+                        "2017-12-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000109",
+                        "9000",
+                        "17-18",
+                        "9",
+                        "1",
+                        "2018-01-19",
+                        "2018-01-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000110",
+                        "10000",
+                        "17-18",
+                        "10",
+                        "1",
+                        "2018-02-19",
+                        "2018-02-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000111",
+                        "11000",
+                        "17-18",
+                        "11",
+                        "1",
+                        "2018-03-19",
+                        "2018-03-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000112",
+                        "12000",
+                        "17-18",
+                        "12",
+                        "1",
+                        "2018-04-19",
+                        "2018-04-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000113",
+                        "1000",
+                        "18-19",
+                        "1",
+                        "1",
+                        "2018-05-19",
+                        "2018-05-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000114",
+                        "2000",
+                        "18-19",
+                        "2",
+                        "1",
+                        "2018-06-19",
+                        "2018-06-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000115",
+                        "3000",
+                        "18-19",
+                        "3",
+                        "1",
+                        "2018-07-19",
+                        "2018-07-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000116",
+                        "4000",
+                        "18-19",
+                        "4",
+                        "1",
+                        "2018-08-19",
+                        "2018-08-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000117",
+                        "5000",
+                        "18-19",
+                        "5",
+                        "1",
+                        "2018-09-19",
+                        "2018-09-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000118",
+                        "6000",
+                        "18-19",
+                        "6",
+                        "1",
+                        "2018-10-19",
+                        "2018-10-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000119",
+                        "12500",
+                        "17-18",
+                        "12",
+                        "1",
+                        "2018-10-19",
+                        "2018-10-20",
+                        "1"});
+            table8.AddRow(new string[] {
+                        "999000120",
+                        "7000",
+                        "18-19",
+                        "7",
+                        "1",
+                        "2018-11-19",
+                        "2018-11-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000121",
+                        "8000",
+                        "18-19",
+                        "8",
+                        "1",
+                        "2018-12-19",
+                        "2018-12-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000122",
+                        "9000",
+                        "18-19",
+                        "9",
+                        "1",
+                        "2019-01-19",
+                        "2019-01-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000123",
+                        "10000",
+                        "18-19",
+                        "10",
+                        "1",
+                        "2019-02-19",
+                        "2019-02-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000124",
+                        "11000",
+                        "18-19",
+                        "11",
+                        "1",
+                        "2019-03-19",
+                        "2019-03-20",
+                        "0"});
+            table8.AddRow(new string[] {
+                        "999000125",
+                        "13000",
+                        "16-17",
+                        "12",
+                        "1",
+                        "2019-03-19",
+                        "2019-03-20",
+                        "1"});
+#line 160
+    testRunner.And("Hmrc return the following submissions for paye scheme", ((string)(null)), table8, "And ");
+#line 182
+ testRunner.When("we refresh levy data for paye scheme on the 03/2019", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 183
+    testRunner.And("all the transaction lines in this scenario have had their transaction date update" +
+                    "d to their created date", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 184
+ testRunner.Then("we should see a level 1 screen with a levy declared of 1100 on the 03/2019", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 185
+ testRunner.And("we should see a level 1 screen with a balance of 13750 on the 03/2019", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
     }
 }
 #pragma warning restore
