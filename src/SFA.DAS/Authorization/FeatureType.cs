b32cc2a4cee8611e04773cf8dade16a72087a355
Steve O'Hara <github@soharadesign.com>
2019-04-18 10:51:24 +0100

    [AML-3529] Added 'NewRegistration' feature to FeatureType

--- src/SFA.DAS/Authorization/FeatureType.cs
+++ src/SFA.DAS/Authorization/FeatureType.cs
@@ -10,6 +10,11 @@
         TransferConnectionRequests,
         Transfers,
 
+        /// <summary>
+        /// Enables the New Registration journey
+        /// </summary>
+        NewRegistration,
+
         // These enums are only used in unit tests - the numbers can be changed 
         Test1 = 100,
         Test2 = 101
