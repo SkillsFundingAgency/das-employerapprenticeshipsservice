6a921690d753ccae312898353b864b736f9e6222
Phil Davies <github@phildavies.co.uk>
2019-04-24 13:25:50 +0100

    webjob host wip

---
+++ src/SFA.DAS.EAS/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SFA.DAS.EAS.Configuration
+{
+    public static class ConfigurationKeys
+    {
+        //todo: piggy back on existing, or create new for homepage?
+        public const string EmployerApprenticeshipsServiceHomepage = "SFA.DAS.EmployerApprenticeshipsService.Homepage";
+    }
+}
---
+++ src/SFA.DAS.EAS/Startup/ConfigurationStartup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.EAS.Configuration;
+
+namespace SFA.DAS.EAS.Startup
+{
+    public static class ConfigurationStartup
+    {
+        public static IHostBuilder ConfigureDasAppConfiguration(this IHostBuilder builder, string[] args)
+        {
+            return builder.ConfigureAppConfiguration((c, b) => b
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{c.HostingEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .AddAzureTableStorage(ConfigurationKeys.EmployerApprenticeshipsServiceHomepage)
+                .AddCommandLine(args));
+        }
+    }
+}
