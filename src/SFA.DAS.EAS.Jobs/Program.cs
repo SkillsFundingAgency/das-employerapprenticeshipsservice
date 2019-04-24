a339623620840fb8b9b8fe8e79ac58a5d09e8e8b
Phil Davies <github@phildavies.co.uk>
2019-04-24 09:14:15 +0100

    add .net core console project to host webjobs

---
+++ src/SFA.DAS.EAS.Jobs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.EAS.Jobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
