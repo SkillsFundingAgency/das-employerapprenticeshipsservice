99e5331f447ad82002a70ba0975fea7a4f1e3921
Phil Davies <github@phildavies.co.uk>
2019-05-01 13:18:49 +0100

    cosmos db wip

--- src/SFA.DAS.EAS.Portal.Jobs/StartupJobs/CreateReadStoreDatabaseJob.cs
+++ src/SFA.DAS.EAS.Portal.Jobs/StartupJobs/CreateReadStoreDatabaseJob.cs
@@ -1,23 +1,72 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.EAS.Portal.Database;
 
 namespace SFA.DAS.EAS.Portal.Jobs.StartupJobs
 {
     // there's a separate task/pr for this, but we need a 'hello world' job to test the host
     public class CreateReadStoreDatabaseJob
     {
+        private readonly IDocumentClient _documentClient;
+
+        public CreateReadStoreDatabaseJob(IDocumentClient documentClient)
+        {
+            _documentClient = documentClient;
+        }
+
         // singleton attribute requires a real storage account, so webjobs has access to blobs (so emulator doesn't work)
         // set env variables AzureWebJobsDashboard & AzureWebJobsStorage (for now) to a real storage account
         // add to readme.md?
         //todo: use secret manager, rather than env variables (easier to have different settings for different projects)
         // ^^ see https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.2&tabs=windows
+        //todo: inject ilogger in ctor?
         [NoAutomaticTrigger]
         [Singleton]
-        public Task CreateReadStoreDatabase(ExecutionContext executionContext, ILogger logger)
+        public async Task CreateReadStoreDatabase(ExecutionContext executionContext, ILogger logger)
         {
-            logger.LogInformation($"{executionContext.InvocationId}: Helo Byd");
-            return Task.CompletedTask;
+            //logger.LogInformation($"{executionContext.InvocationId}: Helo Byd");
+
+            var database = new Microsoft.Azure.Documents.Database
+            {
+                Id = DocumentSettings.DatabaseName
+            };
+
+            var documentCollection = new DocumentCollection
+            {
+                Id = DocumentSettings.AccountsCollectionName,
+                PartitionKey = new PartitionKeyDefinition
+                {
+                    Paths = new Collection<string>
+                    {
+                        "/accountId"
+                    }
+                },
+                UniqueKeyPolicy = new UniqueKeyPolicy
+                {
+                    UniqueKeys = new Collection<UniqueKey>
+                    {
+                        //new UniqueKey
+                        //{
+                        //    Paths = new Collection<string> { "/accountProviderLegalEntityId" }
+                        //}
+                    }
+                }
+            };
+
+            logger.LogInformation($"{executionContext.InvocationId}: Creating database and collection if they don't exist");
+
+            var createDatabaseResponse = await _documentClient.CreateDatabaseIfNotExistsAsync(database);
+            logger.LogInformation($"{executionContext.InvocationId}: Create database returned {createDatabaseResponse.StatusCode}");
+
+            var createDocumentCollectionResponse = await _documentClient.CreateDocumentCollectionIfNotExistsAsync(
+                UriFactory.CreateDatabaseUri(database.Id),
+                documentCollection,
+                new RequestOptions { OfferThroughput = 1000 });
+            logger.LogInformation($"{executionContext.InvocationId}: Create document collection returned {createDocumentCollectionResponse.StatusCode}");
         }
     }
 }
---
+++ src/SFA.DAS.EAS.Portal/Database/DocumentSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SFA.DAS.EAS.Portal.Database
+{
+    // provider permissions has this in its api.client project, but if the only consumers are the worker & web, then might as well live here
+    public static class DocumentSettings
+    {
+        public const string DatabaseName = "SFA.DAS.EAS.Portal.Database";
+        public const string AccountsCollectionName = "Accounts";
+    }
+}
