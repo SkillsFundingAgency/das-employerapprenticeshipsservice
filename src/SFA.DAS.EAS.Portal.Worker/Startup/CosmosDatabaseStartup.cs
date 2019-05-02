454e5d0bc09fe9ba38112ebff4b4749c53a8254f
Phil Davies <github@phildavies.co.uk>
2019-05-02 15:39:40 +0100

    reserve funding handling wip

--- src/SFA.DAS.EAS.Portal.Worker/Startup/CosmosDatabaseStartup.cs
+++ src/SFA.DAS.EAS.Portal.Worker/Startup/CosmosDatabaseStartup.cs
@@ -15,7 +15,9 @@ namespace SFA.DAS.EAS.Portal.Worker.Startup
 
             return services
                 .AddTransient<IDocumentClientFactory, DocumentClientFactory>()
+                //todo: provider permissions adds this as a singleton! if it should be a singleton, why not just use the factory here and not add the factory??
                 .AddTransient(sp => sp.GetService<IDocumentClientFactory>().CreateDocumentClient())
+                .AddTransient<IAccountsRepository, AccountsRepository>()
                 .Configure<CosmosDatabaseConfiguration>(configuration.GetPortalSection("CosmosDatabase"));
         }
     }
