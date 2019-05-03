b1c9eb6f278ddb4d4b385bb749fc78d71ea4e9fa
Phil Davies <github@phildavies.co.uk>
2019-05-03 15:31:05 +0100

    unit test wip

---
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Database/Models/AccountTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Database.Models
+{
+    [TestFixture]
+    [Parallelizable]
+    public class AccountTests : FluentTest<AccountTestsFixture>
+    {
+    }
+
+    public class AccountTestsFixture
+    {
+    }
+}
