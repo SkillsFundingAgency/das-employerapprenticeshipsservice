da27124574a9e9a604982f27c769c74c4fe66bc3
Phil Davies <github@phildavies.co.uk>
2019-05-07 13:12:46 +0100

    client test harness wip

---
+++ src/SFA.DAS.EAS.Portal.Client.TestHarness/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SFA.DAS.EAS.Portal.Client.TestHarness.Scenarios;
+using SFA.DAS.EAS.Portal.Startup;
+
+namespace SFA.DAS.EAS.Portal.Client.TestHarness
+{
+    //todo: logging?
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var host = CreateHostBuilder(args).Build())
+            {
+                await host.StartAsync();
+
+                var getAccount = host.Services.GetService<GetAccountScenario>();
+                await getAccount.Run();
+
+                await host.WaitForShutdownAsync();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+                //todo: structuremap di
+                .ConfigureDasAppConfiguration(args)
+                .ConfigureDasLogging()
+                .UseDasEnvironment()
+                .ConfigureServices(s => s.AddTransient<GetAccountScenario, GetAccountScenario>());
+    }
+}
