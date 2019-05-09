407da42918296c609cef2aafb55bb381fe216be7
Phil Davies <github@phildavies.co.uk>
2019-05-09 14:11:43 +0100

    gotta use a class with IOptionsMonitor

--- src/SFA.DAS.EAS.Portal.Client/Models/Concrete/AccountDto.cs
+++ src/SFA.DAS.EAS.Portal.Client/Models/Concrete/AccountDto.cs
@@ -15,6 +15,7 @@ namespace SFA.DAS.EAS.Portal.Client.Models.Concrete
         public virtual long AccountId { get; private set; }
 
         //todo: return List in dto's, not enumerable (so consumer can add to them without copying - return most specific type)
+        // https://stackoverflow.com/questions/15490633/why-cant-i-use-a-compatible-concrete-type-when-implementing-an-interface
         [JsonProperty("accountLegalEntities")]
         public virtual IEnumerable<IAccountLegalEntityDto<IReservedFundingDto>> AccountLegalEntities { get; private set; }
 
