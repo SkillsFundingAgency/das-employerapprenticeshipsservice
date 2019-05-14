397371fe99767f951fe1f77b39e2365f2a15f15d
Phil Davies <github@phildavies.co.uk>
2019-05-14 14:45:02 +0100

    remove todos only

--- src/SFA.DAS.EAS.Portal.Client/Models/IAccountDto.cs
+++ src/SFA.DAS.EAS.Portal.Client/Models/IAccountDto.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace SFA.DAS.EAS.Portal.Client.Models
 {
-    //todo: can we simplify as we're using covariant?
     public interface IAccountDto
     {
         long AccountId { get; }
