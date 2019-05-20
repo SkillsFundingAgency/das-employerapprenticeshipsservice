97c411d14fb20c6642935707a49e695f7f1e8ec5
Ashley-Bass <ashley.bass@education.gov.uk>
2019-05-20 10:25:01 +0100

    Commented out code to hopefully allow a build to generate new package

--- src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountDashboardViewModel.cs
+++ src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountDashboardViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using SFA.DAS.Authorization;
-using SFA.DAS.EAS.Portal.Types;
 using SFA.DAS.EmployerAccounts.Models.Account;
 
 namespace SFA.DAS.EmployerAccounts.Web.ViewModels
@@ -29,9 +28,9 @@ namespace SFA.DAS.EmployerAccounts.Web.ViewModels
         public bool ApprenticeshipAdded { get; set; }
         public bool ShowSearchBar { get; set; }
         public bool ShowMostActiveLinks { get; set; }
-        public Portal.Types.Account AccountViewModel { get; set; }
+        //public Portal.Types.Account AccountViewModel { get; set; }
         public Guid? RecentlyAddedReservationId { get; set; }
-        public Reservation ReservedFundingToShow => AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.FirstOrDefault(rf => rf.Id == RecentlyAddedReservationId) ?? AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.FirstOrDefault();
-        public bool ShowReservations => AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.Count > 0;
+        //public Reservation ReservedFundingToShow => AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.FirstOrDefault(rf => rf.Id == RecentlyAddedReservationId) ?? AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.FirstOrDefault();
+        //public bool ShowReservations => AccountViewModel?.Organisations?.FirstOrDefault().Reservations?.Count > 0;
     }
 }   
