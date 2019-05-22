f51b41e4b2a221acf916fca289b984c0c7575871
Paul Howes <paul.howes@cognetics.net>
2019-05-22 11:38:54 +0100

    Added controller tests

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Controllers/SearchPensionRegulatorControllerTests/WhenISearchThePensionRegulator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Authentication;
+using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Web.Controllers;
+using SFA.DAS.EmployerAccounts.Web.Orchestrators;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Controllers.SearchPensionRegulatorControllerTests
+{
+    class WhenISearchThePensionRegulator
+    {
+        private SearchPensionRegulatorController _controller;
+        private Mock<IAuthenticationService> _owinWrapper;
+        private Mock<SearchPensionRegulatorOrchestrator> _orchestrator;
+        private Mock<IMultiVariantTestingService> _multiVariantTestingService;
+        private Mock<ICookieStorageService<FlashMessageViewModel>> _flashMessage;
+
+        [SetUp]
+        private void Arrange()
+        {
+            _controller = new SearchPensionRegulatorController(
+                _owinWrapper.Object,
+                _orchestrator.Object,
+                _multiVariantTestingService.Object,
+                _flashMessage.Object,
+                Mock.Of<IMediator>())
+            {
+
+            };
+        }
+    }
+}
