06f779ad2013dac7491a85f480630702f1ad6dbb
Ashley-Bass <ashley.bass@education.gov.uk>
2019-05-23 11:24:02 +0100

    Only show saved favourites on new homepage toggle

--- src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountDashboardViewModel.cs
+++ src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountDashboardViewModel.cs
@@ -30,5 +30,6 @@ namespace SFA.DAS.EmployerAccounts.Web.ViewModels
         public Guid? RecentlyAddedReservationId { get; set; }
         public IReservedFundingDto ReservedFundingToShow { get; set; }
         public string ReservedFundingToShowLegalEntityName { get; set; }
+        public bool ShowSavedFavourites => FeatureToggles.Features.HomePage.Enabled;
     }
 }   
