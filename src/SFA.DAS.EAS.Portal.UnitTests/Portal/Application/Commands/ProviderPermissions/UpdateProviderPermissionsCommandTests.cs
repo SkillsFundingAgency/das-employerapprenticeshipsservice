d185c3b77cc87a07e335be6b6098992ce61c9c91
Phil Davies <github@phildavies.co.uk>
2019-05-24 14:25:49 +0100

    add (empty) unit test for provider permissions

---
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/Commands/ProviderPermissions/UpdateProviderPermissionsCommandTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using SFA.DAS.Testing;
+
+namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Application.Commands.ProviderPermissions
+{
+    [Parallelizable]
+    [TestFixture]
+    public class UpdateProviderPermissionsCommandTests : FluentTest<UpdateProviderPermissionsCommandTestsFixture>
+    {
+    }
+
+    public class UpdateProviderPermissionsCommandTestsFixture
+    {
+    }
+}
