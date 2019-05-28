9a70197671cc1484ab1f54e728dea8f0cb678de0
Kevin <kevin@stephek.co.uk>
2019-05-28 10:00:49 +0100

    CON-503-add-controller-tests

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Controllers/EmployerTeamControllerTests/WhenAgreementsToSign.cs
@@ -0,0 +1,69 @@
+﻿using Moq;
+using NUnit.Framework;
+using SFA.DAS.Authentication;
+using SFA.DAS.Authorization;
+using SFA.DAS.EAS.Portal.Client;
+using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Web.Controllers;
+using SFA.DAS.EmployerAccounts.Web.Orchestrators;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
+using SFA.DAS.HashingService;
+using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Controllers.EmployerTeamControllerTests
+{
+    public class WhenAgreementsToSign
+    {
+        private EmployerTeamController _controller;
+
+        private Mock<IAuthenticationService> mockAuthenticationService;
+        private Mock<IAuthorizationService> mockAuthorizationService;
+        private Mock<IMultiVariantTestingService> mockMultiVariantTestingService;
+        private Mock<ICookieStorageService<FlashMessageViewModel>> mockCookieStorageService;
+        private Mock<EmployerTeamOrchestrator> mockEmployerTeamOrchestrator;
+        private Mock<IPortalClient> mockPortalClient;
+        private Mock<IHashingService> mockHashingService;
+
+        [SetUp]
+        public void Arrange()
+        {
+            mockAuthenticationService = new Mock<IAuthenticationService>();
+            mockAuthorizationService = new Mock<IAuthorizationService>();
+            mockMultiVariantTestingService = new Mock<IMultiVariantTestingService>();
+            mockCookieStorageService = new Mock<ICookieStorageService<FlashMessageViewModel>>();
+            mockEmployerTeamOrchestrator = new Mock<EmployerTeamOrchestrator>();
+            mockPortalClient = new Mock<IPortalClient>();
+            mockHashingService = new Mock<IHashingService>();
+
+            _controller = new EmployerTeamController(
+                mockAuthenticationService.Object,
+                mockAuthorizationService.Object,
+                mockMultiVariantTestingService.Object,
+                mockCookieStorageService.Object,
+                mockEmployerTeamOrchestrator.Object,
+                mockPortalClient.Object,
+                mockHashingService.Object);
+        }
+
+        [Test]
+        public void ThenTheProviderPermissionsDeniedViewIsReturned()
+        {
+            // Arrange
+            var model = new AccountDashboardViewModel();
+            model.PayeSchemeCount = 1;
+            model.AgreementsToSign = true;
+
+            var organisation = new EAS.Portal.Client.Types.Organisation();
+            organisation.Providers.Add(new EAS.Portal.Client.Types.Provider());
+            model.AccountViewModel = new EAS.Portal.Client.Types.Account();
+            model.AccountViewModel.Organisations.Add(organisation);
+
+            //Act
+            var result = _controller.Row1Panel2(model) as PartialViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("ProviderPermissionsDenied", (result.Model as dynamic).ViewName);
+        }
+    }
+}
