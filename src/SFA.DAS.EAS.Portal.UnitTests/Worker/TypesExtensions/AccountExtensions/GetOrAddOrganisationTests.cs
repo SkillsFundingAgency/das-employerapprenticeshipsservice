036ea9e1f383638564c4d1ea902300e9982ff8bd
Phil Davies <github@phildavies.co.uk>
2019-06-14 10:47:50 +0100

    AccountExtensionsTestsFixture wip

--- src/SFA.DAS.EAS.Portal.UnitTests/Worker/TypesExtensions/AccountExtensions/GetOrAddOrganisationTests.cs
+++ src/SFA.DAS.EAS.Portal.UnitTests/Worker/TypesExtensions/AccountExtensions/GetOrAddOrganisationTests.cs
@@ -1,6 +1,3 @@
-using System.Linq;
-using AutoFixture;
-using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.EAS.Portal.Client.Types;
 using SFA.DAS.EAS.Portal.Worker.TypesExtensions;
@@ -14,143 +11,75 @@ namespace SFA.DAS.EAS.Portal.UnitTests.Worker.TypesExtensions.AccountExtensions
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationNotInAccount_ThenShouldCreateAndAddOrganisationToAccount()
         {
-            Test(f => f.GetOrAddOrganisation(), f => f.AssertOrganisationCreatedAndAddedToAccount());
+            Test(f => f.GetOrAddOrganisation(), f => f.AssertEntityCreatedAndAddedToAccount());
         }
 
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationNotInAccount_ThenShouldReturnCreatedOrganisation()
         {
-            Test(f => f.GetOrAddOrganisation(), (f, r) => f.AssertReturnCreatedOrganisation(r));
+            Test(f => f.GetOrAddOrganisation(), (f, r) => f.AssertReturnCreatedEntity(r));
         }
         
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationNotInAccountAndOnAddActionGiven_ThenShouldCreateAndAddMutatedOrganisationToAccount()
         {
             Test(f => f.ArrangeOnAddActionGiven(), f => f.GetOrAddOrganisation(),
-                f => f.AssertOrganisationCreatedAndAddedToAccount(true));
+                f => f.AssertEntityCreatedAndAddedToAccount(true));
         }
 
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationNotInAccountAndOnAddActionGiven_ThenShouldReturnMutatedOrganisation()
         {
             Test(f => f.ArrangeOnAddActionGiven(), f => f.GetOrAddOrganisation(),
-                (f, r) => f.AssertReturnCreatedOrganisation(r, true));
+                (f, r) => f.AssertReturnCreatedEntity(r, true));
         }
 
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationInAccount_ThenShouldNotAddANewOrganisationToAccount()
         {
-            Test(f => f.ArrangeOrganisationInAccount(), f => f.GetOrAddOrganisation(),
-                f => f.AssertOriginalOrganisationsInAccount());
+            Test(f => f.ArrangeEntityInAccount(), f => f.GetOrAddOrganisation(),
+                f => f.AssertOriginalEntityInAccount());
         }
 
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationInAccount_ThenShouldReturnExistingOrganisation()
         {
-            Test(f => f.ArrangeOrganisationInAccount(), f => f.GetOrAddOrganisation(),
-                (f, r) => f.AssertReturnExistingOrganisation(r));
+            Test(f => f.ArrangeEntityInAccount(), f => f.GetOrAddOrganisation(),
+                (f, r) => f.AssertReturnExistingEntity(r));
         }
         
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationInAccountAndOnGetActionGiven_ThenShouldMutateExistingOrganisationInAccount()
         {
-            Test(f => f.ArrangeOrganisationInAccount().ArrangeOnGetActionGiven(),
+            Test(f => f.ArrangeEntityInAccount().ArrangeOnGetActionGiven(),
                 f => f.GetOrAddOrganisation(),
-                (f, r) => f.AssertExistingOrganisationMutatedInAccount());
+                (f, r) => f.AssertExistingEntityMutatedInAccount());
         }
 
         [Test]
         public void GetOrAddOrganisation_WhenOrganisationInAccountAndOnGetActionGiven_ThenShouldReturnMutatedExistingOrganisation()
         {
-            Test(f => f.ArrangeOrganisationInAccount().ArrangeOnGetActionGiven(),
+            Test(f => f.ArrangeEntityInAccount().ArrangeOnGetActionGiven(),
                 f => f.GetOrAddOrganisation(),
-                (f, r) => f.AssertReturnExistingOrganisation(r, true));
+                (f, r) => f.AssertReturnExistingEntity(r, true));
         }
     }
 
-    public class GetOrAddOrganisationTestsFixture : AccountExtensionsTestsFixture<Organisation>
+    public class GetOrAddOrganisationTestsFixture
+        : AccountExtensionsTestsFixture<GetOrAddOrganisationTestsFixture, Organisation, long, string>
     {
-        public long AccountLegalEntityId { get; set; }
-        public string MutatedOrganisationName { get; set; }
-
-        public GetOrAddOrganisationTestsFixture()
+        public GetOrAddOrganisationTestsFixture() 
+            : base(account => account.Organisations, 
+                organisation => organisation.AccountLegalEntityId,
+                (organisation, accountLegalEntity) => organisation.AccountLegalEntityId = accountLegalEntity,
+                (organisation, name) => organisation.Name = name)
         {
-            AccountLegalEntityId = Fixture.Create<long>();
-        }
-        
-        public GetOrAddOrganisationTestsFixture ArrangeOrganisationInAccount()
-        {
-            Account.Organisations.RandomElement().AccountLegalEntityId = AccountLegalEntityId;
-            return this;
-        }
-
-        public GetOrAddOrganisationTestsFixture ArrangeOnAddActionGiven()
-        {
-            OnAdd = organisation => organisation.Name = MutatedOrganisationName = Fixture.Create<string>();
-            return this;
-        }
-
-        public GetOrAddOrganisationTestsFixture ArrangeOnGetActionGiven()
-        {
-            OnGet = organisation => organisation.Name = MutatedOrganisationName = Fixture.Create<string>();
-            return this;
         }
         
         public Organisation GetOrAddOrganisation()
         {
             OriginalAccount = Account.Clone();
-            return Account.GetOrAddOrganisation(AccountLegalEntityId, OnAdd, OnGet);
-        }
-
-        public void AssertOrganisationCreatedAndAddedToAccount(bool mutated = false)
-        {
-            Account.Organisations.Should()
-                .BeEquivalentTo(OriginalAccount.Organisations.Append(ExpectedCreatedOrganisation(mutated)));
-        }
-
-        public void AssertReturnCreatedOrganisation(Organisation returnedOrganisation, bool mutated = false)
-        {
-            returnedOrganisation.Should()
-                .BeEquivalentTo(ExpectedCreatedOrganisation(mutated));
-        }
-        
-        private Organisation ExpectedCreatedOrganisation(bool mutated)
-        {
-            var expectedOrganisation = new Organisation
-            {
-                AccountLegalEntityId = AccountLegalEntityId
-            };
-            if (mutated)
-                expectedOrganisation.Name = MutatedOrganisationName;
-
-            return expectedOrganisation;
-        }
-
-        public void AssertOriginalOrganisationsInAccount()
-        {
-            Account.Organisations.Should().BeEquivalentTo(OriginalAccount.Organisations);
-        }
-        
-        public void AssertExistingOrganisationMutatedInAccount()
-        {
-            ExpectedExistingOrganisation(true);
-            Account.Organisations.Should()
-                .BeEquivalentTo(OriginalAccount.Organisations);
-        }
-        
-        public void AssertReturnExistingOrganisation(Organisation returnedOrganisation, bool mutated = false)
-        {
-            returnedOrganisation.Should()
-                .BeEquivalentTo(ExpectedExistingOrganisation(mutated));
-        }
-        
-        private Organisation ExpectedExistingOrganisation(bool mutated = false)
-        {
-            var expectedExistingOrganisation = OriginalAccount.Organisations.Single(o => o.AccountLegalEntityId == AccountLegalEntityId);
-            if (mutated)
-                expectedExistingOrganisation.Name = MutatedOrganisationName;
-            
-            return expectedExistingOrganisation;
+            return Account.GetOrAddOrganisation(EntityKey, OnAdd, OnGet);
         }
     }
 }
--- src/SFA.DAS.EAS.Portal.UnitTests/Worker/TypesExtensions/AccountExtensions/GetOrAddProviderTests.cs
+++ src/SFA.DAS.EAS.Portal.UnitTests/Worker/TypesExtensions/AccountExtensions/GetOrAddProviderTests.cs
@@ -1,6 +1,3 @@
-using System.Linq;
-using AutoFixture;
-using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.EAS.Portal.Client.Types;
 using SFA.DAS.EAS.Portal.Worker.TypesExtensions;
@@ -14,143 +11,75 @@ namespace SFA.DAS.EAS.Portal.UnitTests.Worker.TypesExtensions.AccountExtensions
         [Test]
         public void GetOrAddProvider_WhenProviderNotInAccount_ThenShouldCreateAndAddProviderToAccount()
         {
-            Test(f => f.GetOrAddProvider(), f => f.AssertProviderCreatedAndAddedToAccount());
+            Test(f => f.GetOrAddProvider(), f => f.AssertEntityCreatedAndAddedToAccount());
         }
 
         [Test]
         public void GetOrAddProvider_WhenProviderNotInAccount_ThenShouldReturnCreatedProvider()
         {
-            Test(f => f.GetOrAddProvider(), (f, r) => f.AssertReturnCreatedProvider(r));
+            Test(f => f.GetOrAddProvider(), (f, r) => f.AssertReturnCreatedEntity(r));
         }
         
         [Test]
         public void GetOrAddProvider_WhenProviderNotInAccountAndOnAddActionGiven_ThenShouldCreateAndAddMutatedProviderToAccount()
         {
             Test(f => f.ArrangeOnAddActionGiven(), f => f.GetOrAddProvider(),
-                f => f.AssertProviderCreatedAndAddedToAccount(true));
+                f => f.AssertEntityCreatedAndAddedToAccount(true));
         }
 
         [Test]
         public void GetOrAddProvider_WhenProviderNotInAccountAndOnAddActionGiven_ThenShouldReturnMutatedProvider()
         {
             Test(f => f.ArrangeOnAddActionGiven(), f => f.GetOrAddProvider(),
-                (f, r) => f.AssertReturnCreatedProvider(r, true));
+                (f, r) => f.AssertReturnCreatedEntity(r, true));
         }
 
         [Test]
         public void GetOrAddProvider_WhenProviderInAccount_ThenShouldNotAddANewProviderToAccount()
         {
-            Test(f => f.ArrangeProviderInAccount(), f => f.GetOrAddProvider(),
-                f => f.AssertOriginalProvidersInAccount());
+            Test(f => f.ArrangeEntityInAccount(), f => f.GetOrAddProvider(),
+                f => f.AssertOriginalEntityInAccount());
         }
 
         [Test]
         public void GetOrAddProvider_WhenProviderInAccount_ThenShouldReturnExistingProvider()
         {
-            Test(f => f.ArrangeProviderInAccount(), f => f.GetOrAddProvider(),
-                (f, r) => f.AssertReturnExistingProvider(r));
+            Test(f => f.ArrangeEntityInAccount(), f => f.GetOrAddProvider(),
+                (f, r) => f.AssertReturnExistingEntity(r));
         }
         
         [Test]
         public void GetOrAddProvider_WhenProviderInAccountAndOnGetActionGiven_ThenShouldMutateExistingProviderInAccount()
         {
-            Test(f => f.ArrangeProviderInAccount().ArrangeOnGetActionGiven(),
+            Test(f => f.ArrangeEntityInAccount().ArrangeOnGetActionGiven(),
                 f => f.GetOrAddProvider(),
-                (f, r) => f.AssertExistingProviderMutatedInAccount());
+                (f, r) => f.AssertExistingEntityMutatedInAccount());
         }
 
         [Test]
         public void GetOrAddProvider_WhenProviderInAccountAndOnGetActionGiven_ThenShouldReturnMutatedExistingProvider()
         {
-            Test(f => f.ArrangeProviderInAccount().ArrangeOnGetActionGiven(),
+            Test(f => f.ArrangeEntityInAccount().ArrangeOnGetActionGiven(),
                 f => f.GetOrAddProvider(),
-                (f, r) => f.AssertReturnExistingProvider(r, true));
+                (f, r) => f.AssertReturnExistingEntity(r, true));
         }
     }
 
-    public class GetOrAddProviderTestsFixture : AccountExtensionsTestsFixture<Provider>
+    public class GetOrAddProviderTestsFixture
+        : AccountExtensionsTestsFixture<GetOrAddProviderTestsFixture, Provider, long, string>
     {
-        public long Ukprn { get; set; }
-        public string MutatedProviderName { get; set; }
-
-        public GetOrAddProviderTestsFixture()
+        public GetOrAddProviderTestsFixture() 
+            : base(account => account.Providers, 
+                provider => provider.Ukprn,
+                (provider, ukprn) => provider.Ukprn = ukprn,
+                (provider, name) => provider.Name = name)
         {
-            Ukprn = Fixture.Create<long>();
-        }
-
-        public GetOrAddProviderTestsFixture ArrangeProviderInAccount()
-        {
-            Account.Providers.RandomElement().Ukprn = Ukprn;
-            return this;
-        }
-
-        public GetOrAddProviderTestsFixture ArrangeOnAddActionGiven()
-        {
-            OnAdd = provider => provider.Name = MutatedProviderName = Fixture.Create<string>();
-            return this;
-        }
-
-        public GetOrAddProviderTestsFixture ArrangeOnGetActionGiven()
-        {
-            OnGet = provider => provider.Name = MutatedProviderName = Fixture.Create<string>();
-            return this;
         }
         
         public Provider GetOrAddProvider()
         {
             OriginalAccount = Account.Clone();
-            return Account.GetOrAddProvider(Ukprn, OnAdd, OnGet);
+            return Account.GetOrAddProvider(EntityKey, OnAdd, OnGet);
         }
-
-        public void AssertProviderCreatedAndAddedToAccount(bool mutated = false)
-        {
-            Account.Providers.Should()
-                .BeEquivalentTo(OriginalAccount.Providers.Append(ExpectedCreatedProvider(mutated)));
-        }
-
-        public void AssertReturnCreatedProvider(Provider returnedProvider, bool mutated = false)
-        {
-            returnedProvider.Should()
-                .BeEquivalentTo(ExpectedCreatedProvider(mutated));
-        }
-        
-        private Provider ExpectedCreatedProvider(bool mutated)
-        {
-            var expectedProvider = new Provider
-            {
-                Ukprn = Ukprn
-            };
-            if (mutated)
-                expectedProvider.Name = MutatedProviderName;
-
-            return expectedProvider;
-        }
-
-        public void AssertOriginalProvidersInAccount()
-        {
-            Account.Providers.Should().BeEquivalentTo(OriginalAccount.Providers);
-        }
-        
-        public void AssertExistingProviderMutatedInAccount()
-        {
-            ExpectedExistingProvider(true);
-            Account.Providers.Should()
-                .BeEquivalentTo(OriginalAccount.Providers);
-        }
-        
-        public void AssertReturnExistingProvider(Provider returnedProvider, bool mutated = false)
-        {
-            returnedProvider.Should()
-                .BeEquivalentTo(ExpectedExistingProvider(mutated));
-        }
-        
-        private Provider ExpectedExistingProvider(bool mutated = false)
-        {
-            var expectedExistingProvider = OriginalAccount.Providers.Single(o => o.Ukprn == Ukprn);
-            if (mutated)
-                expectedExistingProvider.Name = MutatedProviderName;
-            
-            return expectedExistingProvider;
-        }
-    }
+     }
 }
