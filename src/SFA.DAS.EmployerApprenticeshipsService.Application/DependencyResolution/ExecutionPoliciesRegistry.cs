35fdec37901643a87c10b22c78c5036fb9967988
Michael Young <mick.young1981@googlemail.com>
2019-06-19 15:51:17 +0100

    Removed CompaniesHouseInformationService as it wasn't used.
    Identified when looking to move the execution policy.

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/DependencyResolution/ExecutionPoliciesRegistry.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/DependencyResolution/ExecutionPoliciesRegistry.cs
@@ -8,7 +8,6 @@ namespace SFA.DAS.EAS.Application.DependencyResolution
     {
         public ExecutionPoliciesRegistry()
         {
-            For<ExecutionPolicy>().Use<CompaniesHouseExecutionPolicy>().Named(CompaniesHouseExecutionPolicy.Name);
             For<ExecutionPolicy>().Use<HmrcExecutionPolicy>().Named(HmrcExecutionPolicy.Name).SelectConstructor(() => new HmrcExecutionPolicy(null));
             For<ExecutionPolicy>().Use<IdamsExecutionPolicy>().Named(IdamsExecutionPolicy.Name);
             Policies.Add(new ExecutionPolicyPolicy());
