d239a4b80159161439977d1b3921392b74b23929
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-25 18:55:11 +0100

    AML-3726: publish event when levey transaction amount is greater than zero.

--- src/SFA.DAS.EmployerFinance.UnitTests/Events/ProcessDeclarationTests/WhenIProcessDeclarations.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Events/ProcessDeclarationTests/WhenIProcessDeclarations.cs
@@ -3,7 +3,9 @@ using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerFinance.Data;
 using SFA.DAS.EmployerFinance.Events.ProcessDeclaration;
+using SFA.DAS.EmployerFinance.Messages.Events;
 using SFA.DAS.NLog.Logger;
+using SFA.DAS.NServiceBus;
 
 namespace SFA.DAS.EmployerFinance.UnitTests.Events.ProcessDeclarationTests
 {
@@ -12,14 +14,19 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Events.ProcessDeclarationTests
         private ProcessDeclarationsEventHandler _processDeclarationsEvent;
         private Mock<IDasLevyRepository> _dasLevyRepository;
         private Mock<ILog> _logger;
+        private Mock<IEventPublisher> _eventPublisher;
 
         [SetUp]
         public void Arrange()
         {
+            _eventPublisher = new Mock<IEventPublisher>();
             _dasLevyRepository = new Mock<IDasLevyRepository>();
             _logger = new Mock<ILog>();
 
-            _processDeclarationsEvent = new ProcessDeclarationsEventHandler(_dasLevyRepository.Object, _logger.Object);    
+            _processDeclarationsEvent = new ProcessDeclarationsEventHandler(
+                _dasLevyRepository.Object,
+                _logger.Object,
+                _eventPublisher.Object);
         }
 
         [Test]
@@ -48,6 +55,40 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Events.ProcessDeclarationTests
 
             //Assert
             _logger.Verify(x=>x.Info("Process Declarations Called"));
+        }
+
+        [TestCase(100023,0.0,false)]
+        [TestCase(900034,10.0,true)]
+        public async Task ThenLevyAddedToAccountEventIsPublishedForGreaterThanZeroLevyAmount(
+            long accountId,
+            decimal levyAmount,
+            bool eventShouldBePublished)
+        {
+            _dasLevyRepository
+                .Setup(
+                    m => m.ProcessDeclarations(
+                        accountId,
+                        It.IsAny<string>()))
+                .Returns(
+                    Task.FromResult(levyAmount));
+
+            _processDeclarationsEvent
+                .Handle(
+                    new ProcessDeclarationsEvent
+                    {
+                        AccountId = accountId
+                    });
+
+            var numberOfExpectedCalls = eventShouldBePublished ? Times.Once() : Times.Never();
+
+            _eventPublisher
+                .Verify(
+                    m =>
+                        m.Publish(
+                            It.Is<LevyAddedToAccount>(
+                                arg => arg.AccountId == accountId
+                                       && arg.Amount == levyAmount)),
+                    numberOfExpectedCalls);
         }
     }
 }
