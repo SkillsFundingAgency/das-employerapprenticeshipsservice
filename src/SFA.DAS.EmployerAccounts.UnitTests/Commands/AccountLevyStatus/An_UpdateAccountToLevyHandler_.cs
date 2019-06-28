5e7cb87c6eca4ca51a5352c71b3891ce0cf992eb
Neil Winstanley <Winsto@users.noreply.github.com>
2019-06-28 16:56:21 +0100

    AML-3728: add some logging.

--- src/SFA.DAS.EmployerAccounts.UnitTests/Commands/AccountLevyStatus/An_UpdateAccountToLevyHandler_.cs
+++ src/SFA.DAS.EmployerAccounts.UnitTests/Commands/AccountLevyStatus/An_UpdateAccountToLevyHandler_.cs
@@ -3,9 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using SFA.DAS.EmployerAccounts.Commands.AccountLevyStatus;
 using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.AccountLevyStatus
 {
@@ -16,18 +16,28 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.AccountLevyStatus
         private UpdateAccountToLevyHandler _sut;
         private long _accountId = 90125;
         private Mock<IEmployerAccountRepository> _accountRepository;
+        private Mock<ILog> _logger;
+
+        private UpdateAccountToLevy _updateCommand;
 
         [SetUp]
         public void Setup()
         {
             _accountRepository
                 =
-                new  Mock<IEmployerAccountRepository>();
+                new Mock<IEmployerAccountRepository>();
+
+            _logger
+                =
+                new Mock<ILog>();
 
             _sut
                 =
                 new UpdateAccountToLevyHandler(
-                    _accountRepository.Object);
+                    _accountRepository.Object,
+                    _logger.Object);
+
+            _updateCommand = new UpdateAccountToLevy(_accountId);
         }
 
         [Test]
@@ -35,11 +45,11 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.AccountLevyStatus
         {
             _sut
                 .Handle(
-                    new UpdateAccountToLevy(_accountId));
+                    _updateCommand);
 
             _accountRepository
                 .Verify(
-                    m => 
+                    m =>
                         m.SetAccountAsLevy(
                             _accountId));
         }
@@ -52,14 +62,61 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Commands.AccountLevyStatus
                     m =>
                         m.SetAccountAsLevy(
                             It.IsAny<long>()))
-                .ThrowsAsync(new TestException());
+                .Throws<TestException>();
 
             Assert
                 .ThrowsAsync<TestException>(
                     () =>
-                    _sut
-                        .Handle(
-                            new UpdateAccountToLevy(_accountId)));
+                        _sut
+                            .Handle(
+                                _updateCommand));
+        }
+
+        [Test]
+        public async Task Informs_That_Account_Is_To_Be_Updated()
+        {
+            _sut
+                .Handle(
+                    _updateCommand);
+
+            _logger
+                .Verify(
+                    m =>
+                        m.Info(_sut.UpdatedStartedMessage(_updateCommand)));
+        }
+
+        [Test]
+        public async Task Informs_That_Account_Has_Been_Updated_On_Successful_Completion()
+        {
+            _sut
+                .Handle(
+                    _updateCommand);
+
+            _logger
+                .Verify(
+                    m =>
+                        m.Info(_sut.UpdateCompleteMessage(_updateCommand)));
+        }
+
+        [Test]
+        public async Task Does_Not_Inform_That_Account_Has_Been_Updated_When_Error_Occurs()
+        {
+            _accountRepository
+                .Setup(
+                    m =>
+                        m.SetAccountAsLevy(
+                            It.IsAny<long>()))
+                .Throws<TestException>();
+
+            _sut
+                .Handle(
+                    _updateCommand);
+
+            _logger
+                .Verify(
+                    m =>
+                        m.Info(_sut.UpdateCompleteMessage(_updateCommand)),
+                    Times.Never);
         }
     }
 
--- src/SFA.DAS.EmployerAccounts/Commands/AccountLevyStatus/UpdateAccountToLevyHandler.cs
+++ src/SFA.DAS.EmployerAccounts/Commands/AccountLevyStatus/UpdateAccountToLevyHandler.cs
@@ -1,25 +1,53 @@
 ﻿using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.EmployerAccounts.Commands.AccountLevyStatus
 {
     public class UpdateAccountToLevyHandler : AsyncRequestHandler<UpdateAccountToLevy>
     {
         private readonly IEmployerAccountRepository _accountRepositoryObject;
+        private readonly ILog _logger;
 
-        public UpdateAccountToLevyHandler(IEmployerAccountRepository accountRepositoryObject)
+        public UpdateAccountToLevyHandler(
+            IEmployerAccountRepository accountRepositoryObject,
+            ILog logger)
         {
             _accountRepositoryObject = accountRepositoryObject;
+            _logger = logger;
         }
 
-        protected override Task HandleCore(UpdateAccountToLevy message)
+        protected override Task HandleCore(UpdateAccountToLevy command)
+        {
+            _logger
+                .Info(
+                    UpdatedStartedMessage(
+                        command));
+
+            return
+                _accountRepositoryObject
+                    .SetAccountAsLevy(
+                        command
+                        .AccountId)
+                    .ContinueWith(
+                        t =>
+                            _logger
+                            .Info(
+                                    UpdateCompleteMessage(command)),
+                        TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+
+        public string UpdatedStartedMessage(UpdateAccountToLevy updateCommand)
         {
             return
-            _accountRepositoryObject
-                .SetAccountAsLevy(
-                    message
-                        .AccountId);
+                $"About to update Account with id: {updateCommand.AccountId} to Levy status.";
+        }
+
+        public string UpdateCompleteMessage(UpdateAccountToLevy updateCommand)
+        {
+            return
+                $"Updated Account with id: {updateCommand.AccountId} to Levy status.";
         }
     }
 }
