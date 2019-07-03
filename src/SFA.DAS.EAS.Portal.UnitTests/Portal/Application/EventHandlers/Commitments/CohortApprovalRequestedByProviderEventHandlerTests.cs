580a0ce8212b4428756d2663d8abbbd28ad2752a
Phil Davies <github@phildavies.co.uk>
2019-07-03 10:15:57 +0100

    unit tests wip

--- src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/EventHandlers/Commitments/CohortApprovalRequestedByProviderEventHandlerTests.cs
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/EventHandlers/Commitments/CohortApprovalRequestedByProviderEventHandlerTests.cs
@@ -1,10 +1,15 @@
 using NUnit.Framework;
+using SFA.DAS.Testing;
 
 namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Application.EventHandlers.Commitments
 {
     [TestFixture, Parallelizable]
-    public class CohortApprovalRequestedByProviderEventHandlerTests
+    public class CohortApprovalRequestedByProviderEventHandlerTests : FluentTest<CohortApprovalRequestedByProviderEventHandlerTestsFixture>
     {
         
+    }
+
+    public class CohortApprovalRequestedByProviderEventHandlerTestsFixture
+    {
     }
 }
--- src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/EventHandlers/Commitments/CohortApprovedByEmployerEventHandlerTests.cs
+++ src/SFA.DAS.EAS.Portal.UnitTests/Portal/Application/EventHandlers/Commitments/CohortApprovedByEmployerEventHandlerTests.cs
@@ -1,10 +1,74 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Types.Commitment;
+using SFA.DAS.CommitmentsV2.Messages.Events;
+using SFA.DAS.EAS.Portal.Application.EventHandlers.Commitments;
+using SFA.DAS.EAS.Portal.Client.Database.Models;
+using SFA.DAS.Testing;
 
-namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Application.EventHandlers.Comitments
+namespace SFA.DAS.EAS.Portal.UnitTests.Portal.Application.EventHandlers.Commitments
 {
     [TestFixture, Parallelizable]
-    public class CohortApprovedByEmployerEventHandlerTests
+    public class CohortApprovedByEmployerEventHandlerTests : FluentTest<CohortApprovedByEmployerEventHandlerTestsFixture>
     {
+        [Test]
+        public Task Handle_WhenAccountDoesContainOrganisationAndCohort_ThenAccountDocumentIsSavedWithUpdatedCohort()
+        {
+            return TestAsync(f => f.ArrangeAccountDocumentContainsCohort(), f => f.Handle(),
+                f => f.VerifyAccountDocumentSavedWithCohortApproved());
+        }
+    }
+
+    public class CohortApprovedByEmployerEventHandlerTestsFixture
+    {
+        public EventHandlerTestsFixture<CohortApprovedByEmployer, CohortApprovedByEmployerEventHandler> EventHandlerTestsFixture { get; set; }
+
+        public CommitmentView Commitment { get; set; }
+        public CommitmentView ExpectedCommitment { get; set; }
+        public const long AccountLegalEntityId = 456L;
+
+        public CohortApprovedByEmployerEventHandlerTestsFixture()
+        {
+            EventHandlerTestsFixture = new EventHandlerTestsFixture<CohortApprovedByEmployer, CohortApprovedByEmployerEventHandler>();
+        }
+
+        public CohortApprovedByEmployerEventHandlerTestsFixture ArrangeAccountDocumentContainsCohort()
+        {
+            var organisation = EventHandlerTestsFixture.SetUpAccountDocumentWithOrganisation(
+                EventHandlerTestsFixture.Message.AccountId, AccountLegalEntityId);
+
+            organisation.Cohorts.RandomElement().Id = EventHandlerTestsFixture.Message.CommitmentId.ToString();
+            
+            EventHandlerTestsFixture.AccountDocumentService.Setup(s => s.GetOrCreate(
+                EventHandlerTestsFixture.Message.AccountId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(EventHandlerTestsFixture.AccountDocument);
+            
+            return this;
+        }
+
+        public Task Handle()
+        {
+            ExpectedCommitment = Commitment.Clone();
+
+            return EventHandlerTestsFixture.Handle();
+        }
         
+        public CohortApprovedByEmployerEventHandlerTestsFixture VerifyAccountDocumentSavedWithCohortApproved()
+        {
+            EventHandlerTestsFixture.AccountDocumentService.Verify(
+                s => s.Save(It.Is<AccountDocument>(d => CohortIsSetToApproved(d)),It.IsAny<CancellationToken>()),
+                Times.Once);
+            
+            return this;
+        }
+        
+        private bool CohortIsSetToApproved(AccountDocument document)
+        {
+            return document.Account.Organisations.SelectMany(org => org.Cohorts)
+                .Single(co => co.Id == EventHandlerTestsFixture.Message.CommitmentId.ToString()).IsApproved;
+        }
     }
 }
