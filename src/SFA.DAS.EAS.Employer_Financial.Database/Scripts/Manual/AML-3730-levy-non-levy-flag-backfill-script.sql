137b2d43ebe51ed73b6eeef26bd99a928d193473
tom-gough <tom@tgsd.co.uk>
2019-07-12 13:54:41 +0100

    AML-3730 Added levy non levy flag backfill script with updated requirement for positive amount check

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Scripts/Manual/AML-3730-levy-non-levy-flag-backfill-script.sql
@@ -0,0 +1,8 @@
+﻿UPDATE [SFA.DAS.EAS.Employer_Account.Database].[employer_account].[Account]
+SET ApprenticeshipEmployerType = 1
+WHERE Id IN (
+	SELECT DISTINCT AccountId
+	FROM [SFA.DAS.EAS.Employer_Financial.Database].[employer_financial].[TransactionLine]
+	WHERE TransactionType = 1
+	AND Amount > 0
+)
