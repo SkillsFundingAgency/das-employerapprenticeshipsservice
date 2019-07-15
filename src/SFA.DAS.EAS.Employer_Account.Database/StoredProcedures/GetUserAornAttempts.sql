f9055026521a301c62122e2eeea71bc12d6dfd29
Paul Howes <paul.howes@cognetics.net>
2019-07-15 08:49:38 +0100

    AML-3603 Database changes

---
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetUserAornAttempts.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [employer_account].[GetUserAornAttempts]
+(
+	@UserRef UNIQUEIDENTIFIER
+)
+AS
+BEGIN
+	SELECT	u.UserRef,
+			COUNT(*) AS NumberOfFailedAttempts,
+			MAX(uafa.AttemptTimeStamp) AS LastAttemptTimeStamp
+	FROM	[employer_account].[User] u 
+	JOIN	[employer_account].[UserAornFailedAttempts] uafa on u.Id = uafa.UserId
+	WHERE	u.UserRef = @UserRef
+	GROUP BY u.UserRef
+END
