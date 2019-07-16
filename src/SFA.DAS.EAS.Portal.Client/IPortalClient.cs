7bdaec074443d54c305d0010677dd966a409aa54
Phil Davies <github@phildavies.co.uk>
2019-07-16 12:44:15 +0100

    add xml docs for the public interface

--- src/SFA.DAS.EAS.Portal.Client/IPortalClient.cs
+++ src/SFA.DAS.EAS.Portal.Client/IPortalClient.cs
@@ -6,6 +6,13 @@ namespace SFA.DAS.EAS.Portal.Client
 {
     public interface IPortalClient
     {
+        /// <summary>
+        /// Get the account details required to render the homepage.
+        /// </summary>
+        /// <param name="hashedAccountId">The (non-public) hashed account id.</param>
+        /// <param name="accountState">The current state of the account. Set all flags that apply.</param>
+        /// <param name="cancellationToken">An optional cancellation token.</param>
+        /// <returns></returns>
         Task<Account> GetAccount(string hashedAccountId, AccountState accountState,
             CancellationToken cancellationToken = default);
     }
