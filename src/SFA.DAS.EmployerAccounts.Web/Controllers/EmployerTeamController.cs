09a13d72f0e85d68a59ea679655b1b22f0c14405
Phil Davies <github@phildavies.co.uk>
2019-07-17 15:31:23 +0100

    replace todo with tech debt task

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerTeamController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerTeamController.cs
@@ -52,7 +52,6 @@ using SFA.DAS.EmployerAccounts.Models.Portal;
             var hasPayeScheme = HasPayeScheme(response.Data);
             if (FeatureToggles.Features.HomePage.Enabled || !hasPayeScheme && !HasOrganisation(response.Data))
             {
-                //todo: we need to properly handle the case when there isn't an account document because no events for the account have been processed yet
                 response.Data.AccountViewModel = await _portalClient.GetAccount(new GetAccountParameters
                 {
                     HashedAccountId = hashedAccountId,
