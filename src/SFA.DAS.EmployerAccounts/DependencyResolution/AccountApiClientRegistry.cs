d09c7de52775c9275dc87fa881d5fa9889c0179f
Phil Davies <github@phildavies.co.uk>
2019-07-24 14:03:57 +0100

    remove some litter ;-) (they've been added as coments to the PR instead)

--- src/SFA.DAS.EmployerAccounts/DependencyResolution/AccountApiClientRegistry.cs
+++ src/SFA.DAS.EmployerAccounts/DependencyResolution/AccountApiClientRegistry.cs
@@ -5,12 +5,10 @@ namespace SFA.DAS.EmployerAccounts.DependencyResolution
 {
     public class AccountApiClientRegistry : Registry
     {
-        // we add this here, rather than in the client project, so we don't introduce a dependency on structuremap in the client
         public AccountApiClientRegistry()
         {
-            // the client's not very good, but the version of sfa.das.http currently in EAS isn't either, so we go with the client
             For<IAccountApiClient>().Use<AccountApiClient>();
-            // config is registered in ConfigurationRegistry
+            // config for the client is registered in ConfigurationRegistry
         }
     }
 }
