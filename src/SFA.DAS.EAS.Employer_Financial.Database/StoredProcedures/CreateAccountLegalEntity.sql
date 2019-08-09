cd60b3ebfa9fdaed67a7cbeec85d05aa110a184b
tom-gough <tom@tgsd.co.uk>
2019-08-09 10:52:49 +0100

    Added CreateAccountLegalEntity procedure

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/CreateAccountLegalEntity.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [employer_financial].[CreateAccountLegalEntity]
+	@id BIGINT,
+	@accountId BIGINT,
+	@legalEntityId BIGINT,
+	@signedAgreementVersion INT,
+	@signedAgreementId BIGINT,
+	@pendingAgreementId BIGINT,
+	@deleted DATETIME
+AS
+	INSERT INTO [employer_financial].[AccountLegalEntity]
+		([Id],[AccountId],[LegalEntityId],[SignedAgreementVersion],[SignedAgreementId],[PendingAgreementId],[Deleted])
+	VALUES
+		(@id, @accountId, @legalEntityId, @signedAgreementVersion, @signedAgreementId, @pendingAgreementId, @deleted)
