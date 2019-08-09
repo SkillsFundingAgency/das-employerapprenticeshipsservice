bca470bf9f795c1c4d81dc57e009fac093a1f840
tom-gough <tom@tgsd.co.uk>
2019-08-09 10:40:45 +0100

    Added AccountLegalEntity table

---
+++ src/SFA.DAS.EAS.Employer_Financial.Database/Tables/AccountLegalEntity.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [employer_financial].[AccountLegalEntity]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY,
+	[AccountId] BIGINT NOT NULL,
+	[LegalEntityId] BIGINT NOT NULL,
+	[SignedAgreementVersion] INT NULL,
+	[SignedAgreementId] BIGINT NULL,
+	[PendingAgreementId] BIGINT NULL,
+	[Deleted] DATETIME NULL
+)
