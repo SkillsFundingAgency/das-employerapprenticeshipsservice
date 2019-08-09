26aceda55d578f198b455cc8ab6f603a5a3be86a
Phil Davies <github@phildavies.co.uk>
2019-08-09 15:57:39 +0100

    move over GetUserAccounts action wip

---
+++ src/SFA.DAS.EmployerAccounts.Api.Types/AccountAgreementType.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.EmployerAccounts.Api.Types
+{
+    public enum AccountAgreementType
+    {
+        Levy = 0,
+        NonLevyExpressionOfInterest = 1,
+        Inconsistent = 2,
+        Unknown
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Api/Orchestrators/UsersOrchestrator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SFA.DAS.EmployerAccounts.Api.Types;
+using SFA.DAS.EmployerAccounts.Queries.GetUserAccounts;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.EmployerAccounts.Api.Orchestrators
+{
+    public class UsersOrchestrator
+    {
+        private readonly IMediator _mediator;
+        private readonly ILog _logger;
+        private readonly IMapper _mapper;
+
+        public UsersOrchestrator(IMediator mediator, ILog logger, IMapper mapper)
+        {
+            _mediator = mediator;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        //public async Task<OrchestratorResponse<ICollection<AccountDetailViewModel>>> GetUserAccounts(string userRef)
+        //{
+        //    _logger.Info($"Requesting user's accounts for user Ref  {userRef}");
+
+        //    var accounts = await _mediator.SendAsync(new GetUserAccountsQuery { UserRef = userRef });
+
+        //    var viewModels = accounts.Accounts.AccountList.Select(x => _mapper.Map<AccountDetailViewModel>(x)).ToList();
+
+        //    return new OrchestratorResponse<ICollection<AccountDetailViewModel>>
+        //    {
+        //        Data = viewModels,
+        //        Status = HttpStatusCode.OK
+        //    };
+        //}
+
+        public async Task<ICollection<AccountDetailViewModel>> GetUserAccounts(string userRef)
+        {
+            _logger.Info($"Requesting user's accounts for user Ref  {userRef}");
+
+            var accounts = await _mediator.SendAsync(new GetUserAccountsQuery { UserRef = userRef });
+
+            var viewModels = accounts.Accounts.AccountList.Select(x => _mapper.Map<AccountDetailViewModel>(x)).ToList();
+
+            return viewModels;
+        }
+    }
+}
