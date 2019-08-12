1b7fb9662b7cb4e5e5a18068882139aa348208f8
Phil Davies <github@phildavies.co.uk>
2019-08-12 10:08:36 +0100

    tidy up

--- src/SFA.DAS.EmployerAccounts/Queries/GetUserAccounts/GetUserAccountsQueryResponse.cs
+++ src/SFA.DAS.EmployerAccounts/Queries/GetUserAccounts/GetUserAccountsQueryResponse.cs
@@ -4,9 +4,6 @@ namespace SFA.DAS.EmployerAccounts.Queries.GetUserAccounts
 {
     public class GetUserAccountsQueryResponse
     {
-        //note: account has diverged/forked from domain into accounts
-        // and domain account has been continued to be used & updated with api related changes (e.g. ApprenticeshipEmployerType was added)
-        // and accounts account has been used & updated for web (presumably) (e.g. with the transfer methods)
         public Accounts<Account> Accounts { get; set; }
     }
 }
