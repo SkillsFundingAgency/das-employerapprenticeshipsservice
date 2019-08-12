49a73e7c109f4eb9f88a0f88ad98f4cd9a046c2c
Phil Davies <github@phildavies.co.uk>
2019-08-12 09:25:14 +0100

    remove comments only

--- src/SFA.DAS.EmployerAccounts.Api/Orchestrators/UsersOrchestrator.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Orchestrators/UsersOrchestrator.cs
@@ -22,31 +22,12 @@ namespace SFA.DAS.EmployerAccounts.Api.Orchestrators
             _mapper = mapper;
         }
 
-        //public async Task<OrchestratorResponse<ICollection<AccountDetailViewModel>>> GetUserAccounts(string userRef)
-        //{
-        //    _logger.Info($"Requesting user's accounts for user Ref  {userRef}");
-
-        //    var accounts = await _mediator.SendAsync(new GetUserAccountsQuery { UserRef = userRef });
-
-        //    var viewModels = accounts.Accounts.AccountList.Select(x => _mapper.Map<AccountDetailViewModel>(x)).ToList();
-
-        //    return new OrchestratorResponse<ICollection<AccountDetailViewModel>>
-        //    {
-        //        Data = viewModels,
-        //        Status = HttpStatusCode.OK
-        //    };
-        //}
-
         public async Task<ICollection<AccountDetailViewModel>> GetUserAccounts(string userRef)
         {
             _logger.Info($"Requesting user's accounts for user Ref  {userRef}");
 
             var accounts = await _mediator.SendAsync(new GetUserAccountsQuery { UserRef = userRef });
 
-            //todo: doesn't map many fields, such as ApprenticeshipEmployerType!
-            // detangled api returns...
-            // [{"AccountId":4,"HashedAccountId":"G6M7RV","PublicHashedAccountId":"XWGMWV","DasAccountName":"PINK MOOMIN LTD","DateRegistered":"0001-01-01T00:00:00","OwnerEmail":null,"LegalEntities":null,"PayeSchemes":null,"Balance":0.0,"TransferAllowance":0.0,"RemainingTransferAllowance":0.0,"StartingTransferAllowance":0.0,"DasAccountId":"G6M7RV","AccountAgreementType":0,"ApprenticeshipEmployerType":"0"}]
-            // check original
             var viewModels = accounts.Accounts.AccountList.Select(x => _mapper.Map<AccountDetailViewModel>(x)).ToList();
 
             return viewModels;
