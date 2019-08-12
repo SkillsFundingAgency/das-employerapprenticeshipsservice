bc03bcc2a99286455672ed9e1a727fe0131d9e29
Phil Davies <github@phildavies.co.uk>
2019-08-12 11:09:19 +0100

    redirect user accounts call from original api to new accounts api + remove unused UserOrchestrator

--- src/SFA.DAS.EAS.Api/Controllers/EmployerUserController.cs
+++ src/SFA.DAS.EAS.Api/Controllers/EmployerUserController.cs
@@ -1,35 +1,26 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
 using SFA.DAS.EAS.Account.Api.Attributes;
-using SFA.DAS.EAS.Account.Api.Orchestrators;
+using SFA.DAS.EAS.Domain.Configuration;
 
 namespace SFA.DAS.EAS.Account.Api.Controllers
 {
     [RoutePrefix("api/user/{userRef}")]
     public class EmployerUserController : ApiController
     {
-        private readonly UsersOrchestrator _orchestrator;
+        private readonly EmployerApprenticeshipsServiceConfiguration _configuration;
 
-        public EmployerUserController(UsersOrchestrator orchestrator)
+        public EmployerUserController(EmployerApprenticeshipsServiceConfiguration configuration)
         {
-            _orchestrator = orchestrator;
+            _configuration = configuration;
         }
 
         [Route("accounts", Name = "Accounts")]
         [ApiAuthorize(Roles = "ReadUserAccounts")]
         [HttpGet]
-        public async Task<IHttpActionResult> GetUserAccounts(string userRef)
+        public IHttpActionResult GetUserAccounts(string userRef)
         {
-            var result = await _orchestrator.GetUserAccounts(userRef);
-
-            if (result.Status == HttpStatusCode.OK)
-            {
-                return Ok(result.Data);
-            }
-            
-            //TODO: Handle unhappy paths.
-            return Conflict();
+            return Redirect(_configuration.EmployerAccountsApiBaseUrl + $"api/user/{userRef}/accounts");
         }
     }
 }
