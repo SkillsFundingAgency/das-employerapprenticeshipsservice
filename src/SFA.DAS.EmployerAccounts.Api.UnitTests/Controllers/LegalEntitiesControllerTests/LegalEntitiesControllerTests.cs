1eac8fedc61becb53c9ab44b03758b28b4808ec3
Neil Winstanley <Winsto@users.noreply.github.com>
2019-08-14 15:01:09 +0100

    AML-3643: move tests and controller across. All a bit of a mess atm as need to comment out to get building and have failing tests.

---
+++ src/SFA.DAS.EmployerAccounts.Api.UnitTests/Controllers/LegalEntitiesControllerTests/LegalEntitiesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http.Routing;
+using AutoMapper;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Api.Controllers;
+using SFA.DAS.HashingService;
+using SFA.DAS.NLog.Logger;
+
+namespace SFA.DAS.EmployerAccounts.Api.UnitTests.Controllers.LegalEntitiesControllerTests
+{
+    public abstract class LegalEntitiesControllerTests
+    {
+        protected LegalEntitiesController Controller;
+        protected Mock<IMediator> Mediator;
+        protected Mock<ILog> Logger;
+        protected Mock<UrlHelper> UrlHelper;
+        protected Mock<IMapper> Mapper;
+
+        [SetUp]
+        public void Arrange()
+        {
+            Mediator = new Mock<IMediator>();
+            Logger = new Mock<ILog>();
+            Mapper = new Mock<IMapper>();
+            new Mock<IHashingService>();
+            Controller = new LegalEntitiesController( null);
+
+            UrlHelper = new Mock<UrlHelper>();
+            Controller.Url = UrlHelper.Object;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Api/Controllers/LegalEntitiesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MediatR;
+using SFA.DAS.EmployerAccounts.Api.Attributes;
+using SFA.DAS.Validation.WebApi;
+
+namespace SFA.DAS.EmployerAccounts.Api.Controllers
+{
+    [RoutePrefix("api/accounts/{hashedAccountId}/legalentities")]
+    public class LegalEntitiesController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public LegalEntitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("", Name = "GetLegalEntities")]
+        [ApiAuthorize(Roles = "ReadAllEmployerAccountBalances")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetLegalEntities(string hashedAccountId)
+        {
+            throw new NotImplementedException();
+            //var result = await _orchestrator.GetAccount(hashedAccountId);
+
+//            if (result.Data == null)
+//            {
+//                return NotFound();
+//            }
+//            
+//            result.Data.LegalEntities.ForEach(l => l.Href = Url.Route("GetLegalEntity", new { hashedAccountId, legalEntityId = l.Id }));
+//
+//            return Ok(result.Data.LegalEntities);
+        }
+    }
+}
