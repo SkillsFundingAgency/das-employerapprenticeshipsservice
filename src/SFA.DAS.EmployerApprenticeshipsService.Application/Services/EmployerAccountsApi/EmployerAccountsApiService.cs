9708f55379c17a1b1737c18a588e03c6b75caf53
Phil Davies <github@phildavies.co.uk>
2019-08-16 11:11:23 +0100

    EmployerAccountsApiService wip

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Services/EmployerAccountsApi/EmployerAccountsApiService.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Services/EmployerAccountsApi/EmployerAccountsApiService.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SFA.DAS.EAS.Application.Services.EmployerAccountsApi.Http;
+using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.EAS.Application.Services.EmployerAccountsApi
 {
     public class EmployerAccountsApiService
     {
+        private readonly ILog _log;
+        private readonly HttpClient _httpClient;
+
+        public EmployerAccountsApiService(IEmployerAccountsApiHttpClientFactory employerAccountsApiHttpClientFactory, ILog log)
+        {
+            _log = log;
+            //todo: using RestHttpClient would be better, but would need to upgrade the api, which might be a bit much for this story!?
+            _httpClient = employerAccountsApiHttpClientFactory.CreateHttpClient();
+        }
+
+        //        public async Task<Statistics> GetStatistics(CancellationToken cancellationToken = default)
+        public async Task GetStatistics(CancellationToken cancellationToken = default)
+        {
+            _log.Info($"Getting statistics");
+
+            var response = await _httpClient.GetAsync("/api/statistics", cancellationToken);
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            throw new NotImplementedException();
+            //if (!response.IsSuccessStatusCode)
+            //    throw new RestHttpClientException(response, content);
+
+            //return JsonConvert.DeserializeObject<Statistics>(content);
+        }
     }
 }
