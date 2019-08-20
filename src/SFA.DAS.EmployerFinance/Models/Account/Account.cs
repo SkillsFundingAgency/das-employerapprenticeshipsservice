a4ef67a25220dc4c3c2537e8b9dbe29767b00264
Michael Young <mick.young1981@googlemail.com>
2019-08-20 09:06:07 +0100

    Changed EmployerAccountRepository to use finance db context.

--- src/SFA.DAS.EmployerFinance/Models/Account/Account.cs
+++ src/SFA.DAS.EmployerFinance/Models/Account/Account.cs
@@ -5,9 +5,6 @@ namespace SFA.DAS.EmployerFinance.Models.Account
     public class Account
     {
         public virtual long Id { get; set; }
-        public virtual string HashedId { get; set; }
-        public virtual string PublicHashedId { get; set; }
         public virtual string Name { get; set; }
-        public virtual ICollection<AccountLegalEntity> AccountLegalEntities { get; set; } = new List<AccountLegalEntity>();
     }
 }
