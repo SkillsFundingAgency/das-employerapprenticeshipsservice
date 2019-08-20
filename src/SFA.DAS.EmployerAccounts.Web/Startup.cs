ecffe32d867bc4d62af0a2cda088d8c49a01ec83
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-08-20 09:16:44 +0100

    Swap out Employer Sign in for Staff IDAMS

--- src/SFA.DAS.EmployerAccounts.Web/Startup.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Startup.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.Azure;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.IdentityModel.Protocols;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.WsFederation;
+using Newtonsoft.Json;
 using NLog;
 using Owin;
 using SFA.DAS.Authentication;
 using SFA.DAS.EmployerAccounts.Configuration;
+using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Models.Account;
 using SFA.DAS.EmployerAccounts.Web;
 using SFA.DAS.EmployerAccounts.Web.Authentication;
+using SFA.DAS.EmployerAccounts.Web.FeatureToggles;
+using SFA.DAS.EmployerAccounts.Web.Models;
 using SFA.DAS.EmployerAccounts.Web.ViewModels;
 using SFA.DAS.EmployerUsers.WebClientComponents;
 using SFA.DAS.OidcMiddleware;
@@ -40,6 +56,8 @@ namespace SFA.DAS.EmployerAccounts.Web
             var constants = new Constants(config.Identity);
             var urlHelper = new UrlHelper();
 
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = "Cookies",
@@ -53,32 +71,139 @@ namespace SFA.DAS.EmployerAccounts.Web
                 AuthenticationMode = AuthenticationMode.Passive
             });
 
-            app.UseCodeFlowAuthentication(new OidcMiddlewareOptions
-            {
-                BaseUrl = config.Identity.BaseAddress,
-                ClientId = config.Identity.ClientId,
-                ClientSecret = config.Identity.ClientSecret,
-                Scopes = config.Identity.Scopes,
-                AuthorizeEndpoint = constants.AuthorizeEndpoint(),
-                TokenEndpoint = constants.TokenEndpoint(),
-                UserInfoEndpoint = constants.UserInfoEndpoint(),
-                TokenSigningCertificateLoader = GetSigningCertificate(config.Identity.UseCertificate),
-                TokenValidationMethod = config.Identity.UseCertificate ? TokenValidationMethod.SigningKey : TokenValidationMethod.BinarySecret,
-                AuthenticatedCallback = identity =>
-                {
-                    PostAuthentiationAction(
-                        identity,                     
-                        constants,
-                        accountDataCookieStorageService,
-                        hashedAccountIdCookieStorageService);
-                }
-            });
+            // https://skillsfundingagency.atlassian.net/wiki/spaces/ERF/pages/104010807/Staff+IDAMS
+            app.UseWsFederationAuthentication(GetADFSOptions());
+
+            //app.UseCodeFlowAuthentication(new OidcMiddlewareOptions
+            //{
+            //    BaseUrl = config.Identity.BaseAddress,
+            //    ClientId = config.Identity.ClientId,
+            //    ClientSecret = config.Identity.ClientSecret,
+            //    Scopes = config.Identity.Scopes,
+            //    AuthorizeEndpoint = constants.AuthorizeEndpoint(),
+            //    TokenEndpoint = constants.TokenEndpoint(),
+            //    UserInfoEndpoint = constants.UserInfoEndpoint(),
+            //    TokenSigningCertificateLoader = GetSigningCertificate(config.Identity.UseCertificate),
+            //    TokenValidationMethod = config.Identity.UseCertificate ? TokenValidationMethod.SigningKey : TokenValidationMethod.BinarySecret,
+            //    AuthenticatedCallback = identity =>
+            //    {
+            //        PostAuthentiationAction(
+            //            identity,
+            //            constants,
+            //            accountDataCookieStorageService,
+            //            hashedAccountIdCookieStorageService);
+            //    }
+            //});
 
             ConfigurationFactory.Current = new IdentityServerConfigurationFactory(config);
             JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
 
             UserLinksViewModel.ChangePasswordLink = $"{constants.ChangePasswordLink()}{urlHelper.Encode(config.EmployerAccountsBaseUrl + "/service/password/change")}";
             UserLinksViewModel.ChangeEmailLink = $"{constants.ChangeEmailLink()}{urlHelper.Encode(config.EmployerAccountsBaseUrl + "/service/email/change")}";
+        }
+
+        private WsFederationAuthenticationOptions GetADFSOptions()
+        {
+            return new WsFederationAuthenticationOptions
+            {
+                Wtrealm = "https://localhost:44347",
+                MetadataAddress = "https://adfs.preprod.skillsfunding.service.gov.uk/FederationMetadata/2007-06/FederationMetadata.xml",
+                Notifications = Notifications()
+            };
+        }
+
+        private WsFederationAuthenticationNotifications Notifications()
+        {
+            return new WsFederationAuthenticationNotifications
+            {
+                SecurityTokenValidated = OnSecurityTokenValidated,
+                SecurityTokenReceived = nx => OnSecurityTokenReceived(),
+                AuthenticationFailed = nx => OnAuthenticationFailed(nx),
+                MessageReceived = nx => OnMessageReceived(),
+                RedirectToIdentityProvider = nx => OnRedirectToIdentityProvider()
+            };
+        }
+
+        private Task OnRedirectToIdentityProvider()
+        {
+            Logger.Debug("RedirectToIdentityProvider");
+            return Task.FromResult(0);
+        }
+
+        private Task OnMessageReceived()
+        {
+            Logger.Debug("MessageReceived");
+            return Task.FromResult(0);
+        }
+
+        private Task OnSecurityTokenReceived()
+        {
+            Logger.Debug("SecurityTokenReceived");
+            return Task.FromResult(0);
+        }
+
+        private Task OnSecurityTokenValidated(SecurityTokenValidatedNotification<WsFederationMessage, WsFederationAuthenticationOptions> notification)
+        {
+            Logger.Debug("SecurityTokenValidated");
+
+            try
+            {
+                Logger.Debug("Authentication Properties", new Dictionary<string, object>
+                {
+                    {
+                        "claims",
+                        JsonConvert.SerializeObject(
+                            notification.AuthenticationTicket.Identity.Claims.Select(x =>new {x.Value, x.ValueType, x.Type}))
+                    },
+                    {
+                        "authentication-type",
+                        notification.AuthenticationTicket.Identity.AuthenticationType
+                    },
+                    {"role-type", notification.AuthenticationTicket.Identity.RoleClaimType}
+                });
+
+                const string serviceClaimType = "http://service/service";
+
+                if (notification.AuthenticationTicket.Identity.HasClaim(serviceClaimType, "ESF"))
+                {
+                    Logger.Debug("Adding Tier2 Role");
+                    notification.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Role, "Tier2User"));
+
+                    Logger.Debug("Adding ConsoleUser Role");
+                    notification.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Role, "ConsoleUser"));
+                }
+                else if (notification.AuthenticationTicket.Identity.HasClaim(serviceClaimType, "ESS"))
+                {
+                    Logger.Debug("Adding ConsoleUser Role");
+                    notification.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Role, "ConsoleUser"));
+                }
+                else
+                {
+                    throw new SecurityTokenValidationException();
+                }
+
+                var firstName = notification.AuthenticationTicket.Identity.Claims.SingleOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value;
+                var lastName = notification.AuthenticationTicket.Identity.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Surname)?.Value;
+                var userEmail = notification.AuthenticationTicket.Identity.Claims.Single(x => x.Type == ClaimTypes.Upn).Value;
+
+                notification.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Email, userEmail));
+                notification.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Name, $"{firstName} {lastName}"));
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "IDAMS Authentication Callback Error");
+            }
+
+            Logger.Debug("End of callback");
+
+            return Task.FromResult(0);
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<WsFederationMessage, WsFederationAuthenticationOptions> nx)
+        {
+            var logReport = $"AuthenticationFailed, State: {nx.State}, Exception: {nx.Exception.GetBaseException().Message}, Protocol Message: Wct {nx.ProtocolMessage.Wct},\r\nWfresh {nx.ProtocolMessage.Wfresh},\r\nWhr {nx.ProtocolMessage.Whr},\r\nWp {nx.ProtocolMessage.Wp},\r\nWpseudo{nx.ProtocolMessage.Wpseudo},\r\nWpseudoptr {nx.ProtocolMessage.Wpseudoptr},\r\nWreq {nx.ProtocolMessage.Wreq},\r\nWfed {nx.ProtocolMessage.Wfed},\r\nWreqptr {nx.ProtocolMessage.Wreqptr},\r\nWres {nx.ProtocolMessage.Wres},\r\nWreply{nx.ProtocolMessage.Wreply},\r\nWencoding {nx.ProtocolMessage.Wencoding},\r\nWtrealm {nx.ProtocolMessage.Wtrealm},\r\nWresultptr {nx.ProtocolMessage.Wresultptr},\r\nWauth {nx.ProtocolMessage.Wauth},\r\nWattrptr{nx.ProtocolMessage.Wattrptr},\r\nWattr {nx.ProtocolMessage.Wattr},\r\nWa {nx.ProtocolMessage.Wa},\r\nIsSignOutMessage {nx.ProtocolMessage.IsSignOutMessage},\r\nIsSignInMessage {nx.ProtocolMessage.IsSignInMessage},\r\nWctx {nx.ProtocolMessage.Wctx},\r\n";
+            Logger.Debug(logReport);
+            return Task.FromResult(0);
         }
 
         private static Func<X509Certificate2> GetSigningCertificate(bool useCertificate)
