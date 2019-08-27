601fa28abff7e6b683321fb18e196d95777deb98
Neil Winstanley <Winsto@users.noreply.github.com>
2019-08-27 15:21:16 +0100

    AML-3644: remove, now, redundant dependency.

--- src/SFA.DAS.EAS.Api/Controllers/LegalEntitiesController.cs
+++ src/SFA.DAS.EAS.Api/Controllers/LegalEntitiesController.cs
@@ -1,22 +1,18 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Http;
-using MediatR;
 using SFA.DAS.EAS.Account.Api.Attributes;
 using SFA.DAS.EAS.Application.Queries.GetLegalEntity;
 using SFA.DAS.EAS.Domain.Configuration;
-using SFA.DAS.Validation.WebApi;
 
 namespace SFA.DAS.EAS.Account.Api.Controllers
 {
     [RoutePrefix("api/accounts/{hashedAccountId}/legalentities")]
     public class LegalEntitiesController : ApiController
     {
-        private readonly IMediator _mediator;
         private readonly EmployerApprenticeshipsServiceConfiguration _configuration;
 
-        public LegalEntitiesController(IMediator mediator, EmployerApprenticeshipsServiceConfiguration configuration)
+        public LegalEntitiesController(EmployerApprenticeshipsServiceConfiguration configuration)
         {
-            _mediator = mediator;
             _configuration = configuration;
         }
 
