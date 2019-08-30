843555ba5bbb5feb582709d5c2050965f647e883
Neil Winstanley <Winsto@users.noreply.github.com>
2019-08-30 10:37:58 +0100

    AML-3855: Make a start on tryin to spin up api and call in test context. Note: nuget refs may need checking; we tried to just install everything the api referenced to get past assembly load errors.

---
+++ src/SFA.DAS.EmployerAccounts.Api.IntegrationTests/HealthCheckTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace SFA.DAS.EmployerAccounts.Api.IntegrationTests
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class HealthCheckTest
+    {
+        private HttpServer _server;
+
+        [SetUp]
+        public void Startup()
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            _server = new HttpServer(config);
+        }
+
+        [Test]
+        public async Task HealthCheckReturnsOK()
+        {
+            var client = new HttpClient(_server);
+
+            var response = await client.GetAsync(@"https://localhost:44330/api/healthcheck");
+
+            Assert
+                .IsNotNull(response);
+
+            response
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if (_server != null)
+                _server.Dispose();
+        }
+    }
+}
