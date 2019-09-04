49cb6062d80adcc17ab59863a242ff2998f93ffa
Neil Winstanley <Winsto@users.noreply.github.com>
2019-09-04 18:51:40 +0100

    AML-3855: re-arrange POC so it forms the basis for more tests goin forward.

--- src/SFA.DAS.EmployerAccounts.Api.IntegrationTests/GivenEmployerAccountsApi/GivenEmployerAccountsApi.cs
+++ src/SFA.DAS.EmployerAccounts.Api.IntegrationTests/GivenEmployerAccountsApi/GivenEmployerAccountsApi.cs
@@ -1,23 +1,19 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Net;
-using System.Web;
 using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web.Http;
-using NUnit.Framework;
-using FluentAssertions;
 using NServiceBus;
+using NUnit.Framework;
 using StructureMap;
 
-namespace SFA.DAS.EmployerAccounts.Api.IntegrationTests
+namespace SFA.DAS.EmployerAccounts.Api.IntegrationTests.GivenEmployerAccountsApi
 {
     [ExcludeFromCodeCoverage]
-    [TestFixture]
-    public class HealthCheckTest
+    public abstract class GivenEmployerAccountsApi
     {
         private HttpServer _server;
         private IEndpointInstance _endpoint;
         private IEndpointInstance _nServiceBusEndpoint;
+        protected HttpResponseMessage Response;
 
         [SetUp]
         public void Startup()
@@ -33,33 +29,27 @@ namespace SFA.DAS.EmployerAccounts.Api.IntegrationTests
             _nServiceBusEndpoint = WebApiApplication.StartServiceBusEndpoint(container);
 
             _server = new HttpServer(config);
+
+            CallControllerAction();
         }
 
-        [Test]
-        public async Task HealthCheckReturnsOK()
+        private void CallControllerAction()
         {
-            var client = new HttpClient(_server);
-
-            var response = await client.GetAsync(@"https://localhost:44330/api/healthcheck");
-
-            client.Dispose();
-
-            Assert
-                .IsNotNull(response);
-
-            response
-                .StatusCode
-                .Should()
-                .Be(HttpStatusCode.OK);
+            using (var client = new HttpClient(_server))
+            {
+                Response = client.GetAsync(GetRequestUri()).Result;
+            }
         }
+
+        protected abstract string GetRequestUri();
 
         [TearDown]
         public void Cleanup()
         {
+            WebApiApplication.StopServiceBusEndpoint(_nServiceBusEndpoint);
+
             if (_server != null)
                 _server.Dispose();
-
-            WebApiApplication.StopServiceBusEndpoint(_nServiceBusEndpoint);
         }
     }
 }
