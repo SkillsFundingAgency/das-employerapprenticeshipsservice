c24053bf596f86426809bc28411c056912299e48
tom-gough <tom@tgsd.co.uk>
2019-09-11 14:14:37 +0100

    Fixed AccountSignedAgreementCollectionName

--- src/SFA.DAS.EmployerAccounts.ReadStore/SFA.DAS.EmployerAccounts.ReadStore/Data/AccountSignedAgreementRepository.cs
+++ src/SFA.DAS.EmployerAccounts.ReadStore/SFA.DAS.EmployerAccounts.ReadStore/Data/AccountSignedAgreementRepository.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.EmployerAccounts.ReadStore.Data
     public class AccountSignedAgreementRepository : DocumentRepository<AccountSignedAgreement>, IAccountSignedAgreementsRepository
     {
         public AccountSignedAgreementRepository(IDocumentClient documentClient)
-            : base(documentClient, DocumentSettings.DatabaseName, DocumentSettings.AccountUsersCollectionName)
+            : base(documentClient, DocumentSettings.DatabaseName, DocumentSettings.AccountSignedAgreementsCollectionName)
         {
         }
     }
