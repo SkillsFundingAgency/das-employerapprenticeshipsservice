ea63a0d5f2e5892d81d25ca352934e3d46133101
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-09-11 14:19:07 +0100

    Remove now unused code.

--- src/SFA.DAS.EmployerAccounts.Web/Extensions/AccountDashboardViewModelExtensions.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Extensions/AccountDashboardViewModelExtensions.cs
@@ -6,16 +6,6 @@ namespace SFA.DAS.EmployerAccounts.Web.Extensions
 {
     public static class AccountDashboardViewModelExtensions
     {
-        public static bool ShowManageYourLevyLink(this AccountDashboardViewModel model)
-        {
-            if (model.AgreementInfo.Type != AccountAgreementType.NonLevyExpressionOfInterest)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
         public static bool ShowYourFundingReservationsLink(this AccountDashboardViewModel model)
         {
             if(model.ApprenticeshipEmployerType == ApprenticeshipEmployerType.NonLevy)
