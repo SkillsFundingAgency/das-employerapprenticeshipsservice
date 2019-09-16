82177fc2343c8837e4b947ef5bd5309912f4cb90
tom-gough <tom@tgsd.co.uk>
2019-09-16 13:33:07 +0100

    Seed data job for signed agreements

---
+++ src/SFA.DAS.EmployerAccounts.Jobs.UnitTests/OneOffJobs/SeedAccountSignedAgreementsJobTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.CosmosDb.Testing;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.EmployerAccounts.Jobs.RunOnceJobs;
+using SFA.DAS.EmployerAccounts.Models.Account;
+using SFA.DAS.EmployerAccounts.ReadStore.Data;
+using SFA.DAS.EmployerAccounts.ReadStore.Models;
+using SFA.DAS.Testing;
+using SFA.DAS.Testing.Builders;
+using SFA.DAS.Testing.EntityFramework;
+
+namespace SFA.DAS.EmployerAccounts.Jobs.UnitTests.OneOffJobs
+{
+    [TestFixture]
+    [Parallelizable]
+    public class SeedAccountSignedAgreementsJobTests : FluentTest<SeedAccountSignedAgreementsJobTestsFixture>
+    {
+        [Test]
+        public Task Run_WhenRunningJob_ThenShouldOnlyAddAgreementsWhichHaveBeenSigned()
+        {
+            return TestAsync(f => f.Run(),
+                f => f.VerifyAddDocumentWasRun(1).VerifyUserWasMappedCorrectly(f.SignedAgreement));
+        }
+
+        [Test]
+        public Task Run_WhenRunningJob_ThenShouldNotAddAgreementsWhichHaveBeenSignedAndAlreadyAreInTheReadstore()
+        {
+            return TestAsync(f => f.AddExistingSignedAgreementToReadstore(),f => f.Run(),
+                f => f.VerifyAddDocumentWasRun(0));
+        }
+
+        [Test]
+        public Task Run_WhenRunningJob_ThenShouldPassInTheCorrectJobName()
+        {
+            return TestAsync(f => f.Run(),
+                f => f.RunOnceService.Verify(x => x.RunOnce("SeedAccountUsersJob", It.IsAny<Func<Task>>())));
+        }
+    }
+
+    public class SeedAccountSignedAgreementsJobTestsFixture
+    {
+        internal Mock<IRunOnceJobsService> RunOnceService { get; set; }
+        internal Mock<IAccountSignedAgreementsRepository> AccountSignedAgreementsRepository { get; set; }
+        internal Mock<EmployerAccountsDbContext> EmployerAccountsDbContext { get; set; }
+        public Mock<ILogger> Logger { get; set; }
+
+        public ICollection<EmployerAgreement> Agreements = new List<EmployerAgreement>();
+
+        public ICollection<AccountSignedAgreement> ReadStoreAgreements = new List<AccountSignedAgreement>();
+
+        public EmployerAgreement SignedAgreement = new EmployerAgreement{ AccountLegalEntity = new AccountLegalEntity{ AccountId = 112 }, Template = new AgreementTemplate{ AgreementType = AgreementType.Levy, VersionNumber = 3 }, SignedDate = DateTime.Today.AddDays(-1) };
+        public EmployerAgreement UnsignedAgreement = new EmployerAgreement { AccountLegalEntity = new AccountLegalEntity { AccountId = 114 }, Template = new AgreementTemplate { AgreementType = AgreementType.Levy, VersionNumber = 3 } };
+
+        internal SeedAccountSignedAgreementsJob SeedAccountSignedAgreementsJob { get; set; }
+
+        public List<EmployerAgreement> AgreementsToMigrate;
+
+        private readonly string _jobName = typeof(SeedAccountSignedAgreementsJob).Name;
+
+        private readonly DbSetStub<EmployerAgreement> _agreementsDbSet;
+
+        public SeedAccountSignedAgreementsJobTestsFixture()
+        {
+            RunOnceService = new Mock<IRunOnceJobsService>();
+            RunOnceService.Setup(x => x.RunOnce(It.IsAny<string>(), It.IsAny<Func<Task>>()))
+                .Returns((string jobName, Func<Task> function) => function());
+
+            AccountSignedAgreementsRepository = new Mock<IAccountSignedAgreementsRepository>();
+            AccountSignedAgreementsRepository.SetupInMemoryCollection(ReadStoreAgreements);
+
+            AgreementsToMigrate = new List<EmployerAgreement> { SignedAgreement, UnsignedAgreement };
+            _agreementsDbSet = new DbSetStub<EmployerAgreement>(AgreementsToMigrate);
+
+            EmployerAccountsDbContext = new Mock<EmployerAccountsDbContext>();
+            EmployerAccountsDbContext.Setup(x => x.Agreements).Returns(_agreementsDbSet);
+
+            Logger = new Mock<ILogger>();
+
+            SeedAccountSignedAgreementsJob = new SeedAccountSignedAgreementsJob(RunOnceService.Object, AccountSignedAgreementsRepository.Object, new Lazy<EmployerAccountsDbContext>(() => EmployerAccountsDbContext.Object), Logger.Object);
+        }
+
+        public Task Run()
+        {
+            return SeedAccountSignedAgreementsJob.Run();
+        }
+
+        public SeedAccountSignedAgreementsJobTestsFixture AddExistingSignedAgreementToReadstore()
+        {
+
+            ReadStoreAgreements.Add(ObjectActivator.CreateInstance<AccountSignedAgreement>()
+                .Set(x => x.AccountId, SignedAgreement.AccountLegalEntity.AccountId)
+                .Set(x => x.AgreementVersion, SignedAgreement.Template.VersionNumber)
+                .Set(x => x.AgreementType, SignedAgreement.Template.AgreementType.ToString()));
+
+            return this;
+        }
+
+        public SeedAccountSignedAgreementsJobTestsFixture VerifyAddDocumentWasRun(short times)
+        {
+            AccountSignedAgreementsRepository.Verify(x => x.Add(It.IsAny<AccountSignedAgreement>(), null, CancellationToken.None), Times.Exactly(times));
+
+            return this;
+        }
+
+        public SeedAccountSignedAgreementsJobTestsFixture VerifyUserWasMappedCorrectly(EmployerAgreement agreement)
+        {
+            AccountSignedAgreementsRepository.Verify(x => x.Add(It.Is<AccountSignedAgreement>(p => p.AccountId == agreement.AccountLegalEntity.AccountId &&
+                                                                             p.AgreementType == agreement.Template.AgreementType.ToString() &&
+                                                                             p.AgreementVersion == agreement.Template.VersionNumber),
+                null,
+                CancellationToken.None), Times.Once);
+
+            return this;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts.Jobs/RunOnceJobs/SeedAccountSignedAgreementsJob.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.EmployerAccounts.ReadStore.Data;
+using SFA.DAS.EmployerAccounts.ReadStore.Models;
+
+namespace SFA.DAS.EmployerAccounts.Jobs.RunOnceJobs
+{
+    public class SeedAccountSignedAgreementsJob
+    {
+        private readonly IRunOnceJobsService _runOnceJobsService;
+        private readonly IAccountSignedAgreementsRepository _accountSignedAgreementsRepository;
+        private readonly Lazy<EmployerAccountsDbContext> _db;
+        private readonly ILogger _logger;
+        private readonly string _jobName;
+
+        public SeedAccountSignedAgreementsJob(IRunOnceJobsService runOnceJobsService, IAccountSignedAgreementsRepository accountSignedAgreementsRepository, Lazy<EmployerAccountsDbContext> db, ILogger logger)
+        {
+            _runOnceJobsService = runOnceJobsService;
+            _accountSignedAgreementsRepository = accountSignedAgreementsRepository;
+            _db = db;
+            _logger = logger;
+            _jobName = typeof(SeedAccountUsersJob).Name;
+        }
+
+        [NoAutomaticTrigger]
+        public Task Run()
+        {
+            return _runOnceJobsService.RunOnce(_jobName, MigrateUsers);
+        }
+
+        public async Task MigrateUsers()
+        {
+            var agreements = _db.Value.Agreements
+                .Include(x => x.AccountLegalEntity)
+                .Include(x => x.Template)
+                .Where(x => x.SignedDate != null)
+                .ToList();
+
+            _logger.LogInformation("Migrating signed agreements into the read store");
+
+            foreach (var agreement in agreements)
+            {
+                var agreementExists = await CosmosDb.QueryableExtensions.AnyAsync(
+                    _accountSignedAgreementsRepository.CreateQuery(),
+                    x => x.AccountId == agreement.AccountLegalEntity.AccountId && x.AgreementType == agreement.Template.AgreementType.ToString() && x.AgreementVersion == agreement.Template.VersionNumber);
+
+                if (!agreementExists)
+                {
+                    var document = new AccountSignedAgreement(agreement.AccountLegalEntity.AccountId, agreement.Template.VersionNumber, agreement.Template.AgreementType.ToString(), Guid.NewGuid());
+                    await _accountSignedAgreementsRepository.Add(document, null, CancellationToken.None);
+                }
+            }
+
+            _logger.LogInformation("Finished migrating signed agreements into the read store");
+        }
+    }
+}
