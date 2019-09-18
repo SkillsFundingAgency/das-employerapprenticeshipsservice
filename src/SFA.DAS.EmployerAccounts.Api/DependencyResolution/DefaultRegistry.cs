63e94d1df344c97e8c5c4eeb9084a4d2e9628ed4
Neil Winstanley <Winsto@users.noreply.github.com>
2019-09-18 13:49:15 +0100

    AML-3656: resolve missing 'ILoggingContext' issue

--- src/SFA.DAS.EmployerAccounts.Api/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.EmployerAccounts.Api/DependencyResolution/DefaultRegistry.cs
@@ -1,5 +1,8 @@
+using System.Web;
 using SFA.DAS.Authorization;
 using SFA.DAS.Authorization.WebApi;
+using SFA.DAS.EmployerAccounts.Api.Logging;
+using SFA.DAS.NLog.Logger;
 using StructureMap;
 
 namespace SFA.DAS.EmployerAccounts.Api.DependencyResolution
@@ -13,6 +16,8 @@ namespace SFA.DAS.EmployerAccounts.Api.DependencyResolution
                 s.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
                 s.RegisterConcreteTypesAgainstTheFirstInterface();
             });
+
+            For<ILoggingContext>().Use(c => HttpContext.Current == null ? null : new LoggingContext(new HttpContextWrapper(HttpContext.Current)));
         }
     }
 }
