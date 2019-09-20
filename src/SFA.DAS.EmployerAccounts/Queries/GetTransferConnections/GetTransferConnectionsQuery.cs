6a9e76c3e68d884566ba63307b3ee3df8dadfd0d
Neil Winstanley <Winsto@users.noreply.github.com>
2019-09-20 10:55:44 +0100

    AML-3656: remove decoded query parameter from TransferConnections

--- src/SFA.DAS.EmployerAccounts/Queries/GetTransferConnections/GetTransferConnectionsQuery.cs
+++ src/SFA.DAS.EmployerAccounts/Queries/GetTransferConnections/GetTransferConnectionsQuery.cs
@@ -1,14 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-using AutoMapper;
 using MediatR;
-using SFA.DAS.Authorization.ModelBinding;
 
 namespace SFA.DAS.EmployerAccounts.Queries.GetTransferConnections
 {
-    public class GetTransferConnectionsQuery : IAuthorizationContextModel, IAsyncRequest<GetTransferConnectionsResponse>
+    public class GetTransferConnectionsQuery : IAsyncRequest<GetTransferConnectionsResponse>
     {
-        [IgnoreMap]
-        [Required]
-        public long AccountId { get; set; }
+        public string HashedAccountId { get; set; }
     }
 }
