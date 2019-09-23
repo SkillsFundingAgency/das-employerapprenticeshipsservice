45c5fbecb9c624b03dafd6e59db63e735e3d3fa6
Michael Young <mick.young1981@googlemail.com>
2019-09-23 10:01:30 +0100

    Added incorrectly removed method.

---
+++ src/SFA.DAS.EmployerAccounts.Web/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.EmployerAccounts.Web.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString CdnLink(this HtmlHelper html, string folderName, string fileName)
+        {
+            var cdnLocation = StructuremapMvc.StructureMapDependencyScope.Container.GetInstance<Configuration.EmployerAccountsConfiguration>().CdnBaseUrl;
+
+            var trimCharacters = new char[] { '/' };
+            return new MvcHtmlString($"{cdnLocation.Trim(trimCharacters)}/{folderName.Trim(trimCharacters)}/{fileName.Trim(trimCharacters)}");
+        }
+    }
+}
