e13636476f663963f4451c8bb894aa790549f34b
tom-gough <tom@tgsd.co.uk>
2019-10-07 15:09:51 +0100

    zendesk suggestions for new page

--- src/SFA.DAS.EmployerAccounts.Web/Helpers/HtmlHelperExtensions.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Helpers/HtmlHelperExtensions.cs
@@ -38,11 +38,11 @@ namespace SFA.DAS.EmployerAccounts.Web.Helpers
 
     public static class ZenDeskSuggestions
     {
-        public static string RegisterForAnApprenticeshipServiceAccount = "Register-for-an-apprenticeship-service-account";
-        public static string AddPAYESchemesToYourAccount = "Add-PAYE-schemes-to-your-account";
-        public static string ApprenticeshipFunding = "Apprenticeship-funding";
-        public static string EnteredTheWrongPAYESchemeDetails = "Entered-the-wrong-PAYE-scheme-details";
-        public static string UseYourGovernmentGatewayDetails = "Use-your-Government-Gateway-details";
-        public static string ReviewTheEmployerAgreement = "Review-the-employer-agreement";
+        public static string RegisterForAnApprenticeshipServiceAccount = "Register for an apprenticeship service account";
+        public static string AddPAYESchemesToYourAccount = "Add PAYE schemes to your account";
+        public static string ApprenticeshipFunding = "Apprenticeship funding";
+        public static string EnteredTheWrongPAYESchemeDetails = "Entered the wrong PAYE scheme details";
+        public static string UseYourGovernmentGatewayDetails = "Use your Government Gateway details";
+        public static string ReviewTheEmployerAgreement = "Review the employer agreement";
     }
 }
