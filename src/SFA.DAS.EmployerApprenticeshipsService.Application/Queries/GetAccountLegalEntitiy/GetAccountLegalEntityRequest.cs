98205af7d3181b51557bc97cc80efe548d3b2cd5
Michael Young <mick.young1981@googlemail.com>
2019-10-14 16:01:51 +0100

    DELETE DELETE DELETE

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Queries/GetAccountLegalEntitiy/GetAccountLegalEntityRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EAS.Application.Queries.GetAccountLegalEntitiy
-{
-    public class GetAccountLegalEntityRequest : IAsyncRequest<GetAccountLegalEntityResponse>
-    {
-        public long AccountLegalEntityId { get; set; }
-    }
-}
