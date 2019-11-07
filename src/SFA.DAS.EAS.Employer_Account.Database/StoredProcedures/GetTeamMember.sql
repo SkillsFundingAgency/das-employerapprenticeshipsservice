066dc06e3973498c14d5b5ea115a727f87fae100
Michael Young <mick.young1981@googlemail.com>
2019-11-07 10:46:19 +0000

    QUAL-61_RemovedUnnecessaryJoin

--- src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetTeamMember.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/StoredProcedures/GetTeamMember.sql
@@ -3,5 +3,4 @@
 	@externalUserId NVARCHAR(50)
 AS
 	SELECT * FROM [employer_account].[MembershipView] m 
-	INNER JOIN [employer_account].Account a ON a.Id=m.AccountId 
-	WHERE a.HashedId = @hashedAccountId AND UserRef = @externalUserId
+	WHERE m.HashedAccountId = @hashedAccountId AND UserRef = @externalUserId
