af9a385f45931636d98da25184df978de53310a6
Michael Young <mick.young1981@googlemail.com>
2019-11-12 10:33:02 +0000

    Added query to return only pending agreements.

---
+++ src/SFA.DAS.EmployerAccounts/Queries/GetUnsignedEmployerAgreement/GetUnsignedEmployerAgreementQueryHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.HashingService;
+using SFA.DAS.Validation;
+
+namespace SFA.DAS.EmployerAccounts.Queries.GetUnsignedEmployerAgreement
+{
+    public class GetUnsignedEmployerAgreementQueryHandler : IAsyncRequestHandler<GetUnsignedEmployerAgreementRequest, GetUnsignedEmployerAgreementResponse>
+    {
+        private readonly Lazy<EmployerAccountsDbContext> _db;
+        private readonly IHashingService _hashingService;
+        private readonly IValidator<GetUnsignedEmployerAgreementRequest> _validator;
+
+        public GetUnsignedEmployerAgreementQueryHandler(
+            Lazy<EmployerAccountsDbContext> db,
+            IHashingService hashingService,
+            IValidator<GetUnsignedEmployerAgreementRequest> validator
+        )
+        {
+            _db = db;
+            _hashingService = hashingService;
+            _validator = validator;
+        }
+
+        public async Task<GetUnsignedEmployerAgreementResponse> Handle(GetUnsignedEmployerAgreementRequest message)
+        {
+            var validationResult = await _validator.ValidateAsync(message);
+
+            if (!validationResult.IsValid())
+            {
+                throw new InvalidRequestException(validationResult.ValidationDictionary);
+            }
+
+            if (validationResult.IsUnauthorized)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            var accountId = _hashingService.DecodeValue(message.HashedAccountId);
+
+            var accountLegalEntities = _db.Value.AccountLegalEntities.ToList();
+
+            var pendingAgreementId = await _db.Value.AccountLegalEntities
+                .Where(x => x.AccountId == accountId && x.PendingAgreementId != null)
+                .Select(x => x.PendingAgreementId).SingleOrDefaultAsync();
+
+            return new GetUnsignedEmployerAgreementResponse
+            {
+                HashedAgreementId = pendingAgreementId.HasValue ? _hashingService.HashValue(pendingAgreementId.Value) : null
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerAccounts/Queries/GetUnsignedEmployerAgreement/GetUnsignedEmployerAgreementValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.Validation;
+
+namespace SFA.DAS.EmployerAccounts.Queries.GetUnsignedEmployerAgreement
+{
+    public class GetUnsignedEmployerAgreementValidator : IValidator<GetUnsignedEmployerAgreementRequest> 
+    {
+        private readonly IMembershipRepository _membershipRepository;
+
+        public GetUnsignedEmployerAgreementValidator(IMembershipRepository membershipRepository)
+        {
+            _membershipRepository = membershipRepository;
+        }
+
+        public ValidationResult Validate(GetUnsignedEmployerAgreementRequest item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ValidationResult> ValidateAsync(GetUnsignedEmployerAgreementRequest item)
+        {
+            var validationResult = new ValidationResult();
+
+            if (string.IsNullOrEmpty(item.ExternalUserId))
+            {
+                validationResult.AddError(nameof(item.ExternalUserId),"ExternalUserId has not been supplied");
+            }
+            if (string.IsNullOrEmpty(item.HashedAccountId))
+            {
+                validationResult.AddError(nameof(item.HashedAccountId), "HashedAccountId has not been supplied");
+            }
+
+            if (!validationResult.IsValid())
+            {
+                return validationResult;
+            }
+
+            var membership = await _membershipRepository.GetCaller(item.HashedAccountId, item.ExternalUserId);
+
+            if (membership == null)
+            {
+                validationResult.IsUnauthorized = true;
+            }
+                
+            return validationResult;
+        }
+    }
+}
