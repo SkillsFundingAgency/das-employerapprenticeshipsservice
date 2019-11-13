2c040b65f248967da72875d672cf1a83009c2662
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-11-13 14:24:57 +0000

    CON-896_add support button

---
+++ src/SFA.DAS.EAS.Support.Infrastructure/Settings/EmployerAccountsConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.Messaging.AzureServiceBus.StructureMap;
+
+namespace SFA.DAS.EAS.Support.Infrastructure.Settings
+{
+    public class EmployerAccountsConfiguration : ITopicMessagePublisherConfiguration
+    {
+        public string EmployerAccountsBaseUrl { get; set; }
+        public string LegacyServiceBusConnectionString { get; set; }
+        public string MessageServiceBusConnectionString => LegacyServiceBusConnectionString;
+
+    }
+}
