0d7fcfb6e96615fe7dcbb0a3f4b62377f69aa215
Michael Young <mick.young1981@googlemail.com>
2019-11-13 14:58:49 +0000

    Improved performance of GetAccountEmployerAgreements and refactored so it's slightly more readable.

--- src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerAgreementOrchestrator.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerAgreementOrchestrator.cs
@@ -29,9 +29,7 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
 {
     public class EmployerAgreementOrchestrator : UserVerificationOrchestratorBase
     {
-        private readonly ILog _logger;
         private readonly IMapper _mapper;
-        private readonly EmployerApprenticeshipsServiceConfiguration _configuration;
         private readonly IMediator _mediator;
         private readonly IReferenceDataService _referenceDataService;
 
@@ -41,22 +39,11 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
 
         public EmployerAgreementOrchestrator(
             IMediator mediator,
-            ILog logger,
             IMapper mapper,
-            EmployerApprenticeshipsServiceConfiguration configuration,
             IReferenceDataService referenceDataService) : base(mediator)
         {
-            if (mediator == null)
-                throw new ArgumentNullException(nameof(mediator));
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
-            if (referenceDataService == null)
-                throw new ArgumentNullException(nameof(referenceDataService));
-
             _mediator = mediator;
-            _logger = logger;
             _mapper = mapper;
-            _configuration = configuration;
             _referenceDataService = referenceDataService;
         }
 
@@ -309,14 +296,6 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
             return signedPdfEmployerAgreement;
 
         }
-
-        public virtual async Task<bool> UserShownWizard(string userId, string hashedAccountId)
-        {
-            var userResponse = await Mediator.SendAsync(new GetTeamMemberQuery { HashedAccountId = hashedAccountId, TeamMemberId = userId });
-            return userResponse.User.ShowWizard && userResponse.User.Role == Role.Owner;
-        }
-
-
 
         public virtual async Task<OrchestratorResponse<LegalAgreementsToRemoveViewModel>> GetLegalAgreementsToRemove(string hashedAccountId, string userId)
         {
