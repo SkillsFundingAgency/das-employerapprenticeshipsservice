6a58b3502995b12213ce3129b9067351e8d4ba9a
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2019-11-14 14:28:58 +0000

    temp working arrangement

--- src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerTeamOrchestrator.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Orchestrators/EmployerTeamOrchestrator.cs
@@ -160,7 +160,7 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
         {
             try
             {
-                var apiGetAccountTask = _accountApiClient.GetAccount(hashedAccountId);
+                ///var apiGetAccountTask = _accountApiClient.GetAccount(hashedAccountId);
 
                 var accountResponseTask = _mediator.SendAsync(new GetEmployerAccountByHashedIdQuery
                 {
@@ -188,14 +188,7 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
                     ExternalUserId = externalUserId
                 });
 
-                var reservationsResponseTask = _mediator.SendAsync(new GetReservationsRequest
-                {
-                    HashedAccountId = hashedAccountId,
-                    ExternalUserId = externalUserId
-                });
-                                
-                //await Task.WhenAll(apiGetAccountTask, accountStatsResponseTask, userRoleResponseTask, userResponseTask, accountStatsResponseTask, agreementsResponseTask, reservationsResponseTask).ConfigureAwait(false);
-                await Task.WhenAll(accountStatsResponseTask, userRoleResponseTask, userResponseTask, accountStatsResponseTask, agreementsResponseTask).ConfigureAwait(false);
+                await Task.WhenAll(accountStatsResponseTask, userRoleResponseTask, userResponseTask, accountStatsResponseTask, agreementsResponseTask).ConfigureAwait(false);                
 
                 var accountResponse = accountResponseTask.Result;
                 var userRoleResponse = userRoleResponseTask.Result;
@@ -216,8 +209,10 @@ namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
                 var pendingAgreements = agreementsResponse.EmployerAgreements.Where(a => a.HasPendingAgreement).Select(a => new PendingAgreementsViewModel { HashedAgreementId = a.Pending.HashedAgreementId }).ToList();
                 var tasks = tasksResponse?.Tasks.Where(t => t.ItemsDueCount > 0 && t.Type != "AgreementToSign").ToList() ?? new List<AccountTask>();
                 var showWizard = userResponse.User.ShowWizard && userRoleResponse.UserRole == Role.Owner;
-                //var accountDetailViewModel = apiGetAccountTask.Result;
-                var accountDetailViewModel = new AccountDetailViewModel { ApprenticeshipEmployerType = "1" }; // apiGetAccountTask.Result;
+                var accountDetailViewModel = new AccountDetailViewModel()
+                {
+                    ApprenticeshipEmployerType = "1"
+                };
 
                 var viewModel = new AccountDashboardViewModel
                 {
