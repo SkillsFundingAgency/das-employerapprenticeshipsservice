9b9ef8c3832b9641839e063bdffa7a6dd89d91e4
tom-gough <tom@tgsd.co.uk>
2019-11-15 10:43:39 +0000

    WIP pre merge

---
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/CookieConsentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SFA.DAS.Authentication;
+using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
+
+namespace SFA.DAS.EmployerAccounts.Web.Controllers
+{
+    [RoutePrefix("cookieConsent")]
+    public class CookieConsentController : BaseController
+    {
+        public CookieConsentController(
+            IAuthenticationService owinWrapper,
+            IMultiVariantTestingService multiVariantTestingService,
+            ICookieStorageService<FlashMessageViewModel> flashMessage
+        ) : base(owinWrapper, multiVariantTestingService, flashMessage)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("settings")]
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("settings")]
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
