c4302d9eefba21a110a8a67678c396a999b95ffb
vas <vasantha.kasirajan@digital.education.gov.uk>
2019-11-25 16:25:46 +0000

    Added accounts/{hashedaccountid}/teams/{email}/review/

--- src/SFA.DAS.EmployerAccounts.Web/Authorization/AuthorizationConstants.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Authorization/AuthorizationConstants.cs
@@ -6,6 +6,6 @@
         public const string TeamViewRoute = "accounts/{hashedaccountid}/teams/view";
         public const string TeamRoute = "accounts/{hashedaccountid}/teams";
         public const string TeamInvite = "accounts/{hashedaccountid}/teams/invite";
-        public const string TeamReview = "accounts/{hashedaccountid}/teams/{email}/review";
+        public const string TeamReview = "accounts/{hashedaccountid}/teams/{email}/review/";
     }
 }
