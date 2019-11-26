18c3965d7798eca06221b24a6ca1e9186d2cf823
Corey <corey.faulconbridge@digital.education.gov.uk>
2019-11-26 13:27:40 +0000

    Update remove route to correct value

--- src/SFA.DAS.EmployerAccounts.Web/Authorization/AuthorizationConstants.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Authorization/AuthorizationConstants.cs
@@ -7,7 +7,7 @@
         public const string TeamRoute = "accounts/{hashedaccountid}/teams";
         public const string TeamInvite = "accounts/{hashedaccountid}/teams/invite";
         public const string TeamInviteComplete = "accounts/{hashedaccountid}/teams/invite/next";
-        public const string TeamMemberRemove = "accounts/{hashedaccountid}/teams/remove";
+        public const string TeamMemberRemove = "accounts/{hashedaccountid}/teams/{email}/remove";
         public const string TeamReview = "accounts/{hashedaccountid}/teams/{email}/review";
         public const string TeamMemberRoleChange = "accounts/{hashedaccountid}/teams/{email}/role/change";
         public const string TeamMemberInviteResend = "accounts/{hashedaccountid}/teams/resend";
