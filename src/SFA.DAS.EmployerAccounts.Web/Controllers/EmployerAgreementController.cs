329955ae543851707f7a937f7f94ae2f610ceeb2
Paul Howes <paul.howes@cognetics.net>
2019-11-27 13:18:46 +0000

    AML-4038 Fix to change link text

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
@@ -10,6 +10,7 @@ using SFA.DAS.Authentication;
 using SFA.DAS.Authorization.Mvc.Attributes;
 using SFA.DAS.Common.Domain.Types;
 using SFA.DAS.EmployerAccounts.Interfaces;
+using SFA.DAS.EmployerAccounts.Queries.GetAccountLegalEntitiesByHashedAccountId;
 using SFA.DAS.EmployerAccounts.Web.Helpers;
 using SFA.DAS.EmployerAccounts.Web.Orchestrators;
 
@@ -127,6 +128,13 @@ namespace SFA.DAS.EmployerAccounts.Web.Controllers
 
             var response = await _mediator.SendAsync(request);
             var viewModel = _mapper.Map<GetEmployerAgreementResponse, EmployerAgreementViewModel>(response);
+
+            var legalentities = await _mediator.SendAsync(new GetAccountLegalEntitiesByHashedAccountIdRequest
+            {
+                HashedAccountId = request.HashedAccountId
+            });
+
+            viewModel.LegalEntitiesCount = legalentities.LegalEntities.Count;
 
             return View(viewModel);
         }
