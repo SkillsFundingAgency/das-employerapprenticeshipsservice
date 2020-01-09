c14f059a9a50bf49092b63a8ef3cc3f2c84ba13d
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-01-09 15:04:36 +0000

    Contextual Support Widget - Organisations & Agreements, Your Team, PAYE Schemes

---
+++ src/SFA.DAS.EmployerAccounts.Web.UnitTests/Extensions/HtmlHelperExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Web.Extensions;
+using System.Collections;
+
+namespace SFA.DAS.EmployerAccounts.Web.UnitTests.Extensions
+{
+    [TestFixture]
+    public class HtmlHelperExtensionsTests
+    {
+        private string expectedOutput;
+        private string actualOutput;        
+
+        [Test]
+        public void WhenICallSetZenDeskLabelsWithOutLabel_ThenTheOutputIsCorrect()
+        {
+            //Arrange            
+            expectedOutput =
+                $"<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', {{ labels: [] }});</script>";
+
+            //Act
+            actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null).ToString();
+
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestCaseSource(typeof(StringArrayTestDataSource))]
+        public void WhenICallSetZenDeskLabelsWithLabels_ThenTheOutputIsCorrect(string[] labels)
+        {
+            //Arrange
+            var expectedOutput = "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
+
+            var isFirstLabel = true;
+            foreach (var label in labels)
+            {
+                if (!string.IsNullOrEmpty(label))
+                {
+                    if (!isFirstLabel)
+                    {
+                        expectedOutput += ",";
+                    }
+                    isFirstLabel = false;
+
+                    expectedOutput += $"'{ EscapeApostrophes(label) }'";
+                }
+            }
+            expectedOutput += "] });</script>";
+
+            //Act
+            this.actualOutput = HtmlHelperExtensions.SetZenDeskLabels(null, labels).ToString();
+
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+
+    }
+
+    public class StringArrayTestDataSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new string[] { "a string with multiple words", "the title of another page" };
+            yield return new string[] { "ass-dashboard" };
+            yield return new string[] { null };
+        }
+    }
+}
--- src/SFA.DAS.EmployerAccounts.Web/Extensions/HtmlHelperExtensions.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Extensions/HtmlHelperExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SFA.DAS.EmployerAccounts.Configuration;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -21,6 +22,48 @@ namespace SFA.DAS.EmployerAccounts.Web.Extensions
                 .Aggregate("", (x, y) => x + y);
 
             return new MvcHtmlString(htmlAddress);
+        }
+
+        public static MvcHtmlString SetZenDeskLabels(this HtmlHelper html, params string[] labels)
+        {
+            var apiCallString =
+                "<script type=\"text/javascript\">zE('webWidget', 'helpCenter:setSuggestions', { labels: [";
+
+            var isFirstLabel = true;
+            foreach (var label in labels)
+            {
+                if (!string.IsNullOrWhiteSpace(label))
+                {
+                    if (!isFirstLabel)
+                    {
+                        apiCallString += ",";
+                    }
+                    isFirstLabel = false;
+
+                    apiCallString += $"'{ EscapeApostrophes(label) }'";
+                }
+            }
+
+            apiCallString += "] });</script>";
+
+            return MvcHtmlString.Create(apiCallString);
+        }
+
+        private static string EscapeApostrophes(string input)
+        {
+            return input.Replace("'", @"\'");
+        }
+
+        public static string GetZenDeskSnippetKey(this HtmlHelper html)
+        {
+            var configuration = DependencyResolver.Current.GetService<EmployerAccountsConfiguration>();
+            return configuration.ZenDeskSnippetKey;
+        }
+
+        public static string GetZenDeskSnippetSectionId(this HtmlHelper html)
+        {
+            var configuration = DependencyResolver.Current.GetService<EmployerAccountsConfiguration>();
+            return configuration.ZenDeskSectionId;
         }
     }
 }
