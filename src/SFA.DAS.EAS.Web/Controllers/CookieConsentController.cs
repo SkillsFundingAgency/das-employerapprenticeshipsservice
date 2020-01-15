c520230298027fe7b0ea6f7ec2a1898790ad392d
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-01-15 14:45:55 +0000

    Added CookieConsent Route

---
+++ src/SFA.DAS.EAS.Web/Controllers/CookieConsentController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using SFA.DAS.EAS.Web.Extensions;
+
+namespace SFA.DAS.EAS.Web.Controllers
+{
+    [RoutePrefix("cookieConsent")]
+    public class CookieConsentController : Controller
+    {
+        [HttpGet]        
+        public ActionResult CookieConsent()
+        {
+            return Redirect(Url.EmployerAccountsAction("cookieConsent/settings", false));
+        }
+    }
+}
