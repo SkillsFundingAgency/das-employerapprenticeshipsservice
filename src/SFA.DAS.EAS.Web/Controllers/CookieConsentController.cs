24809f33cf972e872626505b6fbcc69dac1c2dde
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-01-17 14:27:37 +0000

    Added route to support existing ones.

--- src/SFA.DAS.EAS.Web/Controllers/CookieConsentController.cs
+++ src/SFA.DAS.EAS.Web/Controllers/CookieConsentController.cs
@@ -6,14 +6,16 @@ namespace SFA.DAS.EAS.Web.Controllers
     public class CookieConsentController : Controller
     {
         [HttpGet]        
-        [Route("cookieConsent")]
+        [Route("cookieConsent", Order = 0)]
+        [Route("cookieConsent/settings", Order = 1)]
         public ActionResult CookieConsent()
         {
             return Redirect(Url.EmployerAccountsAction("cookieConsent/settings", false));
         }
 
         [HttpGet]
-        [Route("accounts/{HashedAccountId}/cookieConsent")]        
+        [Route("accounts/{HashedAccountId}/cookieConsent", Order = 0)]
+        [Route("accounts/{HashedAccountId}/cookieConsent/settings", Order = 1)]
         public ActionResult CookieConsentWithHashedAccountId()
         {
             return Redirect(Url.EmployerAccountsAction("cookieConsent/settings", true));
