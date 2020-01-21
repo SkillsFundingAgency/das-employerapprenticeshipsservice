79ed5624f409f3a670979bb5d644f336b049c408
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-01-21 10:20:49 +0000

    Revert error controller and split out Support access denied to new controller and orchestrator

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/ErrorController.cs
@@ -1,97 +1,10 @@
 ﻿using System.Net;
-using System.Threading.Tasks;
 using System.Web.Mvc;
-using MediatR;
-using SFA.DAS.Authentication;
-using SFA.DAS.Authorization.Mvc.Attributes;
-using SFA.DAS.EAS.Account.Api.Client;
-using SFA.DAS.EmployerAccounts.Interfaces;
-using SFA.DAS.EmployerAccounts.Models.AccountTeam;
-using SFA.DAS.EmployerAccounts.Queries.GetMember;
-using SFA.DAS.EmployerAccounts.Web.Helpers;
-using SFA.DAS.EmployerAccounts.Web.Orchestrators;
-using SFA.DAS.EmployerAccounts.Web.ViewModels;
 
 namespace SFA.DAS.EmployerAccounts.Web.Controllers
 {
-    public class ErrorController : BaseController
+    public class ErrorController : Controller
     {
-        private readonly IAccountApiClient _accountApiClient;
-        private readonly EmployerTeamOrchestrator _employerTeamOrchestrator;
-        private readonly IMediator _mediator;
-
-        public ErrorController(
-            IAuthenticationService owinWrapper, 
-            IMultiVariantTestingService multiVariantTestingService, 
-            ICookieStorageService<FlashMessageViewModel> flashMessage, IAccountApiClient accountApiClient, EmployerTeamOrchestrator employerTeamOrchestrator, IMediator mediator) : base(owinWrapper, multiVariantTestingService, flashMessage)
-        {
-            _accountApiClient = accountApiClient;
-            _employerTeamOrchestrator = employerTeamOrchestrator;
-            _mediator = mediator;
-        }
-
-        [DasAuthorize]
-        [Route("error/accessdenied/{HashedAccountId}")]
-        public async Task<ActionResult> AccessDenied(string hashedAccountId)
-        {
-            var response = new OrchestratorResponse<AccessDeniedViewModel>();
-            var queryResponse = await _mediator.SendAsync(new GetMemberRequest
-            {
-                HashedAccountId = hashedAccountId
-                //Email = email
-            });
-
-            response.Data = MapFrom(queryResponse.TeamMember);
-
-            ViewBag.AccountId = hashedAccountId;
-            return View(response);
-        }
-
-        [ChildActionOnly]
-        public override ActionResult SupportUserBanner(IAccountIdentifier model = null)
-        {
-            EmployerAccounts.Models.Account.Account account = null;
-
-            if (model != null && model.HashedAccountId != null)
-            {
-                var response = AsyncHelper.RunSync(() => GetAccountInformation(model.HashedAccountId));
-
-                account = response.Status != HttpStatusCode.OK ? null : response.Data.Account;
-            }
-
-            return PartialView("_SupportUserBanner", new SupportUserBannerViewModel() { Account = account });
-        }
-
-        private static AccessDeniedViewModel MapFrom(TeamMember teamMember)
-        {
-            return new AccessDeniedViewModel
-            {
-                IsUser = teamMember.IsUser,
-                Id = teamMember.Id,
-                AccountId = teamMember.AccountId,
-                Email = teamMember.Email,
-                Name = teamMember.Name,
-                HashedAccountId = teamMember.HashedAccountId
-            };
-        }
-
-        private async Task<OrchestratorResponse<AccountDashboardViewModel>> GetAccountInformation(string hashedAccountId)
-        {
-            var externalUserId = OwinWrapper.GetClaimValue(ControllerConstants.UserRefClaimKeyName);
-            var response = await _employerTeamOrchestrator.GetAccount(hashedAccountId, externalUserId);
-
-            var flashMessage = GetFlashMessageViewModelFromCookie();
-
-            if (flashMessage != null)
-            {
-                response.FlashMessage = flashMessage;
-                response.Data.EmployerAccountType = flashMessage.HiddenFlashMessageInformation;
-            }
-
-            return response;
-        }
-
-
         [Route("accessdenied")]
         public ActionResult AccessDenied()
         {
---
+++ src/SFA.DAS.EmployerAccounts.Web/Orchestrators/SupportErrorOrchestrator.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerAccounts.Queries.GetEmployerAccount;
+using SFA.DAS.EmployerAccounts.Web.ViewModels;
+
+namespace SFA.DAS.EmployerAccounts.Web.Orchestrators
+{
+    public class SupportErrorOrchestrator
+    {
+        private readonly IMediator _mediator;
+
+        public SupportErrorOrchestrator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        public virtual async Task<OrchestratorResponse<AccountDashboardViewModel>> GetAccount(string hashedAccountId, string externalUserId)
+        {
+            try
+            {
+                var accountResponse = await _mediator.SendAsync(new GetEmployerAccountByHashedIdQuery
+                {
+                    HashedAccountId = hashedAccountId,
+                    UserId = externalUserId
+                });
+
+                var viewModel = new AccountDashboardViewModel
+                {
+                    Account = accountResponse.Account
+                };
+
+                //note: ApprenticeshipEmployerType is already returned by GetEmployerAccountHashedQuery, but we need to transition to calling the api instead.
+                // we could blat over the existing flag, but it's much nicer to store the enum (as above) rather than a byte!
+                //viewModel.Account.ApprenticeshipEmployerType = (byte) ((ApprenticeshipEmployerType) Enum.Parse(typeof(ApprenticeshipEmployerType), apiGetAccountTask.Result.ApprenticeshipEmployerType, true));
+
+                return new OrchestratorResponse<AccountDashboardViewModel>
+                {
+                    Status = HttpStatusCode.OK,
+                    Data = viewModel
+                };
+            }
+        }
+    }
+}
