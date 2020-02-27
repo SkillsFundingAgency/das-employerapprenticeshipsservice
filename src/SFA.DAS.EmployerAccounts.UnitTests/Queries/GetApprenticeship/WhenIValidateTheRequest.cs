85625672898ee43edeec3c788f4b0d9d1ed299a7
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-02-27 15:37:12 +0000

    CON-1403 - Provider Actions

--- src/SFA.DAS.EmployerAccounts.UnitTests/Queries/GetApprenticeship/WhenIValidateTheRequest.cs
+++ src/SFA.DAS.EmployerAccounts.UnitTests/Queries/GetApprenticeship/WhenIValidateTheRequest.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Queries.GetApprenticeship
         }
 
         [Test]
-        public void ThenShouldReturnInValidIfRequestIsValid()
+        public void ThenShouldReturnInValidIfAccountIdNotSet()
         {
             //Act
             var result = _validator.Validate(new GetApprenticeshipRequest { });
