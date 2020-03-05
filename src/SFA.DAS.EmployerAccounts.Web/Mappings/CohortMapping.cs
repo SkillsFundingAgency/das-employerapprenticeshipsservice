814c1a7c98305351f3c17fce61a84e4a5868e172
vas <vasantha.kasirajan@digital.education.gov.uk>
2020-03-05 11:15:56 +0000

    CON - 1403 - Tidy up

--- src/SFA.DAS.EmployerAccounts.Web/Mappings/CohortMapping.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Mappings/CohortMapping.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using SFA.DAS.EmployerAccounts.Models.CommitmentsV2;
-using SFA.DAS.EmployerAccounts.Queries.GetSingleCohort;
 using SFA.DAS.EmployerAccounts.Web.ViewModels;
 
 namespace SFA.DAS.EmployerAccounts.Web.Mappings
@@ -11,12 +10,6 @@ namespace SFA.DAS.EmployerAccounts.Web.Mappings
         {
             CreateMap<Cohort, CohortViewModel>()
                .ForMember(dest => dest.HashedCohortId, opt => opt.MapFrom(src => src.HashedId));
-
-            CreateMap<GetSingleCohortResponse, CohortViewModel>()
-                .ForMember(dest => dest.NumberOfDraftApprentices, opt => opt.Ignore())
-                .ForMember(dest => dest.CohortStatus, opt => opt.Ignore())
-                .ForMember(dest => dest.HashedCohortId, opt => opt.Ignore())
-                .ForMember(dest => dest.Apprenticeships, opt => opt.Ignore());
 
             CreateMap<Apprenticeship, ApprenticeshipViewModel>()
                 .ForMember(dest => dest.ApprenticeshipFullName, opt => opt.MapFrom(src => string.Format("{0} {1}",
