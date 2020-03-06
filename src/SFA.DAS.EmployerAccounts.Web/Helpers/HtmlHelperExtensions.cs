fef4ece97f0dc54c4914ed1477a3e87df2a15aec
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-03-06 17:34:42 +0000

    Update HtmlHelperExtensions.cs

--- src/SFA.DAS.EmployerAccounts.Web/Helpers/HtmlHelperExtensions.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Helpers/HtmlHelperExtensions.cs
@@ -9,6 +9,10 @@ using SFA.DAS.EAS.Account.Api.Client;
 using SFA.DAS.EmployerAccounts.Web.Authorization;
 using SFA.DAS.EmployerAccounts.Web.Extensions;
 using System.Threading.Tasks;
+using System.Web;
+using SFA.DAS.Authorization.EmployerFeatures.Models;
+using SFA.DAS.Authorization.Features.Services;
+using SFA.DAS.EmployerAccounts.Queries.GetAccountEmployerAgreements;
 
 namespace SFA.DAS.EmployerAccounts.Web.Helpers
 {
@@ -35,14 +39,30 @@ namespace SFA.DAS.EmployerAccounts.Web.Helpers
 
         public static bool HasSignedV3AgreementAsync(this HtmlHelper htmlHelper)
         {
-            var authorisationService = DependencyResolver.Current.GetService<IAuthorizationService>();
             var mediator = DependencyResolver.Current.GetService<IMediator>();
-            var hashedAccountId = htmlHelper.ViewContext.RouteData.Values["hashedAccountId"];
-            var accountApiClient = DependencyResolver.Current.GetService<IAccountApiClient>();
-            var test2 = accountApiClient.GetAccount(hashedAccountId.ToString()).GetAwaiter().GetResult();
-            var test = authorisationService.GetAuthorizationResult("EmployerFeature.Transfers");
+            var userId = htmlHelper.ViewContext.RequestContext.HttpContext.GetOwinContext().Authentication.
+                User.Claims.Where(x => x.Type == ControllerConstants.UserRefClaimKeyName);
+            var hashedAccountId = htmlHelper.ViewContext.RouteData.Values[ControllerConstants.AccountHashedIdRouteKeyName].ToString();
+            var agreementResponse = Task.Run(async () => await mediator
+            .SendAsync(new GetAccountEmployerAgreementsRequest
+            {
+                HashedAccountId = hashedAccountId,
+                ExternalUserId = userId.FirstOrDefault().Value
+            })).Result;
+
+            if (agreementResponse.HasPendingAgreements && agreementResponse.EmployerAgreements.Count > 0)
+            {
+                var employerAgreements = agreementResponse.EmployerAgreements;
+                foreach (var _ in from agreement in employerAgreements
+                                  where agreement.Signed?.VersionNumber == 3
+                                  select new { })
+                {
+                    return false;
+                }
+            }
             return true;
         }
+
 
         public static string ReturnToHomePageButtonHref(this HtmlHelper htmlHelper, string accountId)
         {
