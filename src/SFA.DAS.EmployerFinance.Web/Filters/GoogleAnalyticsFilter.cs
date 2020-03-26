cd8fa56495c6ba1429b72d4d8bbfa1aa26add594
Paul Howes <paul.howes@cognetics.net>
2020-03-26 13:51:35 +0000

    GTM & ZenDesk updates

--- src/SFA.DAS.EmployerFinance.Web/Filters/GoogleAnalyticsFilter.cs
+++ src/SFA.DAS.EmployerFinance.Web/Filters/GoogleAnalyticsFilter.cs
@@ -10,20 +10,32 @@ namespace SFA.DAS.EmployerFinance.Web.Filters
         {
             string userId = null;
             string hashedAccountId = null;
+            string userEmail = null;
+            string userName = null;
 
             var thisController = filterContext.Controller as BaseController;
             if (thisController != null)
+            {
                 userId = thisController.OwinWrapper.GetClaimValue(@"sub");
+                userEmail = thisController.OwinWrapper.GetClaimValue(@"email");
+                userName = $"{thisController.OwinWrapper.GetClaimValue(@"firstname")} {thisController.OwinWrapper.GetClaimValue(@"lastname")}";
+            }
 
             if (filterContext.ActionParameters.ContainsKey("hashedAccountId"))
             {
                 hashedAccountId = filterContext.ActionParameters["hashedAccountId"] as string;
             }
+            else if(filterContext.Controller.ControllerContext.RouteData.Values.ContainsKey("hashedAccountId"))
+            {
+                hashedAccountId = filterContext.Controller.ControllerContext.RouteData.Values["hashedAccountId"] as string;
+            }
 
             filterContext.Controller.ViewBag.GaData = new GaData
             {
                 UserId = userId,
-                Acc = hashedAccountId
+                Acc = hashedAccountId,
+                UserEmail = userEmail,
+                UserName = userName
             };
 
             base.OnActionExecuting(filterContext);
@@ -39,7 +51,8 @@ namespace SFA.DAS.EmployerFinance.Web.Filters
             }
             public string DataLoaded { get; set; } = "dataLoaded";
             public string UserId { get; set; }
-
+            public string UserEmail { get; set; }
+            public string UserName { get; set; }
             public string Vpv { get; set; }
             public string Acc { get; set; }
 
