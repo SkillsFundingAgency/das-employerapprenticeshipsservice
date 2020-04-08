4b83b9fc1f1138daf703155f75cbb4deb30c460a
Paul Howes <paul.howes@cognetics.net>
2020-04-08 14:11:16 +0100

    Changes to work as-is

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
@@ -310,12 +310,5 @@ namespace SFA.DAS.EmployerAccounts.Web.Controllers
                 }
             }
         }
-
-        [HttpGet]
-        [Route("agreements/{accountLegalEntityHashedId}/viewallagreements")]
-        public ActionResult ViewAllAgreements(string hashedAccountId, string accountLegalEntityHashedId)
-        {
-           throw new NotImplementedException();
-        }
     }
 }
