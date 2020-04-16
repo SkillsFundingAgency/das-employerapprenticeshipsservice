4e0e6151c98e761c5e0142dce4e80310a1290a2c
Paul Howes <paul.howes@cognetics.net>
2020-04-16 16:27:55 +0100

    Added new nuget package

--- src/SFA.DAS.EmployerAccounts.MessageHandlers.UnitTests/EventHandlers/AccountLevyStatusEventHandlerTests.cs
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers.UnitTests/EventHandlers/AccountLevyStatusEventHandlerTests.cs
@@ -12,33 +12,32 @@ using SFA.DAS.EmployerFinance.Messages.Events;
 
 namespace SFA.DAS.EmployerAccounts.MessageHandlers.UnitTests.EventHandlers
 {
-    //[ExcludeFromCodeCoverage]
-    //[TestFixture]
-    //public class AccountLevyStatusEventHandlerTests
-    //{
-    //    private AccountLevyStatusEventHandler _handler;
-    //    private long _accountId = 90210;
-    //    private Mock<IMediator> _mediatr;
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class AccountLevyStatusEventHandlerTests
+    {
+        private AccountLevyStatusEventHandler _handler;
+        private long _accountId = 90210;
+        private Mock<IMediator> _mediatr;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        _mediatr = new Mock<IMediator>();
-    //        _handler = new AccountLevyStatusEventHandler(_mediatr.Object);
-    //    }
+        [SetUp]
+        public void Setup()
+        {
+            _mediatr = new Mock<IMediator>();
+            _handler = new AccountLevyStatusEventHandler(_mediatr.Object);
+        }
 
-    //    [Test]
-    //    public async Task Handle_AccountLevyStatusEvent()
-    //    {
-    //        await _handler.Handle(new AccountLevyStatusEvent
-    //        {
-    //            AccountId = _accountId,
-    //            ApprenticeEmployerType = ApprenticeshipEmployerType.Levy
-    //        }, 
-    //            Mock.Of<IMessageHandlerContext>());
+        [Test]
+        public async Task Handle_AccountLevyStatusEvent()
+        {
+            await _handler.Handle(new AccountLevyStatusEvent
+            {
+                AccountId = _accountId,
+                ApprenticeshipEmployerType = ApprenticeshipEmployerType.Levy
+            },
+                Mock.Of<IMessageHandlerContext>());
 
-
-    //        _mediatr.Verify(m => m.SendAsync(It.Is<AccountLevyStatusCommand>(cmd => cmd.AccountId == _accountId && cmd.ApprenticeshipEmployerType == ApprenticeshipEmployerType.Levy)));
-    //    }
-    //}
+            _mediatr.Verify(m => m.SendAsync(It.Is<AccountLevyStatusCommand>(cmd => cmd.AccountId == _accountId && cmd.ApprenticeshipEmployerType == ApprenticeshipEmployerType.Levy)));
+        }
+    }
 }
--- src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/AccountLevyStatusEventHandler.cs
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/AccountLevyStatusEventHandler.cs
@@ -6,22 +6,22 @@ using SFA.DAS.EmployerFinance.Messages.Events;
 
 namespace SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers
 {
-    //public class AccountLevyStatusEventHandler : IHandleMessages<AccountLevyStatusEvent>
-    //{
-    //    private readonly IMediator _mediatr;
+    public class AccountLevyStatusEventHandler : IHandleMessages<AccountLevyStatusEvent>
+    {
+        private readonly IMediator _mediatr;
 
-    //    public AccountLevyStatusEventHandler(IMediator mediatr)
-    //    {
-    //        _mediatr = mediatr;
-    //    }
+        public AccountLevyStatusEventHandler(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
 
-    //    public async Task Handle(AccountLevyStatusEvent message, IMessageHandlerContext context)
-    //    {
-    //        await _mediatr.SendAsync(new AccountLevyStatusCommand
-    //            {
-    //                AccountId = message.AccountId,
-    //                ApprenticeshipEmployerType = message.ApprenticeshipEmployerType
-    //            });
-    //    }
-    //}
+        public async Task Handle(AccountLevyStatusEvent message, IMessageHandlerContext context)
+        {
+            await _mediatr.SendAsync(new AccountLevyStatusCommand
+            {
+                AccountId = message.AccountId,
+                ApprenticeshipEmployerType = message.ApprenticeshipEmployerType
+            });
+        }
+    }
 }
