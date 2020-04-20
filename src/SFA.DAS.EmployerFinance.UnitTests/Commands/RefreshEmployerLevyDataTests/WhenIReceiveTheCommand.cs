69a3e9829733f4e0bb0bc66c6f1e711358508493
Paul Howes <paul.howes@cognetics.net>
2020-04-20 12:28:11 +0100

    Updated tests

--- src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshEmployerLevyDataTests/WhenIReceiveTheCommand.cs
+++ src/SFA.DAS.EmployerFinance.UnitTests/Commands/RefreshEmployerLevyDataTests/WhenIReceiveTheCommand.cs
@@ -18,9 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using SFA.DAS.Common.Domain.Types;
 using SFA.DAS.EmployerFinance.Interfaces;
-using SFA.DAS.EmployerFinance.Messages.Commands;
 using SFA.DAS.EmployerFinance.Messages.Events;
 using SFA.DAS.NServiceBus.Testing.Services;
 
@@ -135,8 +133,9 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshEmployerLevyDataTest
                 && e.Amount.Equals(decimal.One)));
 
             Assert.IsTrue(_eventPublisher.Events.OfType<RefreshEmployerLevyDataCompletedEvent>().Any(e =>
-                e.AccountId.Equals(ExpectedAccountId)
-                && e.LevyTransactionValue.Equals(decimal.One)));
+                e.AccountId.Equals(ExpectedAccountId) &&
+                e.LevyImported.Equals(true) &&
+                e.LevyTransactionValue.Equals(decimal.One)));
         }
 
         [Test]
@@ -157,8 +156,9 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Commands.RefreshEmployerLevyDataTest
                 && e.Amount.Equals(decimal.One)));
 
             Assert.IsTrue(_eventPublisher.Events.OfType<RefreshEmployerLevyDataCompletedEvent>().Any(e =>
-                e.AccountId.Equals(ExpectedAccountId)
-                && e.LevyTransactionValue.Equals(decimal.Zero)));
+                e.AccountId.Equals(ExpectedAccountId) &&
+                e.LevyImported.Equals(false) &&
+                e.LevyTransactionValue.Equals(decimal.Zero)));
         }
 
         //[Test]
