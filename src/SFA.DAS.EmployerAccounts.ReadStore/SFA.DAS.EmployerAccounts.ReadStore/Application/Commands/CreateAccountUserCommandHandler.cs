57b245b4418cde1513ff8abb9a64b3b6b26e24f1
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-04-23 18:37:28 +0100

    CON-1721-catch and log exception

--- src/SFA.DAS.EmployerAccounts.ReadStore/SFA.DAS.EmployerAccounts.ReadStore/Application/Commands/CreateAccountUserCommandHandler.cs
+++ src/SFA.DAS.EmployerAccounts.ReadStore/SFA.DAS.EmployerAccounts.ReadStore/Application/Commands/CreateAccountUserCommandHandler.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using NServiceBus.Logging;
 using SFA.DAS.CosmosDb;
 using SFA.DAS.EmployerAccounts.ReadStore.Data;
+using SFA.DAS.EmployerAccounts.ReadStore.Exceptions;
 using SFA.DAS.EmployerAccounts.ReadStore.Mediator;
 using SFA.DAS.EmployerAccounts.ReadStore.Models;
 
@@ -10,27 +13,35 @@ namespace SFA.DAS.EmployerAccounts.ReadStore.Application.Commands
     internal class CreateAccountUserCommandHandler : IReadStoreRequestHandler<CreateAccountUserCommand, Unit>
     {
         private readonly IAccountUsersRepository _accountUsersRepository;
+        private readonly ILog _logger;
 
-        public CreateAccountUserCommandHandler(IAccountUsersRepository accountUsersRepository)
+        public CreateAccountUserCommandHandler(IAccountUsersRepository accountUsersRepository, ILog logger)
         {
             _accountUsersRepository = accountUsersRepository;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(CreateAccountUserCommand request, CancellationToken cancellationToken)
         {
-            var user = await _accountUsersRepository.CreateQuery().SingleOrDefaultAsync(x => x.UserRef == request.UserRef && x.AccountId == request.AccountId, cancellationToken);
-
-            if (user == null)
+            try
             {
-                user = new AccountUser(request.UserRef, request.AccountId, request.Role, request.Created, request.MessageId);
-                await _accountUsersRepository.Add(user, null, cancellationToken);
-            }
-            else
-            {
-                user.Recreate(request.Role, request.Created, request.MessageId);
-                await _accountUsersRepository.Update(user, null, cancellationToken);
-            }
+                var user = await _accountUsersRepository.CreateQuery().SingleOrDefaultAsync(x => x.UserRef == request.UserRef && x.AccountId == request.AccountId, cancellationToken);
 
+                if (user == null)
+                {
+                    user = new AccountUser(request.UserRef, request.AccountId, request.Role, request.Created, request.MessageId);
+                    await _accountUsersRepository.Add(user, null, cancellationToken);
+                }
+                else
+                {
+                    user.Recreate(request.Role, request.Created, request.MessageId);
+                    await _accountUsersRepository.Update(user, null, cancellationToken);
+                }
+            }
+            catch (UserNotRemovedException exception)
+            {
+                _logger.Warn("User has not been removed",exception);
+            }
             return Unit.Value;
         }
     }
---
+++ src/SFA.DAS.EmployerAccounts.ReadStore/SFA.DAS.EmployerAccounts.ReadStore/Exceptions/UserNotRemovedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerAccounts.Web.Exceptions
+{
+    [Serializable]
+    public class UserNotRemovedException : Exception
+    {
+        public UserNotRemovedException() { }
+        public UserNotRemovedException(string message) : base(message) { }
+        public UserNotRemovedException(string message, Exception inner) : base(message, inner) { }
+        protected UserNotRemovedException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
