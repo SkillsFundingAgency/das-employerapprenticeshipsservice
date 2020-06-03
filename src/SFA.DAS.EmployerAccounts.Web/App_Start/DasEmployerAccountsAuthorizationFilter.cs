77a39eedc66181027fa04b9789fe627e11573d74
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-06-03 14:41:19 +0100

    CON-1144-move to separate file

---
+++ src/SFA.DAS.EmployerAccounts.Web/App_Start/DasEmployerAccountsAuthorizationFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SFA.DAS.Authorization.Mvc.Filters;
+using SFA.DAS.Authorization.Services;
+using SFA.DAS.EmployerAccounts.Extensions;
+using SFA.DAS.EmployerAccounts.Web.Authorization;
+
+namespace SFA.DAS.EmployerAccounts.Web
+{
+    public class DasEmployerAccountsAuthorizationFilter : AuthorizationFilter
+    {
+        private readonly IUserContext _userContext;
+        public DasEmployerAccountsAuthorizationFilter(Func<IAuthorizationService> authorizationService, IUserContext userContext) : base(authorizationService)
+        {
+            _userContext = userContext;
+        }
+        
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            if (filterContext.Result is HttpStatusCodeResult)
+            {
+                if (((HttpStatusCodeResult)filterContext.Result).StatusCode.Equals((int)HttpStatusCode.Forbidden) &&
+                    _userContext.IsSupportConsoleUser())
+                {
+                    if (filterContext.HttpContext.Request.RequestContext.RouteData.Values.TryGetValue(RouteValueKeys.AccountHashedId, out var accountHashedId))
+                    {
+                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "SupportError", action = $"accessdenied/{accountHashedId}" }));
+                    }
+                }
+            }
+        }
+    }
+}
--- src/SFA.DAS.EmployerAccounts.Web/App_Start/FilterConfig.cs
+++ src/SFA.DAS.EmployerAccounts.Web/App_Start/FilterConfig.cs
@@ -54,32 +54,6 @@ namespace SFA.DAS.EmployerAccounts.Web
         }
     }
 
-    public class DasEmployerAccountsAuthorizationFilter : AuthorizationFilter
-    {
-        private readonly IUserContext _userContext;
-        public DasEmployerAccountsAuthorizationFilter(Func<IAuthorizationService> authorizationService, IUserContext userContext) : base(authorizationService)
-        {
-            _userContext = userContext;
-        }
-        
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            base.OnActionExecuting(filterContext);
-
-           if (filterContext.Result is HttpStatusCodeResult)
-           {
-                if (((HttpStatusCodeResult)filterContext.Result).StatusCode.Equals((int)HttpStatusCode.Forbidden) &&
-                    _userContext.IsSupportConsoleUser())
-                {
-                    if (filterContext.HttpContext.Request.RequestContext.RouteData.Values.TryGetValue(RouteValueKeys.AccountHashedId, out var accountHashedId))
-                    {
-                        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Error", action = $"accessdenied/{accountHashedId}" }));
-                    }
-                }
-           }
-        }
-    }
-
     public static class GlobalFilterCollectionExtensions
     {
         public static void AddDasEmployerAccountsAuthorizationFilter(this GlobalFilterCollection filters)
