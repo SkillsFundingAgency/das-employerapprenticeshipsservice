122643f14315c7b4ef62296669ac9bc4d8b07089
Paul Graham <paul.graham@coprime.co.uk>
2020-08-14 11:49:21 +0100

    allow multiple audiences

--- src/SFA.DAS.EmployerAccounts.Api/Startup.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Startup.cs
@@ -11,13 +11,13 @@ namespace SFA.DAS.EmployerAccounts.Api
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+            _ = app.UseWindowsAzureActiveDirectoryBearerAuthentication(new WindowsAzureActiveDirectoryBearerAuthenticationOptions
             {
                 Tenant = ConfigurationManager.AppSettings["idaTenant"],
                 TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
                 {
                     RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
-                    ValidAudience = ConfigurationManager.AppSettings["idaAudience"]
+                    ValidAudiences = ConfigurationManager.AppSettings["idaAudience"].ToString().Split(',')
                 }
             });
         }
