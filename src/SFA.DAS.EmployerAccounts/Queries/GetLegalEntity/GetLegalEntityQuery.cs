66e9fc4c8be626d771708fd5d824ed384514b2cf
Ruslan <ruslanss@gmail.com>
2020-09-09 17:53:12 +0100

    EI-347 [SFA.DAS.EmployerAccounts.Api] GetLegalEntity: optionally include agreements of all statuses

--- src/SFA.DAS.EmployerAccounts/Queries/GetLegalEntity/GetLegalEntityQuery.cs
+++ src/SFA.DAS.EmployerAccounts/Queries/GetLegalEntity/GetLegalEntityQuery.cs
@@ -4,14 +4,17 @@ namespace SFA.DAS.EmployerAccounts.Queries.GetLegalEntity
 {
     public class GetLegalEntityQuery : IAsyncRequest<GetLegalEntityResponse>
     {
-        public GetLegalEntityQuery(string accountHashedId, long legalEntityId)
+        public GetLegalEntityQuery(string accountHashedId, long legalEntityId, bool includeAllAgreements)
         {
             AccountHashedId = accountHashedId;
             LegalEntityId = legalEntityId;
+            IncludeAllAgreements = includeAllAgreements;
         }
 
-        public string AccountHashedId { get;  }
+        public string AccountHashedId { get; }
 
         public long LegalEntityId { get; }
+
+        public bool IncludeAllAgreements { get; }
     }
 }
--- src/SFA.DAS.EmployerAccounts/Queries/GetLegalEntity/GetLegalEntityQueryHandler.cs
+++ src/SFA.DAS.EmployerAccounts/Queries/GetLegalEntity/GetLegalEntityQueryHandler.cs
@@ -1,39 +1,50 @@
-﻿using System;
+﻿using MediatR;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.EmployerAccounts.Api.Types;
+using SFA.DAS.EmployerAccounts.Data;
+using SFA.DAS.EmployerAccounts.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.Entity;
-using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using MediatR;
-using SFA.DAS.EmployerAccounts.Data;
-using SFA.DAS.EmployerAccounts.Api.Types;
+using LegalEntity = SFA.DAS.EmployerAccounts.Api.Types.LegalEntity;
 
 namespace SFA.DAS.EmployerAccounts.Queries.GetLegalEntity
 {
     public class GetLegalEntityQueryHandler : IAsyncRequestHandler<GetLegalEntityQuery, GetLegalEntityResponse>
     {
         private readonly Lazy<EmployerAccountsDbContext> _db;
-        private readonly IConfigurationProvider _configurationProvider;
 
-        public GetLegalEntityQueryHandler(Lazy<EmployerAccountsDbContext> db, IConfigurationProvider configurationProvider)
+        public GetLegalEntityQueryHandler(Lazy<EmployerAccountsDbContext> db)
         {
             _db = db;
-            _configurationProvider = configurationProvider;
         }
 
         public async Task<GetLegalEntityResponse> Handle(GetLegalEntityQuery message)
         {
-            var legalEntity = await _db.Value.AccountLegalEntities
-                .Where(l =>
+            var legalEntity = _db.Value.AccountLegalEntities.Where(l =>
                     l.LegalEntityId == message.LegalEntityId &&
                     l.Account.HashedId == message.AccountHashedId &&
                     (l.PendingAgreementId != null || l.SignedAgreementId != null) &&
                     l.Deleted == null)
-                .ProjectTo<LegalEntity>(_configurationProvider, new
-                {
-                    accountHashedId = message.AccountHashedId
-                })
-                .SingleOrDefaultAsync();
+
+                    .Select(x => new LegalEntity
+                    {
+                        Agreements = MapAgreements(x.Agreements, message.IncludeAllAgreements),
+                        DasAccountId = message.AccountHashedId,
+                        AccountLegalEntityId = x.Id,
+                        AccountLegalEntityPublicHashedId = x.PublicHashedId,
+                        LegalEntityId = x.LegalEntityId,
+                        DateOfInception = x.LegalEntity.DateOfIncorporation,
+                        Code = x.LegalEntity.Code,
+                        Sector = x.LegalEntity.Sector,
+                        Status = x.LegalEntity.Status,
+                        PublicSectorDataSource = MapPublicSectorDataSource(x.LegalEntity.PublicSectorDataSource),
+                        Source = MapSource(x.LegalEntity.Source),
+                        SourceNumeric = (short)x.LegalEntity.Source,
+                    }).SingleOrDefault();
+
 
             // TODO: The template version number can now be the same across agreement types so this logic may fail
             var latestAgreement = legalEntity?.Agreements
@@ -47,6 +58,7 @@ namespace SFA.DAS.EmployerAccounts.Queries.GetLegalEntity
                 legalEntity.AgreementSignedByName = latestAgreement.SignedByName;
                 legalEntity.AgreementSignedDate = latestAgreement.SignedDate;
                 legalEntity.AgreementStatus = latestAgreement.Status;
+
             }
 
             return new GetLegalEntityResponse
@@ -55,9 +67,60 @@ namespace SFA.DAS.EmployerAccounts.Queries.GetLegalEntity
             };
         }
 
+        private static string MapSource(OrganisationType legalEntitySource)
+        {
+            switch (legalEntitySource)
+            {
+                case OrganisationType.CompaniesHouse:
+                    return "Companies House";
+                case OrganisationType.Charities:
+                    return "Charities";
+                case OrganisationType.PublicBodies:
+                    return "Public Bodies";
+                case OrganisationType.PensionsRegulator:
+                    return "Pensions Regulator";
+                default:
+                    return "Other";
+            }
+        }
+
+        private static string MapPublicSectorDataSource(byte? publicSectorDataSource)
+        {
+            switch (publicSectorDataSource)
+            {
+                case 1:
+                    return "ONS";
+                case 2:
+                    return "NHS";
+                case 3:
+                    return "Police";
+                default:
+                    return "";
+            }
+        }
+
+        private static List<Agreement> MapAgreements(IEnumerable<EmployerAgreement> agreements, bool includeAllAgreements)
+        {
+            return agreements.Where(x =>
+                includeAllAgreements || (x.StatusId == Models.EmployerAgreement.EmployerAgreementStatus.Pending ||
+                                         x.StatusId == Models.EmployerAgreement.EmployerAgreementStatus.Signed)).Select(
+                a => new Agreement
+                {
+                    Id = a.Id,
+                    AgreementType = a.Template.AgreementType,
+                    TemplateVersionNumber = a.Template.VersionNumber,
+                    Status = (EmployerAgreementStatus)(int)a.StatusId,
+                    SignedById = a.SignedById,
+                    SignedByName = a.SignedByName,
+                    SignedDate = a.SignedDate
+
+                })
+                .ToList();
+        }
+
         private async Task SetEmailAddressForSignatures(LegalEntity legalEntity)
         {
-            var userIds = legalEntity?.Agreements?.Where(a=>a.SignedById != null).Select(a => a.SignedById).ToArray();
+            var userIds = legalEntity?.Agreements?.Where(a => a.SignedById != null).Select(a => a.SignedById).ToArray();
 
             if (userIds != null && userIds.Any())
             {
