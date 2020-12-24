b8a63b5da32cbcb5c2895b812f06ef2dd6df676d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-24 07:54:08 +0000

    FAT2-295 - Add outer API requests

---
+++ src/SFA.DAS.EmployerFinance.UnitTests/Infrastructure/OuterApiRequests/WhenBuildingGetFrameworksRequest.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Infrastructure.OuterApiRequests;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Infrastructure.OuterApiRequests
+{
+    public class WhenBuildingGetFrameworksRequest
+    {
+        [Test]
+        public void Then_The_Url_Is_Correctly_Constructed()
+        {
+            //Arrange
+            var actual = new GetFrameworksRequest();
+            
+            //Act
+            actual.GetUrl.Should().Be("TrainingCourses/frameworks");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerFinance.UnitTests/Infrastructure/OuterApiRequests/WhenBuildingGetProvidersRequest.cs
@@ -0,0 +1,19 @@
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Infrastructure.OuterApiRequests;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Infrastructure.OuterApiRequests
+{
+    public class WhenBuildingGetProvidersRequest
+    {
+        [Test]
+        public void Then_The_Url_Is_Correctly_Constructed()
+        {
+            //Arrange
+            var actual = new GetProvidersRequest();
+            
+            //Act
+            actual.GetUrl.Should().Be("providers");
+        }
+    }
+}
