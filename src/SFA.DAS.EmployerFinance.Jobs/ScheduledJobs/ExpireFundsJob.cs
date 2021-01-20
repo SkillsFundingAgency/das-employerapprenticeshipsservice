ef8e8449d3eff358673d1be058d61cf71d5332ab
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-01-20 10:40:32 +0000

    [CON-3034] Batching message sends

--- src/SFA.DAS.EmployerFinance.Jobs/ScheduledJobs/ExpireFundsJob.cs
+++ src/SFA.DAS.EmployerFinance.Jobs/ScheduledJobs/ExpireFundsJob.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
@@ -35,22 +36,29 @@ namespace SFA.DAS.EmployerFinance.Jobs.ScheduledJobs
 
             logger.LogInformation($"Queueing {nameof(ExpireAccountFundsCommand)} messages for {accounts.Count} accounts.");
 
-            for (int i = 0; i < accounts.Count; i++)
-            {
-                var account = accounts[i];
+            var messageTasks = new List<Task>();
+            var sendCounter = 0;
 
+            foreach (var account in accounts)
+            {
                 var sendOptions = new SendOptions();
 
                 sendOptions.SetMessageId($"{nameof(ExpireAccountFundsCommand)}-{now.Year}-{now.Month}-{account.Id}");
 
-                await _messageSession.Send(new ExpireAccountFundsCommand { AccountId = account.Id }, sendOptions);
+                messageTasks.Add(_messageSession.Send(new ExpireAccountFundsCommand { AccountId = account.Id }, sendOptions));
+                sendCounter++;
 
-                if (i % 1000 == 0)
+                if (sendCounter % 1000 == 0)
                 {
-                    logger.LogInformation($"Queued {i} of {accounts.Count} messages.");
-                    await Task.Delay(5000);
+                    await Task.WhenAll(messageTasks);
+                    logger.LogInformation($"Queued {messageTasks.Count} of {accounts.Count} messages.");
+                    messageTasks.Clear();
+                    await Task.Delay(1000);
                 }
             }
+
+            // await final tasks not % 1000
+            await Task.WhenAll(messageTasks);
 
             logger.LogInformation($"{nameof(ExpireFundsJob)} completed.");
         }
