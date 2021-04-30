3aa81a99ff617c2b3ec9fd938a4f2552053e32d9
dannygb <dan.beavon@education.gov.uk>
2021-04-30 11:50:47 +0100

    [TM-12] Adds Your transfers page & wraps it in a feature toggle

---
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/TransfersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using SFA.DAS.Authorization.Mvc.Attributes;
+
+namespace SFA.DAS.EmployerFinance.Web.Controllers
+{
+    [DasAuthorize("EmployerFeature.TransfersMatching")]
+    [RoutePrefix("transfers")]
+    public class TransfersController : Controller
+    {
+        [HttpGet]
+        [Route("{hashedAccountId}")]
+        public ActionResult Index(string hashedAccountId)
+        {
+            return View();
+        }
+    }
+}
