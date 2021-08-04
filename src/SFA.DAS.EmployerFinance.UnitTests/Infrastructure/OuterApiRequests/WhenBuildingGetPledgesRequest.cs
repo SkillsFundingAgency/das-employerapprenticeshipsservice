273c6ee9cd335c2b2838dc1cc7a888c16d98b373
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-08-04 15:08:40 +0100

    Unit tests, covering new req and service

---
+++ src/SFA.DAS.EmployerFinance.UnitTests/Infrastructure/OuterApiRequests/WhenBuildingGetPledgesRequest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerFinance.Infrastructure.OuterApiRequests;
+
+namespace SFA.DAS.EmployerFinance.UnitTests.Infrastructure.OuterApiRequests
+{
+    public class WhenBuildingGetPledgesRequest
+    {
+        [Test]
+        public void Then_The_Url_Is_Correctly_Constructed()
+        {
+            var accountId = 123;
+
+            var actual = new GetPledgesRequest(accountId);
+
+            actual.GetUrl.Should().Be($"Pledges?accountId={accountId}");
+        }
+    }
+}
