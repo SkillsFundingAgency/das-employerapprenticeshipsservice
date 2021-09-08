ce0a70cf7121e8b1b9658494678762cea77dcb64
Shahzad <hkf-tech@users.noreply.github.com>
2021-09-08 10:03:51 +0100

    sql fix

--- src/SFA.DAS.EAS.Employer_Account.Database/Scripts/Manual/CON-3921-DataFixForTermAndConditionsAcceptedOn.sql
+++ src/SFA.DAS.EAS.Employer_Account.Database/Scripts/Manual/CON-3921-DataFixForTermAndConditionsAcceptedOn.sql
@@ -1,11 +1,12 @@
-﻿-- Update TermsAndConditionsAcceptedOn for all users created afer 27th July 2021
-	 Update [employer_account].[User] 
-     Set TermAndConditionsAcceptedOn = GETDATE()
-     where Id in (
-			Select UserId from (
+﻿-- Update TermsAndConditionsAcceptedOn for all users created afer 28th July 2021
+	 Update U 
+     Set U. TermAndConditionsAcceptedOn = T.CreatedDate
+	 From [employer_account].[User] U
+     Inner join (
+			Select UserId, CreatedDate from (
 				Select *, ROW_NUMBER() over (partition by M.UserId order by M.CreatedDate) As Rank2 
 				from [employer_account].[Membership] M
 			) Result 
 			Where Result.Rank2 = 1
-			and CreatedDate >= CONVERT(DATETIME,'27/07/2021',103) --dd/MM/yyyy
-		)
+			and CreatedDate >= CONVERT(DATETIME,'28/07/2021',103) --dd/MM/yyyy
+		) T on U.Id = T.UserId
