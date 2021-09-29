509973251836a56da0e16a76eaeb972894f4cea5
Paul Howes <paul.howes@cognetics.net>
2021-09-29 15:46:01 +0100

    Modified page layout

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/EmployerAgreementController.cs
@@ -292,6 +292,8 @@ namespace SFA.DAS.EmployerAccounts.Web.Controllers
         public async Task<ActionResult> ViewAllAgreements(string hashedAccountId, string accountLegalEntityHashedId)
         {
             var model = await _orchestrator.GetOrganisationAgreements(accountLegalEntityHashedId);
+            ViewBag.AgreementId = accountLegalEntityHashedId;
+
             return View(model);
         }
     }
