0338388b431116f25c537dd5d86d2238c0df4180
chrisfoster76 <chrisfoster186@googlemail.com>
2021-10-07 14:39:56 +0100

    Removes an unused property

--- src/SFA.DAS.EmployerAccounts/Queries/GetTransferConnections/GetTransferConnectionsQuery.cs
+++ src/SFA.DAS.EmployerAccounts/Queries/GetTransferConnections/GetTransferConnectionsQuery.cs
@@ -4,7 +4,6 @@ namespace SFA.DAS.EmployerAccounts.Queries.GetTransferConnections
 {
     public class GetTransferConnectionsQuery : IAsyncRequest<GetTransferConnectionsResponse>
     {
-        public long? AccountId { get; set; }
         public string HashedAccountId { get; set; }
     }
 }
