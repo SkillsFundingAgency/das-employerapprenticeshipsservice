cbbaf8596c2e08393fdbca0e9c881cf60976f535
Paul Howes <paul.howes@cognetics.net>
2022-01-07 13:49:59 +0000

    Update TransferRequestDto.cs

--- src/SFA.DAS.EmployerAccounts/Dtos/TransferRequestDto.cs
+++ src/SFA.DAS.EmployerAccounts/Dtos/TransferRequestDto.cs
@@ -1,5 +1,6 @@
 ﻿using SFA.DAS.CommitmentsV2.Types;
 using System;
+using SFA.DAS.EmployerAccounts.Models.TransferConnections;
 
 namespace SFA.DAS.EmployerAccounts.Dtos
 {
@@ -9,6 +10,7 @@ namespace SFA.DAS.EmployerAccounts.Dtos
         public AccountDto ReceiverAccount { get; set; }
         public AccountDto SenderAccount { get; set; }
         public TransferApprovalStatus Status { get; set; }
+        public TransferConnectionType Type { get; set; }
         public decimal TransferCost { get; set; }
         public string TransferRequestHashedId { get; set; }
     }
