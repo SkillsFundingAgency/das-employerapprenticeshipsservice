509a33ace4b0989b3a28e12df6d826e0dbed67c2
chrisfoster76 <chrisfoster186@googlemail.com>
2022-04-07 09:05:53 +0100

    Adds missing attribute

--- src/SFA.DAS.EmployerFinance.Web/Controllers/TransfersController.cs
+++ src/SFA.DAS.EmployerFinance.Web/Controllers/TransfersController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using SFA.DAS.Authorization.EmployerUserRoles.Options;
 using SFA.DAS.Authorization.Mvc.Attributes;
 using SFA.DAS.EmployerFinance.Web.Orchestrators;
 
 namespace SFA.DAS.EmployerFinance.Web.Controllers
 {
-    [DasAuthorize("EmployerFeature.TransfersMatching")]
+    [DasAuthorize(EmployerUserRole.Any)]
     [RoutePrefix("accounts/{HashedAccountId}")] public class TransfersController : Controller
     {
         private readonly TransfersOrchestrator _transfersOrchestrator;
