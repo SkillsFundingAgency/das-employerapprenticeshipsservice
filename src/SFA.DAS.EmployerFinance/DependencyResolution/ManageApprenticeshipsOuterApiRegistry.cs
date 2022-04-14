e1fa3d40d385de3393ce015e5df1dbdde60b92b7
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-04-14 14:39:38 +0100

    Tidy up unused using

--- src/SFA.DAS.EmployerFinance/DependencyResolution/ManageApprenticeshipsOuterApiRegistry.cs
+++ src/SFA.DAS.EmployerFinance/DependencyResolution/ManageApprenticeshipsOuterApiRegistry.cs
@@ -2,7 +2,6 @@ using System.Net.Http;
 using SFA.DAS.EmployerFinance.Configuration;
 using SFA.DAS.EmployerFinance.Infrastructure;
 using SFA.DAS.EmployerFinance.Interfaces.OuterApi;
-using SFA.DAS.EmployerFinance.Services;
 using StructureMap;
 
 namespace SFA.DAS.EmployerFinance.DependencyResolution
