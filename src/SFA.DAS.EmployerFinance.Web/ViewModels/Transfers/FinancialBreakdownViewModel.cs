1ffe21017825432cf81c18e7d83cdef5367c9bad
Jawwad Baig <jawwad.baig@digital.education.gov.uk>
2022-04-24 19:38:38 +0100

    TM-302 - Refactoring after Tech Review

--- src/SFA.DAS.EmployerFinance.Web/ViewModels/Transfers/FinancialBreakdownViewModel.cs
+++ src/SFA.DAS.EmployerFinance.Web/ViewModels/Transfers/FinancialBreakdownViewModel.cs
@@ -7,14 +7,12 @@ namespace SFA.DAS.EmployerFinance.Web.ViewModels.Transfers
         public decimal StartingTransferAllowance { get; set; }
         public string FinancialYearString { get; set; }
         public string HashedAccountID { get; set; }
-        public decimal TransferConnections { get; set; }
-        public decimal FundsIn { get; set; }
+        public decimal TransferConnections { get; set; }        
         public decimal Commitments { get; set; }
         public decimal ApprovedPledgeApplications { get; set; }
         public decimal AcceptedPledgeApplications { get; set; }
         public decimal PledgeOriginatedCommitments { get; set; }
-        public DateTime ProjectionStartDate { get; set; }
-        public int NumberOfMonths { get; set; }
+        public DateTime ProjectionStartDate { get; set; }        
         public decimal TotalAvailableTransferAllowance { get { return StartingTransferAllowance - Commitments; } }
         public decimal TotalEstimatedSpend { get { return ApprovedPledgeApplications + AcceptedPledgeApplications + TransferConnections; } }
         public decimal EstimatedRemainingAllowance { get { return TotalAvailableTransferAllowance - TotalEstimatedSpend; } }
--- src/SFA.DAS.EmployerFinance/Infrastructure/OuterApiResponses/Transfers/GetFinancialBreakdownResponse.cs
+++ src/SFA.DAS.EmployerFinance/Infrastructure/OuterApiResponses/Transfers/GetFinancialBreakdownResponse.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerFinance.Infrastructure.OuterApiResponses.Transfers
 {
@@ -12,11 +8,7 @@ namespace SFA.DAS.EmployerFinance.Infrastructure.OuterApiResponses.Transfers
         public decimal ApprovedPledgeApplications { get; set; }
         public decimal AcceptedPledgeApplications { get; set; }
         public decimal PledgeOriginatedCommitments { get; set; }
-        public decimal TransferConnections { get; set; }
-        public decimal FundsIn { get; set; }
-        public int NumberOfMonths { get; set; }
+        public decimal TransferConnections { get; set; }        
         public DateTime ProjectionStartDate { get; set;}
-
-
     }
 }
