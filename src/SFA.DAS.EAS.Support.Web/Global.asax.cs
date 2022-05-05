f57b7180522f49a998ee73d7b069088e41870ed8
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-05-05 14:04:15 +0100

    CON-4716 - Support EAS to accept MI tokens

--- src/SFA.DAS.EAS.Support.Web/Global.asax.cs
+++ src/SFA.DAS.EAS.Support.Web/Global.asax.cs
@@ -20,9 +20,9 @@ namespace SFA.DAS.EAS.Support.Web
         private void Application_Start(object sender, EventArgs e)
         {
             MvcHandler.DisableMvcResponseHeader = true;
-            var ioc = DependencyResolver.Current;
-            var logger = ioc.GetService<ILog>();
-            logger.Info("Starting Web Role");
+            //var ioc = DependencyResolver.Current;
+            //var logger = ioc.GetService<ILog>();
+            //logger.Info("Starting Web Role");
 
             SetupApplicationInsights();
 
@@ -30,11 +30,11 @@ namespace SFA.DAS.EAS.Support.Web
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            var siteConnectorSettings = ioc.GetService<ISiteValidatorSettings>();
-            GlobalConfiguration.Configuration.MessageHandlers.Add(new TokenValidationHandler(siteConnectorSettings, logger));
-            GlobalFilters.Filters.Add(new TokenValidationFilter(siteConnectorSettings, logger));
+            //var siteConnectorSettings = ioc.GetService<ISiteValidatorSettings>();
+            //GlobalConfiguration.Configuration.MessageHandlers.Add(new TokenValidationHandler(siteConnectorSettings, logger));
+            //GlobalFilters.Filters.Add(new TokenValidationFilter(siteConnectorSettings, logger));
             
-            logger.Info("Web role started");
+            //logger.Info("Web role started");
         }
         protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
         {
