a5c47f4394ffe4c5383444fd99bb8df4b5c750b6
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-05-26 10:31:42 +0100

    register finance api config from EmployerFinanceConfiguration. Rather than a client rowkey

--- src/SFA.DAS.EmployerFinance.Api.Client/EmployerFinanceApiClientRegistry.cs
+++ src/SFA.DAS.EmployerFinance.Api.Client/EmployerFinanceApiClientRegistry.cs
@@ -1,13 +1,15 @@
-﻿using SFA.DAS.AutoConfiguration;
+﻿using System;
+using System.Linq.Expressions;
 using StructureMap;
 
 namespace SFA.DAS.EmployerFinance.Api.Client
 {
     public class EmployerFinanceApiClientRegistry : Registry
     {
-        public EmployerFinanceApiClientRegistry()
+        public EmployerFinanceApiClientRegistry(Expression<Func<IContext, EmployerFinanceApiClientConfiguration>> getApiConfig)
         {
-            For<EmployerFinanceApiClientConfiguration>().Use(c => c.GetInstance<IAutoConfigurationService>().Get<EmployerFinanceApiClientConfiguration>("SFA.DAS.EmployerFinance.Api.Client")).Singleton();
+            For<EmployerFinanceApiClientConfiguration>().Use(getApiConfig);
+            For<IEmployerFinanceApiClientConfiguration>().Use<EmployerFinanceApiClientConfiguration>();
             For<IEmployerFinanceApiClient>().Use<EmployerFinanceApiClient>();
         }
     }
