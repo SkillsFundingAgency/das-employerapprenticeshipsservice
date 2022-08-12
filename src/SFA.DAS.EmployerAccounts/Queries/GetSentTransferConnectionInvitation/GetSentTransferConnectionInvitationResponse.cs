2535e2ae4cb3a5d2b091fd9d3fe7a3b8c8977c4b
Chris Woodcock <chris@humesoftware.com>
2022-08-12 15:56:35 +0100

    Removed transfer connections code from Employer Accounts

--- src/SFA.DAS.EmployerAccounts/Queries/GetSentTransferConnectionInvitation/GetSentTransferConnectionInvitationResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.EmployerAccounts.Dtos;
-
-namespace SFA.DAS.EmployerAccounts.Queries.GetSentTransferConnectionInvitation
-{
-    public class GetSentTransferConnectionInvitationResponse
-    {
-        public TransferConnectionInvitationDto TransferConnectionInvitation { get; set; }
-    }
-}
