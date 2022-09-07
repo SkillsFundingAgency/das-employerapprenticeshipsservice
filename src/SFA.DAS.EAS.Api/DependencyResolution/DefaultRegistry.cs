cbb7ca587f6f7183f7442f6f8bcb9be5d24fa44d
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-09-07 16:04:14 +0100

    MAC-129 - Resolved PR comment

--- src/SFA.DAS.EAS.Api/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.EAS.Api/DependencyResolution/DefaultRegistry.cs
@@ -1,18 +1,10 @@
 using System;
-using System.Configuration;
-using System.Data.Common;
-using System.Data.SqlClient;
-using Microsoft.Azure.Services.AppAuthentication;
-using SFA.DAS.EAS.Domain.Configuration;
-using SFA.DAS.EAS.Infrastructure.Data;
 using StructureMap;
 
 namespace SFA.DAS.EAS.Account.Api.DependencyResolution
 {
     public class DefaultRegistry : Registry
     {
-        private const string AzureResource = "https://database.windows.net/";
-
         public DefaultRegistry()
         {
             Scan(s =>
@@ -20,35 +12,6 @@ namespace SFA.DAS.EAS.Account.Api.DependencyResolution
                 s.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith("SFA.DAS"));
                 s.RegisterConcreteTypesAgainstTheFirstInterface();
             });
-
-            var environmentName = ConfigurationManager.AppSettings["EnvironmentName"];            
-
-            For<DbConnection>().Use($"Build DbConnection", c =>
-            {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-
-                return environmentName.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase)
-                    ? new SqlConnection(GetEmployerAccountsConnectionString(c))
-                    : new SqlConnection
-                    {
-                        ConnectionString = GetEmployerAccountsConnectionString(c),
-                        AccessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result
-                    };
-            });
-
-           
-            For<EmployerAccountsDbContext>().Use(c => GetAcccountsDbContext(c));
-        }
-
-        private EmployerAccountsDbContext GetAcccountsDbContext(IContext context)
-        {
-            var db = new EmployerAccountsDbContext(context.GetInstance<DbConnection>());
-            return db;
-        }
-
-        private string GetEmployerAccountsConnectionString(IContext context)
-        {
-            return context.GetInstance<EmployerApprenticeshipsServiceConfiguration>().DatabaseConnectionString;
-        }
+        }      
     }
 }
