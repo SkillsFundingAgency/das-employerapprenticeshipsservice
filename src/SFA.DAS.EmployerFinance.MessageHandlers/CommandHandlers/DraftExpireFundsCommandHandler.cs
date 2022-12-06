2e6d91db0e62630994aa1431dc35bdeb7127c8ef
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-12-06 09:24:54 +0000

    require immediate dispatch and catch error and don't retry.

--- src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/DraftExpireFundsCommandHandler.cs
+++ src/SFA.DAS.EmployerFinance.MessageHandlers/CommandHandlers/DraftExpireFundsCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using NServiceBus;
 using SFA.DAS.EmployerFinance.Data;
@@ -22,36 +23,43 @@ namespace SFA.DAS.EmployerFinance.MessageHandlers.CommandHandlers
         }
         public async Task Handle(DraftExpireFundsCommand message, IMessageHandlerContext context)
         {
-            var now = _currentDateTime.Now;
-            var accounts = await _accountRepository.GetAll();
-            
-            var messageTasks = new List<Task>();
-            var sendCounter = 0;
-
-            _logger.Info($"Queueing {nameof(ExpireAccountFundsCommand)} messages for {accounts.Count} accounts.");
-
-            foreach (var account in accounts)
+            try
             {
-                var sendOptions = new SendOptions();
-                sendOptions.RouteToThisEndpoint();
+                var now = _currentDateTime.Now;
+                var accounts = await _accountRepository.GetAll();
 
-                messageTasks.Add(context.Send(new DraftExpireAccountFundsCommand { AccountId = account.Id, DateTo = message.DateTo }, sendOptions));
-                sendCounter++;
+                var messageTasks = new List<Task>();
+                var sendCounter = 0;
 
-                if (sendCounter % 1000 == 0)
+                _logger.Info($"Queueing {nameof(DraftExpireAccountFundsCommand)} messages for {accounts.Count} accounts.");
+
+                foreach (var account in accounts)
                 {
-                    await Task.WhenAll(messageTasks);
-                    _logger.Info($"Queued {sendCounter} of {accounts.Count} messages.");
-                    messageTasks.Clear();
-                    await Task.Delay(500);
+                    var sendOptions = new SendOptions();
+                    sendOptions.RouteToThisEndpoint();
+                    sendOptions.RequiredImmediateDispatch();
+
+                    messageTasks.Add(context.Send(new DraftExpireAccountFundsCommand { AccountId = account.Id, DateTo = message.DateTo }, sendOptions));
+                    sendCounter++;
+
+                    if (sendCounter % 1000 == 0)
+                    {
+                        await Task.WhenAll(messageTasks).ConfigureAwait(false); ;
+                        _logger.Info($"Queued {sendCounter} of {accounts.Count} messages.");
+                        messageTasks.Clear();
+                        await Task.Delay(500).ConfigureAwait(false); ;
+                    }
                 }
+
+                // await final tasks not % 1000
+                await Task.WhenAll(messageTasks).ConfigureAwait(false);
+
+                _logger.Info($"{nameof(DraftExpireFundsCommandHandler)} completed.");
             }
-
-            // await final tasks not % 1000
-            await Task.WhenAll(messageTasks);
-
-            _logger.Info($"{nameof(DraftExpireFundsCommandHandler)} completed.");
-
+            catch(Exception ex)
+            {
+                _logger.Error(ex, $"{nameof(DraftExpireFundsCommandHandler)} failed");
+            }
         }
     }
 }
