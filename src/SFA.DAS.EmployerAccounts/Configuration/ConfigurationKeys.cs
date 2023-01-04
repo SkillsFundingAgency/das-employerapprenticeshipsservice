6b60a001513453adf7cffd0cff361e68893a3692
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-04 14:15:08 +0000

    Removed unused config key references not used in solution

--- src/SFA.DAS.EmployerAccounts/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerAccounts/Configuration/ConfigurationKeys.cs
@@ -3,14 +3,11 @@
     public static class ConfigurationKeys
     {
         public const string EmployerAccounts = "SFA.DAS.EmployerAccounts";
-        public const string Features = "SFA.DAS.EmployerApprenticeshipsService.FeaturesV2";
-        public const string ReferenceDataApiClient = "SFA.DAS.ReferenceDataApiClient";
-        public const string NotificationsApiClient = "SFA.DAS.EmployerAccounts.Notifications";
-        public const string CommitmentsApiClient = "SFA.DAS.CommitmentsAPI";
-        public const string EmployerFinance = "SFA.DAS.EmployerFinance";
         public const string EmployerAccountsReadStore = "SFA.DAS.EmployerAccounts.ReadStore";
+        public const string NotificationsApiClient = "SFA.DAS.EmployerAccounts.Notifications";
+        public const string Features = "SFA.DAS.EmployerApprenticeshipsService.FeaturesV2";
         public const string AuditApi = "SFA.DAS.AuditApiClient";
-        public const string ReservationsClientApiConfiguration = "SFA.DAS.Reservations.Api.Client";
+        public const string ReferenceDataApiClient = "SFA.DAS.ReferenceDataApiClient";
         public const string EncodingConfig = "SFA.DAS.Encoding";
     }
 }
