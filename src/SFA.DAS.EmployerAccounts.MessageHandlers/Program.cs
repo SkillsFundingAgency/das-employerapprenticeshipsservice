9b725c2c210505d219257ec2834a70e9b248dc90
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-10 14:02:59 +0000

    Refactoring MessageHandlers for Net6

--- src/SFA.DAS.EmployerAccounts.MessageHandlers/Program.cs
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers/Program.cs
@@ -1,65 +1,72 @@
 ﻿using System.Data.Common;
 using System.Net;
-using Microsoft.Azure.WebJobs;
-using NServiceBus;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
 using SFA.DAS.AutoConfiguration;
 using SFA.DAS.EmployerAccounts.Configuration;
 using SFA.DAS.EmployerAccounts.Extensions;
 using SFA.DAS.EmployerAccounts.MessageHandlers.DependencyResolution;
+using SFA.DAS.NServiceBus.Configuration;
 using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
 using SFA.DAS.NServiceBus.Configuration.NLog;
-using SFA.DAS.NServiceBus.SqlServer.Configuration;
 using SFA.DAS.NServiceBus.Configuration.StructureMap;
-using SFA.DAS.UnitOfWork.NServiceBus;
-using StructureMap;
-using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.SqlServer.Configuration;
 using SFA.DAS.UnitOfWork.NServiceBus.Configuration;
+using StructureMap;
 
 namespace SFA.DAS.EmployerAccounts.MessageHandlers
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             var container = IoC.Initialize();
 
             var isDevelopment = container.GetInstance<IEnvironmentService>().IsCurrent(DasEnv.LOCAL);
 
-            var config = new JobHostConfiguration();
+            var hostBuilder = new HostBuilder();
+            hostBuilder.ConfigureWebJobs();
 
             if (isDevelopment)
             {
-                config.UseDevelopmentSettings();
+                hostBuilder.UseEnvironment("development");
             }
 
-            var host = new JobHost(config);
+            using var host = hostBuilder.Build();
 
-            host.Call(typeof(Program).GetMethod(nameof(AsyncMain)), new { isDevelopment, container });
-            host.RunAndBlock();
+            var jobHost = host.Services.GetService(typeof(IJobHost)) as JobHost;
+
+            await host.StartAsync();
+            await jobHost.CallAsync(typeof(Program).GetMethod(nameof(AsyncMain)), new
+            {
+                container
+            });
+            await host.RunAsync();
         }
 
         [NoAutomaticTrigger]
-        public static async Task AsyncMain(CancellationToken cancellationToken, bool isDevelopment, IContainer container)
+        public static async Task AsyncMain(CancellationToken cancellationToken, IContainer container)
         {
             var endpointConfiguration = new EndpointConfiguration("SFA.DAS.EmployerAccounts.MessageHandlers")
                 .UseAzureServiceBusTransport(() => container.GetInstance<EmployerAccountsConfiguration>().ServiceBusConnectionString, container)
                 .UseErrorQueue("SFA.DAS.EmployerAccounts.MessageHandlers-errors")
                 .UseInstallers()
                 .UseLicense(WebUtility.HtmlDecode(container.GetInstance<EmployerAccountsConfiguration>().NServiceBusLicense))
-                .UseSqlServerPersistence(() => container.GetInstance<DbConnection>())
+                .UseSqlServerPersistence(container.GetInstance<DbConnection>)
                 .UseNewtonsoftJsonSerializer()
                 .UseNLogFactory()
                 .UseOutbox()
                 .UseStructureMapBuilder(container)
                 .UseUnitOfWork();
-       
+
             var endpoint = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
 
-            container.Configure(c =>
+            container.Configure(x =>
             {
-                c.For<IMessageSession>().Use(endpoint);
+                x.For<IMessageSession>().Use(endpoint);
             });
 
             while (!cancellationToken.IsCancellationRequested)
