2faef09eb23114154f1765a048a2bc58bd75afa5
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-16 13:39:32 +0000

    No need to explicitly register MultiInstanceFactory and SingleInstanceFactory for Mediatr

--- src/SFA.DAS.EmployerAccounts/DependencyResolution/MediatorRegistry.cs
+++ src/SFA.DAS.EmployerAccounts/DependencyResolution/MediatorRegistry.cs
@@ -5,7 +5,5 @@ public class MediatorRegistry : Registry
     public MediatorRegistry()
     {
         For<IMediator>().Use<Mediator>();
-        For<MultiInstanceFactory>().Use<MultiInstanceFactory>(c => c.GetAllInstances);
-        For<SingleInstanceFactory>().Use<SingleInstanceFactory>(c => c.GetInstance);
     }
 }
