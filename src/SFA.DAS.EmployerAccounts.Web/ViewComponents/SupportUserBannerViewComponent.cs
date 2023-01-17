51899fcf377309c000b9d6836df19867e7a474a6
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-17 10:13:37 +0000

    Implementing ChildAction SupportUserBanner as a ViewComponent

---
+++ src/SFA.DAS.EmployerAccounts.Web/ViewComponents/SupportUserBannerViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using SFA.DAS.EmployerAccounts.Helpers;
+
+namespace SFA.DAS.EmployerAccounts.Web.ViewComponents;
+
+public class SupportUserBannerViewComponent : ViewComponent
+{
+    private readonly EmployerTeamOrchestrator _employerTeamOrchestrator;
+
+    public SupportUserBannerViewComponent(EmployerTeamOrchestrator employerTeamOrchestrator)
+    {
+        _employerTeamOrchestrator = employerTeamOrchestrator;
+    }
+
+    public IViewComponentResult Invoke(IAccountIdentifier model = null)
+    {
+        Account account = null;
+
+        if (model != null && model.HashedAccountId != null)
+        {
+            var externalUserId = OwinWrapper.GetClaimValue(ControllerConstants.UserRefClaimKeyName);
+            var response = AsyncHelper.RunSync(() => _employerTeamOrchestrator.GetAccountSummary(model.HashedAccountId, externalUserId));
+            account = response.Status != HttpStatusCode.OK ? null : response.Data.Account;
+        }
+
+        var consoleUserType = OwinWrapper.GetClaimValue(ClaimTypes.Role) == "Tier2User" ? "Service user (T2 Support)" : "Standard user";
+
+        return View(new SupportUserBannerViewModel
+        {
+            Account = account,
+            ConsoleUserType = consoleUserType
+        });
+    }
+}
