ac366d26ad1db9a7c5ae9a74308cd267a0960961
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-17 12:17:10 +0000

    Refactor to SupportUserBanner and implementations

--- src/SFA.DAS.EmployerAccounts.Web/Controllers/SupportErrorController.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Controllers/SupportErrorController.cs
@@ -19,20 +19,4 @@ public class SupportErrorController : BaseController
         AccessDeniedViewModel model = new AccessDeniedViewModel { HashedAccountId = hashedAccountId };
         return View(model);
     }
-
-    [ChildActionOnly]
-    public override IActionResult SupportUserBanner(IAccountIdentifier model = null)
-    {
-        EmployerAccounts.Models.Account.Account account = null;
-
-        if (model != null && model.HashedAccountId != null)
-        {
-            var externalUserId = OwinWrapper.GetClaimValue(ControllerConstants.UserRefClaimKeyName);
-            var response = AsyncHelper.RunSync(() => _orchestrator.GetAccountSummary(model.HashedAccountId, externalUserId));
-
-            account = response.Status != HttpStatusCode.OK ? null : response.Data.Account;
-        }
-
-        return PartialView("_SupportUserBanner", new SupportUserBannerViewModel() { Account = account });
-    }
 }
