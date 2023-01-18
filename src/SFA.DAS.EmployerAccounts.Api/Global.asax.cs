2fa7c1a0e9369581da374968bd55df256bb2733a
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-18 10:27:52 +0000

    Commenting out old Global.asax classes to delete later

--- src/SFA.DAS.EmployerAccounts.Api/Global.asax.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Global.asax.cs
@@ -1,36 +1,36 @@
-﻿using System.Configuration;
-using System.Web;
-using System.Web.Http;
-using Microsoft.ApplicationInsights.Extensibility;
-using NServiceBus;
-using StructureMap;
-using WebApi.StructureMap;
+﻿//using System.Configuration;
+//using System.Web;
+//using System.Web.Http;
+//using Microsoft.ApplicationInsights.Extensibility;
+//using NServiceBus;
+//using StructureMap;
+//using WebApi.StructureMap;
 
-namespace SFA.DAS.EmployerAccounts.Api
-{
-    public class WebApiApplication : HttpApplication
-    {
-        private IEndpointInstance _endpoint;
-        private ServiceBusEndPointConfigureAndRun _serviceBusEndpointManager;
+//namespace SFA.DAS.EmployerAccounts.Api
+//{
+//    public class WebApiApplication : HttpApplication
+//    {
+//        private IEndpointInstance _endpoint;
+//        private ServiceBusEndPointConfigureAndRun _serviceBusEndpointManager;
 
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+//        protected void Application_Start()
+//        {
+//            GlobalConfiguration.Configure(WebApiConfig.Register);
             
-            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
+//            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
 
-            _serviceBusEndpointManager = 
-                new ServiceBusEndPointConfigureAndRun(
-                GlobalConfiguration.Configuration.DependencyResolver.GetService<IContainer>());
+//            _serviceBusEndpointManager = 
+//                new ServiceBusEndPointConfigureAndRun(
+//                GlobalConfiguration.Configuration.DependencyResolver.GetService<IContainer>());
 
-            _serviceBusEndpointManager
-                .ConfigureAndStartServiceBusEndpoint();
-        }
+//            _serviceBusEndpointManager
+//                .ConfigureAndStartServiceBusEndpoint();
+//        }
 
-        protected void Application_End()
-        {
-            _serviceBusEndpointManager?
-                .StopServiceBusEndpoint();
-        }
-    }
-}
+//        protected void Application_End()
+//        {
+//            _serviceBusEndpointManager?
+//                .StopServiceBusEndpoint();
+//        }
+//    }
+//}
