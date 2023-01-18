d9b97ba2e4e2043f1677c37627387c4e54d1c608
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-18 13:55:08 +0000

    Missing class update

--- src/SFA.DAS.EmployerAccounts.Web/Filters/AnalyticsFilter.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Filters/AnalyticsFilter.cs
@@ -1,72 +1,29 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-using SFA.DAS.EmployerAccounts.Web.Controllers;
-using SFA.DAS.EmployerAccounts.Web.ViewModels;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using SFA.DAS.EmployerAccounts.Web.Extensions;
+using SFA.DAS.EmployerAccounts.Web.Infrastructure;
 
 namespace SFA.DAS.EmployerAccounts.Web.Filters
 {
     public class AnalyticsFilter : Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute
     {
-        public override void OnActionExecuting(Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext filterContext)
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            string userId = null;
-            string hashedAccountId = null;
-            string agreementId = null;
-            string userEmail = null;
-            string userName = null;
-
-            var thisController = filterContext.Controller as BaseController;
-            if (thisController != null)
+            var controller = filterContext.Controller as Controller;
+            if (controller != null)
             {
-                userId = thisController.OwinWrapper.GetClaimValue(@"sub");
-                userEmail = thisController.OwinWrapper.GetClaimValue(@"email");
-                userName = $"{thisController.OwinWrapper.GetClaimValue(@"firstname")} {thisController.OwinWrapper.GetClaimValue(@"lastname")}";
+                var user = controller.User;
+                var userId = user?.GetUserId();
+                controller.ViewBag.GaData = new GaData
+                {
+                    UserId = userId,
+                    Acc = controller.RouteData.Values[RouteValues.EncodedAccountId]?.ToString().ToUpper()
+                };
             }
-
-            if (filterContext.ActionParameters.ContainsKey("hashedAccountId"))
-            {
-                hashedAccountId = filterContext.ActionParameters["hashedAccountId"] as string;
-            }
-
-            if (filterContext.ActionParameters.ContainsKey("agreementId"))
-            {
-                agreementId = filterContext.ActionParameters["agreementId"] as string;
-            }
-
-            Microsoft.AspNetCore.Mvc.Controller.ViewBag.GaData = new GaData
-            {
-                UserId = userId,
-                Acc = hashedAccountId,
-                AgreementId = agreementId,
-                UserEmail = userEmail,
-                UserName = userName
-            };
-
             base.OnActionExecuting(filterContext);
         }
 
-        public override void OnActionExecuted(Microsoft.AspNetCore.Mvc.Filters.ActionExecutedContext filterContext)
-        {
-            if (!(filterContext?.Controller?.ViewBag?.GaData is GaData))
-            {
-                base.OnActionExecuted(filterContext);
-                return;
-            }
-
-            (Microsoft.AspNetCore.Mvc.Controller.ViewBag.GaData as GaData).LevyFlag 
-                = (filterContext.Controller.ViewData.Model as OrchestratorResponse<AccountDashboardViewModel>)?.Data?.ApprenticeshipEmployerType.ToString();
-
-            base.OnActionExecuted(filterContext);
-        }
-
-        public string DataLoaded { get; set; }
-
         public class GaData
         {
-            public GaData()
-            {
-
-            }
             public string DataLoaded { get; set; } = "dataLoaded";
             public string UserId { get; set; }
             public string UserEmail { get; set; }
@@ -77,8 +34,6 @@ namespace SFA.DAS.EmployerAccounts.Web.Filters
 
             public string AgreementId { get; set; }
             public string LevyFlag { get; set; }
-
-            public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
         }
     }
 }
