e172e88d363a168910a3453d946719e5baceaa73
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-18 16:08:32 +0000

    tiding up and removing obsolete classes

--- src/SFA.DAS.EmployerAccounts.Web/App_Start/EmployerAccountPostAuthenticationClaimsHandler.cs
+++ src/SFA.DAS.EmployerAccounts.Web/App_Start/EmployerAccountPostAuthenticationClaimsHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -77,5 +78,4 @@ public class EmployerAccountPostAuthenticationClaimsHandler : ICustomClaims
 
         return claims;
     }
-}
 }
--- src/SFA.DAS.EmployerAccounts/Infrastructure/EmployerClaims.cs
+++ src/SFA.DAS.EmployerAccounts/Infrastructure/EmployerClaims.cs
@@ -4,5 +4,6 @@ public static class EmployerClaims
 {
     public static string IdamsUserIdClaimTypeIdentifier => "http://das/employer/identity/claims/id";
     public static string AccountsClaimsTypeIdentifier => "http://das/employer/identity/claims/associatedAccounts";
-    public static string EmployerEmailClaimsTypeIdentifier => "http://das/employer/identity/claims/email_address";
+    public static string IdamsUserDisplayNameClaimTypeIdentifier = "http://das/employer/identity/claims/display_name";
+    public static string IdamsUserEmailClaimTypeIdentifier = "http://das/employer/identity/claims/email_address";
 }
