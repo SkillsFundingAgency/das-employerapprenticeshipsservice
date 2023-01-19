79fdea21c96a874a83744d4a81465c700b5930da
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-19 12:48:39 +0000

    Typo

--- src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/ExecutionPolicyServiceRegistrations.cs
+++ src/SFA.DAS.EmployerAccounts.Web/StartupExtensions/ExecutionPolicyServiceRegistrations.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.EmployerAccounts.Web.StartupExtensions;
 
 public static class ExecutionPolicyServiceRegistrations
 {
-    public static IServiceCollection AddExectionPolicies(this IServiceCollection services)
+    public static IServiceCollection AddExecutionPolicies(this IServiceCollection services)
     {
         services.AddTransient<ExecutionPolicy, HmrcExecutionPolicy>();
 
