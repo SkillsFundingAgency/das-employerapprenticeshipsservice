77cf8aae0d372f48a25b0c4b33a69213b135695d
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-19 08:17:37 +0000

    Updating tests for OuterApi

---
+++ src/SFA.DAS.EmployerAccounts.UnitTests/Infrastructure/OuterApi/Requests/Reservations/WhenBuildingGetReservationsRequest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Infrastructure.OuterApi.Requests.Reservations;
+
+namespace SFA.DAS.EmployerAccounts.UnitTests.Infrastructure.OuterApi.Requests.Reservations;
+
+public class WhenBuildingGetReservationsRequest
+{
+    [Test]
+    public void Then_The_Url_Is_Correctly_Constructed()
+    {
+        long accountId = 123;
+
+        var actual = new GetReservationsRequest(accountId);
+
+        actual.GetUrl.Should().Be($"reservation/{accountId}");
+    }
+}
