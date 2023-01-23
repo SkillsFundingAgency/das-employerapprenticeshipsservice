7e0a5810a8fcb2baf926399411f734e24503f35e
Jon Harris <jon.harris@digital.education.gov.uk>
2023-01-23 12:25:27 +0000

    Variable naming fix

--- src/SFA.DAS.EmployerAccounts/Services/CookieStorageService.cs
+++ src/SFA.DAS.EmployerAccounts/Services/CookieStorageService.cs
@@ -5,31 +5,31 @@ namespace SFA.DAS.EmployerAccounts.Services;
 public class CookieStorageService<T> : ICookieStorageService<T>
 {
     private readonly ICookieService<T> _cookieService;
-    private readonly HttpContextAccessor httpContextAccessor;
+    private readonly HttpContextAccessor _httpContextAccessor;
 
     public CookieStorageService(ICookieService<T> cookieService, HttpContextAccessor httpContextAccessor)
     {
         _cookieService = cookieService;
-        httpContextAccessor = httpContextAccessor;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public void Create(T item, string cookieName, int expiryDays = 1)
     {
-        _cookieService.Create(httpContextAccessor, cookieName, item, expiryDays);
+        _cookieService.Create(_httpContextAccessor, cookieName, item, expiryDays);
     }
 
     public T Get(string cookieName)
     {
-        return _cookieService.Get(httpContextAccessor, cookieName);
+        return _cookieService.Get(_httpContextAccessor, cookieName);
     }
 
     public void Delete(string cookieName)
     {
-        _cookieService.Delete(httpContextAccessor, cookieName);
+        _cookieService.Delete(_httpContextAccessor, cookieName);
     }
 
     public void Update(string cookieName, T item)
     {
-        _cookieService.Update(httpContextAccessor, cookieName, item);
+        _cookieService.Update(_httpContextAccessor, cookieName, item);
     }
 }
