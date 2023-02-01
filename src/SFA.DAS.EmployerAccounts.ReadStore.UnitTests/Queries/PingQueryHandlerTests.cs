c7016618ed840fa8fe88f8445972b74d89f3d78d
Jon Harris <jon.harris@digital.education.gov.uk>
2023-02-01 12:14:12 +0000

    Refactoring tests due to bugs introduced in upgrade to Net6

--- src/SFA.DAS.EmployerAccounts.ReadStore.UnitTests/Queries/PingQueryHandlerTests.cs
+++ src/SFA.DAS.EmployerAccounts.ReadStore.UnitTests/Queries/PingQueryHandlerTests.cs
@@ -23,7 +23,7 @@ namespace SFA.DAS.EmployerAccounts.ReadStore.UnitTests.Queries
         {
             return TestExceptionAsync(
                 f => f.Handle(),
-                (f, r) => r.ShouldNotThrow<Exception>());
+                (f, r) => r.Should().NotThrowAsync<Exception>());
         }
 
         [Test]
@@ -32,7 +32,7 @@ namespace SFA.DAS.EmployerAccounts.ReadStore.UnitTests.Queries
             return TestExceptionAsync(
                 f => f.SetPingFailure(), 
                 f => f.Handle(),
-                (f, r) => r.ShouldThrow<Exception>().WithMessage("Read store database ping failed"));
+                (f, r) => r.Should().ThrowAsync<Exception>().WithMessage("Read store database ping failed"));
         }
     }
 
