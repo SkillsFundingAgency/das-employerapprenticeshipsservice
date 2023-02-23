55b11695fbb96e36dd3c18afa40fc661f0e9f705
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-02-23 16:24:20 +0000

    use httpContextAccessor in URLAction helper

--- src/SFA.DAS.EmployerAccounts.Web/Helpers/UrlActionHelper.cs
+++ src/SFA.DAS.EmployerAccounts.Web/Helpers/UrlActionHelper.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
-
-namespace SFA.DAS.EmployerAccounts.Web.Helpers;
+﻿namespace SFA.DAS.EmployerAccounts.Web.Helpers;
 
 public class UrlActionHelper : IUrlActionHelper
 {
     private readonly EmployerAccountsConfiguration _configuration;
-    private readonly IActionContextAccessor _actionContextAccessor;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public UrlActionHelper(EmployerAccountsConfiguration configuration, IActionContextAccessor actionContextAccessor)
+    public UrlActionHelper(EmployerAccountsConfiguration configuration, IHttpContextAccessor httpContextAccessor)
     {
         _configuration = configuration;
-        _actionContextAccessor = actionContextAccessor;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public string EmployerAccountsAction(string path)
@@ -51,7 +49,7 @@ public class UrlActionHelper : IUrlActionHelper
     public string EmployerIncentivesAction(string path = "")
     {
         var baseUrl = _configuration.EmployerIncentivesBaseUrl;
-        var hashedAccountId = _actionContextAccessor.ActionContext.RouteData.Values[ControllerConstants.AccountHashedIdRouteKeyName];
+        var hashedAccountId = _httpContextAccessor.HttpContext.Request.RouteValues.GetValueOrDefault(ControllerConstants.AccountHashedIdRouteKeyName);
         return Action(baseUrl, $"{hashedAccountId}/{path}");
     }
 
@@ -92,7 +90,7 @@ public class UrlActionHelper : IUrlActionHelper
 
     private string AccountAction(string baseUrl, string path)
     {
-        var hashedAccountId = _actionContextAccessor.ActionContext.RouteData.Values[ControllerConstants.AccountHashedIdRouteKeyName];
+        var hashedAccountId = _httpContextAccessor.HttpContext.Request.RouteValues.GetValueOrDefault(ControllerConstants.AccountHashedIdRouteKeyName);
         var accountPath = hashedAccountId == null ? $"accounts/{path}" : $"accounts/{hashedAccountId}/{path}";
 
         return Action(baseUrl, accountPath);
@@ -103,7 +101,7 @@ public class UrlActionHelper : IUrlActionHelper
     private string NonAccountsAction(string baseUrl, string path)
     {
 
-        var hashedAccountId = _actionContextAccessor.ActionContext.RouteData.Values[ControllerConstants.AccountHashedIdRouteKeyName];
+        var hashedAccountId = _httpContextAccessor.HttpContext.Request.RouteValues.GetValueOrDefault(ControllerConstants.AccountHashedIdRouteKeyName);
         var commitmentPath = hashedAccountId == null ? $"{path}" : $"{hashedAccountId}/{path}";
 
         return Action(baseUrl, commitmentPath);
