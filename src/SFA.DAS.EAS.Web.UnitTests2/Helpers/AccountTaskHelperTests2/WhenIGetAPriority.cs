f065ad7209c17cf1812b6ca52b085305bdc8d60a
Jason Alexander <jasonalexander.dev@gmail.com>
2023-03-09 19:31:17 +0000

    All web test projects working

---
+++ src/SFA.DAS.EAS.Web.UnitTests2/Helpers/AccountTaskHelperTests2/WhenIGetAPriority.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using SFA.DAS.EAS.Domain.Models.Account;
+using SFA.DAS.EAS.Web.Helpers;
+
+namespace SFA.DAS.EAS.Web.UnitTests.Helpers.AccountTaskHelperTests
+{
+    [TestFixture]
+    public class WhenIGetAPriority
+    {
+
+        [TestCase(1, "LevyDeclarationDue")]
+        [TestCase(2, "AgreementToSign")]
+        [TestCase(3, "AddApprentices")]
+        [TestCase(4, "ApprenticeChangesToReview")]
+        [TestCase(5, "CohortRequestReadyForApproval")]
+        [TestCase(6, "IncompleteApprenticeshipDetails")]
+        public void ThenIShouldRecieveTheCorrectValue(int priority, string taskType)
+        {
+            //Assert
+            Assert.AreEqual(priority, AccountTaskHelper.GetTaskPriority(new AccountTask { Type = taskType }));
+        }
+    }
+}
