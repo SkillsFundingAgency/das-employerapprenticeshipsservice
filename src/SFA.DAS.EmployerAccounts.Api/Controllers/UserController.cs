3dd97cc82afe6df82a869c572caa1d207ce72787
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-03-13 13:15:07 +0000

    Commit Details: Added the endpoint to upsert the user information in the repo Story Details: https://skillsfundingagency.atlassian.net/browse/FAI-804

--- src/SFA.DAS.EmployerAccounts.Api/Controllers/UserController.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Controllers/UserController.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
-using System.Web.Http;
-using MediatR;
+﻿using MediatR;
 using SFA.DAS.EmployerAccounts.Api.Attributes;
+using SFA.DAS.EmployerAccounts.Commands.UpsertRegisteredUser;
 using SFA.DAS.EmployerAccounts.Queries.GetUserByEmail;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace SFA.DAS.EmployerAccounts.Api.Controllers
 {
@@ -20,10 +22,25 @@ namespace SFA.DAS.EmployerAccounts.Api.Controllers
         [Route("")]
         public async Task<IHttpActionResult> Get(string email)
         {
-            var response = await _mediator.SendAsync(new GetUserByEmailQuery{Email = email});
+            var response = await _mediator.SendAsync(new GetUserByEmailQuery { Email = email });
 
             if (response.User == null) return NotFound();
             return Ok(response.User);
+        }
+
+        [HttpPut]
+        [Route("upsert")]
+        public async Task<IHttpActionResult> Upsert([FromBody] UpsertRegisteredUserCommand command)
+        {
+            try
+            {
+                await _mediator.SendAsync(command);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
         }
     }
 }
