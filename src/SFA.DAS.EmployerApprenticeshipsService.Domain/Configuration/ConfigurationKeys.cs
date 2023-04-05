b5a8ce1d224c63d474dbea0f40da1578085ad027
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-04-05 11:25:47 +0100

    remove all configurations keys except EAS

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/ConfigurationKeys.cs
@@ -2,15 +2,6 @@
 {
     public static class ConfigurationKeys
     {
-        public const string LevyDeclarationProvider = "SFA.DAS.LevyAggregationProvider";
-        public const string PaymentProvider = "SFA.DAS.PaymentProvider";
         public const string EmployerApprenticeshipsService = "SFA.DAS.EmployerApprenticeshipsService";
-        public const string ReferenceDataApi = "SFA.DAS.ReferenceDataApiClient";
-        public const string PaymentsApiClient = "SFA.DAS.PaymentsAPI";
-        public const string NotificationsApiClient = "SFA.DAS.EmployerApprenticeshipsService.Notifications";
-        public const string CommitmentsApiClient = "SFA.DAS.CommitmentsAPI";
-        public const string Features = "SFA.DAS.EmployerApprenticeshipsService.FeaturesV2";
-        public const string AuditApi = "SFA.DAS.AuditApiClient";
-        public const string EmployerFinance = "SFA.DAS.EmployerFinance";
     }
 }
