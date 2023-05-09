a805199a858e196a2f265a9e45b32da9ace1ea41
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-05-09 16:11:22 +0100

    accounts doesn't need routing to finance endpoint

--- src/SFA.DAS.EmployerAccounts/Extensions/RoutingExtensions.cs
+++ src/SFA.DAS.EmployerAccounts/Extensions/RoutingExtensions.cs
@@ -9,12 +9,6 @@ public static class RoutingExtensions
     public static void AddRouting(this RoutingSettings routing)
     {
         routing.RouteToEndpoint(
-            typeof(ImportLevyDeclarationsCommand).Assembly,
-            typeof(ImportLevyDeclarationsCommand).Namespace,
-            "SFA.DAS.EmployerFinance.MessageHandlers"
-        );
-
-        routing.RouteToEndpoint(
             typeof(SendEmailCommand).Assembly,
             typeof(SendEmailCommand).Namespace,
             "SFA.DAS.Notifications.MessageHandlers"
