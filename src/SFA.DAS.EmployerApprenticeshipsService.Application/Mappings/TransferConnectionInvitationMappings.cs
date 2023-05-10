f0a0a2545653c914eae514b507ea550f1ab3396b
Jon Harris <jon.harris@digital.education.gov.uk>
2023-05-10 07:59:53 +0100

    Code tidy

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Mappings/TransferConnectionInvitationMappings.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Mappings/TransferConnectionInvitationMappings.cs
@@ -1,19 +1,17 @@
-﻿using System.Linq;
-using AutoMapper;
+﻿using AutoMapper;
 using SFA.DAS.EAS.Account.Api.Types;
 using SFA.DAS.EAS.Domain.Models.TransferConnections;
 
-namespace SFA.DAS.EAS.Application.Mappings
+namespace SFA.DAS.EAS.Application.Mappings;
+
+public class TransferConnectionInvitationMappings : Profile
 {
-    public class TransferConnectionInvitationMappings : Profile
+    public TransferConnectionInvitationMappings()
     {
-        public TransferConnectionInvitationMappings()
-        {
-            CreateMap<TransferConnectionInvitation, TransferConnectionViewModel>()
-                .ForMember(m => m.FundingEmployerAccountId, o => o.MapFrom(i => i.SenderAccount.Id))
-                .ForMember(m => m.FundingEmployerHashedAccountId, o => o.MapFrom(i => i.SenderAccount.HashedId))
-                .ForMember(m => m.FundingEmployerPublicHashedAccountId, o => o.MapFrom(i => i.SenderAccount.PublicHashedId))
-                .ForMember(m => m.FundingEmployerAccountName, o => o.MapFrom(i => i.SenderAccount.Name));
-        }
+        CreateMap<TransferConnectionInvitation, TransferConnectionViewModel>()
+            .ForMember(m => m.FundingEmployerAccountId, o => o.MapFrom(i => i.SenderAccount.Id))
+            .ForMember(m => m.FundingEmployerHashedAccountId, o => o.MapFrom(i => i.SenderAccount.HashedId))
+            .ForMember(m => m.FundingEmployerPublicHashedAccountId, o => o.MapFrom(i => i.SenderAccount.PublicHashedId))
+            .ForMember(m => m.FundingEmployerAccountName, o => o.MapFrom(i => i.SenderAccount.Name));
     }
 }
