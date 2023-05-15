18c6ccbd00207aabe5d45b7958699789c01213cc
Jon Harris <jon.harris@digital.education.gov.uk>
2023-05-15 12:00:39 +0100

    Tidying tests

--- src/SFA.DAS.EAS.Account.Api.UnitTests/Orchestrators/AccountsOrchestratorTests/WhenIGetAnAccount.cs
+++ src/SFA.DAS.EAS.Account.Api.UnitTests/Orchestrators/AccountsOrchestratorTests/WhenIGetAnAccount.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Common.Domain.Types;
@@ -14,27 +10,25 @@ using SFA.DAS.EAS.Application.Services.EmployerAccountsApi;
 using SFA.DAS.EAS.Application.Services.EmployerFinanceApi;
 using SFA.DAS.EAS.Domain.Models.Account;
 using SFA.DAS.EAS.Domain.Models.Transfers;
-using Microsoft.Extensions.Logging;
 using SFA.DAS.Encoding;
 
 namespace SFA.DAS.EAS.Account.Api.UnitTests.Orchestrators.AccountsOrchestratorTests;
 
 internal class WhenIGetAnAccount
 {
-    private AccountsOrchestrator _orchestrator;
-    private Mock<ILogger<AccountsOrchestrator>> _log;
-    private Mock<IEncodingService> _encodingService;
-    private Mock<IEmployerAccountsApiService> _apiService;
-    private Mock<IEmployerFinanceApiService> _financeApiService;
-    private IMapper _mapper;
-    private TransferAllowance _transferAllowance;
+    private AccountsOrchestrator? _orchestrator;
+    private Mock<ILogger<AccountsOrchestrator>>? _log;
+    private Mock<IEncodingService>? _encodingService;
+    private Mock<IEmployerAccountsApiService>? _apiService;
+    private Mock<IEmployerFinanceApiService>? _financeApiService;
+    private IMapper? _mapper;
+    private TransferAllowance? _transferAllowance;
 
     private const decimal AccountBalance = 678.90M;
     private const string HashedAgreementId = "ABC123";
 
-    private AccountDetailViewModel _accountDetailViewModel;
-    private AccountBalance _accountBalanceResult;
-
+    private AccountDetailViewModel? _accountDetailViewModel;
+    private AccountBalance? _accountBalanceResult;
 
     [SetUp]
     public void Arrange()
@@ -58,7 +52,7 @@ internal class WhenIGetAnAccount
         _financeApiService.Setup(x => x.GetTransferAllowance(It.IsAny<string>(), CancellationToken.None)).ReturnsAsync(transferAllowance);
 
         _accountBalanceResult = new AccountBalance { Balance = AccountBalance };
-        var accountBalances = new List<AccountBalance> { _accountBalanceResult };
+        var accountBalances = new List<AccountBalance?> { _accountBalanceResult };
         _financeApiService.Setup(x => x.GetAccountBalances(It.IsAny<List<string>>(), CancellationToken.None)).ReturnsAsync(accountBalances);
     }
 
@@ -66,79 +60,79 @@ internal class WhenIGetAnAccount
     public async Task ThenARequestShouldMakeCallsToGetBalances()
     {
         //Act
-        await _orchestrator.GetAccount(HashedAgreementId);
+        await _orchestrator!.GetAccount(HashedAgreementId);
 
         //Assert            
-        _apiService.VerifyAll();
-        _financeApiService.VerifyAll();
+        _apiService!.VerifyAll();
+        _financeApiService!.VerifyAll();
     }
 
     [Test]
     public async Task ThenResponseShouldHaveBalanceSet()
     {
         //Act
-        var result = await _orchestrator.GetAccount(HashedAgreementId);
+        var result = await _orchestrator!.GetAccount(HashedAgreementId);
 
         //Assert
-        Assert.AreEqual(AccountBalance, result.Data.Balance);
+        Assert.That(result.Data.Balance, Is.EqualTo(AccountBalance));
     }
 
     [Test]
     public async Task ThenResponseShouldHaveTransferAllowanceSet()
     {
         //Act
-        var result = await _orchestrator.GetAccount(HashedAgreementId);
+        var result = await _orchestrator!.GetAccount(HashedAgreementId);
 
         //Assert
-        Assert.AreEqual(_transferAllowance.RemainingTransferAllowance, result.Data.RemainingTransferAllowance);
+        Assert.That(result.Data.RemainingTransferAllowance, Is.EqualTo(_transferAllowance!.RemainingTransferAllowance));
     }
 
     [Test]
     public async Task AndTheAccountHasALevyOverrideThenTheyShouldNotBeAllowedPaymentOnTheService()
     {
-        _accountBalanceResult.LevyOverride = false;
+        _accountBalanceResult!.LevyOverride = false;
 
-        var response = await _orchestrator.GetAccount(HashedAgreementId);
+        var response = await _orchestrator!.GetAccount(HashedAgreementId);
 
-        Assert.IsFalse(response.Data.IsAllowedPaymentOnService);
+        Assert.That(response.Data.IsAllowedPaymentOnService, Is.False);
     }
 
     [Test]
     public async Task AndTheAccountHasAnEoIAgreementThenTheyShouldBeAllowedPaymentOnTheService()
     {
-        _accountDetailViewModel.AccountAgreementType = AccountAgreementType.NonLevyExpressionOfInterest;
+        _accountDetailViewModel!.AccountAgreementType = AccountAgreementType.NonLevyExpressionOfInterest;
 
-        var response = await _orchestrator.GetAccount(HashedAgreementId);
+        var response = await _orchestrator!.GetAccount(HashedAgreementId);
 
-        Assert.IsTrue(response.Data.IsAllowedPaymentOnService);
+        Assert.That(response.Data.IsAllowedPaymentOnService, Is.True);
     }
 
     [Test]
     public async Task AndTheEmployerTypeIsLevyThenTheyShouldBeAllowedPaymentOnTheService()
     {
-        _accountDetailViewModel.ApprenticeshipEmployerType = ApprenticeshipEmployerType.Levy.ToString();
+        _accountDetailViewModel!.ApprenticeshipEmployerType = ApprenticeshipEmployerType.Levy.ToString();
 
-        var response = await _orchestrator.GetAccount(HashedAgreementId);
+        var response = await _orchestrator!.GetAccount(HashedAgreementId);
 
-        Assert.IsTrue(response.Data.IsAllowedPaymentOnService);
+        Assert.That(response.Data.IsAllowedPaymentOnService, Is.True);
     }
 
     [Test]
     public async Task AndTheEmployerTypeIsNoneLevyThenTheyShouldNotBeAllowedPaymentOnTheService()
     {
-        _accountDetailViewModel.ApprenticeshipEmployerType = ApprenticeshipEmployerType.NonLevy.ToString();
+        _accountDetailViewModel!.ApprenticeshipEmployerType = ApprenticeshipEmployerType.NonLevy.ToString();
 
-        var response = await _orchestrator.GetAccount(HashedAgreementId);
+        var response = await _orchestrator!.GetAccount(HashedAgreementId);
 
-        Assert.IsFalse(response.Data.IsAllowedPaymentOnService);
+        Assert.That(response.Data.IsAllowedPaymentOnService, Is.False);
     }
 
-    private IMapper ConfigureMapper()
+    private static IMapper? ConfigureMapper()
     {
         var profiles = Assembly.Load("SFA.DAS.EAS.Account.Api")
             .GetTypes()
-            .Where(t => typeof(Profile).IsAssignableFrom(t))
-            .Select(t => (Profile)Activator.CreateInstance(t))
+            .Where(type => typeof(Profile).IsAssignableFrom(type))
+            .Select(type => (Profile)Activator.CreateInstance(type)!)
             .ToList();
 
         var config = new MapperConfiguration(c =>
