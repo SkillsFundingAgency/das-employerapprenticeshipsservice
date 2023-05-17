9466026a6e7aa2eb8ecc7007559a1eaddea454dc
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-05-17 21:47:27 +0100

    allow anonymous ping

--- src/SFA.DAS.EAS.Api/Controllers/PingController.cs
+++ src/SFA.DAS.EAS.Api/Controllers/PingController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.EAS.Account.Api.Controllers;
 
 [Route("api/ping")]
 public class PingController : ControllerBase
 {
-    [HttpGet]
+    [HttpGet, AllowAnonymous]
     public IActionResult Get()
     {
         return Ok(new { Hello = "World" });
