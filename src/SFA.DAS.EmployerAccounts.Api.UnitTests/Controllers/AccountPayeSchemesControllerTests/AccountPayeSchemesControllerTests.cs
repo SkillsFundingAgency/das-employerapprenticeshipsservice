ca301a1ef65c61d21121651cd3fe867080bbf080
Jon Harris <jon.harris@digital.education.gov.uk>
2023-06-01 07:33:45 +0100

    Adding logging mock to test

--- src/SFA.DAS.EmployerAccounts.Api.UnitTests/Controllers/AccountPayeSchemesControllerTests/AccountPayeSchemesControllerTests.cs
+++ src/SFA.DAS.EmployerAccounts.Api.UnitTests/Controllers/AccountPayeSchemesControllerTests/AccountPayeSchemesControllerTests.cs
@@ -27,7 +27,7 @@ namespace SFA.DAS.EmployerAccounts.Api.UnitTests.Controllers.AccountPayeSchemesC
             Mapper = new Mock<IMapper>();
             EncodingService = new Mock<IEncodingService>();
             var orchestrator = new AccountsOrchestrator(Mediator.Object, Logger.Object, Mapper.Object, EncodingService.Object);
-            Controller = new AccountPayeSchemesController(orchestrator);
+            Controller = new AccountPayeSchemesController(orchestrator, Mock.Of<ILogger<AccountPayeSchemesController>>());
 
             UrlTestHelper = new Mock<IUrlHelper>();
             Controller.Url = UrlTestHelper.Object;
