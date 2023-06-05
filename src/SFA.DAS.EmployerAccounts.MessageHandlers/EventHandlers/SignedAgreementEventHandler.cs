c197cab3c316cf724e653d916ca8a54a2f3ef6e8
Jon Harris <jon.harris@digital.education.gov.uk>
2023-06-05 12:07:43 +0100

    Adding logging to SignedAgreementEventHandler

--- src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/SignedAgreementEventHandler.cs
+++ src/SFA.DAS.EmployerAccounts.MessageHandlers/EventHandlers/SignedAgreementEventHandler.cs
@@ -7,14 +7,18 @@ namespace SFA.DAS.EmployerAccounts.MessageHandlers.EventHandlers;
 public class SignedAgreementEventHandler : IHandleMessages<SignedAgreementEvent>
 {
     private readonly IEventPublisher _messagePublisher;
+    private readonly ILogger<SignedAgreementEventHandler> _loger;
 
-    public SignedAgreementEventHandler(IEventPublisher messagePublisher)
+    public SignedAgreementEventHandler(IEventPublisher messagePublisher, ILogger<SignedAgreementEventHandler> loger)
     {
         _messagePublisher = messagePublisher;
+        _loger = loger;
     }
 
     public async Task Handle(SignedAgreementEvent message, IMessageHandlerContext context)
     {
+        _loger.LogDebug($"Starting {nameof(SignedAgreementEventHandler)} handler.");
+        
         await _messagePublisher.Publish(new AgreementSignedMessage(
             message.AccountId,
             message.AgreementId,
@@ -23,5 +27,7 @@ public class SignedAgreementEventHandler : IHandleMessages<SignedAgreementEvent>
             message.CohortCreated,
             message.UserName,
             message.UserRef.ToString()));
+        
+        _loger.LogDebug($"Completed {nameof(SignedAgreementEventHandler)} handler.");
     }
 }
