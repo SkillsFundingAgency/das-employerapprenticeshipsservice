b4165eec66b7472b35255598db79bb4b21b8c0ba
Jon Harris <jon.harris@digital.education.gov.uk>
2023-06-23 07:22:44 +0100

    Removing test logging

--- src/SFA.DAS.EAS.Support.Web/Controllers/StatusController.cs
+++ src/SFA.DAS.EAS.Support.Web/Controllers/StatusController.cs
@@ -8,14 +8,9 @@ namespace SFA.DAS.EAS.Support.Web.Controllers;
 [Route("api/status")]
 public class StatusController : ControllerBase
 {
-    private readonly ILogger<StatusController> _logger;
-    public StatusController(ILogger<StatusController> logger) => _logger = logger;
-    
     [HttpGet]
     public IActionResult Get()
     {
-        _logger.LogInformation("Retrieving status info from {ControllerName}.", nameof(StatusController));
-
         var model = new
         {
             ServiceName = "SFA DAS Employer Apprenticeship Service Support Site",
