89951f550ef7e83be6b4b08d143fc880d7213096
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-06-28 14:17:00 +0100

    fix post merge

--- src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountTaskListViewModel.cs
+++ src/SFA.DAS.EmployerAccounts.Web/ViewModels/AccountTaskListViewModel.cs
@@ -17,5 +17,7 @@ namespace SFA.DAS.EmployerAccounts.Web.ViewModels
         }
 
         public string SaveProgressRouteName => string.IsNullOrEmpty(HashedAccountId) ? RouteNames.NewAccountSaveProgress : RouteNames.PartialAccountSaveProgress;
+
+        public string AddPayeRouteName => string.IsNullOrEmpty(HashedAccountId) ? RouteNames.EmployerAccountPayBillTriage : RouteNames.AddPayeShutter;
     }
 }
