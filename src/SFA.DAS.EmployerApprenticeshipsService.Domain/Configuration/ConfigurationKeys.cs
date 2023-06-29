19b82811d8d1f5eae423b22e1ba8103b12b4959c
Jon Harris <jon.harris@digital.education.gov.uk>
2023-06-29 15:01:28 +0100

    Namespace tidy

--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/ConfigurationKeys.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Configuration/ConfigurationKeys.cs
@@ -2,9 +2,6 @@
 
 public static class ConfigurationKeys
 {
-    public const string ServiceName = "SFA.DAS.EmployerApprenticeshipsService";
-
-    public static string EmployerApprenticeshipsService => ServiceName;
     public const string AzureActiveDirectoryApiConfiguration = "AzureADApiAuthentication";
-    public const string Identity = $"{ServiceName}:Identity";
+    public const string EmployerAccountApiConfig = "SFA.DAS.EmployerAccountAPI";
 }
--- src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transaction/TransactionLine.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Domain/Models/Transaction/TransactionLine.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SFA.DAS.EAS.Domain.Models.Transaction;
+﻿namespace SFA.DAS.EAS.Domain.Models.Transaction;
 
 public class TransactionLine
 {
