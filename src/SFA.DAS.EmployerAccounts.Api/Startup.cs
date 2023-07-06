6d10414999edc47e5075363b9d2c65f15173eb9a
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-07-06 15:47:32 +0100

    AddEF extension for SynchronizedStorageSession setup

--- src/SFA.DAS.EmployerAccounts.Api/Startup.cs
+++ src/SFA.DAS.EmployerAccounts.Api/Startup.cs
@@ -24,7 +24,9 @@ using SFA.DAS.EmployerAccounts.Data;
 using SFA.DAS.EmployerAccounts.Mappings;
 using SFA.DAS.EmployerAccounts.Queries.GetPayeSchemeByRef;
 using SFA.DAS.EmployerAccounts.ServiceRegistration;
+using SFA.DAS.EmployerAccounts.Startup;
 using SFA.DAS.NServiceBus.Features.ClientOutbox.Data;
+using SFA.DAS.UnitOfWork.DependencyResolution.Microsoft;
 using SFA.DAS.UnitOfWork.EntityFrameworkCore.DependencyResolution.Microsoft;
 using SFA.DAS.UnitOfWork.Mvc.Extensions;
 using SFA.DAS.UnitOfWork.NServiceBus.Features.ClientOutbox.DependencyResolution.Microsoft;
@@ -67,12 +69,15 @@ public class Startup
         services.AddDasHealthChecks(employerAccountsConfiguration);
         services.AddOrchestrators();
 
-        services.AddEntityFrameworkUnitOfWork<EmployerAccountsDbContext>();
+        services
+            .AddUnitOfWork()
+            .AddEntityFramework(employerAccountsConfiguration)
+            .AddEntityFrameworkUnitOfWork<EmployerAccountsDbContext>();
+
         services.AddNServiceBusClientUnitOfWork();
 
         services.AddDatabaseRegistration();
         services.AddDataRepositories();
-        //services.AddEventsApi(employerAccountsConfiguration);
         services.AddExecutionPolicies();
 
         services.AddAutoMapper(typeof(AccountMappings), typeof(Startup));
