fc4bbe78b96aaa5e861af5d0f1049f7f2a83ff59
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2024-05-10 13:25:21 +0100

    fix logging

--- src/SFA.DAS.EmployerApprenticeshipsService.Application/Services/EmployerAccountsApi/EmployerAccountsApiService.cs
+++ src/SFA.DAS.EmployerApprenticeshipsService.Application/Services/EmployerAccountsApi/EmployerAccountsApiService.cs
@@ -40,7 +40,7 @@ public class EmployerAccountsApiService : ApiClientService, IEmployerAccountsApi
 
     public async Task<AccountDetailViewModel> GetAccount(long accountId, CancellationToken cancellationToken = default)
     {
-        _logger.LogInformation("Getting paged accounts: {AccountId}");
+        _logger.LogInformation("Getting account: {AccountId}");
 
         return await GetResponse<AccountDetailViewModel>($"/api/accounts/{accountId}", cancellationToken: cancellationToken);
     }
