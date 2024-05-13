a83c9746d5281832a444b2f5bb2465af1aa65653
Jon Harris <jon.harris@digital.education.gov.uk>
2024-05-13 15:50:18 +0100

    Wiring up redirect for resend invitation

---
+++ src/SFA.DAS.EAS.Api/Controllers/EmployerTeamController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SFA.DAS.EAS.Account.Api.Authorization;
+using SFA.DAS.EAS.Application.Services.EmployerAccountsApi;
+
+namespace SFA.DAS.EAS.Account.Api.Controllers;
+
+[ApiController]
+[Route("api/team")]
+[Authorize(Policy = ApiRoles.ReadUserAccounts)]
+public class EmployerTeamController : ControllerBase
+{
+    private readonly IEmployerAccountsApiService _apiService;
+    private readonly ILogger<EmployerTeamController> _logger;
+
+    public EmployerTeamController(IEmployerAccountsApiService apiService, ILogger<EmployerTeamController> logger)
+    {
+        _apiService = apiService;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [Route("resend-invitation")]
+    public async Task<IActionResult> ResendInvitation([FromBody] ResendInvitationCommand command)
+    {
+        try
+        {
+            await _apiService.RedirectPost("/api/team/resend-invitation", JsonConvert.SerializeObject(command), new CancellationToken());
+            return Ok();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error in {Controller}.{Action}", nameof(EmployerTeamController), nameof(ResendInvitation));
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
+
+public class ResendInvitationCommand
+{
+    public string Email { get; set; }
+    public string HashedAccountId { get; set; }
+    public string ExternalUserId { get; set; }
+    public string FirstName { get; set; }
+}
