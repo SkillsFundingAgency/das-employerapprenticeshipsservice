7a14fb6f396c59d35b223da51303ae7fd59d0208
Jon Harris <jon.harris@digital.education.gov.uk>
2024-05-20 11:59:44 +0100

    logging

--- src/SFA.DAS.EAS.Support.Infrastructure/Config/AzureAdScopeClaimTransformation.cs
+++ src/SFA.DAS.EAS.Support.Infrastructure/Config/AzureAdScopeClaimTransformation.cs
@@ -1,12 +1,28 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json;
 
 namespace SFA.DAS.EAS.Support.Infrastructure.Config;
 
 public class AzureAdScopeClaimTransformation : IClaimsTransformation
 {
+    private readonly ILogger<AzureAdScopeClaimTransformation> _logger;
+
+    public AzureAdScopeClaimTransformation(ILogger<AzureAdScopeClaimTransformation> logger)
+    {
+        _logger = logger;
+    }
+    
     public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
+        _logger.LogInformation("AzureAdScopeClaimTransformation.TransformAsync claims {Claims}", JsonConvert.SerializeObject(principal.Claims.Select(x => new
+        {
+            x.Type,
+            x.Value
+        })));
+        
+        return Task.FromResult(principal);
+        
         var scopeClaims = principal.FindAll(Constants.ScopeClaimType).ToList();
         if (scopeClaims.Count != 1 || !scopeClaims[0].Value.Contains(' '))
         {
