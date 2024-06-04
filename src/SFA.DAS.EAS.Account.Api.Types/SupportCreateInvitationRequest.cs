27489fd2a9095d1d483ea9babf738f9ed15a73d6
Jon Harris <jon.harris@digital.education.gov.uk>
2024-06-04 15:32:01 +0100

    Refactor/tidy

--- src/SFA.DAS.EAS.Account.Api.Types/SupportCreateInvitationRequest.cs
+++ src/SFA.DAS.EAS.Account.Api.Types/SupportCreateInvitationRequest.cs
@@ -3,11 +3,8 @@
 public class SupportCreateInvitationRequest
 {
     public string HashedAccountId { get; set; }
-
     public string NameOfPersonBeingInvited { get; set; }
-
     public string EmailOfPersonBeingInvited { get; set; }
-
     public int RoleOfPersonBeingInvited { get; set; }
     public string SupportUserEmail { get; set; }
 }
--- src/SFA.DAS.EAS.Support.Web/Controllers/InvitationsController.cs
+++ src/SFA.DAS.EAS.Support.Web/Controllers/InvitationsController.cs
@@ -26,9 +26,7 @@ public class InvitationsController(ILogger<InvitationsController> logger, IEmplo
     public class CreateInvitationRequest
     {
         public string NameOfPersonBeingInvited { get; set; }
-
         public string EmailOfPersonBeingInvited { get; set; }
-
         public int RoleOfPersonBeingInvited { get; set; }
         public string SupportUserEmail { get; set; }
     }
